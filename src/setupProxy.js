62bb384be2b0b6ca179eda98d63b76b4e1bff49a
Robinnnnn <rokim8@gmail.com>
2021-03-23 23:47:43 -0400

    more env

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,11 +2,10 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const baseurl =
   process.env.ENV === 'production'
-    ? 'https://api.gallery.so'
-    : 'http://localhost:4000';
+    ? process.env.BASEURL_PRODUCTION
+    : process.env.BASEURL_DEVELOPMENT;
 
 module.exports = function (app) {
-  console.log(process.env);
   app.use(
     '/api',
     createProxyMiddleware({
