61065f3085ec13891242adbfe39ce0505061fdb0
Kaito Hara <kaito.hara@me.com>
2021-03-24 17:36:14 +0900

    Move web-3 use to Auth context

---
+++ src/contexts/auth/Auth.tsx
@@ -0,0 +1,16 @@
+import { memo } from 'react';
+import { Web3ReactProvider } from '@web3-react/core';
+import { Web3Provider } from '@ethersproject/providers';
+
+function getLibrary(provider: any) {
+  const library = new Web3Provider(provider);
+  library.pollingInterval = 12000;
+  return library;
+}
+
+export const Boundary = memo(({ children }) => {
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>{children}</Web3ReactProvider>
+  );
+});
+export default Boundary;
