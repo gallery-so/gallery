5c1721134d504b844aee35f006d91df636e30ba8
Kaito Hara <kaito.hara@me.com>
2021-03-24 13:42:31 +0900

    add web3-react and provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,19 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { createWeb3ReactRoot, Web3ReactProvider } from '@web3-react/core';
+import { Web3Provider } from '@ethersproject/providers';
+
+// const Web3ProviderNetwork = createWeb3ReactRoot('NETWORK');
+function getLibrary(provider: any) {
+  return new Web3Provider(provider)
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <App />
+    </Web3ReactProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
