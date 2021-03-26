3c79e2f8fbbb6bf5ca7a8c205d6963ce43552105
Robinnnnn <rokim8@gmail.com>
2021-03-26 01:06:44 -0400

    404

--- src/scenes/Routes.tsx
+++ src/scenes/Routes.tsx
@@ -1,12 +1,14 @@
 import { Router } from '@reach/router';
 import Home from 'scenes/Home/Home';
 import AuthDemo from 'scenes/AuthDemo/AuthDemo';
+import NotFound from 'scenes/NotFound/NotFound';
 
 export default function Routes() {
   return (
     <Router>
       <Home path="/" />
       <AuthDemo path="/auth" />
+      <NotFound default />
     </Router>
   );
 }
