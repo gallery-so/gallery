65033779372f4bdabba1697318c6358a78c25736
kaitoo1 <80802871+kaitoo1@users.noreply.github.com>
2021-03-26 13:57:32 +0900

    Add Metamask+Walleconnect support (#34)
    Co-authored-by: Kaito Hara <kaito.hara@me.com>

---
+++ src/connectors/index.ts
@@ -0,0 +1,15 @@
+import { InjectedConnector } from '@web3-react/injected-connector';
+import { WalletConnectConnector } from '@web3-react/walletconnect-connector';
+const NETWORK_URL =
+  'https://mainnet.infura.io/v3/84842078b09946638c03157f83405213';
+
+export const injected = new InjectedConnector({
+  supportedChainIds: [1, 3, 4, 5, 42],
+});
+
+export const walletconnect = new WalletConnectConnector({
+  rpc: { 1: NETWORK_URL },
+  bridge: 'https://bridge.walletconnect.org',
+  qrcode: true,
+  pollingInterval: 150000,
+});
