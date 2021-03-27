827f24b9c725b999022f20bd6f24995e5905a0d1
Robinnnnn <rokim8@gmail.com>
2021-03-27 01:34:44 -0400

    refactor

--- src/contexts/modal/ModalContext.tsx
+++ src/contexts/modal/ModalContext.tsx
@@ -8,7 +8,8 @@ import {
   useCallback,
   useMemo,
 } from 'react';
-import styled, { css, keyframes } from 'styled-components';
+import AnimatedModal from './AnimatedModal';
+import { MODAL_TRANSITION_MS } from './constants';
 
 type ModalActions = {
   showModal: (content: ReactElement) => void;
@@ -26,8 +27,6 @@ export const useModal = (): ModalActions => {
 };
 
 type Props = { children: ReactNode };
-
-const MODAL_TRANSITION_MS = 200;
 
 function ModalProvider({ children }: Props) {
   // whether node is actually on the DOM
@@ -64,99 +63,15 @@ function ModalProvider({ children }: Props) {
   return (
     <ModalContext.Provider value={actions}>
       {children}
-      {isMounted && (
-        <_ToggleFade isActive={isActive}>
-          <Overlay onClick={hideModal} />
-          <StyledContentContainer>
-            <_ToggleTranslate isActive={isActive}>
-              <StyledContent>
-                <StyledClose onClick={hideModal}>&#x2715;</StyledClose>
-                {content}
-              </StyledContent>
-            </_ToggleTranslate>
-          </StyledContentContainer>
-        </_ToggleFade>
+      {isMounted && content && (
+        <AnimatedModal
+          isActive={isActive}
+          hideModal={hideModal}
+          content={content}
+        />
       )}
     </ModalContext.Provider>
   );
 }
-
-const fadeIn = keyframes`
-    from { opacity: 0 };
-    to { opacity: 1 };
-`;
-
-const fadeOut = keyframes`
-    from { opacity: 1 };
-    to { opacity: 0 };
-`;
-
-// ease-out like style
-const transitionStyle = `${MODAL_TRANSITION_MS}ms cubic-bezier(0, 0, 0, 1.07)`;
-
-const _ToggleFade = styled.div<{ isActive: boolean }>`
-  animation: ${({ isActive }) =>
-    css`
-      ${isActive ? fadeIn : fadeOut} ${transitionStyle}
-    `};
-`;
-
-const TRANSLATE_PIXELS = 10;
-
-const translateUp = keyframes`
-    from { transform: translateY(${TRANSLATE_PIXELS}px) };
-    to { transform: translateY(0px) };
-`;
-
-const translateDown = keyframes`
-    from { transform: translateY(0px) };
-    to { transform: translateY(${TRANSLATE_PIXELS}px) };
-`;
-
-const _ToggleTranslate = styled.div<{ isActive: boolean }>`
-  animation: ${({ isActive }) =>
-    css`
-      ${isActive ? translateUp : translateDown} ${transitionStyle}
-    `};
-`;
-
-const Overlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  height: 100vh;
-  width: 100vw;
-  background: white;
-  opacity: 0.1;
-
-  // should appear above rest of site
-  z-index: 1;
-
-  // fixes unusual opacity transition bug: https://stackoverflow.com/a/22648685
-  -webkit-backface-visibility: hidden;
-`;
-
-const StyledContentContainer = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-
-  // should appear above the overlay
-  z-index: 2;
-`;
-
-const StyledContent = styled.div`
-  position: relative;
-  padding: 40px;
-  background: white;
-`;
-
-const StyledClose = styled.span`
-  position: absolute;
-  right: 10px;
-  top: 10px;
-  cursor: pointer;
-`;
 
 export default memo(ModalProvider);
---
+++ src/contexts/modal/constants.ts
@@ -0,0 +1,1 @@
+export const MODAL_TRANSITION_MS = 200;
