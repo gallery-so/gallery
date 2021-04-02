1b2ca492d4b84316b5d9a76be68c5312318ea0ac
Robinnnnn <rokim8@gmail.com>
2021-04-02 00:14:34 -0400

    Title, Text

---
+++ src/components/core/Text/Text.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import colors from '../colors';
+
+export const Title = styled.p`
+  font-family: 'Times New Roman';
+  margin: 0;
+
+  font-size: 48px;
+`;
+
+type TextProps = {
+  light?: boolean;
+};
+
+export const Text = styled.p<TextProps>`
+  font-family: 'Helvetica Neue';
+  margin: 0;
+
+  font-size: 12px;
+  color: ${({ light }) => (light ? colors.gray : colors.black)};
+`;
