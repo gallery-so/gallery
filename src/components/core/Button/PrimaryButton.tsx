31cab156797ec10707974519762661963dc9e971
Robinnnnn <rokim8@gmail.com>
2021-04-18 00:53:57 -0400

    disabled button state

--- src/components/core/Button/PrimaryButton.tsx
+++ src/components/core/Button/PrimaryButton.tsx
@@ -7,17 +7,26 @@ type Props = {
   className?: string;
   text: string;
   onClick?: () => void;
+  disabled?: boolean;
 };
 
-function PrimaryButton({ className, text, onClick }: Props) {
+function PrimaryButton({ className, text, onClick, disabled }: Props) {
   return (
-    <StyledPrimaryButton className={className} onClick={onClick}>
+    <StyledPrimaryButton
+      className={className}
+      onClick={onClick}
+      disabled={disabled}
+    >
       <Text color={colors.white}>{text}</Text>
     </StyledPrimaryButton>
   );
 }
 
-const StyledPrimaryButton = styled.button`
+type StyledPrimaryButtonProps = {
+  disabled?: boolean;
+};
+
+const StyledPrimaryButton = styled.button<StyledPrimaryButtonProps>`
   border-style: none;
   padding: 12px 16px;
 
@@ -29,7 +38,9 @@ const StyledPrimaryButton = styled.button`
   text-transform: uppercase;
 
   transition: opacity 0.2s;
-  opacity: 1;
+  opacity: ${({ disabled }) => (disabled ? '0.2' : '1')};
+  pointer-events: ${({ disabled }) => (disabled ? 'none' : 'inherit')};
+
   &:hover {
     opacity: 0.8;
   }
