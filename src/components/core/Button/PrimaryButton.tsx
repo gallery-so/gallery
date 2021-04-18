2239ae293e6c48218aad25b5745fdf9aaffea3e5
Robinnnnn <rokim8@gmail.com>
2021-04-18 00:28:59 -0400

    PrimaryButton component

---
+++ src/components/core/Button/PrimaryButton.tsx
@@ -0,0 +1,32 @@
+import { memo } from 'react';
+import styled from 'styled-components';
+import { Text } from '../Text/Text';
+import colors from '../colors';
+
+type Props = {
+  className?: string;
+  text: string;
+  onClick?: () => void;
+};
+
+function PrimaryButton({ className, text, onClick }: Props) {
+  return (
+    <StyledPrimaryButton className={className} onClick={onClick}>
+      <Text color={colors.white}>{text}</Text>
+    </StyledPrimaryButton>
+  );
+}
+
+const StyledPrimaryButton = styled.button`
+  border-style: none;
+  padding: 12px 16px;
+
+  background: black;
+  color: white;
+
+  cursor: pointer;
+
+  text-transform: uppercase;
+`;
+
+export default memo(PrimaryButton);
