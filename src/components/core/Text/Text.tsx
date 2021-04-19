be40cb450281a76cf456f3c560682a5ab47c922a
Robinnnnn <rokim8@gmail.com>
2021-04-19 02:25:31 -0400

    add subtitle component

--- src/components/core/Text/Text.tsx
+++ src/components/core/Text/Text.tsx
@@ -8,8 +8,23 @@ export const Title = styled.p`
   font-size: 48px;
 `;
 
+type SubtitleProps = {
+  color?: colors;
+};
+
+export const Subtitle = styled.p<SubtitleProps>`
+  font-family: 'Helvetica Neue';
+  margin: 0;
+
+  font-size: 20px;
+  font-weight: 500;
+
+  color: ${({ color }) => color ?? colors.black};
+`;
+
 type TextProps = {
   color?: colors;
+  lineHeight?: 'tight' | 'normal';
 };
 
 export const Text = styled.p<TextProps>`
@@ -17,6 +32,7 @@ export const Text = styled.p<TextProps>`
   margin: 0;
 
   font-size: 14px;
-  line-height: 16px;
+  line-height: ${({ lineHeight }) =>
+    lineHeight === 'tight' ? '16px' : '20px'};
   color: ${({ color }) => color ?? colors.black};
 `;
