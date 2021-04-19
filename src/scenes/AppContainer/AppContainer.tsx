d1ff705426223b440e9e5ae30bc3f4b226853ca7
Robinnnnn <rokim8@gmail.com>
2021-04-19 01:48:51 -0400

    control navbar display based on route

--- src/scenes/AppContainer/AppContainer.tsx
+++ src/scenes/AppContainer/AppContainer.tsx
@@ -1,4 +1,4 @@
-import { memo, ReactNode } from 'react';
+import { memo, ReactNode, useMemo } from 'react';
 import { RouteComponentProps } from '@reach/router';
 import styled from 'styled-components';
 import GlobalNavbar from 'components/GlobalNavbar/GlobalNavbar';
@@ -7,10 +7,16 @@ type Props = {
   children: ReactNode;
 };
 
-function AppContainer({ children }: RouteComponentProps & Props) {
+const ROUTES_WITHOUT_NAVBAR = ['/create'];
+
+function AppContainer({ children, location }: RouteComponentProps & Props) {
+  const shouldDisplayNavbar = useMemo(() => {
+    return !ROUTES_WITHOUT_NAVBAR.includes(location?.pathname ?? '');
+  }, [location?.pathname]);
+
   return (
     <StyledAppContainer>
-      <GlobalNavbar />
+      {shouldDisplayNavbar && <GlobalNavbar />}
       {children}
     </StyledAppContainer>
   );
