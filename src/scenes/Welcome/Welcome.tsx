6a23af4b93bd06afda277db0d1e5f4289bfaba78
kaitoo1 <80802871+kaitoo1@users.noreply.github.com>
2021-04-23 15:03:42 +0900

    Match welcome step with design (#113)

--- src/scenes/Welcome/Welcome.tsx
+++ src/scenes/Welcome/Welcome.tsx
@@ -1,7 +1,9 @@
+import { useCallback } from 'react';
 import { navigate, RouteComponentProps } from '@reach/router';
 import styled from 'styled-components';
-import ActionText from 'components/core/ActionText/ActionText';
-import { useCallback } from 'react';
+import { Text } from 'components/core/Text/Text';
+import PrimaryButton from 'components/core/Button/PrimaryButton';
+import colors from 'components/core/colors';
 
 function Welcome(_: RouteComponentProps) {
   const handleClick = useCallback(() => {
@@ -10,19 +12,37 @@ function Welcome(_: RouteComponentProps) {
 
   return (
     <StyledWelcome>
-      <StyledActionText>YOU ARE PICASSO</StyledActionText>
-      <button onClick={handleClick}></button>
+      <StyledHeader>Welcome to Gallery</StyledHeader>
+      <StyledBodyText>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation
+      </StyledBodyText>
+
+      <PrimaryButton text="Enter Gallery" onClick={handleClick}></PrimaryButton>
     </StyledWelcome>
   );
 }
-const StyledActionText = styled(ActionText)`
-  font-size: 100px;
+const StyledHeader = styled.p`
+  text-align: center;
+  color: black;
+  font-size: 50px;
+  white-space: nowrap;
+  margin-bottom: 24px;
 `;
+
+const StyledBodyText = styled(Text)`
+  margin-bottom: 24px;
+  color: ${colors.gray};
+`;
+
 const StyledWelcome = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   margin-top: 140px;
+  width: min-content;
+  margin: 20vh auto 0;
 `;
 
 export default Welcome;
