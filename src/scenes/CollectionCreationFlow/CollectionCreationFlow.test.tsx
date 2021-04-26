ff0f0d9c1e723d69f7bc6d0b28f997b51f3c6a0f
kaito <80802871+kaitoo1@users.noreply.github.com>
2021-04-26 15:10:54 +0900

    clean up

--- src/scenes/CollectionCreationFlow/CollectionCreationFlow.test.tsx
+++ src/scenes/CollectionCreationFlow/CollectionCreationFlow.test.tsx
@@ -1,10 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import CollectionCreationFlow from './CollectionCreationFlow';
 
-test('CollectionCreateFlow renders steps', () => {
-  render(<CollectionCreationFlow></CollectionCreationFlow>);
-});
-
 test('Next button is disabled by default', () => {
   render(<CollectionCreationFlow></CollectionCreationFlow>);
   const button = screen.getByTestId('wizard-footer-next-button');
