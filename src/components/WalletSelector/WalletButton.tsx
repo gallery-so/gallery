7eddccc40d8363cdd449f0ae4b2b5d2ef7cf06c3
kaitoo1 <80802871+kaitoo1@users.noreply.github.com>
2021-04-28 11:41:47 +0900

    Wallet selector updates (#115)
    * Wallet selector updates
      - Added loading (Connecting...) state
      - Revamped error state
      - refactored WalletSelector and WalletButton components
      - added test case for WalletSelector
    
    * add ellipses to copy

--- src/components/WalletSelector/WalletButton.tsx
+++ src/components/WalletSelector/WalletButton.tsx
@@ -1,50 +1,47 @@
 import { Web3ReactManagerFunctions } from '@web3-react/core/dist/types';
 import { injected } from 'connectors/index';
 import { useCallback } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { AbstractConnector } from '@web3-react/abstract-connector';
 import colors from 'components/core/colors';
 
 type WalletButtonProps = {
-  walletName: string;
+  walletName?: string;
   activate: Web3ReactManagerFunctions['activate'];
-  connector: AbstractConnector;
-  enableIsConnectingState: () => void;
-  isConnecting: boolean;
+  connector?: AbstractConnector;
+  setToPendingState: (connector: AbstractConnector) => void;
+  isPending: boolean;
 };
 
 function WalletButton({
   walletName,
   activate,
   connector,
-  enableIsConnectingState,
-  isConnecting,
+  setToPendingState,
+  isPending,
 }: WalletButtonProps) {
   const handleClick = useCallback(() => {
-    enableIsConnectingState();
-    if (walletName.toLowerCase() === 'metamask') {
+    if (walletName && walletName.toLowerCase() === 'metamask') {
       injected.isAuthorized().then((isAuthorized: boolean) => {
         // TODO: figure out what goes here
       });
     }
-    activate(connector);
-  }, [activate, connector, enableIsConnectingState, walletName]);
+    if (connector) {
+      setToPendingState(connector);
+      activate(connector);
+    }
+  }, [activate, connector, setToPendingState, walletName]);
 
   // injected is the connector type used for browser wallet extensions/dApp browsers
   if (connector === injected) {
     // metamask injects a global API at window.ethereum (web3 for legacy) if it is installed
     if (!(window.web3 || window.ethereum)) {
-      if (walletName.toLowerCase() === 'metamask') {
+      if (walletName && walletName.toLowerCase() === 'metamask') {
         return (
           <StyledExternalLink href="https://metamask.io/" target="_blank">
             <StyledButton data-testid="wallet-button">
               {'Install Metamask'}
-              <Icon
-                src={
-                  require(`assets/icons/${walletName.toLowerCase()}.svg`)
-                    .default
-                }
-              />
+              <Icon src={require('assets/icons/metamask.svg').default} />
             </StyledButton>
           </StyledExternalLink>
         );
@@ -56,19 +53,25 @@ function WalletButton({
     <StyledButton
       data-testid="wallet-button"
       onClick={handleClick}
-      isConnecting
-      disabled={isConnecting}
+      disabled={isPending}
     >
-      {isConnecting ? 'Connecting' : walletName}
-      <Icon
-        src={require(`assets/icons/${walletName.toLowerCase()}.svg`).default}
-      />
+      {isPending ? 'Connecting...' : walletName}
+      {isPending ? (
+        <StyledLoader />
+      ) : (
+        <Icon
+          src={
+            walletName &&
+            require(`assets/icons/${walletName.toLowerCase()}.svg`).default
+          }
+        />
+      )}
     </StyledButton>
   );
 }
 
 type StyledButtonProps = {
-  isConnecting?: boolean;
+  isPending?: boolean;
 };
 
 const Icon = styled.img`
@@ -78,19 +81,26 @@ const Icon = styled.img`
 `;
 
 const StyledButton = styled.button<StyledButtonProps>`
-  background: white;
-  padding: 12px 16px;
-  border: 2px solid ${colors.black};
-  font-size: 16px;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  width: 356px;
+
+  background: white;
+  border: 1px solid ${colors.gray};
+  padding: 12px 16px;
   margin: 10px 0;
+  width: 356px;
+  height: 68px;
+  font-size: 16px;
+
   cursor: pointer;
   :disabled {
     border-color: ${colors.gray};
   }
+  &:hover {
+    border-color: ${colors.black};
+  }
+  transition: border-color 0.2s;
 `;
 
 const StyledExternalLink = styled.a`
@@ -99,6 +109,20 @@ const StyledExternalLink = styled.a`
     text-decoration: underline;
     text-decoration-color: ${colors.gray};
   }
+`;
+
+const spin = keyframes`
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+`;
+
+const StyledLoader = styled.div`
+  border: 4px solid ${colors.black}; /* Light grey */
+  border-top: 4px solid ${colors.white}; /* Blue */
+  border-radius: 50%;
+  width: 30px;
+  height: 30px;
+  animation: ${spin} 2s linear infinite;
 `;
 
 export default WalletButton;
