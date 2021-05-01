0d7e9ed4b2ff43410c660d014385139612478160
Robinnnnn <rokim8@gmail.com>
2021-05-01 03:04:03 -0400

    dynamic button text

--- src/scenes/CollectionCreationFlow/steps/AddNfts/CollectionNamingForm.tsx
+++ src/scenes/CollectionCreationFlow/steps/AddNfts/CollectionNamingForm.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useMemo } from 'react';
 import styled from 'styled-components';
 
 import BigInput from 'components/core/BigInput/BigInput';
@@ -6,18 +6,27 @@ import TextArea from 'components/core/TextArea/TextArea';
 import { Text } from 'components/core/Text/Text';
 import colors from 'components/core/colors';
 import Spacer from 'components/core/Spacer/Spacer';
+import PrimaryButton from 'components/core/Button/PrimaryButton';
 
 function CollectionNamingForm() {
   const [collectionName, setCollectionName] = useState('');
   const [collectionDescription, setCollectionDescription] = useState('');
 
   const handleNameChange = useCallback((event) => {
-    // do something
+    setCollectionName(event.target.value);
   }, []);
 
   const handleDescriptionChange = useCallback((event) => {
-    // do something
+    setCollectionDescription(event.target.value);
   }, []);
+
+  const buttonText = useMemo(() => {
+    const hasEnteredValue =
+      collectionName.length || collectionDescription.length;
+    return hasEnteredValue ? 'submit' : 'skip';
+  }, [collectionName, collectionDescription]);
+
+  const handleClick = useCallback(() => {}, []);
 
   return (
     <StyledCollectionNamingForm>
@@ -31,6 +40,10 @@ function CollectionNamingForm() {
         onChange={handleDescriptionChange}
         placeholder="Collection description (optional)"
       />
+      <Spacer height={20} />
+      <ButtonContainer>
+        <StyledPrimaryButton text={buttonText} onClick={handleClick} />
+      </ButtonContainer>
     </StyledCollectionNamingForm>
   );
 }
@@ -44,6 +57,15 @@ const StyledCollectionNamingForm = styled.div`
 
 const StyledTextArea = styled(TextArea)`
   height: 144px;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const StyledPrimaryButton = styled(PrimaryButton)`
+  width: 90px;
 `;
 
 export default CollectionNamingForm;
