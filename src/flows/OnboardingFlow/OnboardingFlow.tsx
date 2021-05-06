63576dd99f22fe5311ef83f3cb0697a1fb6ac2dd
Robinnnnn <rokim8@gmail.com>
2021-05-06 02:23:13 -0400

    OrganizeCollections => OrganizeGallery

--- src/flows/OnboardingFlow/OnboardingFlow.tsx
+++ src/flows/OnboardingFlow/OnboardingFlow.tsx
@@ -2,9 +2,9 @@ import { memo } from 'react';
 import { RouteComponentProps } from '@reach/router';
 import { Wizard, Steps, Step } from 'react-albus';
 import WizardFooter from './WizardFooter';
+import OrganizeGallery from 'flows/shared/steps/OrganizeGallery/OrganizeGallery';
 import CreateFirstCollection from './steps/CreateFirstCollection';
 import AddNfts from './steps/AddNfts/AddNfts';
-import OrganizeCollections from './steps/OrganizeCollections/OrganizeCollections';
 import AddUserInfo from './steps/AddUserInfo';
 import GalleryWizardProvider from 'contexts/wizard/GalleryWizardContext';
 import { WizardProps } from './types';
@@ -20,7 +20,7 @@ function OnboardingFlow(_: RouteComponentProps) {
                 <Step id="addUserInfo" render={AddUserInfo} />
                 <Step id="create" render={CreateFirstCollection} />
                 <Step id="addNfts" render={AddNfts} />
-                <Step id="organize" render={OrganizeCollections} />
+                <Step id="organize" render={OrganizeGallery} />
               </Steps>
               <WizardFooter {...(wizardProps as WizardProps)} />
             </>
