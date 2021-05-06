1ea4355fa7bb8a45c636cefb3bed55394b5cbeef
Robinnnnn <rokim8@gmail.com>
2021-05-06 02:47:28 -0400

    fix types import

---
+++ src/flows/EditGalleryFlow.tsx/EditGalleryFlow.tsx
@@ -0,0 +1,29 @@
+import { memo } from 'react';
+import { RouteComponentProps } from '@reach/router';
+import { Wizard, Steps, Step } from 'react-albus';
+import GalleryWizardProvider from 'contexts/wizard/GalleryWizardContext';
+import OrganizeGallery from 'flows/shared/steps/OrganizeGallery/OrganizeGallery';
+import OrganizeCollection from 'flows/shared/steps/OrganizeCollection/OrganizeCollection';
+import { WizardProps } from 'flows/shared/types';
+
+function EditGalleryFlow(_: RouteComponentProps) {
+  return (
+    <GalleryWizardProvider>
+      <Wizard
+        render={(wizardProps) => {
+          return (
+            <>
+              <Steps>
+                <Step id="organizeGallery" render={OrganizeGallery} />
+                <Step id="organizeCollection" render={OrganizeCollection} />
+              </Steps>
+              <WizardFooter {...(wizardProps as WizardProps)} />
+            </>
+          );
+        }}
+      />
+    </GalleryWizardProvider>
+  );
+}
+
+export default memo(EditGalleryFlow);
