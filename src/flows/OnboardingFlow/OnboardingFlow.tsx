2bdd67f5691fd5dae61860f78ef74f54487e82bc
kaitoo1 <80802871+kaitoo1@users.noreply.github.com>
2021-05-07 16:36:11 +0900

    wizard transition WIP

--- src/flows/OnboardingFlow/OnboardingFlow.tsx
+++ src/flows/OnboardingFlow/OnboardingFlow.tsx
@@ -1,6 +1,7 @@
 import { memo } from 'react';
 import { RouteComponentProps } from '@reach/router';
 import { Wizard, Steps, Step } from 'react-albus';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import WizardFooter from './WizardFooter';
 import OrganizeGallery from 'flows/shared/steps/OrganizeGallery/OrganizeGallery';
 import OrganizeCollection from 'flows/shared/steps/OrganizeCollection/OrganizeCollection';
@@ -10,6 +11,7 @@ import AddUserInfo from './steps/AddUserInfo';
 import Congratulations from './steps/Congratulations';
 import GalleryWizardProvider from 'contexts/wizard/GalleryWizardContext';
 import { WizardProps } from 'flows/shared/types';
+import './transition.css';
 
 function OnboardingFlow(_: RouteComponentProps) {
   return (
@@ -18,14 +20,27 @@ function OnboardingFlow(_: RouteComponentProps) {
         render={(wizardProps) => {
           return (
             <>
-              <Steps>
-                <Step id="welcome" render={Welcome} />
-                <Step id="addUserInfo" render={AddUserInfo} />
-                <Step id="create" render={CreateFirstCollection} />
-                <Step id="organizeCollection" render={OrganizeCollection} />
-                <Step id="organizeGallery" render={OrganizeGallery} />
-                <Step id="congratulations" render={Congratulations} />
-              </Steps>
+              <TransitionGroup>
+                <CSSTransition
+                  key={wizardProps.step.id}
+                  classNames="fade"
+                  timeout={{ enter: 1300, exit: 300 }}
+                >
+                  <div style={{ position: 'absolute', width: '100%' }}>
+                    <Steps key={wizardProps.step.id} step={wizardProps.step}>
+                      <Step id="welcome" render={Welcome} />
+                      <Step id="addUserInfo" render={AddUserInfo} />
+                      <Step id="create" render={CreateFirstCollection} />
+                      <Step
+                        id="organizeCollection"
+                        render={OrganizeCollection}
+                      />
+                      <Step id="organizeGallery" render={OrganizeGallery} />
+                      <Step id="congratulations" render={Congratulations} />
+                    </Steps>
+                  </div>
+                </CSSTransition>
+              </TransitionGroup>
               <WizardFooter
                 shouldHideFooter={
                   wizardProps.step.id === 'welcome' ||
