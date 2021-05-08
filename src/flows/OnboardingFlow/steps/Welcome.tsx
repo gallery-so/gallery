5e30c619ee68625160c2961615b917d736f9b647
kaitoo1 <80802871+kaitoo1@users.noreply.github.com>
2021-05-08 16:20:56 +0900

    fix welcome positioning

--- src/flows/OnboardingFlow/steps/Welcome.tsx
+++ src/flows/OnboardingFlow/steps/Welcome.tsx
@@ -9,37 +9,39 @@ function Welcome({ next }: WizardContext) {
   return (
     <StyledWelcome>
       <StyledHeader>Welcome to Gallery</StyledHeader>
+      <Spacer height={24} />
       <StyledBodyText>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation
       </StyledBodyText>
-      <Spacer height={20} />
+      <Spacer height={40} />
       <StyledButton text="Enter Gallery" onClick={next} />
     </StyledWelcome>
   );
 }
+
+const StyledWelcome = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
 
 const StyledHeader = styled.p`
   text-align: center;
   color: black;
   font-size: 50px;
   white-space: nowrap;
-  margin-bottom: 24px;
+
+  margin: 0;
 `;
 
 const StyledBodyText = styled(Text)`
-  margin-bottom: 24px;
   color: ${colors.gray50};
-`;
-
-const StyledWelcome = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 140px;
-  width: min-content;
-  margin: 20vh auto 0;
+  width: 400px;
+  text-align: center;
 `;
 
 const StyledButton = styled(Button)`
