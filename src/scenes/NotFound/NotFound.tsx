99d5ba3dbf4b607b2b766a3340e9de1d96c899fe
kaitoo1 <80802871+kaitoo1@users.noreply.github.com>
2021-05-16 06:43:02 +0900

    migrate NotFound

--- src/scenes/NotFound/NotFound.tsx
+++ src/scenes/NotFound/NotFound.tsx
@@ -1,13 +1,19 @@
 import { memo } from 'react';
 import { Link, RouteComponentProps } from '@reach/router';
 import styled from 'styled-components';
-import { Title } from 'components/core/Text/Text';
+import { Display, BodyRegular } from 'components/core/Text/Text';
+import Button from 'components/core/Button/Button';
 
 function NotFound(_: RouteComponentProps) {
   return (
     <StyledNotFound>
-      <Title>NOT FOUND</Title>
-      <Link to="/">go back home</Link>
+      <Display>404</Display>
+      <BodyRegular>
+        Looks like you took a wrong turn. There's not much to see here.
+      </BodyRegular>
+      <Link to="/">
+        <Button>Take me back</Button>
+      </Link>
     </StyledNotFound>
   );
 }
