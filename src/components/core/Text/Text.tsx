a3fadb8f2a8c13924ed417ef5a010f90eb2bd696
kaitoo1 <80802871+kaitoo1@users.noreply.github.com>
2021-05-16 07:39:28 +0900

    update core Text

--- src/components/core/Text/Text.tsx
+++ src/components/core/Text/Text.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import colors from '../colors';
 
-const HEADER_FONT_FAMILY = 'Gallery Display';
+const TITLE_FONT_FAMILY = 'Gallery Display';
 const BODY_FONT_FAMILY = 'Helvetica Neue';
 
 type TextProps = {
@@ -30,21 +30,27 @@ const Paragraph = styled.p<TextProps>`
 `;
 
 export const Display = styled(H1)`
+  font-family: ${TITLE_FONT_FAMILY};
   font-size: 48px;
+  font-weight: 400;
   line-height: 56px;
   letter-spacing: 0px;
   margin: 0;
 `;
 
 export const Subdisplay = styled(H2)`
+  font-family: ${TITLE_FONT_FAMILY};
   font-size: 32px;
+  font-weight: 400;
   line-height: 44px;
   letter-spacing: 0px;
   margin: 0;
 `;
 
 export const Heading = styled(H3)`
+  font-family: ${BODY_FONT_FAMILY};
   font-size: 20px;
+  font-weight: 400;
   line-height: 28px;
   letter-spacing: 0px;
   margin: 0;
@@ -57,7 +63,7 @@ const _TitleBase = styled(Paragraph)`
 `;
 
 export const TitleSerif = styled(_TitleBase)`
-  font-family: ${HEADER_FONT_FAMILY};
+  font-family: ${TITLE_FONT_FAMILY};
 `;
 
 export const TitleRegular = styled(_TitleBase)`
@@ -80,6 +86,14 @@ export const BodyMedium = styled(BodyRegular)`
   font-weight: 500;
 `;
 
+export const ButtonText = styled(Paragraph)`
+  font-family: ${BODY_FONT_FAMILY};
+  font-size: 12px;
+  line-height: 16px;
+  letter-spacing: 0px;
+  margin: 0;
+`;
+
 export const Caption = styled(Paragraph)`
   font-family: ${BODY_FONT_FAMILY};
   font-size: 12px;
@@ -87,51 +101,3 @@ export const Caption = styled(Paragraph)`
   letter-spacing: 0.4px;
   margin: 0px 0px 12px 0px;
 `;
-
-// type TitleProps = {
-//   // TODO: make these enums based on sizing chart in figma
-//   size?: 'normal' | 'mini';
-// };
-
-// export const Title = styled.p<TitleProps>`
-//   font-family: 'Gallery Display';
-//   margin: 0;
-
-//   font-size: ${({ size }) => (size === 'mini' ? '16px' : '48px')};
-// `;
-
-// type SubtitleProps = {
-//   color?: colors;
-//   // TODO: make these enums based on sizing chart in figma
-//   size?: 'normal' | 'large';
-//   // TODO: make these enums based on sizing chart in figma
-//   weight?: 'normal' | 'bold';
-// };
-
-// export const Subtitle = styled.p<SubtitleProps>`
-//   font-family: 'Helvetica Neue';
-//   margin: 0;
-
-//   font-weight: ${({ weight }) => (weight === 'bold' ? 500 : 400)};
-//   font-size: ${({ size }) => (size === 'large' ? '28px' : '20px')};
-
-//   color: ${({ color }) => color ?? colors.black};
-// `;
-
-// type TextProps = {
-//   color?: colors;
-//   // TODO: make these enums based on sizing chart in figma
-//   weight?: 'normal' | 'bold';
-// };
-
-// export const Text = styled.p<TextProps>`
-//   font-family: 'Helvetica Neue';
-//   margin: 0;
-
-//   font-size: 14px;
-//   // TODO: might wanna make this configurable with props
-//   line-height: 20px;
-
-//   font-weight: ${({ weight }) => (weight === 'bold' ? 500 : 400)};
-//   color: ${({ color }) => color ?? colors.black};
-// `;
