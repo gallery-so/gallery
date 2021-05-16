9ef5959490fe6f83242a5061ab3fa3927cb47cf3
kaito <80802871+kaitoo1@users.noreply.github.com>
2021-05-16 18:50:55 +0900

    restructure SortableStagedNft to fix visual bug

--- src/flows/shared/steps/OrganizeCollection/Editor/SortableStagedNft.tsx
+++ src/flows/shared/steps/OrganizeCollection/Editor/SortableStagedNft.tsx
@@ -1,15 +1,20 @@
 import { useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
+import Gradient from 'components/core/Gradient/Gradient';
+import transitions from 'components/core/transitions';
+import { StyledNftPreviewLabel } from 'components/NftPreview/NftPreviewLabel';
 import { memo, useMemo } from 'react';
 import styled from 'styled-components';
 import { Nft } from 'types/Nft';
 import StagedNftImage from './StagedNftImage';
+import UnstageButton from './UnstageButton';
 
 type Props = {
   nft: Nft;
+  index?: number;
 };
 
-function SortableStagedNft({ nft, ...props }: Props) {
+function SortableStagedNft({ nft, index, ...props }: Props) {
   const {
     attributes,
     listeners,
@@ -30,27 +35,57 @@ function SortableStagedNft({ nft, ...props }: Props) {
 
   return (
     <StyledSortableNft
-      ref={setNodeRef}
       id={nft.id}
       active={isDragging}
       // @ts-expect-error
       style={style}
-      {...attributes}
-      {...listeners}
     >
-      <StagedNftImage nft={nft} />
+      <StyledGradient type="top" />
+      <div ref={setNodeRef} {...attributes} {...listeners}>
+        <StagedNftImage nft={nft} />
+      </div>
+      <StyledUnstageButton
+        // TODO make index guaranteed
+        nftIndex={index || 0}
+      />
+      <StyledGradient type="bottom" direction="down" />
     </StyledSortableNft>
   );
 }
+const StyledGradient = styled(Gradient)<{ type: 'top' | 'bottom' }>`
+  position: absolute;
+  ${({ type }) => type}: 0;
 
-// TODO: enable image to scale smoothly when picking up
+  opacity: 0;
+  transition: opacity ${transitions.cubic};
+`;
+
+const StyledUnstageButton = styled(UnstageButton)`
+  opacity: 0;
+  top: 0;
+  transition: opacity ${transitions.cubic};
+`;
+
 const StyledSortableNft = styled.div`
+  position: relative;
   -webkit-backface-visibility: hidden;
   &:focus {
     // ok to remove focus here because there it is not functionally 'in focus' for the user
     outline: none;
   }
   cursor: grab;
+
+  &:hover ${StyledUnstageButton} {
+    opacity: 1;
+  }
+
+  &:hover ${StyledNftPreviewLabel} {
+    opacity: 1;
+  }
+
+  &:hover ${StyledGradient} {
+    opacity: 1;
+  }
 `;
 
 export default memo(SortableStagedNft);
