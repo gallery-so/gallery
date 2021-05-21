f43572c495479a4ad36c9730464e0d91464e11e7
Fabric Softener <fabricsoftener3@gmail.com>
2021-05-20 23:29:45 -0400

    useWindowSize

---
+++ src/hooks/useWindowSize.ts
@@ -0,0 +1,37 @@
+import { useEffect, useMemo, useState } from 'react';
+import { size } from 'components/core/breakpoints';
+
+export default function useWindowSize() {
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowSize;
+}
+
+export function useBreakpoint(): size {
+  const { width } = useWindowSize();
+
+  const breakpoint = useMemo(() => {
+    if (width >= size.desktop) return size.desktop;
+    if (width >= size.tablet) return size.tablet;
+    if (width >= size.mobileLarge) return size.mobileLarge;
+    return size.mobile;
+  }, [width]);
+
+  return breakpoint;
+}
