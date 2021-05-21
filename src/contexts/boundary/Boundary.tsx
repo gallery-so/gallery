6a2888750c60b4d9e5fcbdec53845e4dc2a9fbc5
fbrcsftnr <84365743+fbrcsftnr@users.noreply.github.com>
2021-05-21 01:41:20 -0400

    App loading and error fallbacks (#243)
    * full page loader for suspense boundary
    
    * error boundary

--- src/contexts/boundary/Boundary.tsx
+++ src/contexts/boundary/Boundary.tsx
@@ -1,9 +1,10 @@
 import { Suspense, memo } from 'react';
+import FullPageLoader from 'components/core/Loader/FullPageLoader';
 import ErrorBoundary from './ErrorBoundary';
 
 export const Boundary = memo(({ children }) => {
   return (
-    <Suspense fallback="Loading...">
+    <Suspense fallback={<FullPageLoader />}>
       <ErrorBoundary>{children}</ErrorBoundary>
     </Suspense>
   );
