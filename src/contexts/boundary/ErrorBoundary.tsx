83eeb1c88a97def7d671c1244dec3416a957fda4
Fabric Softener <fabricsoftener3@gmail.com>
2021-05-21 01:35:46 -0400

    error boundary

--- src/contexts/boundary/ErrorBoundary.tsx
+++ src/contexts/boundary/ErrorBoundary.tsx
@@ -1,19 +1,41 @@
+import { Subdisplay } from 'components/core/Text/Text';
 import React from 'react';
+import styled from 'styled-components';
 
 class ErrorBoundary extends React.Component {
   state = { hasError: false, error: null };
+
   static getDerivedStateFromError(error: Error) {
     return {
       hasError: true,
       error,
     };
   }
+
   render() {
     if (this.state.hasError) {
-      // @ts-ignore
-      return <h1>{this.state.error.message}</h1>;
+      return (
+        <StyledErrorView>
+          <Subdisplay>
+            {
+              // @ts-ignore
+              this.state.error?.message ?? 'There was an error'
+            }
+          </Subdisplay>
+        </StyledErrorView>
+      );
     }
     return this.props.children;
   }
 }
+
+const StyledErrorView = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 export default ErrorBoundary;
