cfa62382b7c7949c32c1577f0b8f292c60c6481a
Fabric Softener <fabricsoftener3@gmail.com>
2021-05-24 00:04:41 -0400

    basic mock server

--- src/contexts/swr/SwrContext.tsx
+++ src/contexts/swr/SwrContext.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 import { SWRConfig } from 'swr';
 import { MINUTE } from 'utils/time';
 
+const baseurl =
+  process.env.ENV === 'production'
+    ? 'https://api.gallery.so'
+    : 'http://localhost:3000/api';
+
 export const SwrProvider = React.memo(({ children }) => {
   const value = {
-    fetcher: (url: string) => fetch(`api/${url}`).then((r) => r.json()),
+    fetcher: (path: string) =>
+      fetch(`${baseurl}/glry/v1${path}`).then((r) => r.json()),
     refreshInterval: 5 * MINUTE,
     suspense: true,
   };
+
   return <SWRConfig value={value}>{children}</SWRConfig>;
 });
