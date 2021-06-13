4f60d7f30373aa40da272c9c503e5b3bd48ac39b
Fabric Softener <fabricsoftener3@gmail.com>
2021-06-12 22:00:22 -0400

    ApiResponse type

---
+++ src/types/ApiResponse.ts
@@ -0,0 +1,38 @@
+/**
+ * GALLERY SERVER RESPONSE FORMAT
+ *
+ * Example success response:
+ *   {
+ *     data: {
+ *       env: 'dev',
+ *       msg: 'gallery operational',
+ *     },
+ *     status: 'OK',
+ *   }
+ *
+ * Example error response:
+ *   {
+ *     data: {
+ *       handler_error_user_msg:
+ *         'handler /glry/v1/auth/get_preflight failed unexpectedly',
+ *     },
+ *     status: 'ERROR',
+ *   }
+ */
+type SuccessResponse<T = {}> = {
+  data: T;
+  status: 'OK';
+};
+
+type ErrorResponse = {
+  data: {
+    handler_error_user_msg: string;
+  };
+  status: 'ERROR';
+};
+
+export type ApiResponse<T = {}> = SuccessResponse<T> | ErrorResponse;
+
+export function isErrorResponse(res: ApiResponse): res is ErrorResponse {
+  return res.status === 'ERROR';
+}
