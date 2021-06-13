c0280d1dd1d9cb94925d87a4a4fa95c5dabb41c5
Fabric Softener <fabricsoftener3@gmail.com>
2021-06-12 22:01:10 -0400

    update fetcher to take generics, support post requests

--- src/contexts/swr/fetcher.ts
+++ src/contexts/swr/fetcher.ts
@@ -1,17 +1,31 @@
 import { JWT_LOCAL_STORAGE_KEY } from 'contexts/auth/constants';
+import { isErrorResponse } from 'types/ApiResponse';
 
 const baseurl =
   process.env.ENV === 'production'
     ? 'https://api.gallery.so'
     : 'http://localhost:3000/api';
 
-export default async function fetcher(path: string) {
+export default async function fetcher<ResponseData, RequestBody = {}>(
+  path: string,
+  body?: RequestBody
+): Promise<ResponseData> {
   const localJwt = window.localStorage.getItem(JWT_LOCAL_STORAGE_KEY);
 
   const requestOptions: RequestInit = localJwt
     ? { headers: { Authentication: `Bearer: ${localJwt}` } }
     : {};
 
+  if (body) {
+    requestOptions.method = 'POST';
+    requestOptions.body = JSON.stringify(body);
+  }
+
   const res = await fetch(`${baseurl}/glry/v1${path}`, requestOptions);
-  return await res.json();
+  const payload = await res.json();
+
+  if (isErrorResponse(payload)) {
+    throw new Error(payload.data.handler_error_user_msg);
+  }
+  return payload.data;
 }
