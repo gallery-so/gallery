ef0ca00cdfc4973fb9dc5cc899a9abbc509fe3ee
Robinnnnn <rokim8@gmail.com>
2021-08-08 22:53:11 -0400

    cleanup useNft

--- src/hooks/api/useNft.ts
+++ src/hooks/api/useNft.ts
@@ -1,13 +1,14 @@
 import useSwr from 'swr';
-import { isNftResponseError, Nft, NftResponse } from 'types/Nft';
+import { Nft } from 'types/Nft';
 
 type Props = {
   id: string;
 };
-export default function useNft({ id }: Props): Nft | null {
-  const { data } = useSwr<NftResponse>(`/nfts/get?id=${id}`);
 
-  if (!data || isNftResponseError(data)) {
+export default function useNft({ id }: Props): Nft | null {
+  const { data } = useSwr<Nft>(`/nfts/get?id=${id}`);
+
+  if (!data) {
     return null;
   }
 
