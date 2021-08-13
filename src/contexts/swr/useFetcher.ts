cb5a87142ec5d02b517c772dc0ccb7cf8e15697e
Robinnnnn <rokim8@gmail.com>
2021-08-13 03:20:27 -0400

    update useFetcher to accept custom action

--- src/contexts/swr/useFetcher.ts
+++ src/contexts/swr/useFetcher.ts
@@ -1,6 +1,7 @@
 import { useAuthActions } from 'contexts/auth/AuthContext';
 import { JWT_LOCAL_STORAGE_KEY } from 'contexts/auth/constants';
 import { useCallback } from 'react';
+import { ApiError } from 'src/errors/types';
 
 const baseurl = process.env.REACT_APP_API_BASE_URL;
 
@@ -9,6 +10,8 @@ const ERR_UNAUTHORIZED = 401;
 export type FetcherType = <ResponseData, RequestBody = {}>(
   path: string,
   body?: RequestBody,
+  // a custom string that describes the request, e.g. "fetch user"
+  action?: string,
   unauthorizedErrorHandler?: () => void
 ) => Promise<ResponseData>;
 
@@ -16,6 +19,7 @@ export type FetcherType = <ResponseData, RequestBody = {}>(
 export const _fetch: FetcherType = async (
   path,
   body,
+  action,
   unauthorizedErrorHandler
 ) => {
   const localJwt = window.localStorage.getItem(JWT_LOCAL_STORAGE_KEY);
@@ -33,18 +37,33 @@ export const _fetch: FetcherType = async (
   }
   const res = await fetch(`${baseurl}/glry/v1${path}`, requestOptions);
 
-  // If the response doesnt have a Response stream, such as with a
-  // succesful Update request, res.json() will throw an error
   const responseBody = await res.json().catch((e) => {
-    // TODO: use app-wide error pill
-    console.error('Error parsing response json', e);
+    // certain successful responses won't have a JSON body (e.g. updates)
+    // res.json() will throw an error in these cases, which we catch gracefully
+    if (res.ok) {
+      return {};
+    }
+
+    // attach custom error message if provided
+    if (action) {
+      const apiError = new ApiError(e.message, action);
+      throw apiError;
+    }
+    throw e;
   });
 
   if (!res.ok) {
     if (res.status === ERR_UNAUTHORIZED) {
       unauthorizedErrorHandler?.();
     }
-    throw new Error(responseBody ? responseBody.error : 'Error');
+
+    // All gallery-provided error responses will have an `error` field
+    const serverErrorMessage = responseBody?.error ?? 'Server Error';
+    if (action) {
+      const apiError = new ApiError(serverErrorMessage, action);
+      throw apiError;
+    }
+    throw new Error(serverErrorMessage);
   }
   return responseBody;
 };
@@ -59,5 +78,8 @@ export const _fetch: FetcherType = async (
 export default function useFetcher(): FetcherType {
   const { logOut } = useAuthActions();
 
-  return useCallback((path, body) => _fetch(path, body, logOut), [logOut]);
+  return useCallback(
+    (path, body, action) => _fetch(path, body, action, logOut),
+    [logOut]
+  );
 }
