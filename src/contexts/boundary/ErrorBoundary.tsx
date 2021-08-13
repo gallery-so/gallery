2a49f9e213585f1225042f8832b572a41b8cc4e7
Robinnnnn <rokim8@gmail.com>
2021-08-13 14:33:02 -0400

    updated error boundary, formatError

--- src/contexts/boundary/ErrorBoundary.tsx
+++ src/contexts/boundary/ErrorBoundary.tsx
@@ -1,27 +1,22 @@
 import { Component } from 'react';
 import { Subdisplay } from 'components/core/Text/Text';
 import Page from 'components/core/Page/Page';
+import { formatDetailedError } from 'src/errors/formatError';
 
 class ErrorBoundary extends Component {
-  state = { hasError: false, error: null };
+  state: { error: null | Error } = { error: null };
 
   static getDerivedStateFromError(error: Error) {
-    return {
-      hasError: true,
-      error,
-    };
+    return { error };
   }
 
   render() {
-    if (this.state.hasError) {
+    if (this.state.error !== null) {
+      const { header, description } = formatDetailedError(this.state.error);
       return (
         <Page centered withRoomForFooter={false}>
-          <Subdisplay>
-            {
-              // @ts-ignore
-              this.state.error?.message ?? 'There was an error'
-            }
-          </Subdisplay>
+          <Subdisplay>{header}</Subdisplay>
+          <Subdisplay>{description}</Subdisplay>
         </Page>
       );
     }
