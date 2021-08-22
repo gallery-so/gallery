1a256517aac0311dcc77b54f501e8dc3b488aed0
Robinnnnn <rokim8@gmail.com>
2021-08-22 01:23:53 -0400

    nuke

--- src/scenes/Routes.tsx
+++ src/scenes/Routes.tsx
@@ -3,13 +3,14 @@ import AuthenticatedRoute from 'components/AuthenticatedRoute/AuthenticatedRoute
 import GlobalNavbar from 'components/core/Page/Navbar';
 import GlobalFooter from 'components/core/Page/Footer';
 import FadeTransitioner from 'components/FadeTransitioner/FadeTransitioner';
-import Home from 'scenes/Home/Home';
-import Auth from 'scenes/Auth/Auth';
-import Password from 'scenes/Password/Password';
-import NotFound from 'scenes/NotFound/NotFound';
-import NftDetailPage from 'scenes/NftDetailPage/NftDetailPage';
 import OnboardingFlow from 'flows/OnboardingFlow/OnboardingFlow';
 import EditGalleryFlow from 'flows/EditGalleryFlow/EditGalleryFlow';
+import Home from './Home/Home';
+import Auth from './Auth/Auth';
+import Password from './Password/Password';
+import NotFound from './NotFound/NotFound';
+import NftDetailPage from './NftDetailPage/NftDetailPage';
+import Nuke from './Nuke/Nuke';
 import UserGalleryPage from './UserGalleryPage/UserGalleryPage';
 
 // considered putting this in a different file, but we should tightly couple
@@ -43,6 +44,7 @@ export default function Routes() {
               <AuthenticatedRoute Component={OnboardingFlow} path="/welcome" />
               <AuthenticatedRoute Component={EditGalleryFlow} path="/edit" />
               <NotFound path="/404" />
+              <Nuke path="/nuke" />
               <NftDetailPage path="/:userName/:collectionId/:nftId" />
               <UserGalleryPage path="/:username" />
             </Router>
