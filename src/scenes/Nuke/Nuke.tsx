725013442094f5cf6be7e14b2844d305c454a8f6
Robin <rokim8@gmail.com>
2021-08-22 16:31:27 -0400

    nuke (#322)

---
+++ src/scenes/Nuke/Nuke.tsx
@@ -0,0 +1,31 @@
+import { memo, useEffect } from 'react';
+import { Link, RouteComponentProps } from '@reach/router';
+import styled from 'styled-components';
+import { Display } from 'components/core/Text/Text';
+import Button from 'components/core/Button/Button';
+import Spacer from 'components/core/Spacer/Spacer';
+import Page from 'components/core/Page/Page';
+import { useAuthActions } from 'contexts/auth/AuthContext';
+
+// suggest a user visit this page if they're in a seriously broken state
+function Nuke(_: RouteComponentProps) {
+  const { logOut } = useAuthActions();
+
+  useEffect(logOut, [logOut]);
+
+  return (
+    <Page centered>
+      <Display>Your local data has been nuked</Display>
+      <Spacer height={32} />
+      <Link to="/">
+        <StyledButton text="Take me home" />
+      </Link>
+    </Page>
+  );
+}
+
+const StyledButton = styled(Button)`
+  padding: 0px 24px;
+`;
+
+export default memo(Nuke);
