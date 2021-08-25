132ae228ce8ef70ff5241f58ddd2f1f728cf1151
kaito <80802871+kaitoo1@users.noreply.github.com>
2021-08-25 15:34:01 +0900

    CSimplify api baseurl in .env (#330)

--- dev_server/server.js
+++ dev_server/server.js
@@ -1,28 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const checkPort = require('./checkPort');
 const router = require('./routes');
-
-function getBaseUrl() {
-  switch (process.env.ENV) {
-    case 'production':
-      return process.env.BASEURL_PRODUCTION;
-    case 'dev':
-      return process.env.BASEURL_DEVELOPMENT;
-    case 'hack':
-      return process.env.BASEURL_HACK;
-    case 'local':
-    default:
-      return process.env.BASEURL_LOCAL;
-  }
-}
-
-const baseurl = getBaseUrl();
 
 module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: baseurl,
+      target: process.env.MOCK_SERVER_BASE_URL,
       changeOrigin: true,
       pathRewrite: {
         // drop `/api` from path prefix, since its only purpose is to route local requests here
@@ -37,30 +20,19 @@ module.exports = function (app) {
  * example lifecycle:
  *   1) useSwr('/users')                         // react component
  *   2) http://localhost:3000/api/glry/v1/users  // fetch configured in SwrContext.tsx
- *   3) http://localhost:4000/users              // proxy rewrite in current file
+ *   3) http://localhost:3500/users              // proxy rewrite in current file
  *
  * TODO: allow local development to communicate directly with production AWS servers.
  *       this will probably just be an extra flag when starting the app
  */
 if (process.env.ENV === 'local') {
-  checkPort(process.env.SERVER_PORT)
-    .then(() => {
-      console.log('Running against local mock server');
-      initializeMockServer();
-    })
-    .catch((e) => {
-      if (e.code === 'EADDRINUSE') {
-        console.log('Running against local backend');
-        return;
-      }
-      console.log('Error initializing mock server:', e);
-    });
+  initializeMockServer();
 }
 
 function initializeMockServer() {
   const express = require('express');
   const mockServer = express();
-  const port = process.env.SERVER_PORT;
+  const port = process.env.MOCK_SERVER_PORT;
 
   mockServer.use(function (req, res, next) {
     // artificial latency
@@ -71,7 +43,7 @@ function initializeMockServer() {
     console.log(`test server initialized at http://localhost:${port}`);
   });
 
-  // $ curl http://localhost:4000/health
+  // $ curl http://localhost:3500/health
   mockServer.get('/health', (req, res) => {
     res.send('test server is live');
   });
