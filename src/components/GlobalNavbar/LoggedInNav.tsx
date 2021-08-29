7c972319e8906c4ce2226a241e17956473538e51
kaito <80802871+kaitoo1@users.noreply.github.com>
2021-08-29 10:07:32 +0900

    Move options under 'Edit Profile' to LoggedInNavbar, and get rid of EditProfile (#333)
    * Move options under 'Edit Profile' to LoggedInNavbar, and get rid of EditProfile
    
    * reorder items and increase truncated address length

--- src/components/GlobalNavbar/LoggedInNav.tsx
+++ src/components/GlobalNavbar/LoggedInNav.tsx
@@ -10,15 +10,18 @@ import {
   useAuthenticatedUser,
   useAuthenticatedUserAddress,
 } from 'hooks/api/users/useUser';
+import { useModal } from 'contexts/modal/ModalContext';
+import EditUserInfoModal from 'scenes/UserGalleryPage/EditUserInfoModal';
 
 function truncate(address: string) {
-  return `${address.slice(0, 4)}...${address.slice(-4)}`;
+  return `${address.slice(0, 8)}......${address.slice(-4)}`;
 }
 
 function LoggedInNav() {
   const { logOut } = useAuthActions();
   const user = useAuthenticatedUser();
   const userAddress = useAuthenticatedUserAddress();
+  const { showModal } = useModal();
 
   const truncatedUserAddress = useMemo(() => {
     return truncate(userAddress);
@@ -27,6 +30,14 @@ function LoggedInNav() {
   const handleGalleryRedirect = useCallback(() => {
     navigate(`/${user.username}`);
   }, [user.username]);
+
+  const handleEditNameClick = useCallback(() => {
+    showModal(<EditUserInfoModal />);
+  }, [showModal]);
+
+  const handleEditGalleryClick = useCallback(() => {
+    navigate('/edit');
+  }, []);
 
   return (
     <Container>
@@ -40,6 +51,18 @@ function LoggedInNav() {
             underlineOnHover
           />
         </CopyToClipboard>
+        <Spacer height={12} />
+        <TextButton
+          text="Edit Gallery"
+          onClick={handleEditGalleryClick}
+          underlineOnHover
+        />
+        <Spacer height={12} />
+        <TextButton
+          text="Edit name & Bio"
+          onClick={handleEditNameClick}
+          underlineOnHover
+        />
         <Spacer height={12} />
         <TextButton text="Sign Out" onClick={logOut} underlineOnHover />
       </Dropdown>
