bf3dd4bc0d643c5c9a4e36803589567078518c5f
kaito <80802871+kaitoo1@users.noreply.github.com>
2021-09-14 15:59:11 +0900

    Hide your hidden collections if on authenticated user page

--- src/scenes/UserGalleryPage/UserGalleryCollections.tsx
+++ src/scenes/UserGalleryPage/UserGalleryCollections.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import Spacer from 'components/core/Spacer/Spacer';
 
 import { Collection } from 'types/Collection';
+import { useMemo } from 'react';
 import EmptyGallery from './EmptyGallery';
 import UserGalleryCollection from './UserGalleryCollection';
 
@@ -14,6 +15,8 @@ function UserGalleryCollections({
   collections,
   isAuthenticatedUsersPage,
 }: Props) {
+  const filteredCollections = useMemo(() => collections.filter(collection => !collection.hidden), [collections]);
+
   if (collections.length === 0) {
     const noCollectionsMessage = isAuthenticatedUsersPage
       ? 'Your gallery is empty. Display your NFTs by creating a collection.'
@@ -25,7 +28,7 @@ function UserGalleryCollections({
   // TODO: Consider extracting 48 and 108 into unit consts
   return (
     <StyledUserGalleryCollections>
-      {collections.map((collection, index) => (
+      {filteredCollections.map((collection, index) => (
         <>
           <Spacer height={index === 0 ? 48 : 108} />
           <UserGalleryCollection collection={collection} />
