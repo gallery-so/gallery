e0e92b99a0b235c376b9f293b1a63b3d2ef1be72
Robinnnnn <rokim8@gmail.com>
2021-10-03 16:41:44 -0400

    Router cleanup

--- src/scenes/_Router/Router.tsx
+++ src/scenes/_Router/Router.tsx
@@ -1,7 +1,5 @@
 import { Location, Router } from '@reach/router';
-import AuthenticatedRoute from 'scenes/_Router/AuthenticatedRoute';
-import GlobalNavbar from 'components/core/Page/GlobalNavbar/GlobalNavbar';
-import GlobalFooter from 'components/core/Page/GlobalFooter';
+import GalleryAuthenticatedRoute from 'scenes/_Router/GalleryAuthenticatedRoute';
 import FadeTransitioner from 'components/FadeTransitioner/FadeTransitioner';
 import OnboardingFlow from 'flows/OnboardingFlow/OnboardingFlow';
 import EditGalleryFlow from 'flows/EditGalleryFlow/EditGalleryFlow';
@@ -12,54 +10,64 @@ import Password from '../Password/Password';
 import NftDetailPage from '../NftDetailPage/NftDetailPage';
 import Nuke from '../Nuke/Nuke';
 import UserGalleryPage from '../UserGalleryPage/UserGalleryPage';
-
-// Considered putting this in a different file, but we should tightly couple
-// route updates to this array
-const ROUTES_WITHOUT_NAVBAR = [
-  '/',
-  '/auth',
-  '/password',
-  '/welcome',
-  '/edit',
-  '/nuke',
-];
-const ROUTES_WITH_FOOTER = ['/welcome', '/edit'];
-
-function shouldHideNavbar(pathname: string) {
-  return ROUTES_WITHOUT_NAVBAR.reduce(
-    (previous, curr) => previous || curr.includes(pathname),
-    false,
-  );
-}
-
-function shouldHideFooter(pathname: string) {
-  return ROUTES_WITH_FOOTER.find(route => route === pathname);
-}
+import GalleryRoute from './GalleryRoute';
 
 export default function Routes() {
   return (
     <Location>
       {({ location }) => (
         <>
-          {shouldHideNavbar(location.pathname) ? null : <GlobalNavbar />}
-
           <GalleryNavigationContextProvider locationKey={location.key} pathname={location.pathname}>
             <FadeTransitioner>
-              {/* primary={false} prevents jumpiness on nav: https://github.com/reach/router/issues/242 */}
-              <Router primary={false} location={location}>
-                <Home path="/" />
-                <Auth path="/auth" />
-                <Password path="/password" />
-                <AuthenticatedRoute Component={OnboardingFlow} path="/welcome" />
-                <AuthenticatedRoute Component={EditGalleryFlow} path="/edit" />
-                <Nuke path="/nuke" />
-                <NftDetailPage path="/:userName/:collectionId/:nftId" />
-                <UserGalleryPage path="/:username" />
+              <Router
+                primary={false} // prevents jumpiness on nav: https://github.com/reach/router/issues/242
+                location={location}>
+                <GalleryRoute
+                  path="/"
+                  component={Home}
+                  navbar={false}
+                  footerVisibleOutOfView
+                />
+                <GalleryRoute
+                  path="/password"
+                  component={Password}
+                  navbar={false}
+                  footerVisibleOutOfView
+                />
+                <GalleryRoute
+                  path="/auth"
+                  component={Auth}
+                  navbar={false}
+                  footerVisibleOutOfView
+                />
+                <GalleryAuthenticatedRoute
+                  path="/welcome"
+                  component={OnboardingFlow}
+                  navbar={false}
+                  footer={false}
+                />
+                <GalleryAuthenticatedRoute
+                  path="/edit"
+                  component={EditGalleryFlow}
+                  navbar={false}
+                  footer={false}
+                />
+                <GalleryRoute
+                  path="/nuke"
+                  component={Nuke}
+                  navbar={false}
+                />
+                <GalleryRoute
+                  path="/:userName/:collectionId/:nftId"
+                  component={NftDetailPage}
+                />
+                <GalleryRoute
+                  path="/:username"
+                  component={UserGalleryPage}
+                />
               </Router>
             </FadeTransitioner>
           </GalleryNavigationContextProvider>
-
-          {shouldHideFooter(location.pathname) ? null : <GlobalFooter />}
         </>
       )}
     </Location>
