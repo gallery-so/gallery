5dfa5de157ad66e1a6ad6f18f16ff8edefd11459
Robinnnnn <rokim8@gmail.com>
2021-10-04 01:18:25 -0400

    improve nft detail page positioning

--- src/scenes/NftDetailPage/NftDetailAsset.tsx
+++ src/scenes/NftDetailPage/NftDetailAsset.tsx
@@ -1,47 +1,69 @@
 import breakpoints from 'components/core/breakpoints';
 import { NftMediaType } from 'components/core/enums';
-import { useMemo } from 'react';
 import styled from 'styled-components';
 
 import ImageWithLoading from 'components/ImageWithLoading/ImageWithLoading';
 import { Nft } from 'types/Nft';
 import { getMediaType } from 'utils/nft';
+import { GLOBAL_FOOTER_HEIGHT, GLOBAL_NAVBAR_HEIGHT } from 'components/core/Page/constants';
 import NftDetailAnimation from './NftDetailAnimation';
 import NftDetailVideo from './NftDetailVideo';
 import NftDetailAudio from './NftDetailAudio';
+
+type AssetComponentProps = {
+  nft: Nft;
+  maxHeight: number;
+};
+
+function AssetComponent({ nft, maxHeight }: AssetComponentProps) {
+  const assetType = getMediaType(nft);
+
+  switch (assetType) {
+    case NftMediaType.IMAGE:
+      return <ImageWithLoading src={nft.image_url} alt={nft.name} />;
+    case NftMediaType.AUDIO:
+      return <NftDetailAudio nft={nft} />;
+    case NftMediaType.VIDEO:
+      return <NftDetailVideo nft={nft} maxHeight={maxHeight} />;
+    case NftMediaType.ANIMATION:
+      return <NftDetailAnimation nft={nft} />;
+    default:
+      return <ImageWithLoading src={nft.image_url} alt={nft.name} />;
+  }
+}
 
 type Props = {
   nft: Nft;
 };
 
-function NftDetailAsset({ nft }: Props) {
-  const getAssetComponent = useMemo(() => {
-    const assetType = getMediaType(nft);
-    switch (assetType) {
-      case NftMediaType.IMAGE:
-        return <ImageWithLoading src={nft.image_url} alt={nft.name} />;
-      case NftMediaType.AUDIO:
-        return <NftDetailAudio nft={nft} />;
-      case NftMediaType.VIDEO:
-        return <NftDetailVideo nft={nft} />;
-      case NftMediaType.ANIMATION:
-        return <NftDetailAnimation nft={nft} />;
-      default:
-        return <ImageWithLoading src={nft.image_url} alt={nft.name} />;
-    }
-  }, [nft]);
+// number that determines a reasonable max height for the displayed NFT
+const heightWithoutNavAndFooterGutters = window.screen.availHeight - (2 * (GLOBAL_NAVBAR_HEIGHT + GLOBAL_FOOTER_HEIGHT));
 
-  return <StyledAssetContainer>{getAssetComponent}</StyledAssetContainer>;
+function NftDetailAsset({ nft }: Props) {
+  const maxHeight = Math.min(
+    heightWithoutNavAndFooterGutters,
+    // TODO: this number should be determined by the dimensions of the media itself. once the media is fetched,
+    // we should grab its dimensions and set it on the shimmer context. this will allow us to display very large
+    // NFTs on very large screens
+    600,
+  );
+
+  return (
+    <StyledAssetContainer maxHeight={maxHeight}>
+      <AssetComponent nft={nft} maxHeight={maxHeight} />
+    </StyledAssetContainer>
+  );
 }
 
-const StyledAssetContainer = styled.div`
+const StyledAssetContainer = styled.div<{ maxHeight: number }>`
   align-items: center;
   display: flex;
   flex-direction: column;
   height: 100%;
 
   @media only screen and ${breakpoints.desktop} {
-    width: 430px;
+    width: ${({ maxHeight }) => maxHeight}px;
+    height: ${({ maxHeight }) => maxHeight}px;
   }
 `;
 
