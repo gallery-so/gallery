65e7d2abd9cc6832f710999aba60f498b82da60d
Robinnnnn <rokim8@gmail.com>
2021-10-22 23:59:30 -0400

    fix tests

--- config/jest/cssTransform.cjs
+++ config/jest/cssTransform.cjs
@@ -1,7 +1,7 @@
 // This is a custom Jest transformer turning style imports into empty objects.
 // http://facebook.github.io/jest/docs/en/webpack.html
 
-export const cssTransform = {
+module.exports = {
   process() {
     return 'module.exports = {};';
   },
