5d0e7fe7721054d9ecbca22f26dd669bc9387999
kaito <80802871+kaitoo1@users.noreply.github.com>
2021-11-07 18:27:39 +0900

    control click new tab

--- src/utils/navigate.ts
+++ src/utils/navigate.ts
@@ -2,7 +2,7 @@ import { navigate } from '@reach/router';
 
 // navigates to the given url. Opens in new tab if meta key (command on mac) is pressed
 export function navigateToUrl(url: string, event: React.MouseEvent<HTMLElement>) {
-  if (event.metaKey) {
+  if (event.metaKey || event.ctrlKey) {
     window.open(url);
   } else {
     void navigate(url);
