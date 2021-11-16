60d39f53ae73224fb790942bb9fd4f9078098728
B_EZ <>
2021-11-15 20:41:21 -0800

    Add comment about how we can disable tsc checks on next build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  typescript: {
+    // If we ever move to github actions, we can turn this on
+    // to parallelize CI. Vercel can do a build while we are
+    // typechecking to save time.
+    ignoreBuildErrors: false,
+  },
+
   target: 'serverless',
   async rewrites() {
     return [
