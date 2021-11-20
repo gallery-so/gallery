661c14264f7a2e927f048882aa7d2b3f5a34a078
Robin <rokim8@gmail.com>
2021-11-19 19:05:07 -0500

    Initialize Mixpanel to hit proxy (#463)
    * example
    
    * analytics api url an env var
    
    Co-authored-by: kaito <80802871+kaitoo1@users.noreply.github.com>

--- src/utils/mixpanel.ts
+++ src/utils/mixpanel.ts
@@ -1,12 +1,14 @@
 import mixpanel from 'mixpanel-browser';
 
-if (process.env.REACT_APP_MIXPANEL_TOKEN) {
-  mixpanel.init(process.env.REACT_APP_MIXPANEL_TOKEN);
+if (process.env.REACT_APP_MIXPANEL_TOKEN && process.env.REACT_APP_ANALYTICS_API_URL) {
+  mixpanel.init(process.env.REACT_APP_MIXPANEL_TOKEN, {
+    api_host: process.env.REACT_APP_ANALYTICS_API_URL,
+  });
 }
 
 type EventProps = Record<string, unknown>;
 
-const mixpanelEnabled = process.env.NODE_ENV === 'production';
+const mixpanelEnabled = process.env.REACT_APP_MIXPANEL_TOKEN && process.env.REACT_APP_ANALYTICS_API_URL;
 
 const Mixpanel = {
   track: (eventname: string, props: EventProps = {}) => {
