59e2b8d16d6a13ae688e2af5af64ab022e334f99
Terence Bezman <terencebezman@gmail.com>
2021-11-20 19:23:42 -0800

    Narrow types instead of tyepcasintg

--- src/flows/OnboardingFlow/OnboardingFlow.test.tsx
+++ src/flows/OnboardingFlow/OnboardingFlow.test.tsx
@@ -3,8 +3,11 @@ import OnboardingFlow from './OnboardingFlow';
 
 test.skip('Next button is disabled by default', () => {
   render(<OnboardingFlow />);
-  const button = screen.getByTestId(
-    'wizard-footer-next-button'
-  ) as HTMLButtonElement;
+  const button = screen.getByTestId('wizard-footer-next-button');
+
+  if (!(button instanceof HTMLButtonElement)) {
+    throw Error('Element was not a button');
+  }
+
   expect(button.disabled).toBe(true);
 });
