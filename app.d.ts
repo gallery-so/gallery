28e1d5f0fa950e62121cf5d7cd7d8f7a50425b42
Terence Bezman <terencebezman@gmail.com>
2021-11-20 19:23:42 -0800

    Add app type overrides for window module

---
+++ app.d.ts
@@ -0,0 +1,4 @@
+interface Window {
+  ethereum?: Record<string, unknown>;
+  web3?: Record<string, unknown>;
+}
--- next-env.d.ts
+++ next-env.d.ts
@@ -2,10 +2,5 @@
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
 
-interface Window {
-  ethereum?: Record<string, unknown>;
-  web3?: Record<string, unknown>;
-}
-
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
