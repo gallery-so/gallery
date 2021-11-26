f796d96672e81d9c38d9d76ab8d2dba626b782ea
Robinnnnn <rokim8@gmail.com>
2021-11-26 00:50:10 -0500

    remove comment

--- src/scenes/NftDetailPage/NftDetailAsset.tsx
+++ src/scenes/NftDetailPage/NftDetailAsset.tsx
@@ -106,8 +106,6 @@ const StyledAssetContainer = styled.div<AssetContainerProps>`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  // width: 100%;
-  // height: 100%;
 
   ${({ shouldEnforceSquareAspectRatio }) =>
     shouldEnforceSquareAspectRatio ? 'aspect-ratio: 1' : ''};
