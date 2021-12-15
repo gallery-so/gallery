fcd71c67ad09b5a6cd5c8c2cb42f0b777f8c60bf
Robinnnnn <rokim8@gmail.com>
2021-12-14 22:24:13 -0500

    update swr config

--- src/contexts/swr/SwrContext.tsx
+++ src/contexts/swr/SwrContext.tsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import { memo, useCallback, useMemo } from 'react';
 import { SWRConfig } from 'swr';
-import { MINUTE } from 'utils/time';
+import { MINUTE, SECOND } from 'utils/time';
 import useFetcher from './useFetcher';
 import { syncWithLocalStorage } from './swr-sync-storage';
+import Mixpanel from 'utils/mixpanel';
 
-export const SwrProvider = React.memo(({ children }) => {
+export const SwrProvider = memo(({ children }) => {
   const fetcher = useFetcher();
 
-  const value = {
-    fetcher,
-    refreshInterval: 5 * MINUTE,
-    suspense: true,
-  };
+  const handleLoadingSlow = useCallback((key) => {
+    Mixpanel.track('Slow request', {
+      endpoint: key,
+    });
+  }, []);
+
+  const value = useMemo(
+    () => ({
+      fetcher,
+      suspense: true,
+      // revalidate data every 5 mins. only impacts hooks/data that's on screen
+      refreshInterval: 5 * MINUTE,
+      // prevent auto-revalidation on window focus
+      refreshOnFocus: false,
+      // automatically revalidate if browser loses network connection and recovers
+      revalidateOnReconnect: true,
+      // dedupe requests with the same key within 2 seconds
+      dedupingInterval: 2 * SECOND,
+      // detect slow requests
+      onLoadingSlow: handleLoadingSlow,
+    }),
+    [fetcher, handleLoadingSlow]
+  );
 
   syncWithLocalStorage();
 
