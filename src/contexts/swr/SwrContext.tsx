285874a2ddd975d331905b5cf10aaec0a842dc43
Robinnnnn <rokim8@gmail.com>
2021-12-15 02:49:37 -0500

    bump swr and update localStorage sync

--- src/contexts/swr/SwrContext.tsx
+++ src/contexts/swr/SwrContext.tsx
@@ -2,8 +2,21 @@ import { memo, useCallback, useMemo } from 'react';
 import { SWRConfig } from 'swr';
 import { MINUTE, SECOND } from 'utils/time';
 import useFetcher from './useFetcher';
-import { syncWithLocalStorage } from './swr-sync-storage';
 import Mixpanel from 'utils/mixpanel';
+
+function localStorageProvider() {
+  // When initializing, we restore the data from `localStorage` into a map.
+  const map = new Map(JSON.parse(localStorage.getItem('app-cache') ?? '[]'));
+
+  // Before unloading the app, we write back all the data into `localStorage`.
+  window.addEventListener('beforeunload', () => {
+    const appCache = JSON.stringify(Array.from(map.entries()));
+    localStorage.setItem('app-cache', appCache);
+  });
+
+  // We still use the map for write & read for performance.
+  return map;
+}
 
 export const SwrProvider = memo(({ children }) => {
   const fetcher = useFetcher();
@@ -17,11 +30,13 @@ export const SwrProvider = memo(({ children }) => {
   const value = useMemo(
     () => ({
       fetcher,
+      provider: localStorageProvider,
       suspense: true,
       // revalidate data every 5 mins. only impacts hooks/data that's on screen
       refreshInterval: 5 * MINUTE,
       // prevent auto-revalidation on window focus
       revalidateOnFocus: false,
+      // revalidateOnMount: false,
       // automatically revalidate if browser loses network connection and recovers
       revalidateOnReconnect: true,
       // dedupe requests with the same key within 2 seconds
@@ -31,8 +46,6 @@ export const SwrProvider = memo(({ children }) => {
     }),
     [fetcher, handleLoadingSlow]
   );
-
-  syncWithLocalStorage();
 
   return <SWRConfig value={value}>{children}</SWRConfig>;
 });
