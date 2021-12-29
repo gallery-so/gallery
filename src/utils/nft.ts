7cf02d0c72d552e45f83e61b8d3098e630f68923
kaito <80802871+kaitoo1@users.noreply.github.com>
2021-12-29 18:36:16 +0900

    support mathcastles (#494)

--- src/utils/nft.ts
+++ src/utils/nft.ts
@@ -22,6 +22,7 @@ const animationDomains = [
   'https://api.artblocks',
   'https://generator.artblocks',
   'https://api.heaven.computer',
+  'https://tokens.mathcastles.xyz',
 ];
 
 export function getFileExtension(url: string) {
