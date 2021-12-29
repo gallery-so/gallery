07b7a5bcc7254ce0a68f562fbae6d38c55183737
kaito <80802871+kaitoo1@users.noreply.github.com>
2021-12-29 10:48:08 +0900

    only show on desktop

---
+++ src/scenes/MemberListPage/MemberListImagePreview.tsx
@@ -0,0 +1,111 @@
+import styled, { keyframes } from 'styled-components';
+import { Directions } from 'src/components/core/enums';
+import { useMemo } from 'react';
+
+const verticalOffset = [0, 130, 260];
+const horizontalOffset = [0, 90, 180];
+
+function shuffle(array: number[]) {
+  return array.sort(() => 0.5 - Math.random());
+}
+
+// Randomly generates positions for each nftUrl, ensuring an image will always occupy top/midle/bottom and left/middle/right.
+function getImagePositions(nftUrls: string[]) {
+  // shuffle the offset arrays so images can end up in any combination of top/middle/bottom and left/middle/right.
+  const shuffledVerticalOffset = shuffle(verticalOffset);
+  const shuffledHorizontalOffset = shuffle(horizontalOffset);
+
+  // offset each image by one of the three possible offset amounts and additionally randomly offset by up to 40px.
+  return nftUrls.map((url, index) => ({
+    top: shuffledVerticalOffset[index] + Math.random() * 40,
+    left: shuffledHorizontalOffset[index] + Math.random() * 40,
+  }));
+}
+
+// Randomly generates parameters for keyframe animation for each image, so that each image floats a bit differently.
+// x and y values have a 50-50 chance of being positive or negative, so that the images float in a random direction.
+function getAnimationMovement() {
+  return {
+    x: Math.random() * 10 * (Math.random() > 0.5 ? 1 : -1),
+    y: Math.random() * 10 * (Math.random() > 0.5 ? 1 : -1),
+    time: Math.random() * 2 + 6, // animation duration is 6-8 seconds
+  };
+}
+
+type Props = {
+  direction: Directions.LEFT | Directions.RIGHT;
+  nftUrls: string[];
+};
+
+function MemberListImagePreview({ direction, nftUrls }: Props) {
+  const animationMovements = useMemo(() => nftUrls.map(() => getAnimationMovement()), [nftUrls]);
+  const imagePositions = useMemo(() => getImagePositions(nftUrls), [nftUrls]);
+
+  return (
+    <StyledPreview>
+      <StyledPreviewImageWrapper direction={direction}>
+        {nftUrls.map((url, index) => (
+          <StyledPreviewImage
+            key={url}
+            src={`${url}=w250`}
+            top={imagePositions[index].top}
+            left={imagePositions[index].left}
+            movement={animationMovements[index]}
+          />
+        ))}
+      </StyledPreviewImageWrapper>
+    </StyledPreview>
+  );
+}
+
+const StyledPreview = styled.div`
+  position: relative;
+`;
+
+type StyledPreviewImageWrapperProps = {
+  direction: Directions.LEFT | Directions.RIGHT;
+};
+
+const StyledPreviewImageWrapper = styled.div<StyledPreviewImageWrapperProps>`
+  position: absolute;
+  top: -250px;
+  left: ${({ direction }) => (direction === Directions.RIGHT ? '120px' : '-500px')};
+`;
+
+const float = (x: number, y: number) => keyframes`
+  0% {
+    transform: translate(0px, 0px);
+  }
+
+  50% {
+    transform: translate(${x}px, ${y}px);
+  }
+
+  100% {
+    transform: translate(0px, 0px);
+  }
+`;
+
+type Movement = {
+  x: number;
+  y: number;
+  time: number;
+};
+
+type StyledPreviewImageProps = {
+  top: number;
+  left: number;
+  movement: Movement;
+};
+
+const StyledPreviewImage = styled.img<StyledPreviewImageProps>`
+  position: absolute;
+  top: ${({ top }) => top}px;
+  left: ${({ left }) => left}px;
+  background-color: white;
+  box-shadow: rgba(0, 0, 0, 0.2) 0px 18px 50px -10px;
+  animation: ${({ movement }) => float(movement.x, movement.y)} ${({ movement }) => movement.time}s
+    ease-in-out infinite;
+`;
+
+export default MemberListImagePreview;
--- src/scenes/MemberListPage/MemberListOwner.tsx
+++ src/scenes/MemberListPage/MemberListOwner.tsx
@@ -6,6 +6,8 @@ import GalleryLink from 'components/core/GalleryLink/GalleryLink';
 import breakpoints from 'components/core/breakpoints';
 import useDebounce from 'hooks/useDebounce';
 import { Directions } from 'src/components/core/enums';
+import MemberListImagePreview from './MemberListImagePreview';
+import detectMobileDevice from 'utils/detectMobileDevice';
 
 type Props = {
   owner: MembershipOwner;
@@ -30,6 +32,8 @@ function MemberListOwner({ owner, direction }: Props) {
     setShowPreview(false);
   }, []);
 
+  const isMobile = useMemo(() => detectMobileDevice(), []);
+
   return (
     <StyledOwner>
       <div onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
@@ -37,14 +41,8 @@ function MemberListOwner({ owner, direction }: Props) {
           <StyledUsername>{owner.username}</StyledUsername>
         </GalleryLink>
       </div>
-      {showPreview && (
-        <StyledPreview>
-          <StyledPreviewImageWrapper direction={direction}>
-            <StyledPreviewImage src={`${owner.preview_nfts[0]}=s250`} top={100} left={40} />
-            <StyledPreviewImage src={`${owner.preview_nfts[1]}=s250`} top={0} left={0} />
-            <StyledPreviewImage src={`${owner.preview_nfts[2]}=s250`} top={200} left={140} />
-          </StyledPreviewImageWrapper>
-        </StyledPreview>
+      {!isMobile && showPreview && (
+        <MemberListImagePreview direction={direction} nftUrls={owner.preview_nfts} />
       )}
     </StyledOwner>
   );
@@ -67,31 +65,6 @@ const StyledUsername = styled(Heading)`
   overflow: hidden;
   text-overflow: ellipsis;
   margin-right: 8px;
-`;
-
-const StyledPreview = styled.div`
-  position: relative;
-`;
-
-type StyledPreviewImageWrapperProps = {
-  direction: Directions.LEFT | Directions.RIGHT;
-};
-
-const StyledPreviewImageWrapper = styled.div<StyledPreviewImageWrapperProps>`
-  position: absolute;
-  top: -150px;
-  left: ${({ direction }) => (direction === Directions.RIGHT ? '200px' : '-400px')};
-`;
-
-type StyledPreviewImageProps = {
-  top: number;
-  left: number;
-};
-
-const StyledPreviewImage = styled.img<StyledPreviewImageProps>`
-  position: absolute;
-  top: ${({ top }) => top}px;
-  left: ${({ left }) => left}px;
 `;
 
 export default MemberListOwner;
