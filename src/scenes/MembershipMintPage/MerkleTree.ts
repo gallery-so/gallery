b288c6ae0411d7343191cf535abb2b453e27baff
kaito <80802871+kaitoo1@users.noreply.github.com>
2022-01-04 16:15:50 +0900

    add types to MerkleTree

--- src/scenes/MembershipMintPage/MerkleTree.ts
+++ src/scenes/MembershipMintPage/MerkleTree.ts
@@ -2,15 +2,18 @@
 // SPDX-License-Identifier: MIT
 // from https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.0.1/test/helpers/merkleTree.js
 
-import { bufferToHex } from 'ethereumjs-util';
+import { bufferToHex, keccak256 } from 'ethereumjs-util';
 import web3 from 'web3';
 
 export default class MerkleTree {
-  constructor(elements) {
+  elements: Buffer[];
+  layers: Buffer[][];
+
+  constructor(elements: string[]) {
     // Filter empty strings and hash elements
     this.elements = elements
       .filter((el) => el)
-      .map((el) => Buffer.from(web3.utils.hexToBytes(web3.utils.sha3(el, { encoding: 'hex' }))));
+      .map((el) => Buffer.from(web3.utils.hexToBytes(web3.utils.sha3(el)!)));
     // Sort elements
     this.elements.sort(Buffer.compare);
 
@@ -21,9 +24,9 @@ export default class MerkleTree {
     this.layers = this.getLayers(this.elements);
   }
 
-  getLayers(elements) {
+  getLayers(elements: Buffer[]) {
     if (elements.length === 0) {
-      return [['']];
+      throw new Error('Empty tree');
     }
 
     const layers = [];
@@ -37,8 +40,8 @@ export default class MerkleTree {
     return layers;
   }
 
-  getNextLayer(elements) {
-    return elements.reduce((layer, el, idx, arr) => {
+  getNextLayer(elements: Buffer[]) {
+    return elements.reduce((layer: Buffer[], el, idx, arr) => {
       if (idx % 2 === 0) {
         // Hash the current element with its pair element
         layer.push(this.combinedHash(el, arr[idx + 1]));
@@ -48,7 +51,7 @@ export default class MerkleTree {
     }, []);
   }
 
-  combinedHash(first, second) {
+  combinedHash(first: Buffer, second: Buffer) {
     if (!first) {
       return second;
     }
@@ -57,9 +60,7 @@ export default class MerkleTree {
       return first;
     }
 
-    return Buffer.from(
-      web3.utils.hexToBytes(web3.utils.sha3(this.sortAndConcat(first, second), { encoding: 'hex' }))
-    );
+    return Buffer.from(keccak256(this.sortAndConcat(first, second)));
   }
 
   getRoot() {
@@ -70,14 +71,12 @@ export default class MerkleTree {
     return bufferToHex(this.getRoot());
   }
 
-  getProof(el) {
+  getProof(el: string) {
     let idx = this.bufIndexOf(el, this.elements);
 
     if (idx === -1) {
       throw new Error('Element does not exist in Merkle tree');
     }
-
-    console.log({ idx });
 
     return this.layers.reduce((proof, layer) => {
       const pairElement = this.getPairElement(idx, layer);
@@ -92,14 +91,13 @@ export default class MerkleTree {
     }, []);
   }
 
-  getHexProof(el) {
-    console.log({ el });
+  getHexProof(el: string) {
     const proof = this.getProof(el);
 
     return this.bufArrToHexArr(proof);
   }
 
-  getPairElement(idx, layer) {
+  getPairElement(idx: number, layer: Buffer[]) {
     const pairIdx = idx % 2 === 0 ? idx + 1 : idx - 1;
 
     if (pairIdx < layer.length) {
@@ -109,12 +107,12 @@ export default class MerkleTree {
     return null;
   }
 
-  bufIndexOf(el, arr) {
+  bufIndexOf(el: string, arr: Buffer[]) {
     let hash;
 
     // Convert element to 32 byte hash if it is not one already
     if (el.length !== 32 || !Buffer.isBuffer(el)) {
-      hash = Buffer.from(web3.utils.hexToBytes(web3.utils.sha3(el, { encoding: 'hex' })));
+      hash = Buffer.from(web3.utils.hexToBytes(web3.utils.sha3(el)!));
     } else {
       hash = el;
     }
@@ -128,21 +126,19 @@ export default class MerkleTree {
     return -1;
   }
 
-  bufDedup(elements) {
+  bufDedup(elements: Buffer[]) {
     return elements.filter((el, idx) => idx === 0 || !elements[idx - 1].equals(el));
   }
 
-  bufArrToHexArr(arr) {
+  bufArrToHexArr(arr: Buffer[]) {
     if (arr.some((el) => !Buffer.isBuffer(el))) {
       throw new Error('Array is not an array of buffers');
     }
 
-    console.log('arr', arr);
-
     return arr.map((el) => '0x' + el.toString('hex'));
   }
 
-  sortAndConcat(...args) {
+  sortAndConcat(...args: Buffer[]) {
     return Buffer.concat([...args].sort(Buffer.compare));
   }
 }
