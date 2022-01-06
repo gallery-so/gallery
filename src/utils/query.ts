aa8cb39fa1a9bbbf57c54012f60a7a27b7c757cb
Terence Bezman <terencebezman@gmail.com>
2022-01-05 17:17:24 -0800

    Remove unused hook useQuery

--- src/utils/query.ts
+++
@@ -1,6 +0,0 @@
-import { useLocation } from '@reach/router';
-
-export default function useQuery(query: string) {
-  const search = new URLSearchParams(useLocation().search);
-  return search.get(query);
-}
