4060fb62341b439d7c48027b20de9658c07eaf62
Terence Bezman <terencebezman@gmail.com>
2022-01-05 17:17:31 -0800

    Remove next config rewrites

--- next.config.js
+++ next.config.js
@@ -21,14 +21,4 @@ module.exports = {
     // Enables the styled-components SWC transform
     styledComponents: true,
   },
-
-  async rewrites() {
-    return [
-      // Rewrite everything to `pages/index`
-      {
-        source: '/:any*',
-        destination: '/',
-      },
-    ];
-  },
 };
