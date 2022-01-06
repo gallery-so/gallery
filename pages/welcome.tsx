75f768bd5e2e0187bac81bb2fed0642041288d08
Terence Bezman <terencebezman@gmail.com>
2022-01-05 20:21:15 -0800

    Fix welcome page

--- pages/welcome.tsx
+++ pages/welcome.tsx
@@ -2,5 +2,5 @@ import GalleryAuthenticatedRoute from 'scenes/_Router/GalleryAuthenticatedRoute'
 import OnboardingFlow from 'flows/OnboardingFlow/OnboardingFlow';
 
 export default function Welcome() {
-  return <GalleryAuthenticatedRoute path="/welcome" component={OnboardingFlow} freshLayout />;
+  return <GalleryAuthenticatedRoute element={<OnboardingFlow />} freshLayout />;
 }
