e1bc9b8c1ebc863082559a62717de2c2d327b254
kaito <80802871+kaitoo1@users.noreply.github.com>
2022-01-06 15:34:52 +0900

    make sure we render gifs as image not animation (#501)
    * make sure we render gifs as image not animation
    
    * same change for getting image url
    
    * null check url

--- src/utils/nft.ts
+++ src/utils/nft.ts
@@ -7,8 +7,13 @@ export function getResizedNftImageUrlWithFallback(nft: Nft, size = 288): string 
   const {
     image_url,
     image_original_url,
+    animation_original_url,
     asset_contract: { contract_image_url },
   } = nft;
+
+  if (animation_original_url?.endsWith('.gif')) {
+    return animation_original_url;
+  }
 
   // Resizes if google image: https://developers.google.com/people/image-sizing
   if (image_url?.includes('googleusercontent')) {
@@ -41,6 +46,8 @@ export function getMediaTypeForAssetUrl(assetUrl: string) {
       return NftMediaType.AUDIO;
     case 'glb':
       return NftMediaType.MODEL;
+    case 'gif':
+      return NftMediaType.IMAGE;
     default:
       return NftMediaType.ANIMATION;
   }
