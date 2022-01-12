732bf9e67c8fbef23e88895cbc0913784cdb06d1
Connor Rothschild <connorrothschild@gmail.com>
2022-01-12 16:54:19 -0600

    testing

--- src/scenes/_Router/GalleryAuthenticatedRoute.tsx
+++ src/scenes/_Router/GalleryAuthenticatedRoute.tsx
@@ -5,10 +5,10 @@ import Route, { GalleryRouteProps } from './GalleryRoute';
 // TODO: may want to remember where the user was going and redirect them
 // to their desired route upon authentication
 export default function GalleryAuthenticatedRoute(props: GalleryRouteProps) {
-  const isAuthenticated = useIsAuthenticated();
-  if (!isAuthenticated) {
-    return <GalleryRedirect to="/" />;
-  }
+  // const isAuthenticated = useIsAuthenticated();
+  // if (!isAuthenticated) {
+  // return <GalleryRedirect to="/" />;
+  // }
 
   return <Route {...props} />;
 }
