9b9953e9618c3ea742cd2aed2718b7598e5b5213
Robinnnnn <rokim8@gmail.com>
2022-01-20 18:17:50 -0500

    comment out gallery explorer for now

--- src/scenes/Home/Home.tsx
+++ src/scenes/Home/Home.tsx
@@ -4,9 +4,9 @@ import Button from 'components/core/Button/Button';
 import Page from 'components/core/Page/Page';
 import GalleryIntro from 'components/GalleryTitleIntro/GalleryTitleIntro';
 import { useRouter } from 'next/router';
-import GalleryLink from 'components/core/GalleryLink/GalleryLink';
-import TextButton from 'components/core/Button/TextButton';
-import Spacer from 'components/core/Spacer/Spacer';
+// import GalleryLink from 'components/core/GalleryLink/GalleryLink';
+// import TextButton from 'components/core/Button/TextButton';
+// import Spacer from 'components/core/Spacer/Spacer';
 
 function Home() {
   const { push } = useRouter();
@@ -21,10 +21,10 @@ function Home() {
     <Page centered>
       <GalleryIntro />
       <StyledButton text="Sign In" onClick={handleEnterGallery} />
-      <Spacer height={16} />
+      {/* <Spacer height={16} />
       <GalleryLink to="/members">
         <TextButton text="explore galleries" />
-      </GalleryLink>
+      </GalleryLink> */}
     </Page>
   );
 }
