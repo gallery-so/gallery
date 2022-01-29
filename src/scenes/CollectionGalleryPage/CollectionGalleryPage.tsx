421433157eb1f0091d28376cc11a780b17bdedde
Jakz <jakzaizzat@gmail.com>
2022-01-29 20:39:41 +0800

    Remove useSuggestion from Collection page

--- src/scenes/CollectionGalleryPage/CollectionGalleryPage.tsx
+++ src/scenes/CollectionGalleryPage/CollectionGalleryPage.tsx
@@ -1,53 +1,8 @@
-import breakpoints, { pageGutter, size } from 'components/core/breakpoints';
+import breakpoints, { pageGutter } from 'components/core/breakpoints';
 import Page from 'components/core/Page/Page';
 import styled from 'styled-components';
-import detectMobileDevice from 'utils/detectMobileDevice';
-import { useToastActions } from 'contexts/toast/ToastContext';
-import { useCallback, useEffect, useMemo } from 'react';
-import usePersistedState from 'hooks/usePersistedState';
-import { useBreakpoint } from 'hooks/useWindowSize';
-import usePrevious from 'hooks/usePrevious';
 import Head from 'next/head';
 import CollectionGallery from './CollectionGallery';
-
-// suggest mobile users to use landscape mode for 3-column experience.
-// use localStorage to prevent showing the toast again.
-function useSuggestion() {
-  const screenWidth = useBreakpoint();
-  const { pushToast, dismissToast: removeToastFromView } = useToastActions();
-  const [dismissed, setDismissed] = usePersistedState(
-    'dismissed_mobile_landscape_suggestion',
-    false
-  );
-
-  const markToastAsDismissedInLocalStorage = useCallback(() => {
-    setDismissed(true);
-  }, [setDismissed]);
-
-  const previousWidth = usePrevious<size>(screenWidth);
-
-  const rotationDetected = useMemo(
-    () => previousWidth === size.mobile && screenWidth !== size.mobile,
-    [previousWidth, screenWidth]
-  );
-
-  useEffect(() => {
-    if (!dismissed && detectMobileDevice() && screenWidth === size.mobile) {
-      pushToast('Rotate your phone to view in landscape mode', markToastAsDismissedInLocalStorage);
-    }
-
-    if (rotationDetected) {
-      removeToastFromView();
-    }
-  }, [
-    pushToast,
-    dismissed,
-    markToastAsDismissedInLocalStorage,
-    screenWidth,
-    rotationDetected,
-    removeToastFromView,
-  ]);
-}
 
 type CollectionGalleryPageProps = {
   username: string;
@@ -55,8 +10,6 @@ type CollectionGalleryPageProps = {
 };
 
 function CollectionGalleryPage({ collectionId, username }: CollectionGalleryPageProps) {
-  useSuggestion();
-
   const headTitle = `${username} | Gallery`;
 
   return (
