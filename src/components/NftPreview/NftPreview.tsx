ee66352d418874ac1cc69de47331faeda5a40ed1
kaito <80802871+kaitoo1@users.noreply.github.com>
2022-02-02 08:29:24 +0900

    refactor gallery to use display:grid instead of flex (#558)

--- src/components/NftPreview/NftPreview.tsx
+++ src/components/NftPreview/NftPreview.tsx
@@ -1,6 +1,5 @@
-import { LAYOUT_GAP_BREAKPOINTS } from 'constants/layout';
 import styled from 'styled-components';
-import breakpoints, { size } from 'components/core/breakpoints';
+import { size } from 'components/core/breakpoints';
 import Gradient from 'components/core/Gradient/Gradient';
 import transitions from 'components/core/transitions';
 import { useCallback, useMemo } from 'react';
@@ -10,13 +9,11 @@ import { useNavigateToUrl } from 'utils/navigate';
 import { useBreakpoint } from 'hooks/useWindowSize';
 import NftPreviewLabel from './NftPreviewLabel';
 import NftPreviewAsset from './NftPreviewAsset';
-import { DisplayLayout } from 'components/core/enums';
 
 type Props = {
   nft: Nft;
   collectionId: string;
   columns: number;
-  mobileLayout: DisplayLayout;
 };
 
 const SINGLE_COLUMN_NFT_WIDTH = 600;
@@ -31,7 +28,7 @@ const LAYOUT_DIMENSIONS: Record<number, number> = {
   6: 134,
 };
 
-function NftPreview({ nft, collectionId, columns, mobileLayout }: Props) {
+function NftPreview({ nft, collectionId, columns }: Props) {
   const navigateToUrl = useNavigateToUrl();
 
   const username = window.location.pathname.split('/')[1];
@@ -44,7 +41,7 @@ function NftPreview({ nft, collectionId, columns, mobileLayout }: Props) {
       if (storage) storage.setItem('prevPage', window.location.pathname);
       navigateToUrl(`/${username}/${collectionId}/${nft.id}`, event);
     },
-    [collectionId, navigateToUrl, nft.id, username]
+    [collectionId, navigateToUrl, nft.id, storage, username]
   );
   const screenWidth = useBreakpoint();
 
@@ -54,16 +51,8 @@ function NftPreview({ nft, collectionId, columns, mobileLayout }: Props) {
     [columns, screenWidth]
   );
 
-  const NftPreviewComponent = useMemo(() => {
-    if (screenWidth === size.mobile && mobileLayout === DisplayLayout.LIST) {
-      return StyledNftPreviewList;
-    }
-
-    return StyledNftPreviewGrid;
-  }, [mobileLayout, screenWidth]);
-
   return (
-    <NftPreviewComponent key={nft.id} columns={columns}>
+    <StyledNftPreview key={nft.id} columns={columns}>
       <StyledLinkWrapper onClick={handleNftClick}>
         <ShimmerProvider>
           {/* // we'll request images at double the size of the element so that it looks sharp on retina */}
@@ -74,7 +63,7 @@ function NftPreview({ nft, collectionId, columns, mobileLayout }: Props) {
           </StyledNftFooter>
         </ShimmerProvider>
       </StyledLinkWrapper>
-    </NftPreviewComponent>
+    </StyledNftPreview>
   );
 }
 
@@ -119,39 +108,6 @@ const StyledNftPreview = styled.div<{ columns: number }>`
   &:hover ${StyledNftFooter} {
     opacity: 1;
   }
-
-  margin-bottom: 40px;
-`;
-
-const StyledNftPreviewGrid = styled(StyledNftPreview)`
-  // width looks complex but it allows us to conditionally apply different width rules based on the # of columns in the collection:
-  // - if single columm, use hardcoded width because the NFT isnt as wide as the whole page (unless on mobile, in which case we use the mobile width)
-  // - if more columns, use calc to automatically set width based on column #
-  // this is important because while we *could* use hardcoded widths for desktop, we need to use dynamic widths for tablet
-
-  width: ${({ columns }) =>
-    `calc((100% - ${LAYOUT_GAP_BREAKPOINTS.mobileSmall * columns}px) / ${columns});`}
-  margin: ${LAYOUT_GAP_BREAKPOINTS.mobileSmall / 2}px;
-
-  @media only screen and ${breakpoints.mobileLarge} {
-    width: ${({ columns }) =>
-      columns === 1
-        ? `${SINGLE_COLUMN_NFT_WIDTH}px;`
-        : `calc((100% - ${LAYOUT_GAP_BREAKPOINTS.mobileLarge * columns}px) / ${columns});`}
-    margin: ${LAYOUT_GAP_BREAKPOINTS.mobileLarge / 2}px;
-  }
-
-  @media only screen and ${breakpoints.desktop} {
-    width: ${({ columns }) =>
-      columns === 1
-        ? `${SINGLE_COLUMN_NFT_WIDTH}px;`
-        : `calc((100% - ${LAYOUT_GAP_BREAKPOINTS.desktop * columns}px) / ${columns});`}
-    margin: ${LAYOUT_GAP_BREAKPOINTS.desktop / 2}px;
-  }
-`;
-
-const StyledNftPreviewList = styled(StyledNftPreview)`
-  width: 100%;
 `;
 
 export default NftPreview;
--- src/constants/layout.ts
+++ src/constants/layout.ts
@@ -1,9 +1,3 @@
 export const DEFAULT_COLUMNS = 3;
 export const MIN_COLUMNS = 1;
 export const MAX_COLUMNS = 6;
-
-export const LAYOUT_GAP_BREAKPOINTS: Record<string, number> = {
-  mobileSmall: 10,
-  mobileLarge: 20,
-  desktop: 40,
-};
