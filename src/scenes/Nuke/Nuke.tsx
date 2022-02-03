5cdf1283f645891a4c928be19bccc85e17b1de20
Robin <rokim8@gmail.com>
2022-02-02 22:01:14 -0500

    Aggressive nuke (#566)

--- src/scenes/Nuke/Nuke.tsx
+++ src/scenes/Nuke/Nuke.tsx
@@ -6,12 +6,18 @@ import Spacer from 'components/core/Spacer/Spacer';
 import Page from 'components/core/Page/Page';
 import GalleryLink from 'components/core/GalleryLink/GalleryLink';
 import { useAuthActions } from 'contexts/auth/AuthContext';
+import { useSWRConfig } from 'swr';
 
 // Suggest a user visit this page if they're in a seriously broken state
 function Nuke() {
   const { logOut } = useAuthActions();
+  const { cache } = useSWRConfig();
 
-  useEffect(logOut, [logOut]);
+  useEffect(() => {
+    // @ts-expect-error `clear` method not described in SWR types, but it's available
+    cache.clear();
+    logOut();
+  }, [logOut, cache]);
 
   return (
     <Page centered>
