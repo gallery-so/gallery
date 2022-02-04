f3478e0621a9c3206db90420054192cb41ad5ca8
Kevin Ingersoll <kingersoll@gmail.com>
2022-02-04 10:30:21 -0800

    demonstrate meta tags via server props

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -2,8 +2,9 @@ import UserGalleryPage from 'scenes/UserGalleryPage/UserGalleryPage';
 import GalleryRoute from 'scenes/_Router/GalleryRoute';
 import { GetServerSideProps } from 'next';
 import GalleryRedirect from 'scenes/_Router/GalleryRedirect';
+import { MetaTagProps } from 'pages/_app';
 
-type UserGalleryProps = {
+type UserGalleryProps = MetaTagProps & {
   username?: string;
 };
 
@@ -15,8 +16,21 @@ export default function UserGallery({ username }: UserGalleryProps) {
   return <GalleryRoute element={<UserGalleryPage username={username} />} />;
 }
 
-export const getServerSideProps: GetServerSideProps<UserGalleryProps> = async ({ params }) => ({
-  props: {
-    username: params?.username ? (params.username as string) : undefined,
-  },
-});
+export const getServerSideProps: GetServerSideProps<UserGalleryProps> = async ({ params }) => {
+  const username = params?.username ? (params.username as string) : undefined;
+  return {
+    props: {
+      username,
+      metaTags: username
+        ? [
+            {
+              property: 'og:image',
+              content: `/api/opengraph/image?${new URLSearchParams({
+                path: `/opengraph/user/${username}`,
+              }).toString()}`,
+            },
+          ]
+        : null,
+    },
+  };
+};
