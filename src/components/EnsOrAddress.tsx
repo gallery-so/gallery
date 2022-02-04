d7ba8bc14803bf921f19e0609620e73b796c9742
Robinnnnn <rokim8@gmail.com>
2022-02-04 03:32:24 -0500

    handle nonexistent address prop

--- src/components/EnsOrAddress.tsx
+++ src/components/EnsOrAddress.tsx
@@ -4,12 +4,14 @@ const fetcher = async (...args: Parameters<typeof fetch>) =>
   fetch(...args).then(async (res) => res.json());
 
 type Props = {
-  address: string;
+  address?: string;
 };
 
 export const EnsOrAddress = ({ address }: Props) => {
   const { data } = useSWR(
-    `https://api.ensideas.com/ens/resolve/${encodeURIComponent(address.toLowerCase())}`,
+    address
+      ? `https://api.ensideas.com/ens/resolve/${encodeURIComponent(address.toLowerCase())}`
+      : null,
     fetcher
   );
 
