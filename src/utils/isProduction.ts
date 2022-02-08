df6763520534ed00628d6623952b45c14c2cff03
Kevin Ingersoll <kingersoll@gmail.com>
2022-02-08 13:52:56 -0800

    fix isProduction

--- src/utils/isProduction.ts
+++ src/utils/isProduction.ts
@@ -1,7 +1,3 @@
 export default function isProduction() {
-  console.log('NEXT_PUBLIC_VERCEL_ENV', process.env.NEXT_PUBLIC_VERCEL_ENV);
-  console.log('NODE_ENV', process.env.NODE_ENV);
-  return (
-    process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' || process.env.NODE_ENV === 'production'
-  );
+  return process.env.NEXT_PUBLIC_VERCEL_ENV === 'production';
 }
