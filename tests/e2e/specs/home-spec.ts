e63f135f863948b7a8c9eb1d732d71770c810e48
jakzaizzat <jakzaizzat@gmail.com>
2022-02-11 21:41:15 +0800

    Added synpress

---
+++ tests/e2e/specs/home-spec.ts
@@ -0,0 +1,22 @@
+describe('Homepage test', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('should render the homepage', () => {
+    cy.contains('Gallery', { matchCase: false }).should('be.exist');
+  });
+
+  it('should render a sign in button', () => {
+    cy.get('[data-testid="sign-in-button"]').should('be.exist');
+  });
+
+  it('should render an explorer button', () => {
+    cy.get('[data-testid="explore-button"]').should('be.exist');
+  });
+
+  it('should redirect to members page when click the explore button', () => {
+    cy.get('[data-testid="explore-button"]').click();
+    cy.url().should('include', '/members');
+  });
+});
