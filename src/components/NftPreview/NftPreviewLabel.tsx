5d8400f2feee9b117c769493509dbb186a59dd4f
Connor Rothschild <connorrothschild@gmail.com>
2022-02-17 00:04:52 -0600

    add forced ellipsis to collection/nft title on mobile

--- src/components/NftPreview/NftPreviewLabel.tsx
+++ src/components/NftPreview/NftPreviewLabel.tsx
@@ -2,6 +2,7 @@ import colors from 'components/core/colors';
 import styled from 'styled-components';
 import { BodyRegular } from 'components/core/Text/Text';
 import { Nft } from 'types/Nft';
+import breakpoints from 'components/core/breakpoints';
 
 type Props = {
   nft: Nft;
@@ -11,8 +12,12 @@ type Props = {
 function NftPreviewLabel({ nft, className }: Props) {
   return (
     <StyledNftPreviewLabel className={className}>
-      <StyledBodyRegular color={colors.white}>{nft.name}</StyledBodyRegular>
-      <StyledBodyRegular color={colors.white}>{nft.token_collection_name}</StyledBodyRegular>
+      <StyledBodyRegular color={colors.white} lines={1}>
+        {nft.name}
+      </StyledBodyRegular>
+      <StyledBodyRegular color={colors.white} lines={2}>
+        {nft.token_collection_name}
+      </StyledBodyRegular>
     </StyledNftPreviewLabel>
   );
 }
@@ -27,14 +32,35 @@ export const StyledNftPreviewLabel = styled.div`
   padding: 8px;
   z-index: 10;
   justify-content: end;
-  height: 100%;
+  // height: 100%;
   // this helps position the label correctly in Safari
   // Safari differs from Chrome in how it renders height: 100% on position: absolute elements
   min-height: 56px;
 `;
 
-const StyledBodyRegular = styled(BodyRegular)`
+const StyledBodyRegular = styled(BodyRegular)<{ lines: number }>`
   margin: 0;
+  text-shadow: 1px 1px 3px rgba(0, 0, 0, 1);
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: ${({ lines }) => lines};
+  overflow: hidden;
+  font-size: 12px;
+  line-height: 16px;
+  text-overflow: ellipsis;
+  // word-break: break-word;
+  // word-break: break-all;
+
+    @media only screen and ${breakpoints.tablet} {
+      font-size: 14px;
+      line-height: 20px;
+      display: unset;
+      -webkit-box-orient: unset;
+      -webkit-line-clamp: unset;
+      overflow: visible;
+      text-overflow: unset;
+    }
+  }
 `;
 
 export default NftPreviewLabel;
