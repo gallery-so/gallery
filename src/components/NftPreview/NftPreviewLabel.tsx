a7e128d5e7ce9f2ccf9d4d020ca629180e874a78
Connor Rothschild <connorrothschild@gmail.com>
2022-02-22 21:01:55 -0600

    Adjust styling of NFT preview text on small screens (#610)
    * add forced ellipsis to collection/nft title on mobile
    
    * reduce intensity of text shadow
    
    * force line breaks anywhere in string whenever screen is very small
    
    * undo testing edits
    
    * fix text-shadow overflow: hidden bug by appling padding/negative margin

--- src/components/NftPreview/NftPreviewLabel.tsx
+++ src/components/NftPreview/NftPreviewLabel.tsx
@@ -2,6 +2,7 @@ import colors from 'components/core/colors';
 import styled from 'styled-components';
 import { BodyRegular } from 'components/core/Text/Text';
 import { Nft } from 'types/Nft';
+import breakpoints from 'components/core/breakpoints';
 
 type Props = {
   nft: Nft;
@@ -11,8 +12,12 @@ type Props = {
 function NftPreviewLabel({ nft, className }: Props) {
   return (
     <StyledNftPreviewLabel className={className}>
-      <StyledBodyRegular color={colors.white}>{nft.name}</StyledBodyRegular>
-      <StyledBodyRegular color={colors.white}>{nft.token_collection_name}</StyledBodyRegular>
+      <StyledBodyRegular color={colors.white} lines={1}>
+        {nft.name}
+      </StyledBodyRegular>
+      <StyledBodyRegular color={colors.white} lines={2}>
+        {nft.token_collection_name}
+      </StyledBodyRegular>
     </StyledNftPreviewLabel>
   );
 }
@@ -27,14 +32,46 @@ export const StyledNftPreviewLabel = styled.div`
   padding: 8px;
   z-index: 10;
   justify-content: end;
-  height: 100%;
   // this helps position the label correctly in Safari
   // Safari differs from Chrome in how it renders height: 100% on position: absolute elements
   min-height: 56px;
 `;
 
-const StyledBodyRegular = styled(BodyRegular)`
+const StyledBodyRegular = styled(BodyRegular)<{ lines: number }>`
+  word-wrap: break-word;
+  word-break: break-all;
+
   margin: 0;
+  text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.7);
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  line-clamp: ${({ lines }) => lines};
+  -webkit-line-clamp: ${({ lines }) => lines};
+  overflow: hidden;
+  font-size: 12px;
+  line-height: 16px;
+  text-overflow: ellipsis;
+  padding-right: 16px;
+  margin-right: -16px;
+
+  @media only screen and ${breakpoints.mobileLarge} {
+    word-wrap: unset;
+    word-break: unset;
+  }
+
+  @media only screen and ${breakpoints.tablet} {
+    font-size: 14px;
+    line-height: 20px;
+    padding-right: 0;
+    margin-right: 0;
+    display: unset;
+    -webkit-box-orient: unset;
+    line-clamp: unset;
+    -webkit-line-clamp: unset;
+    overflow: visible;
+    text-overflow: unset;
+  }
+}
 `;
 
 export default NftPreviewLabel;
