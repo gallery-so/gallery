b95d55f9c73456077589908def1382062743127e
Jakz <jakzaizzat@gmail.com>
2022-02-23 11:05:44 +0800

    Logged In Navbar Improvements (#608)
    * Added sign out to manage modal wallet
    
    * Restructure the logged in navbar
    
    * Added chaining to username
    
    * Remove wallet address from dropdown
    
    * Rename wallet modal title

--- src/components/core/Page/GlobalNavbar/LoggedInNav.tsx
+++ src/components/core/Page/GlobalNavbar/LoggedInNav.tsx
@@ -1,34 +1,20 @@
-import { useCallback, useMemo } from 'react';
-import { useAuthActions } from 'contexts/auth/AuthContext';
+import { useCallback } from 'react';
 import TextButton from 'components/core/Button/TextButton';
 import Dropdown from 'components/core/Dropdown/Dropdown';
 import Spacer from 'components/core/Spacer/Spacer';
-import CopyToClipboard from 'components/CopyToClipboard/CopyToClipboard';
-import { useAuthenticatedUser, useAuthenticatedUserAddress } from 'hooks/api/users/useUser';
+import { useAuthenticatedUser } from 'hooks/api/users/useUser';
 import { useModal } from 'contexts/modal/ModalContext';
 import EditUserInfoModal from 'scenes/UserGalleryPage/EditUserInfoModal';
 import ManageWalletsModal from 'scenes/Modals/ManageWalletsModal';
-import { truncateAddress } from 'utils/wallet';
 import NavElement from './NavElement';
 import { useRouter } from 'next/router';
 
 function LoggedInNav() {
-  const { logOut } = useAuthActions();
   const user = useAuthenticatedUser();
-  const userAddress = useAuthenticatedUserAddress();
   const { showModal } = useModal();
   const { push } = useRouter();
 
-  const truncatedUserAddress = useMemo(() => truncateAddress(userAddress), [userAddress]);
-
-  const handleGalleryRedirect = useCallback(() => {
-    const authenticatedUserIsOnTheirOwnPage = window.location.pathname.slice(1) === user.username;
-    if (authenticatedUserIsOnTheirOwnPage) {
-      return;
-    }
-
-    void push(`/${user.username}`);
-  }, [push, user.username]);
+  const username = user?.username;
 
   const handleManageWalletsClick = useCallback(() => {
     showModal(<ManageWalletsModal />);
@@ -45,23 +31,15 @@ function LoggedInNav() {
   return (
     <>
       <NavElement>
-        <TextButton onClick={handleGalleryRedirect} text="My Gallery" />
+        <Dropdown mainText="Edit Profile">
+          <TextButton text="Edit name & Bio" onClick={handleEditNameClick} underlineOnHover />
+          <Spacer height={12} />
+          <TextButton text="Edit Gallery" onClick={handleEditGalleryClick} underlineOnHover />
+        </Dropdown>
       </NavElement>
       <Spacer width={24} />
       <NavElement>
-        <Dropdown mainText="Account">
-          <CopyToClipboard textToCopy={userAddress}>
-            <TextButton text={truncatedUserAddress} disableTextTransform underlineOnHover />
-          </CopyToClipboard>
-          <Spacer height={12} />
-          <TextButton text="Manage Wallets" onClick={handleManageWalletsClick} underlineOnHover />
-          <Spacer height={12} />
-          <TextButton text="Edit Gallery" onClick={handleEditGalleryClick} underlineOnHover />
-          <Spacer height={12} />
-          <TextButton text="Edit name & Bio" onClick={handleEditNameClick} underlineOnHover />
-          <Spacer height={12} />
-          <TextButton text="Sign Out" onClick={logOut} underlineOnHover />
-        </Dropdown>
+        <TextButton onClick={handleManageWalletsClick} text={username} />
       </NavElement>
     </>
   );
--- src/scenes/Modals/ManageWalletsModal.tsx
+++ src/scenes/Modals/ManageWalletsModal.tsx
@@ -1,5 +1,10 @@
+import ActionText from 'components/core/ActionText/ActionText';
 import breakpoints from 'components/core/breakpoints';
+import Spacer from 'components/core/Spacer/Spacer';
 import ManageWallets from 'components/ManageWallets/ManageWallets';
+import { useAuthActions } from 'contexts/auth/AuthContext';
+import { useModal } from 'contexts/modal/ModalContext';
+import { useCallback } from 'react';
 import styled from 'styled-components';
 
 type Props = {
@@ -7,9 +12,21 @@ type Props = {
 };
 
 function ManageWalletsModal({ newAddress }: Props) {
+  const { hideModal } = useModal();
+  const { logOut } = useAuthActions();
+
+  const handleLogOut = useCallback(() => {
+    void logOut();
+    void hideModal();
+  }, [hideModal, logOut]);
+
   return (
     <StyledManageWalletsModal>
       <ManageWallets newAddress={newAddress} />
+      <Spacer height={24}></Spacer>
+      <SignOutWrapper>
+        <ActionText onClick={handleLogOut}>Sign out</ActionText>
+      </SignOutWrapper>
     </StyledManageWalletsModal>
   );
 }
@@ -18,6 +35,11 @@ const StyledManageWalletsModal = styled.div`
   @media only screen and ${breakpoints.tablet} {
     width: 480px;
   }
+`;
+
+const SignOutWrapper = styled.div`
+  display: flex;
+  justify-content: center;
 `;
 
 export default ManageWalletsModal;
