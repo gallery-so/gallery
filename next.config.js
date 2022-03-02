2b0de9ccf45cfb5853b72e5d6dce32e45a5dcc33
Robinnnnn <rokim8@gmail.com>
2022-03-02 02:07:31 -0500

    config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const relayConfig = require('./relay.config');
+const { withSentryConfig } = require('@sentry/nextjs');
 
-module.exports = {
+const moduleExports = {
   typescript: {
     // If we ever move to github actions, we can turn this on
     // to parallelize CI. Vercel can do a build while we are
@@ -26,3 +27,17 @@ module.exports = {
     relay: relayConfig,
   },
 };
+
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
