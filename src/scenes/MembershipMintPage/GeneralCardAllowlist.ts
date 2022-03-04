b501f215257d9940a23cb7d86008eaf427c952da
Robinnnnn <rokim8@gmail.com>
2022-03-04 12:25:42 -0500

    fwb AL

--- src/scenes/MembershipMintPage/GeneralCardAllowlist.ts
+++ src/scenes/MembershipMintPage/GeneralCardAllowlist.ts
@@ -2,6 +2,7 @@ import isProduction from 'utils/isProduction';
 import ALLOW_LIST_LEGGENDA from './data/leggenda';
 import ALLOW_LIST_POOLSUITE from './data/poolsuite';
 import ALLOW_LIST_CRYPTO_COVEN from './data/cryptocoven';
+import ALLOW_LIST_FWB from './data/fwb';
 import ALLOW_LIST_CUSTOM from './data/custom';
 
 const ALLOWLIST_DEV: string[] = [];
@@ -10,6 +11,7 @@ const ALLOWLIST_PROD = [
   ...ALLOW_LIST_LEGGENDA,
   ...ALLOW_LIST_POOLSUITE,
   ...ALLOW_LIST_CRYPTO_COVEN,
+  ...ALLOW_LIST_FWB,
   ...ALLOW_LIST_CUSTOM,
 ].map((s) => s.toLowerCase());
 
