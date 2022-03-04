458a4da9b15bf4815dfe390242deddc635bedf23
Robin <rokim8@gmail.com>
2022-03-04 01:37:29 -0500

    Consolidate StyledBackLink (#632)

---
+++ src/components/NavbarBackLink/NavbarBackLink.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import breakpoints, { pageGutter } from 'components/core/breakpoints';
+
+// mimics a navbar element on the top left corner
+const StyledBackLink = styled.div`
+  height: 80px;
+  display: flex;
+  align-items: center;
+
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 3;
+
+  padding: 0 ${pageGutter.mobile}px;
+
+  @media only screen and ${breakpoints.tablet} {
+    padding: 0 ${pageGutter.tablet}px;
+  }
+`;
+
+export default StyledBackLink;
