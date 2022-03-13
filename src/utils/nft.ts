628c3c6d4d6df9da5bffe123e2dc33659797a0e7
Terence Bezman <terencebezman@gmail.com>
2022-03-12 19:18:16 -0800

    Add graphqlGetResizedNftImageURlWithFallback for graphql implementation and backward compatability

--- src/utils/nft.ts
+++ src/utils/nft.ts
@@ -4,6 +4,23 @@ import { GOOGLE_CONTENT_IMG_URL } from './regex';
 
 const FALLBACK_URL = 'https://i.ibb.co/q7DP0Dz/no-image.png';
 
+export function graphqlGetResizedNftImageUrlWithFallback(url: string | null, size = 288): string {
+  if (url?.endsWith('.gif')) {
+    return url;
+  }
+
+  // Resizes if google image: https://developers.google.com/people/image-sizing
+  if (url?.includes('googleusercontent')) {
+    if (url.match(GOOGLE_CONTENT_IMG_URL)) {
+      return url.replace(GOOGLE_CONTENT_IMG_URL, `=w${size}`);
+    }
+
+    return `${url}=w${size}`;
+  }
+
+  return url || FALLBACK_URL;
+}
+
 export function getResizedNftImageUrlWithFallback(nft: Nft, size = 288): string {
   const {
     image_url,
