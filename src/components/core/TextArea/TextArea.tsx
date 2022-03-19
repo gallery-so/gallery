93c24a0c72b59c12409cbef189c95f2871f5d02b
Connor Rothschild <connorrothschild@gmail.com>
2022-03-19 17:12:38 -0500

    improve handling of collectors note size changes

--- src/components/core/TextArea/TextArea.tsx
+++ src/components/core/TextArea/TextArea.tsx
@@ -1,4 +1,4 @@
-import { ChangeEventHandler } from 'react';
+import { ChangeEventHandler, useRef, useEffect, useState, forwardRef } from 'react';
 import styled from 'styled-components';
 import noop from 'utils/noop';
 import colors from '../colors';
@@ -10,16 +10,21 @@ type TextAreaProps = {
   placeholder: string;
   defaultValue?: string;
   autoFocus?: boolean;
+  textAreaHeight?: string;
 };
 
-export function TextArea({
-  className,
-  onChange = noop,
-  placeholder,
-  defaultValue,
-  autoFocus = false,
-}: TextAreaProps) {
-  return (
+export const TextArea = forwardRef(
+  (
+    {
+      className,
+      onChange = noop,
+      placeholder,
+      defaultValue,
+      autoFocus = false,
+      textAreaHeight,
+    }: TextAreaProps,
+    ref
+  ) => (
     <StyledTextArea
       className={className}
       placeholder={placeholder}
@@ -30,11 +35,13 @@ export function TextArea({
       autoCorrect="off"
       autoCapitalize="off"
       spellCheck="false"
+      textAreaHeight={textAreaHeight}
+      ref={ref}
     />
-  );
-}
+  )
+);
 
-const StyledTextArea = styled.textarea`
+const StyledTextArea = styled.textarea<TextAreaProps>`
   width: 100%;
   height: 100%;
   padding: 16px;
@@ -44,6 +51,7 @@ const StyledTextArea = styled.textarea`
   resize: none;
   font-size: 14px;
   line-height: 20px;
+  ${({ textAreaHeight }) => `min-height: ${textAreaHeight}`};
 `;
 
 export default TextArea;
@@ -70,9 +78,69 @@ export function TextAreaWithCharCount({
   );
 }
 
+export function AutoResizingTextAreaWithCharCount({
+  className,
+  currentCharCount,
+  maxCharCount,
+  ...textAreaProps
+}: TextAreaWithCharCountProps) {
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  // const [text, setText] = useState(textAreaProps.defaultValue);
+
+  const DEFAULT_TEXTAREA_HEIGHT = 'auto';
+
+  const [textAreaHeight, setTextAreaHeight] = useState(DEFAULT_TEXTAREA_HEIGHT);
+  const [parentHeight, setParentHeight] = useState(DEFAULT_TEXTAREA_HEIGHT);
+
+  useEffect(() => {
+    if (textAreaRef.current) {
+      setParentHeight(`${textAreaRef.current.scrollHeight}px`);
+      setTextAreaHeight(`${textAreaRef.current.scrollHeight}px`);
+    }
+  }, [textAreaProps.defaultValue]);
+
+  const onChangeHandler = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    // This is needed to reduce height of textarea on text delete
+    if (textAreaRef.current) {
+      setTextAreaHeight(DEFAULT_TEXTAREA_HEIGHT);
+      setParentHeight(`${textAreaRef.current.scrollHeight}px`);
+    }
+
+    // setText(event.target.value);
+
+    if (textAreaProps.onChange) {
+      textAreaProps.onChange(event);
+    }
+  };
+
+  return (
+    <StyledTextAreaWithCharCount className={className}>
+      <StyledParentContainer
+        style={{
+          minHeight: parentHeight,
+        }}
+      >
+        <TextArea
+          {...textAreaProps}
+          ref={textAreaRef}
+          textAreaHeight={textAreaHeight}
+          onChange={onChangeHandler}
+        />
+        <StyledCharacterCounter error={currentCharCount > maxCharCount}>
+          {currentCharCount}/{maxCharCount}
+        </StyledCharacterCounter>
+      </StyledParentContainer>
+    </StyledTextAreaWithCharCount>
+  );
+}
+
 const StyledTextAreaWithCharCount = styled.div`
   position: relative;
   border: 1px solid ${colors.gray50};
+`;
+
+const StyledParentContainer = styled.div`
+  padding-bottom: 20px;
 `;
 
 const StyledCharacterCounter = styled(Caption)<{ error: boolean }>`
--- src/scenes/NftDetailPage/NftDetailNote.tsx
+++ src/scenes/NftDetailPage/NftDetailNote.tsx
@@ -1,8 +1,8 @@
 import { BodyRegular } from 'components/core/Text/Text';
 import Spacer from 'components/core/Spacer/Spacer';
 import TextButton from 'components/core/Button/TextButton';
-import { useCallback, useState, useMemo, useRef } from 'react';
-import { TextAreaWithCharCount } from 'components/core/TextArea/TextArea';
+import { useCallback, useState, useMemo, useRef, useEffect } from 'react';
+import { AutoResizingTextAreaWithCharCount } from 'components/core/TextArea/TextArea';
 import unescape from 'lodash.unescape';
 import styled from 'styled-components';
 import useUpdateNft from 'hooks/api/nfts/useUpdateNft';
@@ -14,7 +14,6 @@ import { GLOBAL_FOOTER_HEIGHT } from 'components/core/Page/constants';
 import { useTrack } from 'contexts/analytics/AnalyticsContext';
 
 const MAX_CHAR_COUNT = 1200;
-const MIN_NOTE_HEIGHT = 150;
 
 type NoteEditorProps = {
   nftCollectorsNote: string;
@@ -25,8 +24,9 @@ function NoteEditor({ nftCollectorsNote, nftId }: NoteEditorProps) {
   // Generic error that doesn't belong to collector's note
   const [generalError, setGeneralError] = useState('');
 
-  const [noteHeight, setNoteHeight] = useState(MIN_NOTE_HEIGHT);
   const [isEditing, setIsEditing] = useState(false);
+
+  const [collectorsNoteHeight, setCollectorsNoteHeight] = useState(0);
 
   const [collectorsNote, setCollectorsNote] = useState(nftCollectorsNote ?? '');
   const unescapedCollectorsNote = useMemo(() => unescape(collectorsNote), [collectorsNote]);
@@ -34,6 +34,12 @@ function NoteEditor({ nftCollectorsNote, nftId }: NoteEditorProps) {
   const hasCollectorsNote = useMemo(() => collectorsNote.length > 0, [collectorsNote]);
 
   const collectorsNoteRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (collectorsNoteRef.current) {
+      setCollectorsNoteHeight(collectorsNoteRef.current.scrollHeight);
+    }
+  }, [collectorsNote]);
 
   const scrollDown = useCallback(() => {
     if (collectorsNoteRef.current) {
@@ -47,9 +53,6 @@ function NoteEditor({ nftCollectorsNote, nftId }: NoteEditorProps) {
 
   const handleEditCollectorsNote = useCallback(() => {
     setIsEditing(true);
-
-    // TODO Expand note to full height first time it is opened
-    // Currently user has to type first to expand it fully
 
     // Scroll down - wait so that element exists before scrolling to bottom of it
     setTimeout(() => {
@@ -98,14 +101,6 @@ function NoteEditor({ nftCollectorsNote, nftId }: NoteEditorProps) {
   const handleNoteChange = useCallback(
     (event: React.ChangeEvent<HTMLTextAreaElement>) => {
       setCollectorsNote(event.target?.value);
-      setNoteHeight(event.target?.scrollHeight);
-
-      // On clear, reset note height (textarea scrollHeight does not decrease on its own, it only increases)
-      // So we use this hard reset if the user deletes all content. Could have more elegant solution
-      // TODO Reduce size to text content on any delete
-      if (event.target?.value === '') {
-        setNoteHeight(MIN_NOTE_HEIGHT);
-      }
 
       // Scroll down as the user input goes off the screen
       // Need setTimeout so that textarea height is updated
@@ -117,7 +112,7 @@ function NoteEditor({ nftCollectorsNote, nftId }: NoteEditorProps) {
   );
 
   return (
-    <div tabIndex={0} onKeyDown={handleKeyDown} ref={collectorsNoteRef}>
+    <div onKeyDown={handleKeyDown} ref={collectorsNoteRef}>
       <StyledTitleAndButtonContainer>
         {/* We also include isEditing as an option here so the user can click save with an empty note (e.g. delete their note) */}
         {hasCollectorsNote || isEditing ? (
@@ -138,6 +133,10 @@ function NoteEditor({ nftCollectorsNote, nftId }: NoteEditorProps) {
         )}
       </StyledTitleAndButtonContainer>
 
+      {/* Need this to be equal to note height to prevent jitter while typing */}
+      <div
+        style={{ height: collectorsNoteHeight, width: '100%', position: 'absolute', zIndex: -1 }}
+      ></div>
       {generalError && (
         <>
           <Spacer height={8} />
@@ -147,6 +146,7 @@ function NoteEditor({ nftCollectorsNote, nftId }: NoteEditorProps) {
 
       <Spacer height={8} />
 
+      {/* Create a dummy textbox of the same height so that, when the element switches from the above to this one, there is not a jump to the top of the screen before scrollDown applies */}
       {isEditing ? (
         <StyledTextAreaWithCharCount
           footerHeight={GLOBAL_FOOTER_HEIGHT}
@@ -155,12 +155,10 @@ function NoteEditor({ nftCollectorsNote, nftId }: NoteEditorProps) {
           defaultValue={unescapedCollectorsNote}
           currentCharCount={unescapedCollectorsNote.length}
           maxCharCount={MAX_CHAR_COUNT}
-          noteHeight={noteHeight}
         />
       ) : (
         <StyledCollectorsNote
           footerHeight={GLOBAL_FOOTER_HEIGHT}
-          minNoteHeight={MIN_NOTE_HEIGHT}
           onDoubleClick={handleEditCollectorsNote}
         >
           <Markdown text={collectorsNote} />
@@ -179,7 +177,7 @@ function NoteViewer({ nftCollectorsNote }: NoteViewerProps) {
     <>
       <BodyRegular>Collector&rsquo;s Note</BodyRegular>
       <Spacer height={8} />
-      <StyledCollectorsNote footerHeight={GLOBAL_FOOTER_HEIGHT} minNoteHeight={MIN_NOTE_HEIGHT}>
+      <StyledCollectorsNote footerHeight={GLOBAL_FOOTER_HEIGHT}>
         <Markdown text={nftCollectorsNote} />
       </StyledCollectorsNote>
     </>
@@ -231,17 +229,16 @@ const StyledTitleAndButtonContainer = styled.div`
 `;
 
 type TextAreaProps = {
-  noteHeight: number;
   footerHeight: number;
 };
 
 // These two are intentionally styled the same so that editing is seamless
-const StyledTextAreaWithCharCount = styled(TextAreaWithCharCount)<TextAreaProps>`
+const StyledTextAreaWithCharCount = styled(AutoResizingTextAreaWithCharCount)<TextAreaProps>`
   border: none;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
 
   textarea {
-    ${({ noteHeight }) => `min-height: ${noteHeight}px`};
-
     color: #808080;
     margin: 0;
     padding: 0;
@@ -252,6 +249,8 @@ const StyledTextAreaWithCharCount = styled(TextAreaWithCharCount)<TextAreaProps>
 
     border-bottom: none;
     background: none;
+    -ms-overflow-style: none;
+    scrollbar-width: none;
   }
 
   p {
@@ -271,7 +270,7 @@ const StyledTextAreaWithCharCount = styled(TextAreaWithCharCount)<TextAreaProps>
 
 type CollectorsNoteProps = {
   footerHeight: number;
-  minNoteHeight: number;
+  isHidden?: boolean;
 };
 
 const StyledCollectorsNote = styled(BodyRegular)<CollectorsNoteProps>`
@@ -289,10 +288,6 @@ const StyledCollectorsNote = styled(BodyRegular)<CollectorsNoteProps>`
 
   p:last-of-type {
     margin-bottom: 40px; /* line-height * 2, because textarea leaves one line at bottom + char count */
-  }
-
-  @media only screen and ${breakpoints.tablet} {
-    min-height: ${({ minNoteHeight }) => minNoteHeight}px;
   }
 `;
 
