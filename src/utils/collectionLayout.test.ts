dc4052def1bfad67317fae4e2d0b9fa41e058b2e
Terence Bezman <terencebezman@gmail.com>
2022-03-22 19:11:33 -0700

    Fix type issues

--- src/utils/collectionLayout.test.ts
+++ src/utils/collectionLayout.test.ts
@@ -1,9 +1,11 @@
+import { EditModeNft } from 'flows/shared/steps/OrganizeCollection/types';
 import { getWhitespacePositionsFromStagedItems, insertWhitespaceBlocks } from './collectionLayout';
 
-function generateTestNft() {
+function generateTestNft(): EditModeNft {
   return {
     id: '123',
     name: 'test',
+    nft: {},
     description: 'test',
     image: {
       url: 'https://example.com/test.jpg',
@@ -17,16 +19,16 @@ function generateTestNft() {
 describe.skip('getWhitespacePositionsFromStagedItems', () => {
   it('computes the correct whitespace list given a list of staged items', () => {
     const stagedItems = [
-      { id: 'blank-1', whitespace: 'whitespace' },
-      { id: 'blank-2', whitespace: 'whitespace' },
+      { id: 'blank-1', whitespace: 'whitespace' } as const,
+      { id: 'blank-2', whitespace: 'whitespace' } as const,
       generateTestNft(),
-      { id: 'blank-3', whitespace: 'whitespace' },
+      { id: 'blank-3', whitespace: 'whitespace' } as const,
       generateTestNft(),
       generateTestNft(),
       generateTestNft(),
-      { id: 'blank-4', whitespace: 'whitespace' },
+      { id: 'blank-4', whitespace: 'whitespace' } as const,
       generateTestNft(),
-      { id: 'blank-5', whitespace: 'whitespace' },
+      { id: 'blank-5', whitespace: 'whitespace' } as const,
     ];
     const whitespaceList = getWhitespacePositionsFromStagedItems(stagedItems);
     expect(whitespaceList).toEqual([0, 0, 1, 4, 5]);
