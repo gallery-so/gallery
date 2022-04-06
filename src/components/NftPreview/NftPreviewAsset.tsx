c35603f439a2326447cf47a09fd87eb878e99e96
Robinnnnn <rokim8@gmail.com>
2022-04-05 23:48:52 -0400

    NftPreviewAsset

--- src/components/NftPreview/NftPreviewAsset.tsx
+++ src/components/NftPreview/NftPreviewAsset.tsx
@@ -1,11 +1,7 @@
-import { NftMediaType } from 'components/core/enums';
-import { useMemo } from 'react';
 import styled from 'styled-components';
 
 import ImageWithLoading from 'components/ImageWithLoading/ImageWithLoading';
-import { Nft } from 'types/Nft';
-import { getMediaTypeForAssetUrl, graphqlGetResizedNftImageUrlWithFallback } from 'utils/nft';
-import { useSetContentIsLoaded } from 'contexts/shimmer/ShimmerContext';
+import { graphqlGetResizedNftImageUrlWithFallback } from 'utils/nft';
 import { useFragment } from 'react-relay';
 import { graphql } from 'relay-runtime';
 import { NftPreviewAssetFragment$key } from '__generated__/NftPreviewAssetFragment.graphql';
@@ -24,13 +20,13 @@ function NftPreviewAsset({ nftRef, size }: Props) {
           __typename
 
           ... on VideoMedia {
-            contentRenderURLs @required(action: NONE) {
-              medium @required(action: NONE)
+            previewURLs @required(action: NONE) {
+              large @required(action: NONE)
             }
           }
           ... on ImageMedia {
-            contentRenderURLs @required(action: NONE) {
-              medium @required(action: NONE)
+            previewURLs @required(action: NONE) {
+              large @required(action: NONE)
             }
           }
           ... on UnknownMedia {
@@ -44,24 +40,11 @@ function NftPreviewAsset({ nftRef, size }: Props) {
     nftRef
   );
 
-  const setContentIsLoaded = useSetContentIsLoaded();
-
-  if (nft.media?.__typename === 'VideoMedia') {
-    return (
-      <StyledVideo
-        src={`${nft.media.contentRenderURLs.medium}#t=0.5`}
-        onLoadStart={setContentIsLoaded}
-        preload="metadata"
-      />
-    );
-  } else if (nft.media?.__typename === 'ImageMedia') {
-    return (
-      <ImageWithLoading
-        src={graphqlGetResizedNftImageUrlWithFallback(nft.media.contentRenderURLs.medium, size)}
-        alt={nft.name ?? ''}
-      />
-    );
-  } else if (nft.media?.__typename === 'UnknownMedia') {
+  if (
+    nft.media?.__typename === 'VideoMedia' ||
+    nft.media?.__typename === 'ImageMedia' ||
+    nft.media?.__typename === 'UnknownMedia'
+  ) {
     return (
       <ImageWithLoading
         src={graphqlGetResizedNftImageUrlWithFallback(nft.media.previewURLs.large, size)}
