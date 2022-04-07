dcb9c022548cdb9a388d5ac9b14abf0e2673d63f
jakzaizzat <jakzaizzat@gmail.com>
2022-04-07 09:49:04 +0800

    Switch to metamask window if it didnt active

--- cypress/integration/pages/page.ts
+++ cypress/integration/pages/page.ts
@@ -1,7 +1,11 @@
 export default class Page {
   acceptMetamaskAccessRequest() {
     cy.wait(1000);
-    cy.acceptMetamaskAccess();
+    cy.acceptMetamaskAccess(false);
+    if (!cy.isMetamaskWindowActive) {
+      cy.switchToMetamaskWindow();
+      cy.switchToMetamaskNotification();
+    }
     cy.wait(500);
   }
 }
