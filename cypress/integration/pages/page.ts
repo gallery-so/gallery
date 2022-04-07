01bef67da72ccb99aefba511136280bc673fe22b
jakzaizzat <jakzaizzat@gmail.com>
2022-04-07 11:30:41 +0800

    Encapsulate metamask sign method

--- cypress/integration/pages/page.ts
+++ cypress/integration/pages/page.ts
@@ -2,10 +2,13 @@ export default class Page {
   acceptMetamaskAccessRequest() {
     cy.wait(1000);
     cy.acceptMetamaskAccess(false);
+    // Handle intermittent issue the metamask popup is not shown
     if (!cy.isMetamaskWindowActive) {
       cy.switchToMetamaskWindow();
       cy.switchToMetamaskNotification();
     }
+    cy.confirmMetamaskSignatureRequest();
+    cy.switchToCypressWindow();
     cy.wait(500);
   }
 }
