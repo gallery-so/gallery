5519094c9c42163c7471d450240c9c7607d07ac3
Robinnnnn <rokim8@gmail.com>
2022-04-11 19:47:12 -0400

    refactor NftDetailView, NftDetailPage, toss unused useUser hooks

--- src/hooks/api/nfts/useAllNfts.ts
+++ src/hooks/api/nfts/useAllNfts.ts
@@ -1,32 +1,15 @@
 import { useCallback } from 'react';
 import { useSWRConfig } from 'swr';
-import { Nft } from 'types/Nft';
 import { useAuthenticatedUser } from '../users/useUser';
-import useGet from '../_rest/useGet';
-
-export type GetAllNFtsResponse = {
-  nfts: Nft[];
-};
 
 type QueryProps = {
   userId: string;
 };
 
-export const getAllNftsAction = 'fetch all nfts';
+const getAllNftsAction = 'fetch all nfts';
 
 function getAllNftsCacheKey({ userId }: QueryProps) {
   return [`/nfts/user_get?user_id=${userId}`, getAllNftsAction];
-}
-
-export default function useAllNfts(): Nft[] {
-  const user = useAuthenticatedUser();
-  const data = useGet<GetAllNFtsResponse>(`/nfts/user_get?user_id=${user.id}`, getAllNftsAction);
-
-  if (!data) {
-    throw new Error(`No NFTs were found for user id: ${user.id}`);
-  }
-
-  return data?.nfts;
 }
 
 // use this hook to force SWR to refetch all nfts for user
--- src/hooks/api/users/useUser.ts
+++ src/hooks/api/users/useUser.ts
@@ -31,11 +31,6 @@ function getUserBaseUrlWithQuery({ id, username, address }: Props) {
   return `${getUserBaseUrl}?${query}`;
 }
 
-// Allows access to user cache within SWR. useful for mutations
-export function getUserCacheKey(queryParameters: Props) {
-  return [getUserBaseUrlWithQuery(queryParameters), getUserAction];
-}
-
 export default function useUser(props: Props): User | undefined {
   const validParameters = props.id ?? props.username ?? props.address;
 
@@ -49,20 +44,6 @@ function useCurrentUser(makeRequest = true): User | undefined {
   return data;
 }
 
-// TODO: we can probably remove this by refactoring the components that use it
-// use this hook to get the current logged in user when when you don't always expect one to be returned.
-// ie in a component that is rendered for all users
-export function usePossiblyAuthenticatedUser() {
-  const state = useAuthState();
-  const isLoggedIn = useMemo(() => isLoggedInState(state), [state]);
-  const user = useCurrentUser(isLoggedIn);
-  if (isLoggedIn && !user) {
-    throw new Error('Authenticated user not found');
-  }
-
-  return user;
-}
-
 // use this hook to get the current logged in user when you expect one to be returned.
 // ie in a component that is only rendered for authenticated users
 export function useAuthenticatedUser() {
@@ -72,16 +53,4 @@ export function useAuthenticatedUser() {
   }
 
   return user;
-}
-
-export function useAuthenticatedUserAddresses() {
-  const user = useAuthenticatedUser();
-
-  return user.addresses;
-}
-
-export function useAuthenticatedUsername() {
-  const user = useAuthenticatedUser();
-
-  return user.username;
 }
