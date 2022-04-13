ccbb2b71d5fbd0374b0b8003d9fef14e687f5ee0
Robinnnnn <rokim8@gmail.com>
2022-04-13 02:28:11 -0400

    toss useIsAuthenticated

--- src/contexts/auth/useIsAuthenticated.ts
+++
@@ -1,7 +0,0 @@
-import { useAuthState } from './AuthContext';
-import { isLoggedInState } from './typeguards';
-
-export default function useIsAuthenticated() {
-  const authState = useAuthState();
-  return isLoggedInState(authState);
-}
