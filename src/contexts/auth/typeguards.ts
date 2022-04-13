b920a41878af1e6369bf7a6f1e977085df8e9f13
Robinnnnn <rokim8@gmail.com>
2022-04-13 02:35:24 -0400

    move auth typeguard to useAuthenticatedUserId

--- src/contexts/auth/typeguards.ts
+++
@@ -1,8 +0,0 @@
-import { AuthState } from './AuthContext';
-import { LOGGED_IN } from './types';
-
-export function isLoggedInState(state: AuthState): state is LOGGED_IN {
-  return (
-    typeof state === 'object' && state !== null && 'type' in state && state.type === 'LOGGED_IN'
-  );
-}
