987a89d457e0bed5befe0ca7768cbfea22fb344e
jakzaizzat <jakzaizzat@gmail.com>
2022-04-15 07:59:12 +0800

    Refactor the banner component

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,22 +1,30 @@
 import colors from 'components/core/colors';
-import { BaseM } from 'components/core/Text/Text';
+import { BaseM, TitleS } from 'components/core/Text/Text';
 import useIsAuthenticated from 'contexts/auth/useIsAuthenticated';
 import usePersistedState from 'hooks/usePersistedState';
 import { useCallback } from 'react';
 import styled from 'styled-components';
 
 type Props = {
+  title?: string;
   text: string;
   requireAuth?: boolean;
+  children?: React.ReactNode;
+  localStorageKey?: string;
 };
 
-const JAN_18_MIGRATION_BANNER_DISMISSED_LOCAL_STORAGE_KEY = 'jan_18_migration_banner_dismissed';
+export default function Banner({
+  children,
+  localStorageKey,
+  text,
+  title,
+  requireAuth = false,
+}: Props) {
+  const LOCAL_STORAGE_KEY = localStorageKey;
 
-export default function Banner({ text, requireAuth = false }: Props) {
-  const [dismissed, setDismissed] = usePersistedState(
-    JAN_18_MIGRATION_BANNER_DISMISSED_LOCAL_STORAGE_KEY,
-    false
-  );
+  const [dismissed, setDismissed] = LOCAL_STORAGE_KEY
+    ? usePersistedState(LOCAL_STORAGE_KEY, false)
+    : [false, () => {}];
 
   const isAuthenticated = useIsAuthenticated();
 
@@ -25,30 +33,50 @@ export default function Banner({ text, requireAuth = false }: Props) {
   }, [setDismissed]);
 
   return dismissed || (requireAuth && !isAuthenticated) ? null : (
-    <StyledBanner>
-      <StyledClose onClick={hideBanner}>&#x2715;</StyledClose>
-      <BaseM color={colors.white}>{text}</BaseM>
-    </StyledBanner>
+    <StyledContainer>
+      <StyledBanner>
+        <StyledText>
+          {title && <TitleS>{title}</TitleS>}
+          <BaseM color={colors.offBlack}>{text}</BaseM>
+        </StyledText>
+        <StyledAction>
+          {children}
+          <StyledClose onClick={hideBanner}>&#x2715;</StyledClose>
+        </StyledAction>
+      </StyledBanner>
+    </StyledContainer>
   );
 }
 
+const StyledContainer = styled.div`
+  padding: 24px;
+`;
+
 const StyledBanner = styled.div`
-  background: black;
-  text-align: center;
+  background: white;
+  text-align: left;
   width: 100%;
-  padding: 20px 40px;
+  padding: 8px 16px;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
-  position: absolute;
   top: 0;
   z-index: 1;
+  border: 1px solid ${colors.shadow};
+  position: relative;
 `;
 
 const StyledClose = styled.span`
-  position: absolute;
-  right: 0px;
-  padding: 12px 12px;
   cursor: pointer;
-  color: white;
+  color: #141414;
+`;
+
+const StyledText = styled.div`
+  display: flex;
+  gap: 12px;
+`;
+
+const StyledAction = styled.div`
+  display: flex;
+  gap: 24px;
 `;
