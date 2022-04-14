02f2ba2a0b8db30c9ebff7b5f09e32c4fe443e9b
Robinnnnn <rokim8@gmail.com>
2022-04-13 20:51:04 -0400

    dumb down SwrContext

--- src/contexts/swr/SwrContext.tsx
+++ src/contexts/swr/SwrContext.tsx
@@ -1,62 +1,24 @@
 import { memo, useCallback, useMemo } from 'react';
 import { SWRConfig } from 'swr';
-import { MINUTE, SECOND } from 'utils/time';
-import useFetcher from './useFetcher';
-import { useTrack } from 'contexts/analytics/AnalyticsContext';
-
-function localStorageProvider() {
-  // When initializing, we restore the data from `localStorage` into a map.
-  const map = new Map(JSON.parse(localStorage.getItem('app-cache') ?? '[]'));
-
-  // Before unloading the app, we write back all the data into `localStorage`.
-  window.addEventListener('beforeunload', () => {
-    if (window.location.pathname === '/nuke') {
-      return;
-    }
-
-    const arrayMap = Array.from(map.entries()) as Array<[string, any]>;
-    // ignore inflight requests (labeled with $req$) and errors (labeled with $err$) in the cache.
-    // in fact, aggressively caching an error response can result in pain!
-    const saneArrayMap = arrayMap.filter(
-      (arr) =>
-        !(
-          arr[0].includes('$req$') ||
-          arr[0].includes('$err$') ||
-          // ignore response for getting current user because the client could receive a 204 No Content
-          // if the cookie is invalid. SWR doesn't interpret the 204 as invalid, and instead caches it,
-          // which in turn breaks the hooks that immediately load the value.
-          arr[0].includes('/users/get/current')
-        )
-    );
-    const appCache = JSON.stringify(saneArrayMap);
-    localStorage.setItem('app-cache', appCache);
-  });
-
-  // We still use the map for write & read for performance.
-  return map;
-}
+import { SECOND } from 'utils/time';
+import { vanillaFetcher } from './useFetcher';
+import { useReportError } from 'contexts/errorReporting/ErrorReportingContext';
 
 export const SwrProvider = memo(({ children }) => {
-  const fetcher = useFetcher();
-
-  const track = useTrack();
+  const reportError = useReportError();
 
   const handleLoadingSlow = useCallback(
     (key) => {
-      track('Slow request', { endpoint: key });
+      reportError(new Error('Slow request detected'), { tags: { endpoint: key } });
     },
-    [track]
+    [reportError]
   );
 
   const value = useMemo(
     () => ({
       suspense: true,
       // custom fetcher
-      fetcher,
-      // sync store with localStorage
-      provider: localStorageProvider,
-      // revalidate data every 5 mins. only impacts hooks/data that's on screen
-      refreshInterval: 5 * MINUTE,
+      fetcher: vanillaFetcher,
       // prevent auto-revalidation on window focus
       revalidateOnFocus: false,
       // automatically revalidate if browser loses network connection and recovers
@@ -66,7 +28,7 @@ export const SwrProvider = memo(({ children }) => {
       // detect slow requests
       onLoadingSlow: handleLoadingSlow,
     }),
-    [fetcher, handleLoadingSlow]
+    [handleLoadingSlow]
   );
 
   return <SWRConfig value={value}>{children}</SWRConfig>;
