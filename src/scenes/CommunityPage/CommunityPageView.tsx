1b29f7e5044a37b8644fe2e3f557643a8b5305f5
kaito <80802871+kaitoo1@users.noreply.github.com>
2022-04-14 11:42:20 +0900

    truncate long community descriptions and reduce width

--- src/scenes/CommunityPage/CommunityPageView.tsx
+++ src/scenes/CommunityPage/CommunityPageView.tsx
@@ -1,5 +1,5 @@
 import Spacer from 'components/core/Spacer/Spacer';
-import { BaseM, TitleL, TitleS } from 'components/core/Text/Text';
+import { BaseM, TitleL, TitleS, TitleXS } from 'components/core/Text/Text';
 import MemberListFilter from 'scenes/MemberListPage/MemberListFilter';
 import styled from 'styled-components';
 import MemberListPageProvider from 'contexts/memberListPage/MemberListPageContext';
@@ -9,6 +9,9 @@ import Markdown from 'components/core/Markdown/Markdown';
 import { graphql } from 'relay-runtime';
 import { useFragment } from 'react-relay';
 import { CommunityPageViewFragment$key } from '__generated__/CommunityPageViewFragment.graphql';
+import { useCallback, useMemo, useState } from 'react';
+import TextButton from 'components/core/Button/TextButton';
+import breakpoints from 'components/core/breakpoints';
 
 type Props = {
   communityRef: CommunityPageViewFragment$key;
@@ -28,18 +31,36 @@ export default function CommunityPageView({ communityRef }: Props) {
   const { name, description } = community;
   const isMobile = useIsMobileWindowWidth();
 
+  const truncatedDescription = useMemo(() => {
+    if (!description) {
+      return '';
+    }
+    return description.length > 330 ? `${description.slice(0, 327).trim()}...` : description;
+  }, [description]);
+
+  const [showFullDescription, setShowFullDescription] = useState(false);
+
+  const handleShowMoreClick = useCallback(() => {
+    setShowFullDescription((prev) => !prev);
+  }, []);
+
   return (
     <MemberListPageProvider>
       <Spacer height={128} />
       <StyledHeader>
         <TitleL>{name}</TitleL>
         {description && (
-          <>
+          <StyledDescription>
             <Spacer height={8} />
             <BaseM>
-              <Markdown text={description} />
+              <Markdown text={showFullDescription ? description : truncatedDescription} />
             </BaseM>
-          </>
+            <Spacer height={8} />
+            <TextButton
+              text={showFullDescription ? 'Show less' : 'Show More'}
+              onClick={handleShowMoreClick}
+            />
+          </StyledDescription>
         )}
       </StyledHeader>
       <Spacer height={isMobile ? 65 : 96} />
@@ -54,6 +75,12 @@ export default function CommunityPageView({ communityRef }: Props) {
     </MemberListPageProvider>
   );
 }
+
+const StyledDescription = styled.div`
+  @media only screen and ${breakpoints.tablet} {
+    width: 50%;
+  }
+`;
 
 const StyledListWrapper = styled.div`
   display: flex;
