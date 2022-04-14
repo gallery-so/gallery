cf23af982db2b3db4d9c92760394421014365fdd
Robinnnnn <rokim8@gmail.com>
2022-04-13 21:31:01 -0400

    refactor formatError, toss REST-based ApiError

--- src/contexts/boundary/ErrorBoundary.tsx
+++ src/contexts/boundary/ErrorBoundary.tsx
@@ -4,7 +4,7 @@ import { BaseXL, BaseM } from 'components/core/Text/Text';
 import Page from 'components/core/Page/Page';
 import Spacer from 'components/core/Spacer/Spacer';
 import GalleryLink from 'components/core/GalleryLink/GalleryLink';
-import { formatDetailedError } from 'errors/formatError';
+import formatError from 'errors/formatError';
 import { captureException } from '@sentry/nextjs';
 
 class ErrorBoundary extends Component {
@@ -24,13 +24,11 @@ class ErrorBoundary extends Component {
 
   render() {
     if (this.state.error !== null) {
-      const { header, description } = formatDetailedError(this.state.error);
+      const errorMessage = formatError(this.state.error);
 
       return (
         <Page centered topPadding>
-          <BaseXL>{header}</BaseXL>
-          <Spacer height={8} />
-          <BaseM>{description}</BaseM>
+          <BaseXL>{errorMessage}</BaseXL>
           <Spacer height={48} />
           <StyledReachOut>
             If you&apos;re continuing to see this error, reach out to us on{' '}
--- src/errors/formatError.ts
+++ src/errors/formatError.ts
@@ -1,35 +1,7 @@
-import { genericErrorMessage } from './constants';
-import { ApiError } from './types';
-
-export function formatDetailedError(error: Error) {
-  if (error instanceof ApiError) {
-    return {
-      header: `Error while attempting to ${error.customMessage}`,
-      description: error.message,
-    };
-  }
-
-  if (error.message) {
-    return {
-      header: `Error: ${error.message}`,
-      description: '',
-    };
-  }
-
-  return {
-    header: '',
-    description: genericErrorMessage,
-  };
-}
-
 export default function formatError(error: Error) {
-  if (error instanceof ApiError) {
-    return `Error while attempting to ${error.customMessage}: ${error.message}`;
-  }
-
   if (error.message) {
     return `Error: ${error.message}`;
   }
 
-  return genericErrorMessage;
+  return 'Sorry, the server is currently unavailable. Please try again later or ping us on Discord.';
 }
