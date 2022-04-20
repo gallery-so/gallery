ee579db14f64398997666ec5e90bf90e92d632ce
Connor Rothschild <connorrothschild@gmail.com>
2022-04-19 20:25:27 -0500

    improve useKeyDown hook

--- src/hooks/useKeyDown.tsx
+++ src/hooks/useKeyDown.tsx
@@ -1,33 +1,40 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 export default function useKeyDown(targetKey: string) {
   // State for keeping track of whether key is pressed
   const [keyPressed, setKeyPressed] = useState<boolean>(false);
 
   // If pressed key is our target key then set to true
-  function downHandler({ key }: KeyboardEvent) {
-    if (key === targetKey) {
-      setKeyPressed(true);
-    }
-  }
+  const downHandler = useCallback(
+    ({ key }: KeyboardEvent) => {
+      if (key === targetKey) {
+        setKeyPressed(true);
+      }
+    },
+    [targetKey]
+  );
+
   // If released key is our target key then set to false
-  const upHandler = ({ key }: KeyboardEvent) => {
-    if (key === targetKey) {
-      setKeyPressed(false);
-    }
-  };
+  const upHandler = useCallback(
+    ({ key }: KeyboardEvent) => {
+      if (key === targetKey) {
+        setKeyPressed(false);
+      }
+    },
+    [targetKey]
+  );
 
   // Add event listeners
   useEffect(() => {
     window.addEventListener('keydown', downHandler);
     window.addEventListener('keyup', upHandler);
+    console.log('clicking ');
     // Remove event listeners on cleanup
     return () => {
       window.removeEventListener('keydown', downHandler);
       window.removeEventListener('keyup', upHandler);
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []); // Empty array ensures that effect is only run on mount and unmount
+  }, [downHandler, upHandler]);
 
   // If the user is not currently focused on the body, return
   const activeEl = document.activeElement;
