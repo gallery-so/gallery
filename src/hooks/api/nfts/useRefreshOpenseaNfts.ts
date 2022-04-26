d11343ec071e579742e4720121636006b016192e
Robinnnnn <rokim8@gmail.com>
2022-04-26 18:25:07 -0400

    handle opensea refresh errors

--- src/hooks/api/nfts/useRefreshOpenseaNfts.ts
+++ src/hooks/api/nfts/useRefreshOpenseaNfts.ts
@@ -1,10 +1,11 @@
 import { usePromisifiedMutation } from 'hooks/usePromisifiedMutation';
 import { useCallback } from 'react';
 import { graphql } from 'relay-runtime';
+import { useRefreshOpenseaNftsMutation } from '__generated__/useRefreshOpenseaNftsMutation.graphql';
 
 // this will be deprecated once we're off opensea
 export default function useRefreshOpenseaNfts() {
-  const [refreshOpenseaNfts] = usePromisifiedMutation(
+  const [refreshOpenseaNfts] = usePromisifiedMutation<useRefreshOpenseaNftsMutation>(
     graphql`
       mutation useRefreshOpenseaNftsMutation($addresses: String) {
         refreshOpenSeaNfts(addresses: $addresses) {
@@ -15,6 +16,18 @@ export default function useRefreshOpenseaNfts() {
   );
 
   return useCallback(async () => {
-    await refreshOpenseaNfts({ variables: {} });
+    const response = await refreshOpenseaNfts({
+      variables: {},
+    });
+
+    if (response.refreshOpenSeaNfts?.__typename === 'ErrOpenSeaRefreshFailed') {
+      throw new Error(
+        'Error while fetching latest NFTs. Opensea may be temporarily unavailable. Please try again later.'
+      );
+    }
+    // TODO: log the user out
+    if (response.refreshOpenSeaNfts?.__typename === 'ErrNotAuthorized') {
+      throw new Error('You are not authorized!');
+    }
   }, [refreshOpenseaNfts]);
 }
