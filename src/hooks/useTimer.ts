b1eb4d31965bb1fc4d5927a0e3158b13dc2e096f
jakzaizzat <jakzaizzat@gmail.com>
2022-04-27 08:54:23 +0800

    Add usetimer hook

--- src/hooks/useTimer.ts
+++ src/hooks/useTimer.ts
@@ -1,5 +1,33 @@
+import { useEffect, useState } from 'react';
+
+// TODO: Abstract this as a props
+const mintDate = new Date('2024-01-01T00:00:00');
+
 export default function useTimer() {
+  const currentTime = Date.now();
+
+  const [countDown, setCountDown] = useState(mintDate.getTime() - currentTime);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountDown(mintDate.getTime() - Date.now());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [countDown]);
+
+  const days = Math.floor(countDown / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((countDown % (1000 * 60)) / 1000);
+
+  const hasEnded = countDown <= 0;
+
   return {
-    currentTime: Date.now(),
+    days,
+    hours,
+    minutes,
+    seconds,
+    hasEnded,
   };
 }
