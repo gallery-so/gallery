86a40d62edbd97c7a0d105dc4b84a47c67f0a19b
kaito <80802871+kaitoo1@users.noreply.github.com>
2022-04-28 06:44:54 +0900

    disable other umbrella contracts

--- pages/community/[contractAddress]/index.tsx
+++ pages/community/[contractAddress]/index.tsx
@@ -13,6 +13,9 @@ type CommunityPageProps = MetaTagProps & {
 
 export const DISABLED_CONTRACTS = [
   '0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270', // Art Blocks
+  '0x495f947276749ce646f68ac8c248420045cb7b5e', // OS
+  '0xf6793da657495ffeff9ee6350824910abc21356c', // Rarible
+  '0x3b3ee1931dc30c1957379fac9aba94d1c48a5405', // Foundation
 ];
 
 export default function CommunityPage({ contractAddress }: CommunityPageProps) {
