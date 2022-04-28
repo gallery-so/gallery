34f9f33744b10f04713f68f187ad75f370044127
robin <rokim8@gmail.com>
2022-04-28 01:26:02 -0400

    Sentry tracing (#720)
    * bump sentry, add tracingOrigins
    
    * trace middleware
    
    * add op type
    
    * lower rample rate for prod

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -8,6 +8,12 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 const SENTRY_ENV = process.env.NEXT_PUBLIC_VERCEL_ENV;
 
+const IS_PROD = SENTRY_ENV === 'production';
+
+const SENTRY_TRACING_ORIGIN = IS_PROD ? 'api.gallery.so' : 'api.dev.gallery.so';
+
+const SENTRY_TRACING_SAMPLE_RATE = IS_PROD ? 0.2 : 1.0;
+
 Sentry.init({
   // DSNs are safe to keep public because they only allow submission of
   // new events and related event data; they do not allow read access to
@@ -15,8 +21,13 @@ Sentry.init({
   // user can send events to your organization with any information they
   // want, this is a rare occurrence.
   dsn: SENTRY_DSN || 'https://bd40a4affc1740e8b7516502389262fe@o1135798.ingest.sentry.io/6187637',
+  integrations: [
+    new Sentry.BrowserTracing({
+      tracingOrigins: [SENTRY_TRACING_ORIGIN],
+    }),
+  ],
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1.0,
+  tracesSampleRate: SENTRY_TRACING_SAMPLE_RATE,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
--- src/contexts/relay/relayFetchFunction.ts
+++ src/contexts/relay/relayFetchFunction.ts
@@ -1,5 +1,7 @@
-import { FetchFunction, GraphQLResponse } from 'relay-runtime';
+import { FetchFunction, GraphQLResponse, RequestParameters } from 'relay-runtime';
 import { _fetch, baseurl } from 'contexts/swr/fetch';
+import { startTransaction, getCurrentHub } from '@sentry/nextjs';
+import { Transaction } from '@sentry/types';
 
 /**
  * This is how Relay takes an arbitrary GraphQL request, and asks for a response.
@@ -7,7 +9,11 @@ import { _fetch, baseurl } from 'contexts/swr/fetch';
  * an example.
  */
 export const relayFetchFunction: FetchFunction = async (request, variables) => {
-  return _fetch<GraphQLResponse>(`${baseurl}/glry/graphql/query`, {
+  // ---------- begin pre-request hooks
+  const transaction = initSentryTracing(request);
+  // ---------- end pre-request hooks
+
+  const response = _fetch<GraphQLResponse>(`${baseurl}/glry/graphql/query`, {
     body: {
       query: request.text,
       variables,
@@ -16,4 +22,36 @@ export const relayFetchFunction: FetchFunction = async (request, variables) => {
       'Content-Type': 'application/json',
     },
   });
+
+  // ---------- begin post-request hooks
+  teardownSentryTracing(transaction);
+  // ---------- end post-request hooks
+
+  return response;
 };
+
+/**
+ * Configure sentry tracing.
+ *
+ * Currently, we have Browser Tracing handled in `sentry.client.config.js`, where Queries that correspond with
+ * page loads and page navigations are tracked out-of-the-box.
+ *
+ * However, there are other requests that are fired outside of simple page events – most notably Mutations.
+ * This middleware enables us to auto-trace Mutations without manually writing them within each hook.
+ *
+ * More on tracing config:
+ * https://docs.sentry.io/platforms/javascript/guides/nextjs/performance/instrumentation/custom-instrumentation
+ */
+function initSentryTracing(request: RequestParameters): Transaction | null {
+  if (request.operationKind === 'mutation') {
+    const transaction = startTransaction({ name: request.name, op: 'mutation' });
+    getCurrentHub().configureScope((scope) => scope.setSpan(transaction));
+    return transaction;
+  }
+
+  return null;
+}
+
+function teardownSentryTracing(transaction: Transaction | null) {
+  transaction?.finish();
+}
