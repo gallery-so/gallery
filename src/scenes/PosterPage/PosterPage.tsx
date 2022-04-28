5fb9f8184b369b8dc613e892858158aeede15a18
jakzaizzat <jakzaizzat@gmail.com>
2022-04-28 12:14:35 +0800

    Increase the z-index of bottom navbar

--- src/scenes/PosterPage/PosterPage.tsx
+++ src/scenes/PosterPage/PosterPage.tsx
@@ -129,7 +129,7 @@ const StyledCallToAction = styled.div<{ hasEnded?: boolean }>`
     align-items: center;
     text-align: ${({ hasEnded }) => (hasEnded ? 'text-center' : 'text-left')};
     position: fixed;
-    z-index: 20;
+    z-index: 30;
     bottom: 0;
     left: 0;
     width: 100%;
