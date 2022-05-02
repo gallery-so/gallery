6aaa7f7bd6a7cbfce90cd3ba641d383b23c9afd6
jakzaizzat <jakzaizzat@gmail.com>
2022-05-02 20:18:41 +0800

    Downgrade synpress

--- cypress/integration/pages/page.ts
+++ cypress/integration/pages/page.ts
@@ -1,14 +1,15 @@
 export default class Page {
   acceptMetamaskAccessRequest() {
-    cy.wait(1000);
-    cy.acceptMetamaskAccess(false);
-    // Handle intermittent issue the metamask popup is not shown
-    if (!cy.isMetamaskWindowActive) {
-      cy.switchToMetamaskWindow();
-      cy.switchToMetamaskNotification();
-    }
-    cy.confirmMetamaskSignatureRequest();
-    cy.switchToCypressWindow();
-    cy.wait(500);
+    cy.acceptMetamaskAccess(true);
+    // cy.wait(1000);
+    // cy.acceptMetamaskAccess(false);
+    // // Handle intermittent issue the metamask popup is not shown
+    // if (!cy.isMetamaskWindowActive) {
+    //   cy.switchToMetamaskWindow();
+    //   cy.switchToMetamaskNotification();
+    // }
+    // cy.confirmMetamaskSignatureRequest();
+    // cy.switchToCypressWindow();
+    // cy.wait(500);
   }
 }
