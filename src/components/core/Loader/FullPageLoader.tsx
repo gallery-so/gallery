a8a47688be436215ba9e9f62c58143196d65b86d
Robinnnnn <rokim8@gmail.com>
2022-05-08 01:58:58 -0400

    refactor FullPageLoader

--- src/components/core/Loader/FullPageLoader.tsx
+++ src/components/core/Loader/FullPageLoader.tsx
@@ -1,10 +1,25 @@
-import Page from '../Page/Page';
+import styled from 'styled-components';
 import CapitalGLoader from './CapitalGLoader';
 
 export default function FullPageLoader() {
   return (
-    <Page centered topPadding>
+    <StyledFullPageLoader>
       <CapitalGLoader />
-    </Page>
+    </StyledFullPageLoader>
   );
 }
+
+const StyledFullPageLoader = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 100vh;
+
+  // the following ensures the loader appears above the nav/footer;
+  // without this, the nav/footer would appear before the main content
+  // if the content takes a while to load.
+  position: relative;
+  z-index: 4;
+  background: white;
+`;
