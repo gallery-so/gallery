55aca081f6543817351842fe9f7f5bf18ef96986
Robinnnnn <rokim8@gmail.com>
2022-05-09 23:52:23 -0400

    implement navbar hover fade in/out

--- src/components/core/Page/constants.ts
+++ src/components/core/Page/constants.ts
@@ -1,6 +1,6 @@
 // Note: These values are the same on purpose! Changing this will require
 // re-thinking how the app centers content between nav + footer
-export const GLOBAL_NAVBAR_HEIGHT = 80;
+export const GLOBAL_NAVBAR_HEIGHT = 72;
 export const GLOBAL_FOOTER_HEIGHT = 80;
 export const GLOBAL_FOOTER_HEIGHT_MOBILE = 134;
 
