04b3f0bb7aeb13eb67fb1498190246243f5bd44c
Robinnnnn <rokim8@gmail.com>
2022-05-10 23:42:20 -0400

    toss unused fullPageHeightWithoutNavbarAndFooter

--- src/contexts/globalLayout/constants.ts
+++ src/contexts/globalLayout/constants.ts
@@ -3,5 +3,3 @@
 export const GLOBAL_NAVBAR_HEIGHT = 72;
 export const GLOBAL_FOOTER_HEIGHT = 80;
 export const GLOBAL_FOOTER_HEIGHT_MOBILE = 134;
-
-export const fullPageHeightWithoutNavbarAndFooter = `calc(100vh - ${GLOBAL_NAVBAR_HEIGHT}px - ${GLOBAL_FOOTER_HEIGHT}px)`;
