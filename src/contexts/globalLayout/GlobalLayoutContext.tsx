cb118a4ff2c125a362963a7ac4e067fcd1b541d9
Robinnnnn <rokim8@gmail.com>
2022-05-12 21:39:40 -0400

    explanatory comments

--- src/contexts/globalLayout/GlobalLayoutContext.tsx
+++ src/contexts/globalLayout/GlobalLayoutContext.tsx
@@ -66,7 +66,7 @@ export const useGlobalLayoutActions = (): GlobalLayoutActions => {
 
 type Props = { children: ReactNode };
 
-// the action that causes the fade
+// the action that triggered the fade animation
 type FadeTriggerType = 'route' | 'scroll' | 'hover';
 
 const GlobalLayoutContextProvider = memo(({ children }: Props) => {
@@ -79,83 +79,85 @@ const GlobalLayoutContextProvider = memo(({ children }: Props) => {
     {}
   );
 
-  const [isPageInSuspenseState, setIsPageInSuspenseState] = useState(false);
-  // storing this in ref to prevent triggering unnecessary re-renders / side-effects
-  const isPageInSuspenseRef = useRef(false);
-
+  // whether the global banner is visible
   const [isBannerVisible, setBannerVisible] = useState(false);
 
-  const [isNavbarVisible, setNavbarVisible] = useState(false);
-  const debounced = useDebounce(isNavbarVisible, 300);
-  const throttled = useThrottle(isNavbarVisible, 300);
-  const debouncedNavbarVisible = useMemo(() => {
-    if (isPageInSuspenseState && isNavbarVisible) {
+  /**
+   * the action that triggered the fade animation.
+   *
+   * there are 3 types of triggers:
+   * - route-based setting determined by `GalleryRoute.tsx`
+   * - scroll-based setting determined by scroll position
+   * - hover-based setting determined by mouse position
+   */
+  const [fadeType, setFadeType] = useState<FadeTriggerType>('route');
+
+  /**
+   * we track whether the child route is in suspense in both React State and Ref:
+   * - `state` is used to determine whether we should debounce or throttle nav animation
+   * - `ref` is used to stabilizing callbacks that areused for scroll, hover handling
+   *
+   * why do we care if the app is in suspense?
+   * - it allows us to disable visibility of the navbar against the loading fallback
+   *
+   * can't we just address the above with z-index?
+   * - no, because when both the loading fallback and the navbar have between 0-1 opacity,
+   *   both elements are displayed on the screen.
+   */
+  const [isPageInSuspenseState, setIsPageInSuspenseState] = useState(false);
+  const isPageInSuspenseRef = useRef(false);
+
+  /**
+   * navbar-related states.
+   *
+   * - `isNavbarEnabled` is the raw state that's set by "dumb" components / interactions
+   *   that simply wanna display / hide the nav, according to its internal logic
+   * - we then need to `debounce` or `throttle` this state because many interactions could be
+   *   fighting to set this state
+   * - the difference between debouncing vs. throttling comes down to whether we need to
+   *   delay the navbar fade animation vs. display it immediately, respectively.
+   * - `wasNavbarVisible` is used to determine whether we should continue displaying the navbar
+   *   across transitions. for example, if the user navigates from /a => /b and the navbar
+   *   is visible in both scenarios, we should keep it in view, without any fade animations.
+   */
+  const [isNavbarEnabled, setIsNavbarEnabled] = useState(false);
+  const debounced = useDebounce(isNavbarEnabled, FADE_TRANSITION_TIME_MS);
+  const throttled = useThrottle(isNavbarEnabled, FADE_TRANSITION_TIME_MS);
+  const isNavbarVisible = useMemo(() => {
+    if (isPageInSuspenseState && isNavbarEnabled) {
       return debounced;
     }
     return throttled;
-  }, [isNavbarVisible, isPageInSuspenseState, throttled, debounced]);
+  }, [isNavbarEnabled, isPageInSuspenseState, throttled, debounced]);
+  const wasNavbarVisible = usePrevious(isNavbarVisible) ?? false;
 
-  const wasNavbarVisible = usePrevious(debouncedNavbarVisible) ?? false;
-
-  // the action that triggered the fade
-  const [fadeType, setFadeType] = useState<FadeTriggerType>('route');
-
-  // whether the route wants the navbar disabled. in this case, scrolling / hovering
-  // should not reveal the navbar
+  // whether the route wants the navbar disabled. this is an override where scrolling / hovering
+  // will not make the navbar appear
   const forcedHiddenByRouteRef = useRef(false);
 
+  // touchscreen devices will have different behavior
   const isTouchscreen = useRef(isTouchscreenDevice());
 
   /**
-   * There is a race condition that arises if a scroll-based fade is triggered
-   * immediately after a route-based fade. We use a timestamp to determine whether
-   * a fade was recently triggered by route change.
+   * There is a race condition that arises if a scroll-based fade is triggered immediately after
+   * a route-based fade, causing the nav to appear prematurely. We use a timestamp to determine
+   * whether an animation was recently triggered by route change. (For some reason, this bug
+   * couldn't be fixed with throttling)
    */
   const lastFadeTriggeredByRouteTimestampRef = useRef(0);
 
-  const fadeNavbarOnScroll = useCallback(() => {
-    // handle override. the route gets ultimate power over whether the navbar is displayed
-    if (forcedHiddenByRouteRef.current) {
-      return;
-    }
-    // if we recently triggered a route transition, ignore scroll-related side effects
-    if (Date.now() - lastFadeTriggeredByRouteTimestampRef.current < 100) {
-      return;
-    }
-    // if we're mid-suspense, don't trigger any scroll-related side effects
-    if (isPageInSuspenseRef.current) {
-      return;
-    }
-    setNavbarVisible(window.scrollY <= GLOBAL_NAVBAR_HEIGHT);
-    setFadeType('scroll');
-  }, []);
-
-  useEffect(() => {
-    window.addEventListener('scroll', fadeNavbarOnScroll);
-    return () => {
-      window.removeEventListener('scroll', fadeNavbarOnScroll);
-    };
-  }, [fadeNavbarOnScroll]);
-
+  //-------------- ROUTE ---------------
   const handleFadeNavbarFromGalleryRoute = useCallback((visible: boolean) => {
-    setNavbarVisible(visible);
     setFadeType('route');
+    setIsNavbarEnabled(visible);
 
     forcedHiddenByRouteRef.current = !visible;
     lastFadeTriggeredByRouteTimestampRef.current = Date.now();
   }, []);
 
-  const handleFadeNavbarOnHover = useCallback((visible: boolean) => {
-    // prevent navbar from being accessible via touch-based hover; otherwise, users trying
-    // to click something near the top of the screen will trigger the navbar instead
-    if (isTouchscreen.current) {
-      return;
-    }
-    // prevent navbar from fading out if user is near the top of the page
-    if (!visible && window.scrollY <= GLOBAL_NAVBAR_HEIGHT) {
-      return;
-    }
-    // handle override. the route gets ultimate power over whether the navbar is displayed
+  //-------------- SCROLL --------------
+  const handleFadeNavbarOnScroll = useCallback(() => {
+    // the route gets ultimate override over whether the navbar is displayed
     if (forcedHiddenByRouteRef.current) {
       return;
     }
@@ -168,13 +170,47 @@ const GlobalLayoutContextProvider = memo(({ children }: Props) => {
       return;
     }
 
-    setNavbarVisible(visible);
+    setFadeType('scroll');
+    setIsNavbarEnabled(window.scrollY <= GLOBAL_NAVBAR_HEIGHT);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleFadeNavbarOnScroll);
+    return () => {
+      window.removeEventListener('scroll', handleFadeNavbarOnScroll);
+    };
+  }, [handleFadeNavbarOnScroll]);
+
+  //-------------- HOVER ---------------
+  const handleFadeNavbarOnHover = useCallback((visible: boolean) => {
+    // handle override. the route gets ultimate power over whether the navbar is displayed
+    if (forcedHiddenByRouteRef.current) {
+      return;
+    }
+    // if we recently triggered a route transition, ignore hover-related side effects
+    if (Date.now() - lastFadeTriggeredByRouteTimestampRef.current < 100) {
+      return;
+    }
+    // if we're mid-suspense, don't trigger any hover-related side effects
+    if (isPageInSuspenseRef.current) {
+      return;
+    }
+    // prevent touchscreen users from triggering the navbar by tapping near the top of the screen
+    if (isTouchscreen.current) {
+      return;
+    }
+    // prevent nav from fading out if user is near the top of the page and hovers in/out of nav area
+    if (!visible && window.scrollY <= GLOBAL_NAVBAR_HEIGHT) {
+      return;
+    }
+
     setFadeType('hover');
+    setIsNavbarEnabled(visible);
   }, []);
 
   const state = useMemo(
-    () => ({ isNavbarVisible: debouncedNavbarVisible, wasNavbarVisible, isPageInSuspenseRef }),
-    [debouncedNavbarVisible, wasNavbarVisible]
+    () => ({ isNavbarVisible, wasNavbarVisible, isPageInSuspenseRef }),
+    [isNavbarVisible, wasNavbarVisible]
   );
 
   const actions: GlobalLayoutActions = useMemo(
@@ -193,7 +229,7 @@ const GlobalLayoutContextProvider = memo(({ children }: Props) => {
     <>
       <GlobalNavbarWithFadeEnabled
         queryRef={query}
-        isVisible={debouncedNavbarVisible}
+        isVisible={isNavbarVisible}
         isBannerVisible={isBannerVisible}
         wasVisible={wasNavbarVisible}
         fadeType={fadeType}
@@ -212,19 +248,19 @@ const GlobalLayoutContextProvider = memo(({ children }: Props) => {
 type GlobalNavbarWithFadeEnabledProps = {
   queryRef: GlobalLayoutContextNavbarFragment$key;
   isVisible: boolean;
-  isBannerVisible: boolean;
   wasVisible: boolean;
-  fadeType: FadeTriggerType;
   handleFadeNavbarOnHover: (visible: boolean) => void;
+  fadeType: FadeTriggerType;
+  isBannerVisible: boolean;
 };
 
 function GlobalNavbarWithFadeEnabled({
   queryRef,
   isVisible,
   wasVisible,
-  isBannerVisible,
-  fadeType,
   handleFadeNavbarOnHover,
+  fadeType,
+  isBannerVisible,
 }: GlobalNavbarWithFadeEnabledProps) {
   const query = useFragment(
     graphql`
@@ -236,14 +272,15 @@ function GlobalNavbarWithFadeEnabled({
     queryRef
   );
 
+  // transition styles on the navbar are in accordance with our route transitions
   const transitionStyles = useMemo(() => {
-    // FADING OUT
+    //----------- FADING OUT -----------
     // always fade out navbar without delay
     if (wasVisible) {
       return `opacity ${FADE_TRANSITION_TIME_MS}ms ease-in-out;`;
     }
 
-    // FADING IN
+    //----------- FADING IN ------------
     if (!wasVisible) {
       // if scrolling, fade-in navbar without delay
       if (fadeType === 'scroll' || fadeType === 'hover') {
@@ -251,11 +288,7 @@ function GlobalNavbarWithFadeEnabled({
       }
       // if moving between routes, fade-in navbar with delay
       if (fadeType === 'route') {
-        // return `opacity ${FADE_TRANSITION_TIME_MS}ms ease-in-out`;
         return `opacity ${FADE_TRANSITION_TIME_MS}ms ease-in-out ${NAVIGATION_TRANSITION_TIME_MS}ms`;
-        // return `opacity ${FADE_TRANSITION_TIME_MS}ms ease-in-out ${
-        //   NAVIGATION_TRANSITION_TIME_MS + 300
-        // }ms`;
       }
     }
   }, [wasVisible, fadeType]);
@@ -266,16 +299,16 @@ function GlobalNavbarWithFadeEnabled({
     if (!isVisible) {
       if (
         // for touch-enabled devices, we need to keep the upper area of the screen clickable;
-        // without this, clicking on the upper area would reveal the nav
+        // without this, users wouldn't be able to click near the top of the device
         isTouchscreen.current ||
         // if the route doesn't want the navbar to be in view, we need to remove it completely
         // so that the user can click on elements where the navbar would be
         fadeType === 'route'
       ) {
         // this is normally as simple as setting the navbar z-index to -1, but doing so right
-        // away makes it look like the navbar immediately disappears; therefore we need to
-        // delay until the navbar is out of sight.
-        setTimeout(() => setZIndex(-1), 300);
+        // away makes it look like the navbar vanishes immediately; therefore we add a delay
+        // until the navbar has already faded out of sight.
+        setTimeout(() => setZIndex(-1), FADE_TRANSITION_TIME_MS);
       }
     }
     setZIndex(2);
@@ -300,13 +333,16 @@ function GlobalNavbarWithFadeEnabled({
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
       >
-        {isBannerVisible ? (
-          isFeatureEnabled(FeatureFlag.POSTER_PAGE) ? (
-            <PosterBanner queryRef={query} />
-          ) : (
-            <Banner text="" queryRef={query} />
-          )
-        ) : null}
+        {
+          // we'll re-think the behavior of this banner. in the meantime, if enabled, it'll appear over the banner
+          isBannerVisible ? (
+            isFeatureEnabled(FeatureFlag.POSTER_PAGE) ? (
+              <PosterBanner queryRef={query} />
+            ) : (
+              <Banner text="" queryRef={query} />
+            )
+          ) : null
+        }
         <GlobalNavbar queryRef={query} />
       </StyledGlobalNavbarWithFadeEnabled>
     </>
