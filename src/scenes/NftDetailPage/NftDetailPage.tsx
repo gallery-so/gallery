f055b4b9fdf5f604fc2d604c6ac09557b5f4e115
Robinnnnn <rokim8@gmail.com>
2022-05-20 00:13:33 -0400

    ensure proper routing

--- src/scenes/NftDetailPage/NftDetailPage.tsx
+++ src/scenes/NftDetailPage/NftDetailPage.tsx
@@ -95,9 +95,8 @@ function NftDetailPage({ nftId: initialNftId, collectionId: initialCollectionId 
     return { selectedNftIndex: index, selectedNft: collection[index] };
   }, [nftId, collection]);
 
-  const {
-    query: { username },
-  } = useRouter();
+  const { query: urlQuery, push, pathname } = useRouter();
+  const { username } = urlQuery;
 
   if (!username || Array.isArray(username)) {
     throw new Error('something has gone horribly wrong!');
@@ -127,14 +126,30 @@ function NftDetailPage({ nftId: initialNftId, collectionId: initialCollectionId 
     ])
   );
 
+  // TODO: write comment
+  const pushToNftById = useCallback(
+    (nftId: string) => {
+      const querystring = new URLSearchParams(urlQuery as Record<string, string>).toString();
+      const currentLocation = `${pathname}?${querystring}`;
+      push(
+        // href
+        currentLocation,
+        // as
+        `/${username}/${initialCollectionId}/${nftId}`
+      );
+    },
+    [initialCollectionId, pathname, push, urlQuery, username]
+  );
+
   const handleNextPress = useCallback(() => {
     if (nextNft) {
       setNftId(nextNft.nft.dbid);
       setMountedNfts((prevMountedNfts) => {
         return shiftNftCarousel(Directions.RIGHT, prevMountedNfts, selectedNftIndex, collection);
       });
+      pushToNftById(nextNft.nft.dbid);
     }
-  }, [collection, nextNft, selectedNftIndex]);
+  }, [collection, nextNft, pushToNftById, selectedNftIndex]);
 
   const handlePrevPress = useCallback(() => {
     if (prevNft) {
