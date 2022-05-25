937650dbb7e522353969921af687ac96953e71ef
robin <rokim8@gmail.com>
2022-05-25 15:47:47 -0400

    Increase modal, NFT detail transition pixels (#780)

--- src/components/core/transitions.ts
+++ src/components/core/transitions.ts
@@ -1,7 +1,7 @@
 export const ANIMATED_COMPONENT_TRANSITION_MS = 300;
 export const ANIMATED_COMPONENT_TIMEOUT_MS = 4000;
 // translation for larger items like modals, popovers
-export const ANIMATED_COMPONENT_TRANSLATION_PIXELS_LARGE = 12;
+export const ANIMATED_COMPONENT_TRANSLATION_PIXELS_LARGE = 16;
 // translation for smaller items like tooltips
 export const ANIMATED_COMPONENT_TRANSLATION_PIXELS_SMALL = 8;
 
