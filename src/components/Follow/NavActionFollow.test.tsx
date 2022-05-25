1ff6528bc9e2a3227bfd4942ec84282b8f6e8cb1
Terence Bezman <terence@Terences-Mac-Studio.local>
2022-05-24 20:56:57 -0700

    create sample test

--- src/components/Follow/NavActionFollow.test.tsx
+++ src/components/Follow/NavActionFollow.test.tsx
@@ -1,36 +1,90 @@
-import { render, cleanup } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import NavActionFollow from './NavActionFollow';
-import { createMockEnvironment, MockPayloadGenerator } from 'relay-test-utils';
-import { useFragment, useLazyLoadQuery, useRelayEnvironment } from 'react-relay/hooks';
-// import { graphql } from 'react-relay';
+import { useLazyLoadQuery } from 'react-relay/hooks';
+import { graphql } from 'react-relay';
+import { RelayProvider } from 'contexts/relay/RelayProvider';
+import { Suspense } from 'react';
+import { NavActionFollowTestQuery } from '__generated__/NavActionFollowTestQuery.graphql';
+import nock from 'nock';
+import { baseurl } from 'contexts/swr/fetch';
+import { NavActionFollowTestQueryQuery } from 'src/__generated__/operations';
+import ModalProvider from 'contexts/modal/ModalContext';
 
-import graphql from 'babel-plugin-relay/macro';
-
-const mockResolver = {
-  User: (): any => ({
-    id: 'id',
-  }),
-};
-
-afterEach(cleanup);
-test('', () => {
-  // const Environment = useRelayEnvironment();
-  const environment = createMockEnvironment();
-
-  const data = useLazyLoadQuery(
+const Fixture = () => {
+  const query = useLazyLoadQuery<NavActionFollowTestQuery>(
     graphql`
-      query NavActionFollowTestQuery @relay_test_operation {
+      query NavActionFollowTestQuery {
+        viewer {
+          ... on Viewer {
+            user {
+              ...NavActionFollowFragment
+            }
+          }
+        }
+
         ...NavActionFollowQueryFragment
       }
     `,
     {}
   );
 
-  const { getByText } = render(<NavActionFollow userRef={queryRef.user} queryRef={queryRef} />);
+  if (!query.viewer?.user) {
+    throw new Error('Yikes, should have had a user');
+  }
 
-  environment.mock.resolveMostRecentOperation((operation) =>
-    MockPayloadGenerator.generate(operation, mockResolver)
+  return <NavActionFollow userRef={query.viewer.user} queryRef={query} />;
+};
+
+test.only('it works', async () => {
+  const response: NavActionFollowTestQueryQuery = {
+    __typename: 'Query',
+    viewer: {
+      __typename: 'Viewer',
+      user: {
+        __typename: 'GalleryUser',
+        dbid: 'some_user_dbid',
+        id: 'some_user_id',
+        following: [
+          {
+            __typename: 'GalleryUser',
+            username: 'some username',
+            dbid: 'some_follower_user_dbid',
+            bio: 'soem value',
+
+            // @ts-expect-error
+            id: 'some_follower_user_id',
+          },
+        ],
+        followers: [
+          {
+            __typename: 'GalleryUser',
+            id: 'some_follower_user_id',
+            username: 'some username',
+            dbid: 'some_follower_user_dbid',
+            bio: 'soem value',
+          },
+        ],
+      },
+
+      id: 'some_viewer_id',
+    },
+  };
+
+  nock(baseurl).post('/glry/graphql/query').reply(200, { data: response });
+
+  const { debug } = render(
+    <RelayProvider>
+      <ModalProvider>
+        <Suspense fallback={null}>
+          <Fixture />
+        </Suspense>
+      </ModalProvider>
+    </RelayProvider>
   );
+
+  await new Promise((resolve) => setTimeout(resolve, 500));
+
+  debug();
 });
 
 // describe('NavActionFollow', () => {
