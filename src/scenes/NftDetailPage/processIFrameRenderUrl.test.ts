9ddab03053da4bb678f1ea2ca4d8bafe26e6a0e0
Robinnnnn <rokim8@gmail.com>
2022-05-30 01:23:28 -0400

    fix test

--- src/scenes/NftDetailPage/processIFrameRenderUrl.test.ts
+++ src/scenes/NftDetailPage/processIFrameRenderUrl.test.ts
@@ -4,14 +4,18 @@ describe('processIFrameRenderUrl', () => {
   describe('youtube', () => {
     it('handles youtube URLs', () => {
       const result = processIFrameRenderUrl('https://www.youtube.com/watch?v=FthSdzBltP4');
-      expect(result).toEqual('https://www.youtube.com/embed/FthSdzBltP4?autoplay=1');
+      expect(result).toEqual(
+        'https://www.youtube.com/embed/FthSdzBltP4?autoplay=1&controls=0&loop=1&modestbranding=1'
+      );
     });
 
     it('handles youtube URLs with params', () => {
       const result = processIFrameRenderUrl(
         'https://www.youtube.com/watch?v=FthSdzBltP4&chain=ethereum'
       );
-      expect(result).toEqual('https://www.youtube.com/embed/FthSdzBltP4?chain=ethereum&autoplay=1');
+      expect(result).toEqual(
+        'https://www.youtube.com/embed/FthSdzBltP4?chain=ethereum&autoplay=1&controls=0&loop=1&modestbranding=1'
+      );
     });
 
     it('ignores other URLs', () => {
