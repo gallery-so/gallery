92f5b6632246e6a63d733f978663615aad038003
kaito <80802871+kaitoo1@users.noreply.github.com>
2022-06-02 23:52:39 +0900

    change to 0

--- src/scenes/UserGalleryPage/UserGalleryCollection.tsx
+++ src/scenes/UserGalleryPage/UserGalleryCollection.tsx
@@ -163,7 +163,6 @@ function UserGalleryCollection({ queryRef, collectionRef, mobileLayout }: Props)
         </StyledCollectionTitleWrapper>
         {unescapedCollectorsNote && (
           <>
-            <Spacer height={4} />
             <StyledCollectorsNote>
               <Markdown text={unescapedCollectorsNote} />
             </StyledCollectorsNote>
