c01fb7eeebdda2c10fe99c6757c86345f44a4a00
jakzaizzat <jakzaizzat@gmail.com>
2022-06-03 00:05:03 +0800

    Added opensea asset url

--- src/constants/poster.ts
+++ src/constants/poster.ts
@@ -3,3 +3,4 @@ import { GALLERY_MEMORABILIA_CONTRACT_ADDRESS } from 'hooks/useContract';
 export const MINT_DATE = '2022-06-18T23:00:00-04:00'; // time is in EST (GMT-04:00)
 export const NFT_TOKEN_ID = 2;
 export const OPENSEA_URL = `https://testnets.opensea.io/assets/rinkeby/${GALLERY_MEMORABILIA_CONTRACT_ADDRESS}/${NFT_TOKEN_ID}`;
+export const OPENSEA_RINKEBY_URL = `https://rinkeby.opensea.io/assets/rinkeby/${GALLERY_MEMORABILIA_CONTRACT_ADDRESS}/${NFT_TOKEN_ID}`;
