9a129dc90dfc2e9956aecc8509f6c1df738fb0cb
jakzaizzat <jakzaizzat@gmail.com>
2022-06-08 09:34:05 +0800

    Remove the fallback value

--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -8,14 +8,11 @@ import { network } from 'connectors/index';
 import { useActiveWeb3React } from './useWeb3';
 
 export const PREMIUM_MEMBERSHIP_CONTRACT_ADDRESS =
-  process.env.NEXT_PUBLIC_PREMIUM_MEMBERSHIP_CONTRACT_ADDRESS ||
-  '0xe01569ca9b39E55Bc7C0dFa09F05fa15CB4C7698';
+  process.env.NEXT_PUBLIC_PREMIUM_MEMBERSHIP_CONTRACT_ADDRESS;
 export const GENERAL_MEMBERSHIP_CONRTACT_ADDRESS =
-  process.env.NEXT_PUBLIC_GENERAL_MEMBERSHIP_CONTRACT_ADDRESS ||
-  '0xe3d0fe9b7e0b951663267a3ed1e6577f6f79757e';
+  process.env.NEXT_PUBLIC_GENERAL_MEMBERSHIP_CONTRACT_ADDRESS;
 export const GALLERY_MEMENTOS_CONTRACT_ADDRESS =
-  process.env.NEXT_PUBLIC_GALLERY_MEMENTOS_CONTRACT_ADDRESS ||
-  '0x35ae1f85389D2bbd48113909DA6182304952f809';
+  process.env.NEXT_PUBLIC_GALLERY_MEMENTOS_CONTRACT_ADDRESS;
 
 // account is not optional
 function getSigner(library: Web3Provider, account: string): JsonRpcSigner {
@@ -58,13 +55,14 @@ function useContract(address: string, abi: any) {
 }
 
 export function usePremiumMembershipCardContract() {
-  return useContract(PREMIUM_MEMBERSHIP_CONTRACT_ADDRESS, PREMIUM_MEMBERSHIP_CONTRACT_ABI);
+  return useContract(PREMIUM_MEMBERSHIP_CONTRACT_ADDRESS || '', PREMIUM_MEMBERSHIP_CONTRACT_ABI);
 }
 
 export function useGeneralMembershipCardContract() {
-  return useContract(GENERAL_MEMBERSHIP_CONRTACT_ADDRESS, GENERAL_MEMBERSHIP_CONTRACT_ABI);
+  console.log(GENERAL_MEMBERSHIP_CONRTACT_ADDRESS);
+  return useContract(GENERAL_MEMBERSHIP_CONRTACT_ADDRESS || '', GENERAL_MEMBERSHIP_CONTRACT_ABI);
 }
 
 export function useMintPosterContract() {
-  return useContract(GALLERY_MEMENTOS_CONTRACT_ADDRESS, GALLERY_MEMENTOS_CONTRACT_ABI);
+  return useContract(GALLERY_MEMENTOS_CONTRACT_ADDRESS || '', GALLERY_MEMENTOS_CONTRACT_ABI);
 }
