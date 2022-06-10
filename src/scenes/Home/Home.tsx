693ce88bcf9a28bffb2881854298c0c9ecf35998
robin <rokim8@gmail.com>
2022-06-10 11:15:40 -0400

    [gotw] walt (#827)

--- src/scenes/Home/Home.tsx
+++ src/scenes/Home/Home.tsx
@@ -29,7 +29,7 @@ function Home() {
         <Spacer width={8} />
         <BaseS>·</BaseS>
         <Spacer width={8} />
-        <NavLink to="/laury">Gallery of the Week</NavLink>
+        <NavLink to="/walt">Gallery of the Week</NavLink>
       </StyledLinkContainer>
     </StyledHomePage>
   );
