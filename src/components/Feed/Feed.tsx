eb5c80836f1af14a31185ad0b3513dad0c4e2dc3
kaito <80802871+kaitoo1@users.noreply.github.com>
2022-06-17 15:43:00 +0900

    add fade in

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -1,7 +1,11 @@
 import ActionText from 'components/core/ActionText/ActionText';
+import colors from 'components/core/colors';
+import { fadeIn } from 'components/core/keyframes';
 import Spacer from 'components/core/Spacer/Spacer';
+import transitions from 'components/core/transitions';
 import NavbarGLink from 'components/NavbarGLink';
 import { useGlobalLayoutActions } from 'contexts/globalLayout/GlobalLayoutContext';
+import { useRouter } from 'next/router';
 import { useCallback, useEffect, useState } from 'react';
 import styled from 'styled-components';
 
@@ -18,6 +22,11 @@ function FeedNavbarControl({ setFeedMode, initialFeedMode }: ControlProps) {
   // internally track the feed mode state so we can avoid adding it to the dep array
   const [feedModeCopy, setFeedModeCopy] = useState<FeedMode>(initialFeedMode);
 
+  const { pathname } = useRouter();
+  useEffect(() => {
+    console.log(pathname);
+  }, [pathname]);
+
   const handleFollowingModeClick = useCallback(() => {
     setFeedMode(FOLLOWING);
     setFeedModeCopy(FOLLOWING);
@@ -30,27 +39,59 @@ function FeedNavbarControl({ setFeedMode, initialFeedMode }: ControlProps) {
 
   return (
     <StyledFeedNavbarControl>
-      <ActionText onClick={handleFollowingModeClick} focused={feedModeCopy === FOLLOWING}>
-        Following
-      </ActionText>
+      <StyledTextWrapperLeft>
+        <StyledNavControlText
+          onClick={handleFollowingModeClick}
+          focused={feedModeCopy === FOLLOWING}
+        >
+          Following
+        </StyledNavControlText>
+      </StyledTextWrapperLeft>
       <Spacer width={10} />
       <NavbarGLink />
       <Spacer width={10} />
-      <ActionText onClick={handleWorldwideModeClick} focused={feedModeCopy === WORLDWIDE}>
-        Worldwide
-      </ActionText>
+      <StyledTextWrapper>
+        <StyledNavControlText
+          onClick={handleWorldwideModeClick}
+          focused={feedModeCopy === WORLDWIDE}
+        >
+          Worldwide
+        </StyledNavControlText>
+      </StyledTextWrapper>
     </StyledFeedNavbarControl>
   );
 }
+
+const StyledNavControlText = styled(ActionText)<{ focused: boolean }>`
+  animation: ${fadeIn} ${transitions.cubic} 700ms 1 forwards;
+  color: ${({ focused }) => (focused ? colors.offBlack : colors.metal)};
+
+  opacity: 0;
+
+  // transition: opacity ${transitions.cubic} ease-in-out;
+`;
+
+const StyledTextWrapper = styled.div`
+  display: flex;
+  flex: 1;
+`;
+
+const StyledTextWrapperLeft = styled(StyledTextWrapper)`
+  justify-content: flex-end;
+`;
 
 const StyledFeedNavbarControl = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  width: 100%;
 `;
+
+// - signing out should remove "Following/Worldwide" from the navbar
 
 export default function Feed() {
   const [feedMode, setFeedMode] = useState<FeedMode>(WORLDWIDE);
+  // const [startFadeOut, setStartFadeout] = useState(false);
 
   const { setCustomNavCenterContent } = useGlobalLayoutActions();
   useEffect(() => {
@@ -59,7 +100,9 @@ export default function Feed() {
     );
 
     return () => {
-      setCustomNavCenterContent(null);
+      setTimeout(() => {
+        setCustomNavCenterContent(null);
+      }, 200);
     };
     // don't add feedMode to the dep array. we only pass it in to set the initial state for the nav control, so we don't call setCustomNavCenterContent every time the feed mode changes
     // eslint-disable-next-line react-hooks/exhaustive-deps
