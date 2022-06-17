c1b06c215a8400268b816a15bbe88accde421ab9
Robinnnnn <rokim8@gmail.com>
2022-06-17 02:11:43 -0400

    oops

--- pages/maintenance.tsx
+++ pages/maintenance.tsx
@@ -1,6 +1,6 @@
 import MaintenancePage from 'scenes/MaintenancePage/MaintenancePage';
 import GalleryRoute from 'scenes/_Router/GalleryRoute';
 
-export default function Nuke() {
+export default function Maintenance() {
   return <GalleryRoute element={<MaintenancePage />} navbar={false} />;
 }
