5bce844073d7c9659545967096eaae764415a7b4
kaito <80802871+kaitoo1@users.noreply.github.com>
2022-06-21 13:18:04 +0900

    remove maintenance banner (#844)

--- src/constants/storageKeys.ts
+++ src/constants/storageKeys.ts
@@ -1,4 +1,3 @@
 export const GNOSIS_NONCE_STORAGE_KEY = 'gallery_gnosis_nonce';
 export const MOBILE_GALLERY_LAYOUT_STORAGE_KEY = 'gallery_mobile_gallery_layout';
 export const USER_SIGNIN_ADDRESS_LOCAL_STORAGE_KEY = 'gallery_user_signin_address';
-export const MAINTENANCE_BANNER_6_18_STORAGE_KEY = 'gallery_maintenance_banner_6_18_storage_key';
