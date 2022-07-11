3e823ef91b6df667d797714719bcde73aca5c8af
robin <rokim8@gmail.com>
2022-07-11 15:38:57 -0400

    Stabilize auth query (#886)

--- pages/welcome.tsx
+++ pages/welcome.tsx
@@ -1,5 +1,6 @@
 import OnboardingFlow from 'flows/OnboardingFlow/OnboardingFlow';
 import useAuthPayloadQuery from 'hooks/api/users/useAuthPayloadQuery';
+import { useRef } from 'react';
 import GalleryRedirect from 'scenes/_Router/GalleryRedirect';
 import GalleryRoute from 'scenes/_Router/GalleryRoute';
 
@@ -7,7 +8,10 @@ export default function Welcome() {
   // The onboarding flow can only be accessed if the user has been redirected from the auth pipeline,
   // which populates the internal URL queries with necessary data to create a profile
   const authPayloadQuery = useAuthPayloadQuery();
-  if (!authPayloadQuery) {
+  // Stabilize the return value; otherwise the user gets kicked out to the homepage at the tail end
+  // of the welcome flow as the query changes
+  const stableQuery = useRef(authPayloadQuery);
+  if (!stableQuery) {
     return <GalleryRedirect to="/" />;
   }
 
