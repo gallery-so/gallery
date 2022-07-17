1d141e4d1428ad1f7de5f08e2a984fd25fe72a41
Robinnnnn <rokim8@gmail.com>
2022-07-17 17:00:35 -0400

    add 10 col user ids

--- src/contexts/collectionEditor/useMaxColumns.ts
+++ src/contexts/collectionEditor/useMaxColumns.ts
@@ -2,6 +2,11 @@ import { useFragment } from 'react-relay';
 import { graphql } from 'relay-runtime';
 import { useMaxColumnsFragment$key } from '__generated__/useMaxColumnsFragment.graphql';
 
+const USER_IDS_WITH_10_COLUMNS_ENABLED = [
+  'a3ff91986625382ff776067619200efe',
+  '2BzTRFDU9wuQCYW6E62cY6J6XvU',
+];
+
 export default function useMaxColumns(viewerRef: useMaxColumnsFragment$key) {
   const viewer = useFragment(
     graphql`
@@ -15,7 +20,7 @@ export default function useMaxColumns(viewerRef: useMaxColumnsFragment$key) {
   );
 
   // allow larger # columns for figure31 user profile
-  if (viewer.user?.dbid === 'a3ff91986625382ff776067619200efe') {
+  if (USER_IDS_WITH_10_COLUMNS_ENABLED.includes(viewer.user?.dbid ?? '')) {
     return 10;
   }
 
