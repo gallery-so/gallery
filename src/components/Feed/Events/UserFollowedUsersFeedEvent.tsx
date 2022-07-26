6231f4a646d2b9248f281bf4cb170192138f85e4
jakzaizzat <jakzaizzat@gmail.com>
2022-07-26 10:12:46 +0800

    Fix query ref

--- src/components/Feed/Events/UserFollowedUsersFeedEvent.tsx
+++ src/components/Feed/Events/UserFollowedUsersFeedEvent.tsx
@@ -61,6 +61,7 @@ export default function UserFollowedUsersFeedEvent({ eventRef, queryRef, feedMod
             }
           }
         }
+        ...FollowButtonQueryFragment
         ...HoverCardOnUsernameFollowFragment
       }
     `,
