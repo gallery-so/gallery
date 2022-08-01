65593ffcfdcde20177aeebb1da81b347847e1996
Robinnnnn <rokim8@gmail.com>
2022-08-01 15:57:28 -0400

    stroke-miterlimit => strokeMiterLimit

--- src/icons/Video.tsx
+++ src/icons/Video.tsx
@@ -31,12 +31,12 @@ export default function Video({
       <path
         d="M10.6666 8.00012L14.3333 11.0001V5.00012L10.6666 8.00012Z"
         stroke={color}
-        stroke-miterlimit="10"
+        strokeMiterlimit="10"
       />
       <path
         d="M2.33337 3.66675H9.66671L10.6667 4.66675V11.3334L9.66671 12.3334H2.33337L1.33337 11.3334V4.66675L2.33337 3.66675Z"
         stroke={color}
-        stroke-miterlimit="10"
+        strokeMiterlimit="10"
       />
     </svg>
   );
