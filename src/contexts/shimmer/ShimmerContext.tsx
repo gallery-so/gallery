222dbf18f09d91f830c591437a994415cadcdef2
Robinnnnn <rokim8@gmail.com>
2022-08-11 02:44:25 -0400

    use inherit width on shimmer context; update aspect ratio to use naturalWidth, naturalHeight

--- src/contexts/shimmer/ShimmerContext.tsx
+++ src/contexts/shimmer/ShimmerContext.tsx
@@ -10,7 +10,7 @@ import {
 } from 'react';
 import styled from 'styled-components';
 
-type AspectRatio = 'wide' | 'square' | 'tall';
+type AspectRatio = 'wide' | 'square' | 'tall' | 'unknown';
 
 type ShimmerState = {
   aspectRatio: null | number;
@@ -62,17 +62,22 @@ const ShimmerProvider = memo(({ children }: Props) => {
     () => ({
       setContentIsLoaded: (event?: SyntheticEvent) => {
         if (event) {
-          // @ts-expect-error: issues with TS image onload event
-          const aspectRatio = event.target.width / event.target.height;
+          // @ts-expect-error: the element that fires the event on load may be different (e.g. image vs. video vs. iframe).
+          // `naturalWidth` and `naturalHeight` are available on images and represent their original dimensions, whereas
+          // `width` and `height` are the literal dimensions that are rendered on the page.
+          const aspectRatio = event.target?.naturalWidth / event.target?.naturalHeight;
           if (aspectRatio === 1) {
             setAspectRatioType('square');
           } else if (aspectRatio > 1) {
             setAspectRatioType('wide');
           } else if (aspectRatio < 1) {
             setAspectRatioType('tall');
+          } else {
+            setAspectRatioType('unknown');
           }
 
-          setAspectRatio(aspectRatio);
+          // if an aspect ratio is indeterminate, we give it a square viewport
+          setAspectRatio(aspectRatio || 1);
         }
 
         setIsLoaded(true);
@@ -112,12 +117,12 @@ type VisibleProps = { visible: boolean };
 const StyledShimmerComponent = styled.div<VisibleProps>`
   position: absolute;
   display: ${({ visible }) => (visible ? 'block' : 'none')};
-  width: 100%;
+  width: inherit;
 `;
 
 const StyledChildren = styled.div<VisibleProps>`
   height: 100%;
-  width: 100%;
+  width: inherit;
   display: flex;
   justify-content: center;
   align-items: center;
