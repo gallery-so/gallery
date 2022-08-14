59dc581f9fa00882bb19c3be95a75c9291a25436
Connor Rothschild <connorrothschild@gmail.com>
2022-08-14 18:39:03 -0500

    new landing page + shop button

--- src/components/GalleryTitleIntro/GalleryTitleIntro.tsx
+++ src/components/GalleryTitleIntro/GalleryTitleIntro.tsx
@@ -1,21 +1,11 @@
-import styled from 'styled-components';
 import { TitleM } from 'components/core/Text/Text';
-import Spacer from 'components/core/Spacer/Spacer';
 
 function GalleryTitleIntro() {
   return (
-    <>
-      <StyledLogo src="/icons/logo-large.svg" />
-      <Spacer height={8} />
-      <TitleM>
-        <strong>Share your </strong>collection <strong>with the world.</strong>
-      </TitleM>
-    </>
+    <TitleM>
+      <strong>Share your </strong>collection <strong>with the world.</strong>
+    </TitleM>
   );
 }
-
-const StyledLogo = styled.img`
-  height: 32px;
-`;
 
 export default GalleryTitleIntro;
