84f009a934a940e62b8643f22bd6163f35c104aa
Jakz <jakzaizzat@gmail.com>
2022-08-18 17:07:02 +0800

    [GAL-127] Render links in community page description (#953)
    * render url in community description
    
    * Move to util function

--- src/scenes/CommunityPage/CommunityPageView.tsx
+++ src/scenes/CommunityPage/CommunityPageView.tsx
@@ -12,6 +12,7 @@ import { useCallback, useEffect, useRef, useState } from 'react';
 import TextButton from 'components/core/Button/TextButton';
 import breakpoints from 'components/core/breakpoints';
 import TokenHolderList from 'components/TokenHolderList/TokenHolderList';
+import formatUrl from 'utils/formatUrl';
 
 type Props = {
   communityRef: CommunityPageViewFragment$key;
@@ -57,6 +58,8 @@ export default function CommunityPageView({ communityRef }: Props) {
     }
   }, [descriptionRef]);
 
+  const formattedDescription = formatUrl(description || '');
+
   return (
     <MemberListPageProvider>
       <Spacer height={80} />
@@ -66,7 +69,7 @@ export default function CommunityPageView({ communityRef }: Props) {
           <StyledDescriptionWrapper>
             <Spacer height={4} />
             <StyledBaseM showExpandedDescription={showExpandedDescription} ref={descriptionRef}>
-              <Markdown text={description} />
+              <Markdown text={formattedDescription} />
             </StyledBaseM>
             <Spacer height={8} />
             {isLineClampEnabled && (
--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -4,3 +4,5 @@ export const NO_CONSECUTIVE_PERIODS_OR_UNDERSCORES = /^(?=[\w.]*$)(?!.*[_.]{2})[
 
 // ends with =s256, =s128, etc.
 export const GOOGLE_CONTENT_IMG_URL = /=s\d{3}$/;
+
+export const VALID_URL = /https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9(@:%_\+.~#?&\/=]*)/;
