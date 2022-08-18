4527593d497f3d8d441e89e5eabb1dc3f5c3747c
Connor Rothschild <connorrothschild@gmail.com>
2022-08-18 11:01:33 -0500

    better error message

--- src/scenes/MerchStorePage/MerchMintButton.tsx
+++ src/scenes/MerchStorePage/MerchMintButton.tsx
@@ -1,6 +1,7 @@
 import GalleryLink from 'components/core/GalleryLink/GalleryLink';
 import Spacer from 'components/core/Spacer/Spacer';
-import ErrorText from 'components/core/Text/ErrorText';
+// import ErrorText from 'components/core/Text/ErrorText';
+import colors from 'components/core/colors';
 import { BaseM } from 'components/core/Text/Text';
 import { useEffect, useMemo } from 'react';
 import { useToastActions } from 'contexts/toast/ToastContext';
@@ -95,7 +96,8 @@ export default function MintButton({ onMintSuccess, quantity, tokenId }: Props) 
       {error && (
         <>
           {!isMobile && <Spacer height={8} />}
-          <ErrorText message={error} />
+          {/* <ErrorText message={error} /> */}
+          <BaseMError color={colors.error} dangerouslySetInnerHTML={{ __html: error }}></BaseMError>
         </>
       )}
     </>
@@ -117,4 +119,12 @@ const StyledButton = styled(Button)`
 const StyledBaseMWithWrap = styled(BaseM)`
   word-break: break-all;
   width: 100%;
+`;
+
+const BaseMError = styled(BaseM)`
+  color: ${colors.error};
+
+  & a {
+    color: ${colors.error};
+  }
 `;
