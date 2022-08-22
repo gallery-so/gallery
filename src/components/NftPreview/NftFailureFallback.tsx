47040630c1bfa7eb3d57efebaee458c5c4eec8b4
Terence Bezman <terence.bezman@coinbase.com>
2022-08-22 13:13:57 -0700

    add tiny mode for nft failure fallback

--- src/components/NftPreview/NftFailureFallback.tsx
+++ src/components/NftPreview/NftFailureFallback.tsx
@@ -3,14 +3,17 @@ import colors from 'src/components/core/colors';
 import { RefreshIcon } from '../../icons/RefreshIcon';
 import Spacer from 'components/core/Spacer/Spacer';
 import { BaseM } from 'components/core/Text/Text';
-import { useCallback } from 'react';
+import { MouseEventHandler, useCallback } from 'react';
+
+type Size = 'tiny' | 'medium';
 
 type Props = {
   onClick: () => void;
   refreshing: boolean;
+  size?: Size;
 };
 
-export function NftFailureFallback({ onClick, refreshing }: Props) {
+export function NftFailureFallback({ onClick, refreshing, size = 'medium' }: Props) {
   const handleClick = useCallback(() => {
     if (refreshing) {
       return;
@@ -19,10 +22,19 @@ export function NftFailureFallback({ onClick, refreshing }: Props) {
     onClick();
   }, [onClick, refreshing]);
 
+  const spaceY = {
+    tiny: 4,
+    medium: 16,
+  }[size];
+
   return (
     <Wrapper onClick={handleClick}>
-      {refreshing ? <Label>Loading...</Label> : <Label>Could not load</Label>}
-      <Spacer height={16} />
+      {refreshing ? (
+        <Label size={size}>Loading...</Label>
+      ) : (
+        <Label size={size}>Could not load</Label>
+      )}
+      <Spacer height={spaceY} />
       <IconButton>
         <RefreshIcon />
       </IconButton>
@@ -30,11 +42,15 @@ export function NftFailureFallback({ onClick, refreshing }: Props) {
   );
 }
 
-const Label = styled(BaseM)`
+const Label = styled(BaseM)<{ size: Size }>`
   color: ${colors.metal};
+
+  ${({ size }) => (size === 'tiny' ? 'font-size: 10px;' : '')}
+  ${({ size }) => (size === 'tiny' ? 'line-height: 12px;' : '')}
 `;
 
 const IconButton = styled.div`
+  display: flex;
   background: transparent;
 `;
 
@@ -44,7 +60,7 @@ const Wrapper = styled.button`
   // Button resets
   border: none;
   margin: 0;
-  padding: 8px 16px;
+  // padding: 8px 16px;
 
   width: 100%;
 
