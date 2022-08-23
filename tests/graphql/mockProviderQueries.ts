57cf088f0c7fa892ce654741a8cb6d124c451087
Terence Bezman <terence.bezman@coinbase.com>
2022-08-23 13:46:10 -0700

    delete testing stuff

--- tests/graphql/mockProviderQueries.ts
+++
@@ -1,7 +0,0 @@
-import { mockDebuggerQuery } from './mockDebuggerQuery';
-import { mockGlobalLayoutQuery } from './mockGlobalLayoutQuery';
-
-export function mockProviderQueries() {
-  mockDebuggerQuery();
-  mockGlobalLayoutQuery();
-}
