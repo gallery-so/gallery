b31bff7893a6b3143763a9b7582d0a946604a51a
Terence Bezman <terencebezman@gmail.com>
2022-08-25 22:05:47 -0700

    keyBy lodash

--- src/utils/collectionLayout.ts
+++ src/utils/collectionLayout.ts
@@ -13,19 +13,11 @@ type Section<T> = {
 };
 type CollectionWithLayout<T> = Record<string, Section<T>>;
 
-type CollectionLayout = {
-  sections: number[];
-  sectionLayout: Array<{
-    whitespace: number[];
-    columns: number;
-  }>;
-};
-
 // Given a list of tokens in the collection and the collection layout settings,
 // returns an object that represents the full structure of the collection layout with sections, items, and whitespace blocks.
 export function parseCollectionLayout<T>(
   tokens: ReadonlyArray<T>,
-  collectionLayout: CollectionLayout,
+  collectionLayout: any,
   ignoreWhitespace: boolean = false
 ): CollectionWithLayout<T> {
   const parsedCollection = collectionLayout.sections.reduce(
