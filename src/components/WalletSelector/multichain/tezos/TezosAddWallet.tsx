ee3e6fa6064c29070228c6c9d3e9e71f2dbf8b68
jakzaizzat <jakzaizzat@gmail.com>
2022-08-30 16:42:25 +0800

    Added tezos account to existing user

---
+++ src/components/WalletSelector/multichain/tezos/TezosAddWallet.tsx
@@ -0,0 +1,279 @@
+import { Button } from 'components/core/Button/Button';
+import colors from 'components/core/colors';
+import { BaseM, TitleS } from 'components/core/Text/Text';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import styled from 'styled-components';
+import { Web3Error } from 'types/Error';
+import Spacer from 'components/core/Spacer/Spacer';
+import {
+  ADDRESS_ALREADY_CONNECTED,
+  CONFIRM_ADDRESS,
+  INITIAL,
+  PendingState,
+  PROMPT_SIGNATURE,
+} from 'types/Wallet';
+import { useModalActions } from 'contexts/modal/ModalContext';
+import ManageWalletsModal from 'scenes/Modals/ManageWalletsModal';
+import {
+  isEarlyAccessError,
+  useTrackAddWalletAttempt,
+  useTrackAddWalletError,
+  useTrackAddWalletSuccess,
+} from 'contexts/analytics/authUtil';
+import { captureException } from '@sentry/nextjs';
+import { EthereumAddWalletFragment$key } from '__generated__/EthereumAddWalletFragment.graphql';
+import { removeNullValues } from 'utils/removeNullValues';
+import { useFragment } from 'react-relay';
+import { graphql } from 'relay-runtime';
+import useAddWallet from 'components/WalletSelector/mutations/useAddWallet';
+import useCreateNonce from 'components/WalletSelector/mutations/useCreateNonce';
+import { normalizeError } from '../normalizeError';
+import { WalletError } from '../WalletError';
+import { DAppClient } from '@airgap/beacon-sdk';
+import { RequestSignPayloadInput, SigningType } from '@airgap/beacon-types';
+import { char2Bytes } from '@taquito/utils';
+import { generatePayload, getNonceNumber } from './tezosUtils';
+
+type Props = {
+  queryRef: EthereumAddWalletFragment$key;
+  reset: () => void;
+};
+
+let beaconClient: DAppClient;
+
+if (typeof window !== 'undefined') {
+  beaconClient = new DAppClient({ name: 'Gallery' });
+}
+
+// This Pending screen is dislayed after the connector has been activated, while we wait for a signature
+export const TezosAddWallet = ({ queryRef, reset }: Props) => {
+  const [pendingState, setPendingState] = useState<PendingState>(INITIAL);
+  const [isConnecting, setIsConnecting] = useState(false);
+  const [error, setError] = useState<Error>();
+  const [address, setAddress] = useState<string>();
+
+  const query = useFragment(
+    graphql`
+      fragment EthereumAddWalletFragment on Query {
+        viewer {
+          ... on Viewer {
+            user {
+              wallets {
+                chainAddress @required(action: NONE) {
+                  address
+                }
+              }
+            }
+          }
+        }
+
+        ...ManageWalletsModalFragment
+      }
+    `,
+    queryRef
+  );
+
+  const { viewer } = query;
+
+  const authenticatedUserAddresses = useMemo(
+    () => removeNullValues(viewer?.user?.wallets?.map((wallet) => wallet?.chainAddress.address)),
+    [viewer?.user?.wallets]
+  );
+
+  const { showModal } = useModalActions();
+
+  const openManageWalletsModal = useCallback(
+    (address: string) => {
+      showModal({
+        content: <ManageWalletsModal queryRef={query} newAddress={address} />,
+        headerText: 'Connect your wallet',
+      });
+    },
+    [showModal, query]
+  );
+
+  const createNonce = useCreateNonce();
+  const trackAddWalletAttempt = useTrackAddWalletAttempt();
+  const trackAddWalletSuccess = useTrackAddWalletSuccess();
+  const trackAddWalletError = useTrackAddWalletError();
+
+  /**
+   * Add Wallet Pipeline:
+   * 1. Fetch nonce from server with provided wallet address
+   * 2. Sign nonce with wallet (beacon)
+   * 3. Add wallet address to user's account
+   */
+  const addWallet = useAddWallet();
+  const attemptAddWallet = useCallback(
+    async (address: string, publicKey: string) => {
+      try {
+        setIsConnecting(true);
+        setPendingState(PROMPT_SIGNATURE);
+
+        trackAddWalletAttempt('Tezos');
+        const { nonce, user_exists: userExists } = await createNonce(address, 'Tezos');
+
+        if (userExists) {
+          throw { code: 'EXISTING_USER' } as Web3Error;
+        }
+
+        const payload = generatePayload(nonce, address);
+        const { signature } = await beaconClient.requestSignPayload(payload);
+
+        const nonceNumber = getNonceNumber(nonce);
+
+        const { signatureValid } = await addWallet({
+          authMechanism: {
+            eoa: {
+              signature,
+              nonce: nonceNumber,
+              chainPubKey: {
+                pubKey: publicKey,
+                chain: 'Tezos',
+              },
+            },
+          },
+          chainAddress: {
+            address,
+            chain: 'Tezos',
+          },
+        });
+
+        trackAddWalletSuccess('Tezos');
+        openManageWalletsModal(address);
+        setIsConnecting(false);
+
+        return signatureValid;
+      } catch (error) {
+        setIsConnecting(false);
+        trackAddWalletError('Tezos', error);
+        // ignore early access errors
+        if (!isEarlyAccessError(error)) {
+          // capture all others
+          captureException(error);
+        }
+
+        // Fall back to generic error message
+        if (error instanceof Error) {
+          const web3Error: Web3Error = { code: 'AUTHENTICATION_ERROR', ...error };
+          setError(web3Error);
+        } else {
+          setError(normalizeError(error));
+        }
+      }
+    },
+    [
+      trackAddWalletAttempt,
+      createNonce,
+      addWallet,
+      trackAddWalletSuccess,
+      openManageWalletsModal,
+      trackAddWalletError,
+    ]
+  );
+
+  useEffect(() => {
+    async function authenticate() {
+      try {
+        const { publicKey, address } = await beaconClient.requestPermissions();
+        if (!address || !publicKey) return;
+
+        if (authenticatedUserAddresses.includes(address)) {
+          setPendingState(ADDRESS_ALREADY_CONNECTED);
+          return;
+        }
+
+        await attemptAddWallet(address, publicKey);
+      } catch (error) {}
+
+      // if (isMetamask) {
+      //   // For metamask, prompt the user to confirm the address provided by the extension is the one they want to connect with
+      //   setPendingState(CONFIRM_ADDRESS);
+      // }
+    }
+
+    void authenticate();
+  }, [authenticatedUserAddresses, attemptAddWallet]);
+
+  if (error) {
+    return (
+      <WalletError
+        address={address}
+        error={error}
+        reset={() => {
+          setError(undefined);
+          reset();
+        }}
+      />
+    );
+  }
+
+  if (pendingState === ADDRESS_ALREADY_CONNECTED && address) {
+    return (
+      <div>
+        <TitleS>Connect with Ethereum</TitleS>
+        <Spacer height={8} />
+        <BaseM>The following address is already connected to this account:</BaseM>
+        <Spacer height={8} />
+        <BaseM color={colors.offBlack}>{address}</BaseM>
+        {/* {isMetamask && (
+          <>
+            <Spacer height={8} />
+            <BaseM>
+              If you want to connect a different address via Metamask, please switch accounts in the
+              extension and try again.
+            </BaseM>
+          </>
+        )} */}
+      </div>
+    );
+  }
+
+  // right now we only show this case for Metamask
+  //   if (pendingState === CONFIRM_ADDRESS && address) {
+  //     return (
+  //       <div>
+  //         <TitleS>Connect with Ethereum</TitleS>
+  //         <Spacer height={8} />
+  //         <BaseM>Confirm the following wallet address:</BaseM>
+  //         <Spacer height={8} />
+  //         <BaseM color={colors.offBlack}>{address}</BaseM>
+  //         <Spacer height={16} />
+  //         <BaseM>
+  //           If you want to connect a different address via Metamask, please switch accounts in the
+  //           extension and try again.
+  //         </BaseM>
+  //         <Spacer height={24} />
+  //         <StyledButton onClick={() => attemptAddWallet(address)} disabled={isConnecting}>
+  //           {isConnecting ? 'Connecting...' : 'Confirm'}
+  //         </StyledButton>
+  //       </div>
+  //     );
+  //   }
+
+  if (pendingState === PROMPT_SIGNATURE) {
+    return (
+      <div>
+        <TitleS>Connect with Tezos</TitleS>
+        <Spacer height={8} />
+        <BaseM>Sign the message with your wallet.</BaseM>
+      </div>
+    );
+  }
+
+  // Default view for when pendingState === INITIAL
+  return (
+    <div>
+      <TitleS>Connect with Tezos</TitleS>
+      <Spacer height={8} />
+      <BaseM>Approve your wallet to connect to Gallery.</BaseM>
+    </div>
+  );
+};
+
+const StyledButton = styled(Button)`
+  align-self: flex-end;
+  padding: 16px;
+  width: 100%;
+  height: 100%;
+`;
