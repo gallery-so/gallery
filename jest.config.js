4767e4fb82178cea885ae166bcbe5ae85b1fe5e8
Terence Bezman <terencebezman@gmail.com>
2022-08-30 20:52:05 -0700

    [GAL-363] Update Failed NFTs w/o Refreshing Page (#985)
    * update testing dependencies
    
    * use next.js jest config to eliminate babel
    
    * import extend-expect in testse
    
    * add a bunch of graphql test helpers
    
    * add test id to refresh button for tests
    
    * bump gallery codegen
    
    * remove ts nocheck
    
    * add some really shady code to flush relay's fragment resource cache
    
    * add test to ensure things are working
    
    * use unknown instead of any
    
    * update unit test job to run graphql codegen
    
    * include codegen in prod build
    
    * remove extra fragment spread

--- jest.config.js
+++ jest.config.js
@@ -1,35 +1,41 @@
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { pathsToModuleNameMapper } = require('ts-jest');
 const {
   compilerOptions: { paths },
 } = require('./tsconfig.json');
 
-module.exports = {
-  // Have to use babel for tests until next js supports the relay transform for jest compilation
-  transform: {
-    '\\.[jt]sx?$': [
-      'babel-jest',
-      {
-        presets: ['next/babel'],
-        plugins: ['relay'],
-      },
-    ],
-  },
+const nextJest = require('next/jest');
 
-  testEnvironment: 'jsdom',
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+});
 
-  modulePaths: ['<rootDir>'],
+module.exports = async () => {
+  const nextJestConfig = await createJestConfig({
+    setupFilesAfterEnv: ['<rootDir>/tests/setupFileAfterEnv.js'],
+    modulePaths: ['<rootDir>'],
 
-  moduleNameMapper: {
-    // Ensure all ouf our aliases from tsconfig work in jext
-    ...pathsToModuleNameMapper(paths),
+    moduleNameMapper: {
+      // Ensure all ouf our aliases from tsconfig work in jext
+      ...pathsToModuleNameMapper(paths),
 
-    // CSS imports should do nothing
-    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+      // CSS imports should do nothing
+      '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
 
-    // Images get a static string
-    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
+      // Images get a static string
+      '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
 
-    // React Markdown is an ESM Module which makes jest very angry
-    'react-markdown': '<rootDir>/test/__mocks__/react-markdown.js',
-  },
+      // React Markdown is an ESM Module which makes jest very angry
+      'react-markdown': '<rootDir>/test/__mocks__/react-markdown.js',
+    },
+    testEnvironment: 'jest-environment-jsdom',
+  })();
+
+  return {
+    ...nextJestConfig,
+
+    // Rainbow kit ships code which is not transpiled. We have to tell Jest to transpile it
+    // https://stackoverflow.com/questions/55794280/jest-fails-with-unexpected-token-on-import-statement
+    transformIgnorePatterns: ['node_modules/(?!(react-native' + '|@rainbow-me/rainbowkit' + ')/)'],
+  };
 };
---
+++ tests/graphql/mockGraphqlQuery.ts
@@ -0,0 +1,10 @@
+import nock from 'nock';
+import { getGraphqlHost, getGraphqlPath } from '../../src/contexts/relay/relayFetchFunction';
+
+export function mockGraphqlQuery(operationName: string, response: unknown) {
+  return nock(getGraphqlHost())
+    .post(getGraphqlPath(), (body) => body.operationName === operationName)
+    .reply(200, {
+      data: response,
+    });
+}
