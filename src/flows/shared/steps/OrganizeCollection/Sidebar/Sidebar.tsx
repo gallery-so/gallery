6ed51322cefd9819140cc28c205cb60c44af585f
Terence Bezman <terencebezman@gmail.com>
2022-08-31 11:24:14 -0700

    refactor sidebar to not be weird champ

--- src/flows/shared/steps/OrganizeCollection/Sidebar/Sidebar.tsx
+++ src/flows/shared/steps/OrganizeCollection/Sidebar/Sidebar.tsx
@@ -1,7 +1,7 @@
-import { memo, useCallback, useMemo, useState } from 'react';
+import { memo, useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import styled from 'styled-components';
 
-import { TitleS } from 'components/core/Text/Text';
+import { TitleS, TitleXS } from 'components/core/Text/Text';
 import Spacer from 'components/core/Spacer/Spacer';
 import { FOOTER_HEIGHT } from 'flows/shared/components/WizardFooter/WizardFooter';
 import TextButton from 'components/core/Button/TextButton';
@@ -12,16 +12,38 @@ import SearchBar from './SearchBar';
 import { useWizardState } from 'contexts/wizard/WizardDataProvider';
 import colors from 'components/core/colors';
 import { graphql, useFragment } from 'react-relay';
-import { SidebarFragment$key } from '__generated__/SidebarFragment.graphql';
+import { SidebarFragment$data, SidebarFragment$key } from '__generated__/SidebarFragment.graphql';
 import { removeNullValues } from 'utils/removeNullValues';
 import useIs3ac from 'hooks/oneOffs/useIs3ac';
 import { SidebarViewerFragment$key } from '__generated__/SidebarViewerFragment.graphql';
 import { EditModeToken } from '../types';
-import { AutoSizer, List, ListRowProps } from 'react-virtualized';
-import { COLUMN_COUNT, SIDEBAR_ICON_DIMENSIONS, SIDEBAR_ICON_GAP } from 'constants/sidebar';
+import { AutoSizer, Index, List, ListRowProps } from 'react-virtualized';
+import {
+  COLUMN_COUNT,
+  SIDEBAR_COLLECTION_TITLE_BOTTOM_SPACE,
+  SIDEBAR_COLLECTION_TITLE_HEIGHT,
+  SIDEBAR_ICON_DIMENSIONS,
+  SIDEBAR_ICON_GAP,
+} from 'constants/sidebar';
 import AddBlankBlock from './AddBlankBlock';
 import keyBy from 'lodash.keyby';
 import { SidebarNftIconFragment$key } from '../../../../../../__generated__/SidebarNftIconFragment.graphql';
+import { MultichainWalletSelector } from 'components/WalletSelector/multichain/MultichainWalletSelector';
+import { ETHEREUM } from 'types/Wallet';
+import { TezosAuthenticateWallet } from 'components/WalletSelector/multichain/tezos/TezosAuthenticateWallet';
+import {
+  Chain,
+  SidebarChains,
+} from 'flows/shared/steps/OrganizeCollection/Sidebar/SidebarChainSelector';
+import { groupBy } from 'graphql/jsutils/groupBy';
+import { readInlineData } from 'relay-runtime';
+import { SidebarGroupByChainFragment$key } from '../../../../../../__generated__/SidebarGroupByChainFragment.graphql';
+import {
+  SidebarTokensFragment$data,
+  SidebarTokensFragment$key,
+} from '../../../../../../__generated__/SidebarTokensFragment.graphql';
+import IconContainer from 'components/core/Markdown/IconContainer';
+import { ExpandedIcon } from 'flows/shared/steps/OrganizeCollection/Sidebar/ExpandedIcon';
 
 type Props = {
   sidebarTokens: SidebarTokensState;
@@ -34,8 +56,10 @@ function Sidebar({ tokensRef, sidebarTokens, viewerRef }: Props) {
     graphql`
       fragment SidebarFragment on Token @relay(plural: true) {
         dbid
-        ...SidebarNftIconFragment
+        chain
+
         ...SearchBarFragment
+        ...SidebarTokensFragment
         ...getVideoOrImageUrlForNftPreviewFragment
       }
     `,
@@ -53,12 +77,14 @@ function Sidebar({ tokensRef, sidebarTokens, viewerRef }: Props) {
     viewerRef
   );
 
-  const is3ac = useIs3ac(viewer.user?.dbid);
-
-  const tokens = removeNullValues(allTokens);
-
-  const [debouncedSearchQuery, setDebouncedSearchQuery] = useState('');
   const [searchResults, setSearchResults] = useState<string[]>([]);
+  const [selectedChain, setSelectedChain] = useState<Chain>('Ethereum');
+  const [debouncedSearchQuery, setDebouncedSearchQuery] = useState('');
+
+  const is3ac = useIs3ac(viewer.user?.dbid);
+  const nonNullTokens = removeNullValues(allTokens);
+
+  const { isRefreshingNfts, handleRefreshNfts } = useWizardState();
 
   const sidebarTokensAsArray = useMemo(() => convertObjectToArray(sidebarTokens), [sidebarTokens]);
 
@@ -77,15 +103,16 @@ function Sidebar({ tokensRef, sidebarTokens, viewerRef }: Props) {
     return sidebarTokensAsArray;
   }, [debouncedSearchQuery, searchResults, sidebarTokens, sidebarTokensAsArray]);
 
-  const nftFragmentsKeyedByID = useMemo(() => keyBy(tokens, (token) => token.dbid), [tokens]);
+  const nftFragmentsKeyedByID = useMemo(
+    () => keyBy(nonNullTokens, (token) => token.dbid),
+    [nonNullTokens]
+  );
 
-  const nonNullTokens = useMemo(() => {
+  const nonNullEditModeTokens = useMemo(() => {
     return tokensFilteredBySearch.filter((editModeToken) =>
       Boolean(nftFragmentsKeyedByID[editModeToken.id])
     );
   }, [nftFragmentsKeyedByID, tokensFilteredBySearch]);
-
-  const { isRefreshingNfts, handleRefreshNfts } = useWizardState();
 
   return (
     <StyledSidebar>
@@ -104,23 +131,41 @@ function Sidebar({ tokensRef, sidebarTokens, viewerRef }: Props) {
           }
         </Header>
         <SearchBar
-          tokensRef={tokens}
+          tokensRef={nonNullTokens}
           setSearchResults={setSearchResults}
           setDebouncedSearchQuery={setDebouncedSearchQuery}
         />
       </StyledSidebarContainer>
-      <SidebarTokens nftFragmentsKeyedByID={nftFragmentsKeyedByID} tokens={nonNullTokens} />
-      <Spacer height={12} />
+      <SidebarChains selected={selectedChain} onChange={setSelectedChain} />
+      <SidebarTokens tokenRefs={nonNullTokens} editModeTokens={nonNullEditModeTokens} />
     </StyledSidebar>
   );
 }
 
 type SidebarTokensProps = {
-  nftFragmentsKeyedByID: { [id: string]: SidebarNftIconFragment$key };
-  tokens: EditModeToken[];
+  tokenRefs: SidebarTokensFragment$key;
+  editModeTokens: EditModeToken[];
 };
 
-const SidebarTokens = ({ nftFragmentsKeyedByID, tokens }: SidebarTokensProps) => {
+const SidebarTokens = ({ tokenRefs, editModeTokens }: SidebarTokensProps) => {
+  const tokens = useFragment(
+    graphql`
+      fragment SidebarTokensFragment on Token @relay(plural: true) {
+        dbid
+
+        ...SidebarNftIconFragment
+
+        contract {
+          name
+          contractAddress {
+            address
+          }
+        }
+      }
+    `,
+    tokenRefs
+  );
+
   const [erroredTokenIds, setErroredTokenIds] = useState(new Set());
 
   const handleMarkErroredTokenId = useCallback((id) => {
@@ -139,84 +184,198 @@ const SidebarTokens = ({ nftFragmentsKeyedByID, tokens }: SidebarTokensProps) =>
     });
   }, []);
 
-  const displayedTokens = useMemo(() => {
-    const validTokens = [];
-    const unsupportedTokens = [];
+  const [collectionExpandedMap, setCollectionExpandedMap] = useState<Map<string, boolean>>(
+    new Map()
+  );
 
-    for (const token of tokens) {
-      if (erroredTokenIds.has(token.token.dbid)) {
-        unsupportedTokens.push(token);
-        continue;
-      }
-      validTokens.push(token);
-    }
-
-    return [...validTokens, ...unsupportedTokens];
-  }, [erroredTokenIds, tokens]);
-
-  /**
-   * We render a row with three token.
-   */
-  type TokenOrWhitespace = EditModeToken | 'whitespace';
-  const rows = useMemo(() => {
-    const rows: TokenOrWhitespace[][] = [];
-
-    let row: TokenOrWhitespace[] = ['whitespace'];
-
-    displayedTokens.forEach((token, index) => {
-      row.push(token);
-
-      // if the row is full, push it to the rows array
-      if (row.length % COLUMN_COUNT === 0) {
-        rows.push(row);
-        row = [];
-        // make sure the final row gets pushed in even if it isn't full
-      } else if (row.length && index === displayedTokens.length - 1) {
-        rows.push(row);
-      }
-    });
-
-    return rows;
-  }, [displayedTokens]);
-
-  const rowRenderer = ({ key, style, index }: ListRowProps) => {
-    const row = rows[index];
-
-    if (!row) {
-      return null;
-    }
-
-    return (
-      <Selection key={key} style={style}>
-        {row.map((tokenOrWhitespace) => {
-          if (tokenOrWhitespace === 'whitespace') {
-            return <AddBlankBlock key="whitespace" />;
-          }
-          return (
-            <SidebarNftIcon
-              key={tokenOrWhitespace.token.dbid}
-              tokenRef={nftFragmentsKeyedByID[tokenOrWhitespace.token.dbid]}
-              editModeToken={tokenOrWhitespace}
-              handleTokenRenderError={handleMarkErroredTokenId}
-              handleTokenRenderSuccess={handleMarkSuccessTokenId}
-            />
-          );
-        })}
-      </Selection>
-    );
+  type CollectionGroup = {
+    title: string;
+    address: string;
+    tokens: Array<{
+      token: SidebarTokensFragment$data[number];
+      editModeToken: EditModeToken;
+    }>;
   };
 
-  const rowHeight = SIDEBAR_ICON_DIMENSIONS + SIDEBAR_ICON_GAP;
+  const groups: CollectionGroup[] = useMemo(() => {
+    const tokensKeyedById = keyBy(tokens, (token) => token.dbid);
+
+    const map: Record<string, CollectionGroup> = {};
+
+    for (const editModeToken of editModeTokens) {
+      const token = tokensKeyedById[editModeToken.id];
+
+      if (!token?.contract?.contractAddress?.address || !token?.contract?.name) {
+        continue;
+      }
+
+      if (token.contract.contractAddress.address && token.contract.name) {
+        const group = map[token.contract.contractAddress.address] ?? {
+          title: token.contract.name,
+          address: token.contract.contractAddress.address,
+          tokens: [],
+        };
+
+        map[token.contract.contractAddress.address] = group;
+
+        group.tokens.push({
+          token,
+          editModeToken,
+        });
+      }
+    }
+
+    return Object.values(map);
+  }, [editModeTokens, tokens]);
+
+  const toggleExpanded = useCallback((address: string) => {
+    setCollectionExpandedMap((previous) => {
+      const newMap = new Map(previous);
+
+      // Default to expanded
+      const expanded = newMap.get(address) ?? true;
+
+      newMap.set(address, !expanded);
+
+      return newMap;
+    });
+  }, []);
+
+  type TokenOrWhitespace =
+    | { token: SidebarTokensFragment$data[number]; editModeToken: EditModeToken }
+    | 'whitespace';
+
+  type VirtualizedRow =
+    | { type: 'collection-title'; expanded: boolean; address: string; title: string }
+    | { type: 'tokens'; tokens: TokenOrWhitespace[]; expanded: boolean };
+
+  const rows: VirtualizedRow[] = useMemo(() => {
+    const rows: VirtualizedRow[] = [];
+
+    for (const group of groups) {
+      const tokensSortedByErrored = [...group.tokens].sort((a, b) => {
+        const aIsErrored = erroredTokenIds.has(a.token.dbid);
+        const bIsErrored = erroredTokenIds.has(b.token.dbid);
+
+        if (aIsErrored === bIsErrored) {
+          return 0;
+        } else if (aIsErrored) {
+          return -1;
+        } else {
+          return 1;
+        }
+      });
+
+      // Default to expanded
+      const expanded = collectionExpandedMap.get(group.address) ?? true;
+
+      rows.push({ type: 'collection-title', expanded, address: group.address, title: group.title });
+
+      const COLUMNS_PER_ROW = 3;
+      for (let i = 0; i < tokensSortedByErrored.length; i += COLUMNS_PER_ROW) {
+        const rowTokens = tokensSortedByErrored.slice(i, i + COLUMNS_PER_ROW);
+
+        rows.push({ type: 'tokens', tokens: rowTokens, expanded });
+      }
+    }
+
+    return rows;
+  }, [collectionExpandedMap, erroredTokenIds, groups]);
+
+  console.log({ rows, groups });
+
+  const rowRenderer = useCallback(
+    ({ key, style, index }: ListRowProps) => {
+      const row = rows[index];
+
+      if (!row) {
+        return null;
+      }
+
+      if (row.type === 'collection-title') {
+        return (
+          <CollectionTitleContainer
+            expanded={row.expanded}
+            onClick={() => toggleExpanded(row.address)}
+            key={key}
+            style={style}
+          >
+            <ExpandedIcon expanded={row.expanded} />
+
+            <CollectionTitleText title={row.title}>{row.title}</CollectionTitleText>
+          </CollectionTitleContainer>
+        );
+      }
+
+      if (row.type === 'tokens') {
+        if (!row.expanded) {
+          return null;
+        }
+
+        return (
+          <Selection key={key} style={style}>
+            {row.tokens.map((tokenOrWhitespace) => {
+              if (tokenOrWhitespace === 'whitespace') {
+                return <AddBlankBlock key="whitespace" />;
+              }
+
+              return (
+                <SidebarNftIcon
+                  key={tokenOrWhitespace.token.dbid}
+                  tokenRef={tokenOrWhitespace.token}
+                  editModeToken={tokenOrWhitespace.editModeToken}
+                  handleTokenRenderError={handleMarkErroredTokenId}
+                  handleTokenRenderSuccess={handleMarkSuccessTokenId}
+                />
+              );
+            })}
+          </Selection>
+        );
+      }
+    },
+    [handleMarkErroredTokenId, handleMarkSuccessTokenId, rows, toggleExpanded]
+  );
+
+  const rowHeightCalculator = useCallback(
+    ({ index }: Index) => {
+      console.log('rowHeightCalculator', index);
+      const row = rows[index];
+
+      if (row?.type === 'tokens') {
+        if (!row.expanded) {
+          return 0;
+        }
+
+        return SIDEBAR_ICON_DIMENSIONS + SIDEBAR_ICON_GAP;
+      } else if (row?.type === 'collection-title') {
+        if (row.expanded) {
+          return SIDEBAR_COLLECTION_TITLE_HEIGHT + SIDEBAR_COLLECTION_TITLE_BOTTOM_SPACE;
+        } else {
+          return SIDEBAR_COLLECTION_TITLE_HEIGHT;
+        }
+      }
+
+      // Maybe we should report an error here
+      return 0;
+    },
+    [rows]
+  );
+
+  const ref = useRef<List | null>(null);
+  useEffect(() => {
+    ref.current?.recomputeRowHeights();
+  }, [collectionExpandedMap]);
 
   return (
     <StyledListTokenContainer>
       <AutoSizer>
         {({ width, height }) => (
           <List
+            ref={ref}
             style={{ outline: 'none' }}
             rowRenderer={rowRenderer}
             rowCount={rows.length}
-            rowHeight={rowHeight}
+            rowHeight={rowHeightCalculator}
             width={width}
             height={height}
           />
@@ -226,7 +385,25 @@ const SidebarTokens = ({ nftFragmentsKeyedByID, tokens }: SidebarTokensProps) =>
   );
 };
 
+const CollectionTitleText = styled(TitleXS)`
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+`;
+
+const CollectionTitleContainer = styled.div<{ expanded: boolean }>`
+  display: flex;
+  align-items: center;
+
+  padding-bottom: ${({ expanded }) =>
+    expanded ? `${SIDEBAR_COLLECTION_TITLE_BOTTOM_SPACE}px` : '0px'};
+`;
+
 const StyledSidebar = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  padding: 16px;
   height: calc(100vh - ${FOOTER_HEIGHT}px);
   border-right: 1px solid ${colors.porcelain};
   user-select: none;
@@ -236,11 +413,8 @@ const StyledSidebar = styled.div`
   }
 `;
 
-const LEFT_SIDEBAR_HEADER_HEIGHT = 120;
-
 const StyledSidebarContainer = styled.div`
-  padding: 16px;
-  height: ${LEFT_SIDEBAR_HEADER_HEIGHT}px;
+  padding-bottom: 8px;
 
   &::-webkit-scrollbar {
     display: none;
@@ -249,7 +423,7 @@ const StyledSidebarContainer = styled.div`
 
 const StyledListTokenContainer = styled.div`
   width: 100%;
-  height: calc(100% - ${LEFT_SIDEBAR_HEADER_HEIGHT}px);
+  flex-grow: 1;
 `;
 
 const Header = styled.div`
@@ -263,7 +437,6 @@ const Header = styled.div`
 const Selection = styled.div`
   display: flex;
   grid-gap: ${SIDEBAR_ICON_GAP}px;
-  padding-left: 16px;
 `;
 
 // This has the styling from InteractiveLink but we cannot use InteractiveLink because it is a TextButton
