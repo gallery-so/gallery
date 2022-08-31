519b24bb0c4ff28606b303c617ef70e27ccf831f
Terence Bezman <terencebezman@gmail.com>
2022-08-31 11:55:56 -0700

    disable tezos for now ;)

--- src/flows/shared/steps/OrganizeCollection/Sidebar/SidebarChainSelector.tsx
+++ src/flows/shared/steps/OrganizeCollection/Sidebar/SidebarChainSelector.tsx
@@ -5,7 +5,7 @@ import { RefreshIcon } from 'icons/RefreshIcon';
 
 const chains = [
   { name: 'Ethereum', shortName: 'ETH', icon: '/icons/ethereum_logo.svg' },
-  { name: 'Tezos', shortName: 'TEZ', icon: '/icons/tezos_logo.svg' },
+  // { name: 'Tezos', shortName: 'TEZ', icon: '/icons/tezos_logo.svg' },
   { name: 'POAP', shortName: 'POAP', icon: '/icons/poap_logo.svg' },
 ] as const;
 
