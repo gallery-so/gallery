370e300d7bf5b853e896873eb32c5ffd89ff0d46
jakzaizzat <jakzaizzat@gmail.com>
2022-09-08 10:07:26 +0800

    Update cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -15,12 +15,12 @@ export default defineConfig({
   requestTimeout: 30000,
   projectId: 'igfgns',
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config);
     },
     baseUrl: 'http://localhost:3000',
     specPattern: 'cypress/integration/specs/**/*.{js,jsx,ts,tsx}',
+    supportFile: 'cypress/support/e2e.js',
+    fixturesFolder: 'cypress/fixtures',
   },
 });
