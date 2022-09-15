b93b4d514ea7d72e32c56062d414958f56062b48
Robinnnnn <rokim8@gmail.com>
2022-09-15 01:08:34 -0400

    3ac fixes

--- src/hooks/oneOffs/useIs3acProfilePage.ts
+++ src/hooks/oneOffs/useIs3acProfilePage.ts
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
 
-export function useIs3acProfilePage() {
+export default function useIs3acProfilePage() {
   const { query } = useRouter();
 
   return typeof query?.username === 'string' && query?.username?.toLowerCase() === '3ac';
