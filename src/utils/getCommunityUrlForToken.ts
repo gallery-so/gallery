66bd6aaf2e3e1d0d246c434456249e7247e6886d
Terence Bezman <terencebezman@gmail.com>
2022-09-15 18:15:10 -0700

    support get community url with tezos

--- src/utils/getCommunityUrlForToken.ts
+++ src/utils/getCommunityUrlForToken.ts
@@ -1,7 +1,14 @@
 import { readInlineData } from 'relay-runtime';
 import { graphql } from 'react-relay';
 import { getCommunityUrlForTokenFragment$key } from '../../__generated__/getCommunityUrlForTokenFragment.graphql';
-import { DISABLED_CONTRACTS } from '../../pages/community/[contractAddress]';
+
+export const DISABLED_CONTRACTS = [
+  '0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270', // Art Blocks
+  '0x495f947276749ce646f68ac8c248420045cb7b5e', // OS
+  '0xf6793da657495ffeff9ee6350824910abc21356c', // Rarible
+  '0x3b3ee1931dc30c1957379fac9aba94d1c48a5405', // Foundation
+  '0xdfde78d2baec499fe18f2be74b6c287eed9511d7', // Braindrops
+];
 
 export function getCommunityUrlForToken(
   tokenRef: getCommunityUrlForTokenFragment$key
@@ -28,7 +35,11 @@ export function getCommunityUrlForToken(
     return null;
   }
 
-  return token.contract.chain === 'POAP'
-    ? `/community/poap/${token.contract.contractAddress.address}`
-    : `/community/${token.contract.contractAddress.address}`;
+  if (token.contract.chain === 'POAP') {
+    return `/community/poap/${token.contract.contractAddress.address}`;
+  } else if (token.contract.chain === 'Tezos') {
+    return `/community/tezos/${token.contract.contractAddress.address}`;
+  } else {
+    return `/community/${token.contract.contractAddress.address}`;
+  }
 }
