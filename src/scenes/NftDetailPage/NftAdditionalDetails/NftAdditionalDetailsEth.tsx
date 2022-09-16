a66d8b9e031d2d7287fe2299bfc64b366d095030
Terence Bezman <terencebezman@gmail.com>
2022-09-15 20:59:09 -0700

    tezos nft additional details component

--- src/scenes/NftDetailPage/NftAdditionalDetails/NftAdditionalDetailsEth.tsx
+++ src/scenes/NftDetailPage/NftAdditionalDetails/NftAdditionalDetailsEth.tsx
@@ -1,9 +1,5 @@
 import { graphql, useFragment, useLazyLoadQuery } from 'react-relay';
-import { NftAdditionalDetailsNonPOAPQuery } from '../../../../__generated__/NftAdditionalDetailsNonPOAPQuery.graphql';
-import { useReportError } from 'contexts/errorReporting/ErrorReportingContext';
-import { useToastActions } from 'contexts/toast/ToastContext';
-import { useRefreshToken } from 'hooks/api/tokens/useRefreshToken';
-import { useCallback, useMemo } from 'react';
+import { useMemo } from 'react';
 import { getOpenseaExternalUrl, hexHandler } from 'utils/getOpenseaExternalUrl';
 import { VStack } from 'components/core/Spacer/Stack';
 import { BaseM, TitleXS } from 'components/core/Text/Text';
@@ -12,18 +8,20 @@ import InteractiveLink from 'components/core/InteractiveLink/InteractiveLink';
 import isFeatureEnabled from 'utils/graphql/isFeatureEnabled';
 import { FeatureFlag } from 'components/core/enums';
 import styled from 'styled-components';
-import { NftAdditionalDetailsNonPOAPFragment$key } from '../../../../__generated__/NftAdditionalDetailsNonPOAPFragment.graphql';
+import { NftAdditionalDetailsEthFragment$key } from '../../../../__generated__/NftAdditionalDetailsEthFragment.graphql';
 import { CopyableAddress } from 'components/CopyableAddress';
+import { useRefreshMetadata } from 'scenes/NftDetailPage/NftAdditionalDetails/useRefreshMetadata';
+import { NftAdditionalDetailsEthQuery } from '../../../../__generated__/NftAdditionalDetailsEthQuery.graphql';
 
 type NftAdditionaDetailsNonPOAPProps = {
   showDetails: boolean;
-  tokenRef: NftAdditionalDetailsNonPOAPFragment$key;
+  tokenRef: NftAdditionalDetailsEthFragment$key;
 };
 
-export function NftAdditionalDetailsNonPOAP({ tokenRef }: NftAdditionaDetailsNonPOAPProps) {
+export function NftAdditionalDetailsEth({ tokenRef }: NftAdditionaDetailsNonPOAPProps) {
   const token = useFragment(
     graphql`
-      fragment NftAdditionalDetailsNonPOAPFragment on Token {
+      fragment NftAdditionalDetailsEthFragment on Token {
         dbid
         externalUrl
         tokenId
@@ -38,56 +36,26 @@ export function NftAdditionalDetailsNonPOAP({ tokenRef }: NftAdditionaDetailsNon
             ...CopyableAddressFragment
           }
         }
+
+        ...useRefreshMetadataFragment
       }
     `,
     tokenRef
   );
 
   // TODO: We should thread a query ref down to this somehow
-  const query = useLazyLoadQuery<NftAdditionalDetailsNonPOAPQuery>(
+  const query = useLazyLoadQuery<NftAdditionalDetailsEthQuery>(
     graphql`
-      query NftAdditionalDetailsNonPOAPQuery {
+      query NftAdditionalDetailsEthQuery {
         ...isFeatureEnabledFragment
       }
     `,
     {}
   );
 
-  const { dbid, tokenId, contract, externalUrl } = token;
+  const { tokenId, contract, externalUrl } = token;
 
-  const reportError = useReportError();
-  const { pushToast } = useToastActions();
-  const [refreshToken, isRefreshing] = useRefreshToken();
-
-  const handleRefreshMetadata = useCallback(async () => {
-    try {
-      pushToast({
-        message: 'This piece is being updated with the latest metadata. Check back in few minutes.',
-        autoClose: true,
-      });
-
-      await refreshToken(dbid);
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        reportError(error, {
-          tags: {
-            tokenId: token.dbid,
-          },
-        });
-
-        pushToast({
-          message: error.message,
-          autoClose: true,
-        });
-      } else {
-        reportError('Error while refreshing token, unknown error');
-
-        pushToast({
-          message: "Something went wrong, we're looking into it now.",
-        });
-      }
-    }
-  }, [dbid, pushToast, refreshToken, reportError, token.dbid]);
+  const [refresh, isRefreshing] = useRefreshMetadata(token);
 
   const openSeaExternalUrl = useMemo(() => {
     if (contract?.contractAddress?.address && tokenId) {
@@ -125,7 +93,7 @@ export function NftAdditionalDetailsNonPOAP({ tokenRef }: NftAdditionaDetailsNon
           <>
             <InteractiveLink href={openSeaExternalUrl}>View on OpenSea</InteractiveLink>
             {isFeatureEnabled(FeatureFlag.REFRESH_METADATA, query) && (
-              <InteractiveLink onClick={handleRefreshMetadata} disabled={isRefreshing}>
+              <InteractiveLink onClick={refresh} disabled={isRefreshing}>
                 Refresh metadata
               </InteractiveLink>
             )}
