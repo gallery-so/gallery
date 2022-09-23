4abef584dbb8a467c5b5a82d66f39fe4aa6f8883
robin <rokim8@gmail.com>
2022-09-23 02:24:36 -0400

    [GAL-451] Fix videos on iOS safari (#1023)
    * fix videos on ios safari
    
    * type fix
    
    * more comments

--- src/errors/CouldNotRenderNftError.ts
+++ src/errors/CouldNotRenderNftError.ts
@@ -2,10 +2,12 @@ import { Primitive } from 'relay-runtime/lib/store/RelayStoreTypes';
 
 export class CouldNotRenderNftError extends Error {
   public metadata: Record<string, Primitive>;
+  public componentName: string;
 
   constructor(componentName: string, reason: string, metadata?: Record<string, Primitive>) {
     super(`${componentName}: ${reason}`);
 
+    this.componentName = componentName;
     this.metadata = metadata ?? {};
 
     Object.setPrototypeOf(this, CouldNotRenderNftError.prototype);
