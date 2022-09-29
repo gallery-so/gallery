35471dacb1759858df952cd18624636f6d908e7a
robin <rokim8@gmail.com>
2022-09-29 17:18:33 -0400

    Fix fontface unit test (#1036)

--- tests/setupFileAfterEnv.js
+++ tests/setupFileAfterEnv.js
@@ -1,3 +1,15 @@
 import '@testing-library/jest-dom';
 
 jest.mock('next/router', () => require('next-router-mock'));
+
+class FontFace {
+  constructor(name, path) {
+    this.name = name;
+    this.path = path;
+  }
+  load() {
+    return Promise.resolve();
+  }
+}
+
+window.FontFace = FontFace;
