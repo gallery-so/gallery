2164237259c8f3d271b2f882d50bd54afe9a5a83
Jakz <jakzaizzat@gmail.com>
2022-10-01 00:51:56 +0800

    Fix Create first collection UI (#1038)
    * Fix ui alignment
    
    * align text center

--- src/flows/OnboardingFlow/steps/CreateFirstCollection.tsx
+++ src/flows/OnboardingFlow/steps/CreateFirstCollection.tsx
@@ -32,8 +32,8 @@ function CreateFirstCollection({ next }: WizardContext) {
 
   return (
     <FullPageCenteredStep withFooter>
-      <VStack gap={24}>
-        <VStack gap={8}>
+      <VStack gap={24} align="center">
+        <VStack gap={8} align="center">
           <BaseXL>Create your first collection</BaseXL>
           <StyledBodyText>
             Organize your gallery with collections. Use them to group NFTs by creator, theme, or
