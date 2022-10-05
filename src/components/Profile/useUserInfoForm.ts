982b5f9f7613b8c7d012cb85163c49f5f59af957
Terence Bezman <terencebezman@gmail.com>
2022-10-05 12:44:16 -0700

    [GAL-466] Block "Next" step until username is valid (#1044)
    * clean up useUserInfoForm
    
    * clean up AdduserInfo
    
    * disable next button until username is valid

--- src/components/Profile/useUserInfoForm.ts
+++ src/components/Profile/useUserInfoForm.ts
@@ -18,6 +18,7 @@ import useDebounce from 'hooks/useDebounce';
 import useAuthPayloadQuery from 'hooks/api/users/useAuthPayloadQuery';
 import { useTrackCreateUserSuccess } from 'contexts/analytics/authUtil';
 import { useUserInfoFormIsUsernameAvailableQuery } from '../../../__generated__/useUserInfoFormIsUsernameAvailableQuery.graphql';
+import { useReportError } from 'contexts/errorReporting/ErrorReportingContext';
 
 type Props = {
   onSuccess: (username: string) => void;
@@ -60,22 +61,31 @@ export default function useUserInfoForm({
   existingBio,
   userId,
 }: Props) {
+  const [bio, setBio] = useState(existingBio ?? '');
   const [username, setUsername] = useState(existingUsername ?? '');
-  const [usernameError, setUsernameError] = useState('');
+
   const usernameFieldIsDirty = useRef(false);
 
-  const [bio, setBio] = useState(existingBio ?? '');
+  // This cannot be derived from a "null" `usernameError`
+  // since the value starts off as empty when the form is empty
+  const [isUsernameValid, setIsUsernameValid] = useState(false);
+  const [usernameError, setUsernameError] = useState('');
 
   // Generic error that doesn't belong to username / bio
   const [generalError, setGeneralError] = useState('');
+
   const updateUser = useUpdateUser();
   const createUser = useCreateUser();
+  const reportError = useReportError();
   const authPayloadQuery = useAuthPayloadQuery();
+  const isUsernameAvailableFetcher = useIsUsernameAvailableFetcher();
   const trackCreateUserSuccess = useTrackCreateUserSuccess(
     authPayloadQuery?.userFriendlyWalletName
   );
 
-  const handleCreateOrEditUser = useCallback(async () => {
+  const debouncedUsername = useDebounce(username, 500);
+
+  const handleCreateOrEditUser = useCallback(async (): Promise<{ success: boolean }> => {
     setGeneralError('');
 
     if (usernameError) {
@@ -94,15 +104,19 @@ export default function useUserInfoForm({
         if (!authPayloadQuery) {
           throw new Error('Auth signature for creating user not found');
         }
+
         await createUser(authPayloadQuery, username, bio);
         trackCreateUserSuccess();
       }
+
       onSuccess(username);
+
       return { success: true };
     } catch (error: unknown) {
       if (error instanceof Error) {
         setGeneralError(formatError(error));
       }
+
       return { success: false };
     }
   }, [
@@ -119,53 +133,79 @@ export default function useUserInfoForm({
 
   const handleUsernameChange = useCallback((username: string) => {
     setUsername(username);
+
+    setIsUsernameValid(false);
     usernameFieldIsDirty.current = true;
   }, []);
 
-  const isUsernameAvailableFetcher = useIsUsernameAvailableFetcher();
-
-  const debouncedUsername = useDebounce(username, 500);
-
-  // validate username
-  useEffect(() => {
-    async function validateUsername() {
+  useEffect(
+    function validateUsername() {
       setGeneralError('');
 
-      if (debouncedUsername.length >= 2) {
-        const clientSideUsernameError = validate(debouncedUsername, [
-          required,
-          minLength(2),
-          maxLength(20),
-          alphanumericUnderscores,
-          noConsecutivePeriodsOrUnderscores,
-        ]);
+      if (debouncedUsername.length < 2) {
+        return;
+      }
 
-        setUsernameError(clientSideUsernameError || '');
+      const clientSideUsernameError = validate(debouncedUsername, [
+        required,
+        minLength(2),
+        maxLength(20),
+        alphanumericUnderscores,
+        noConsecutivePeriodsOrUnderscores,
+      ]);
 
-        if (usernameFieldIsDirty.current && !clientSideUsernameError) {
-          const isUsernameAvailable = await isUsernameAvailableFetcher(debouncedUsername);
+      if (clientSideUsernameError) {
+        setUsernameError(clientSideUsernameError);
+
+        return;
+      } else {
+        setUsernameError('');
+      }
+
+      isUsernameAvailableFetcher(debouncedUsername)
+        .then((isUsernameAvailable) => {
           if (!isUsernameAvailable) {
             setUsernameError('Username is taken');
+          } else {
+            setIsUsernameValid(true);
           }
-        }
-      }
-    }
+        })
+        .catch((error) => {
+          if (error instanceof Error) {
+            reportError(error);
+          } else {
+            reportError('Failure while validating username', {
+              tags: { username: debouncedUsername },
+            });
+          }
 
-    validateUsername();
-  }, [debouncedUsername, isUsernameAvailableFetcher]);
-
-  const values = useMemo(
-    () => ({
-      username,
-      onUsernameChange: handleUsernameChange,
-      usernameError,
-      bio,
-      onBioChange: setBio,
-      generalError,
-      onEditUser: handleCreateOrEditUser,
-    }),
-    [bio, generalError, handleCreateOrEditUser, handleUsernameChange, username, usernameError]
+          setGeneralError(
+            "Something went wrong while validating your username. We're looking into it."
+          );
+        });
+    },
+    [debouncedUsername, isUsernameAvailableFetcher, reportError]
   );
 
-  return values;
+  return useMemo(
+    () => ({
+      bio,
+      username,
+      generalError,
+      usernameError,
+      isUsernameValid,
+      onBioChange: setBio,
+      onEditUser: handleCreateOrEditUser,
+      onUsernameChange: handleUsernameChange,
+    }),
+    [
+      bio,
+      generalError,
+      handleCreateOrEditUser,
+      handleUsernameChange,
+      isUsernameValid,
+      username,
+      usernameError,
+    ]
+  );
 }
