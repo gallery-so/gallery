cd921f044ebadd4a8721c42b223c69b0431b6565
Terence Bezman <terencebezman@gmail.com>
2022-10-10 16:45:47 -0400

    Setup GraphQL Subscriptions (#1049)
    * fetch latest schema
    
    * install graphql-ws
    
    * setup relay subscribe function
    
    * test subscription
    
    * throw error if missing environment variable
    
    * Update env sample to include websocket url
    
    * remove test page
    
    * use fallback instead of hard blocking

--- src/contexts/relay/RelayProvider.tsx
+++ src/contexts/relay/RelayProvider.tsx
@@ -4,6 +4,7 @@ import { Environment, RelayEnvironmentProvider } from 'react-relay';
 import { Network, RecordSource, Store } from 'relay-runtime';
 import { RecordMap } from 'relay-runtime/lib/store/RelayStoreTypes';
 import { relayFetchFunction } from 'contexts/relay/relayFetchFunction';
+import { relaySubscribeFunction } from 'contexts/relay/relaySubscribeFunction';
 
 export const serializeRelayEnvironment = (environment: Environment) =>
   environment.getStore().getSource().toJSON();
@@ -11,13 +12,13 @@ export const serializeRelayEnvironment = (environment: Environment) =>
 export const createServerSideRelayEnvironment = (): Environment =>
   new RelayModernEnvironment({
     store: new Store(new RecordSource({})),
-    network: Network.create(relayFetchFunction),
+    network: Network.create(relayFetchFunction, relaySubscribeFunction),
   });
 
 export const createRelayEnvironmentFromRecords = (records?: RecordMap): Environment =>
   new RelayModernEnvironment({
     store: new Store(new RecordSource(records ?? {})),
-    network: Network.create(relayFetchFunction),
+    network: Network.create(relayFetchFunction, relaySubscribeFunction),
   });
 
 export function RelayProvider({
