9409be05ec8e8d4f3fb9ff56d5640b7e46161237
Terence Bezman <terencebezman@gmail.com>
2022-10-10 16:37:39 -0400

    use fallback instead of hard blocking

--- src/contexts/relay/relaySubscribeFunction.ts
+++ src/contexts/relay/relaySubscribeFunction.ts
@@ -4,14 +4,9 @@ import { Client, createClient } from 'graphql-ws';
 let websocketClient: Client;
 
 if (typeof window !== 'undefined') {
-  if (!process.env.NEXT_PUBLIC_GRAPHQL_SUBSCRIPTION_URL) {
-    throw new Error(
-      'Could not create websocket client w/o `NEXT_PUBLIC_GRAPHQL_SUBSCRIPTION_URL` environment variable set.'
-    );
-  }
-
   websocketClient = createClient({
-    url: process.env.NEXT_PUBLIC_GRAPHQL_SUBSCRIPTION_URL,
+    url:
+      process.env.NEXT_PUBLIC_GRAPHQL_SUBSCRIPTION_URL ?? 'ws://localhost:4000/glry/graphql/query',
   });
 }
 
