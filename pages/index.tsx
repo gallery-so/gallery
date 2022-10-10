f3dc22265d8f6918b0f41bcfcf6b43978af384c5
Terence Bezman <terencebezman@gmail.com>
2022-10-10 14:08:44 -0400

    remove test page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,6 @@
 import LandingPageScene from 'scenes/LandingPage/LandingPage';
 import GalleryRoute from 'scenes/_Router/GalleryRoute';
-import { useSubscription } from 'react-relay';
-import { graphql } from 'relay-runtime';
-import { pagesSubscription } from '../__generated__/pagesSubscription.graphql';
 
 export default function Index() {
-  useSubscription<pagesSubscription>({
-    variables: {},
-    onNext: (response) => {
-      console.log(response);
-    },
-    subscription: graphql`
-      subscription pagesSubscription {
-        newNotification {
-          id
-          seen
-        }
-      }
-    `,
-  });
-
   return <GalleryRoute element={<LandingPageScene />} navbar={false} />;
 }
