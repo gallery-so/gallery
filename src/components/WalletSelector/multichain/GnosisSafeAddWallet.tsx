c2aa3e6de2eaeaf639e360776cadfd49a8711935
jakzaizzat <jakzaizzat@gmail.com>
2022-10-13 13:40:53 +0800

    refactor pending gnosis

--- src/components/WalletSelector/multichain/GnosisSafeAddWallet.tsx
+++ src/components/WalletSelector/multichain/GnosisSafeAddWallet.tsx
@@ -1,7 +1,7 @@
 import { Web3Provider } from '@ethersproject/providers';
 import { useWeb3React } from '@web3-react/core';
 import colors from 'components/core/colors';
-import { BaseM, BaseXL } from 'components/core/Text/Text';
+import { BaseM } from 'components/core/Text/Text';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import GnosisSafePendingMessage from '../GnosisSafePendingMessage';
 import { normalizeError } from './normalizeError';
@@ -39,7 +39,7 @@ import { WalletError } from './WalletError';
 import { useConnectGnosisSafe } from './useConnectGnosisSafe';
 import { walletconnect } from '../../../connectors';
 import { VStack } from 'components/core/Spacer/Stack';
-import styled from 'styled-components';
+import { EmptyState } from 'components/EmptyState/EmptyState';
 
 type Props = {
   queryRef: GnosisSafeAddWalletFragment$key;
@@ -168,7 +168,7 @@ export const GnosisSafeAddWallet = ({ queryRef, reset }: Props) => {
         await listenForGnosisSignature(address, nonce, walletconnect);
 
         await authenticateWithBackend(address, nonce);
-      } catch (error: unknown) {
+      } catch (error) {
         handleError(error);
       }
     },
@@ -198,7 +198,7 @@ export const GnosisSafeAddWallet = ({ queryRef, reset }: Props) => {
         // Once signed, call the backend as usual
         void authenticateWithBackend(account, nonce);
       }
-    } catch (error: unknown) {
+    } catch (error) {
       handleError(error);
     }
   }, [account, authenticateWithBackend, handleError, pendingState, nonce]);
@@ -263,11 +263,12 @@ export const GnosisSafeAddWallet = ({ queryRef, reset }: Props) => {
 
   if (pendingState === ADDRESS_ALREADY_CONNECTED && account) {
     return (
-      <VStack>
-        <StyledTitle>Connect with Gnosis Safe</StyledTitle>
-        <BaseM>The following address is already connected to this account:</BaseM>
-        <BaseM color={colors.offBlack}>{account.toLowerCase()}</BaseM>
-      </VStack>
+      <EmptyState title="Connect with Gnosis Safe">
+        <VStack>
+          <BaseM>The following address is already connected to this account:</BaseM>
+          <BaseM color={colors.offBlack}>{account.toLowerCase()}</BaseM>
+        </VStack>
+      </EmptyState>
     );
   }
 
@@ -280,7 +281,3 @@ export const GnosisSafeAddWallet = ({ queryRef, reset }: Props) => {
     />
   );
 };
-
-const StyledTitle = styled(BaseXL)`
-  font-weight: 700;
-`;
