e95ccaa93c4b6590b4db0ceb7c76bb3639aee5e7
Terence Bezman <terencebezman@gmail.com>
2022-10-14 14:54:08 -0400

    handle removing admires

--- src/components/Feed/Socialize/AdmireButton.tsx
+++ src/components/Feed/Socialize/AdmireButton.tsx
@@ -1,5 +1,5 @@
 import { useFragment } from 'react-relay';
-import { ConnectionHandler, graphql } from 'relay-runtime';
+import { ConnectionHandler, graphql, SelectorStoreUpdater } from 'relay-runtime';
 import { AdmireButtonFragment$key } from '__generated__/AdmireButtonFragment.graphql';
 import { AuthModal } from 'hooks/useAuthModal';
 import { AdditionalContext, useReportError } from 'contexts/errorReporting/ErrorReportingContext';
@@ -10,6 +10,7 @@ import { useModalActions } from 'contexts/modal/ModalContext';
 import { usePromisifiedMutation } from 'hooks/usePromisifiedMutation';
 import { AdmireButtonMutation } from '../../../../__generated__/AdmireButtonMutation.graphql';
 import { AdmireIcon } from 'icons/SocializeIcons';
+import { AdmireButtonRemoveMutation } from '../../../../__generated__/AdmireButtonRemoveMutation.graphql';
 
 type AdmireButtonProps = {
   eventRef: AdmireButtonFragment$key;
@@ -66,6 +67,17 @@ export function AdmireButton({ eventRef, queryRef, onPotentialLayoutShift }: Adm
     }
   `);
 
+  const [removeAdmire] = usePromisifiedMutation<AdmireButtonRemoveMutation>(graphql`
+    mutation AdmireButtonRemoveMutation($eventId: DBID!) @raw_response_type {
+      removeAdmire(feedEventId: $eventId) {
+        ... on RemoveAdmirePayload {
+          __typename
+          admireID
+        }
+      }
+    }
+  `);
+
   const reportError = useReportError();
   const { pushToast } = useToastActions();
   const { showModal } = useModalActions();
@@ -78,6 +90,91 @@ export function AdmireButton({ eventRef, queryRef, onPotentialLayoutShift }: Adm
     event.id,
     'NotesModal_interactions'
   );
+
+  const handleRemoveAdmire = useCallback(async () => {
+    const errorMetadata: AdditionalContext['tags'] = {
+      eventId: event.dbid,
+    };
+
+    function pushErrorToast() {
+      pushToast({
+        autoClose: true,
+        message: `Something went wrong while unadmiring this post. We're actively looking into it.`,
+      });
+    }
+
+    const updater: SelectorStoreUpdater<AdmireButtonRemoveMutation['response']> = (
+      store,
+      response
+    ) => {
+      if (response?.removeAdmire?.__typename === 'RemoveAdmirePayload') {
+        const pageInfo = store.get(interactionsConnection)?.getLinkedRecord('pageInfo');
+
+        pageInfo?.setValue(((pageInfo?.getValue('total') as number) ?? 1) - 1, 'total');
+
+        store.get(event.id)?.setValue(false, 'hasViewerAdmiredEvent');
+      }
+    };
+
+    try {
+      const response = await removeAdmire({
+        updater: updater,
+        optimisticUpdater: updater,
+        optimisticResponse: {
+          removeAdmire: {
+            __typename: 'RemoveAdmirePayload',
+          },
+        },
+        variables: {
+          eventId: event.dbid,
+        },
+      });
+
+      if (response.removeAdmire?.__typename === 'RemoveAdmirePayload') {
+        // Tell the virtualized list that some data has changed
+        // therefore this cell's height might change.
+        //
+        // Ideally, this lives in a useEffect inside of the
+        // changing data's component, but right now the virtualized
+        // list is remounting the component every update, causing
+        // an infinite useEffect to occur
+        setTimeout(() => {
+          onPotentialLayoutShift();
+        }, 100);
+      } else {
+        pushErrorToast();
+
+        reportError(
+          `Could not unadmire feed event, typename was ${response.removeAdmire?.__typename}`,
+          {
+            tags: errorMetadata,
+          }
+        );
+      }
+    } catch (error) {
+      console.log(error);
+      pushErrorToast();
+
+      if (error instanceof Error) {
+        reportError(error);
+      } else {
+        reportError(`Could not remove admire on feed event for an unknown reason`, {
+          tags: errorMetadata,
+        });
+      }
+    }
+  }, [
+    admire,
+    event.dbid,
+    event.id,
+    interactionsConnection,
+    notesModalConnection,
+    onPotentialLayoutShift,
+    pushToast,
+    query,
+    reportError,
+    showModal,
+  ]);
 
   const handleAdmire = useCallback(async () => {
     if (query.viewer?.__typename !== 'Viewer') {
@@ -99,18 +196,21 @@ export function AdmireButton({ eventRef, queryRef, onPotentialLayoutShift }: Adm
       });
     }
 
+    const updater: SelectorStoreUpdater<AdmireButtonMutation['response']> = (store, response) => {
+      if (response?.admireFeedEvent?.__typename === 'AdmireFeedEventPayload') {
+        const pageInfo = store.get(interactionsConnection)?.getLinkedRecord('pageInfo');
+
+        pageInfo?.setValue(((pageInfo?.getValue('total') as number) ?? 0) + 1, 'total');
+
+        store.get(event.id)?.setValue(true, 'hasViewerAdmiredEvent');
+      }
+    };
+
     try {
       const optimisticAdmireId = Math.random().toString();
       const response = await admire({
-        updater: (store, response) => {
-          if (response.admireFeedEvent?.__typename === 'AdmireFeedEventPayload') {
-            const pageInfo = store.get(interactionsConnection)?.getLinkedRecord('pageInfo');
-
-            pageInfo?.setValue(((pageInfo?.getValue('total') as number) ?? 0) + 1, 'total');
-
-            store.get(event.id)?.setValue(true, 'hasViewerAdmiredEvent');
-          }
-        },
+        optimisticUpdater: updater,
+        updater: updater,
         optimisticResponse: {
           admireFeedEvent: {
             __typename: 'AdmireFeedEventPayload',
@@ -177,5 +277,10 @@ export function AdmireButton({ eventRef, queryRef, onPotentialLayoutShift }: Adm
     showModal,
   ]);
 
-  return <AdmireIcon onClick={handleAdmire} active={event.hasViewerAdmiredEvent ?? false} />;
+  return (
+    <AdmireIcon
+      onClick={event.hasViewerAdmiredEvent ? handleRemoveAdmire : handleAdmire}
+      active={event.hasViewerAdmiredEvent ?? false}
+    />
+  );
 }
