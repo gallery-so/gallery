d5bd0ea0e9ad7cdeea6d0c7112f894c970503958
Jackie Luo <jackiehluo@gmail.com>
2022-10-21 14:48:29 -0400

    clean up cypress screenshots

--- cypress/integration/pages/page.ts
+++ cypress/integration/pages/page.ts
@@ -6,14 +6,4 @@ export default class Page {
     cy.switchToCypressWindow();
     cy.wait(1000);
   }
-
-  screenshotPageAcrossDevices(path: string) {
-    cy.visit(path);
-    cy.wait(5000);
-    cy.screenshot(); // desktop
-    cy.viewport('ipad-2');
-    cy.screenshot(); // tablet
-    cy.viewport('iphone-8');
-    cy.screenshot(); // mobile
-  }
 }
