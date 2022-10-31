af295b8ecd081e60a64d9691d62baa0020bb7b6b
Terence Bezman <terencebezman@gmail.com>
2022-10-31 12:38:24 -0400

    halloween easter egg (#1084)
    * halloween easter egg
    
    * make it every two minutes
    
    * make ghost visible for two seconds

--- pages/home.tsx
+++ pages/home.tsx
@@ -4,6 +4,16 @@ import HomeScene from 'scenes/Home/Home';
 import GalleryRoute from 'scenes/_Router/GalleryRoute';
 import { homeQuery } from '__generated__/homeQuery.graphql';
 import { NOTES_PER_PAGE } from 'components/Feed/Socialize/NotesModal/NotesModal';
+import usePersistedState from 'hooks/usePersistedState';
+import { useCallback, useEffect, useRef } from 'react';
+import { HStack } from 'components/core/Spacer/Stack';
+import { BaseXL, TitleDiatypeL } from 'components/core/Text/Text';
+import styled from 'styled-components';
+import colors from 'components/core/colors';
+
+const TIME_RANGE = 60 * 1000 * 2;
+const MIN_TIME_BETWEEN_APPEARANCES = 5000;
+const GHOST_VISIBLE_TIME = 2000;
 
 export default function Home() {
   const query = useLazyLoadQuery<homeQuery>(
@@ -25,8 +35,104 @@ export default function Home() {
       viewerLast: ITEMS_PER_PAGE,
     }
   );
+  const [points, setPoints] = usePersistedState('HALLOWEEN_EASTER_EGG_POINTS', 0);
 
-  return <GalleryRoute element={<HomeScene queryRef={query} />} navbar={true} />;
+  const ghostRef = useRef<HTMLDivElement | null>(null);
+  const animationTimeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+  const cancelAnimations = useCallback(() => {
+    if (animationTimeoutRef.current) {
+      clearTimeout(animationTimeoutRef.current);
+    }
+  }, []);
+
+  const hideGhost = useCallback(() => {
+    if (ghostRef.current) {
+      ghostRef.current.style.opacity = '0';
+    }
+
+    setTimeout(() => {
+      if (ghostRef.current) {
+        ghostRef.current.style.pointerEvents = 'none';
+      }
+    }, 100);
+  }, []);
+
+  const showGhost = useCallback(() => {
+    const width = window.innerWidth - 200;
+    const height = window.innerHeight - 200;
+
+    const randomX = Math.random() * width;
+    const randomY = Math.random() * height;
+
+    if (ghostRef.current) {
+      ghostRef.current.style.pointerEvents = 'initial';
+      ghostRef.current.style.opacity = '1';
+      ghostRef.current.style.transform = `translateX(${randomX}px) translateY(${randomY}px)`;
+    }
+  }, []);
+
+  const queueGhost = useCallback(() => {
+    const nextTime = Math.random() * TIME_RANGE + MIN_TIME_BETWEEN_APPEARANCES;
+
+    animationTimeoutRef.current = setTimeout(() => {
+      showGhost();
+
+      animationTimeoutRef.current = setTimeout(() => {
+        hideGhost();
+
+        queueGhost();
+      }, GHOST_VISIBLE_TIME);
+    }, nextTime);
+  }, [hideGhost, showGhost]);
+
+  const handleGhostClick = useCallback(() => {
+    setPoints((previous) => previous + 1);
+
+    cancelAnimations();
+    hideGhost();
+    queueGhost();
+  }, [cancelAnimations, hideGhost, queueGhost, setPoints]);
+
+  useEffect(() => {
+    queueGhost();
+
+    return () => {
+      cancelAnimations();
+    };
+  }, [cancelAnimations, hideGhost, queueGhost, showGhost]);
+
+  return (
+    <GalleryRoute
+      element={
+        <>
+          {points > 0 && (
+            <div style={{ position: 'fixed', bottom: 0, left: 0 }}>
+              <HStack
+                gap={12}
+                style={{
+                  padding: '8px',
+                  borderTop: `1px solid ${colors.porcelain}`,
+                  borderRight: `1px solid ${colors.porcelain}`,
+                }}
+              >
+                <TitleDiatypeL>👻</TitleDiatypeL>
+                <HStack align="center" gap={6}>
+                  <TitleDiatypeL>Score: </TitleDiatypeL>
+                  <BaseXL>{points}</BaseXL>
+                </HStack>
+              </HStack>
+            </div>
+          )}
+          <StyledGhost onClick={handleGhostClick} ref={ghostRef}>
+            👻
+          </StyledGhost>
+          <HomeScene queryRef={query} />
+        </>
+      }
+      navbar={true}
+    />
+  );
 }
 
 /**
@@ -64,3 +170,11 @@ export const getServerSideProps = async () => {
     props: {},
   };
 };
+
+const StyledGhost = styled.div`
+  position: fixed;
+  z-index: 9999;
+  opacity: 0;
+  transition: opacity 100ms ease-in-out;
+  font-size: 32px;
+`;
