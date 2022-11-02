3f35fecb1610afc3a13b16963a1a87ff99ebcd54
Terence Bezman <terencebezman@gmail.com>
2022-11-02 15:44:10 -0400

    fix suspense boundary

---
+++ src/components/NotificationsModal/NotificationList.tsx
@@ -0,0 +1,99 @@
+import { usePaginationFragment } from 'react-relay';
+import { graphql } from 'relay-runtime';
+import { NotificationListFragment$key } from '__generated__/NotificationListFragment.graphql';
+import { useCallback, useMemo } from 'react';
+import { TitleDiatypeL, TitleXS } from 'components/core/Text/Text';
+import { VStack } from 'components/core/Spacer/Stack';
+import { Notification } from './Notification';
+import styled from 'styled-components';
+import Loader from 'components/core/Loader/Loader';
+
+export const NOTIFICATIONS_PER_PAGE = 10;
+
+type NotificationListProps = {
+  queryRef: NotificationListFragment$key;
+};
+
+export function NotificationList({ queryRef }: NotificationListProps) {
+  const {
+    data: query,
+    loadPrevious,
+    hasPrevious,
+    isLoadingPrevious,
+  } = usePaginationFragment(
+    graphql`
+      fragment NotificationListFragment on Query
+      @refetchable(queryName: "NotificationsModalRefetchableQuery") {
+        viewer {
+          ... on Viewer {
+            notifications(last: $notificationsLast, before: $notificationsBefore)
+              @connection(key: "NotificationsModalFragment_notifications") {
+              edges {
+                node {
+                  id
+                  ...NotificationFragment
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+    queryRef
+  );
+
+  const nonNullNotifications = useMemo(() => {
+    const notifications = [];
+
+    for (const edge of query.viewer?.notifications?.edges ?? []) {
+      if (edge?.node) {
+        notifications.push(edge.node);
+      }
+    }
+
+    notifications.reverse();
+
+    return notifications;
+  }, [query.viewer?.notifications?.edges]);
+
+  const handleSeeMore = useCallback(() => {
+    loadPrevious(NOTIFICATIONS_PER_PAGE);
+  }, [loadPrevious]);
+
+  const hasNotifications = nonNullNotifications.length > 0;
+
+  return (
+    <NotificationsContent grow>
+      {hasNotifications ? (
+        <>
+          {nonNullNotifications.map((notification) => {
+            return <Notification key={notification.id} notificationRef={notification} />;
+          })}
+
+          {hasPrevious && (
+            <SeeMoreContainer role="button" onClick={handleSeeMore}>
+              {isLoadingPrevious ? <Loader size="mini" /> : <TitleXS>See more</TitleXS>}
+            </SeeMoreContainer>
+          )}
+        </>
+      ) : (
+        <>
+          <EmptyNotificationsText>Nothing to see here yet.</EmptyNotificationsText>
+        </>
+      )}
+    </NotificationsContent>
+  );
+}
+
+const SeeMoreContainer = styled(VStack)`
+  padding: 16px 12px;
+  cursor: pointer;
+`;
+
+const EmptyNotificationsText = styled(TitleDiatypeL)`
+  text-align: center;
+`;
+
+const NotificationsContent = styled(VStack)`
+  width: 100%;
+`;
--- src/components/NotificationsModal/NotificationsModal.tsx
+++ src/components/NotificationsModal/NotificationsModal.tsx
@@ -1,87 +1,41 @@
-import { useFragment } from 'react-relay';
+import { useLazyLoadQuery } from 'react-relay';
 import { graphql } from 'relay-runtime';
-import { NotificationsModalFragment$key } from '__generated__/NotificationsModalFragment.graphql';
-import { VStack } from 'components/core/Spacer/Stack';
 import styled from 'styled-components';
 import { MODAL_PADDING_THICC_PX } from 'contexts/modal/constants';
-import { TitleDiatypeL, TitleDiatypeM, TitleXS } from 'components/core/Text/Text';
-import { Notification } from 'components/NotificationsModal/Notification';
-import { useMemo } from 'react';
+import { TitleDiatypeM } from 'components/core/Text/Text';
+import { NotificationsModalQuery } from '../../../__generated__/NotificationsModalQuery.graphql';
+import {
+  NotificationList,
+  NOTIFICATIONS_PER_PAGE,
+} from 'components/NotificationsModal/NotificationList';
+import { Suspense } from 'react';
 
 type NotificationsModalProps = {
-  queryRef: NotificationsModalFragment$key;
   fullscreen: boolean;
 };
 
-export function NotificationsModal({ queryRef, fullscreen }: NotificationsModalProps) {
-  const query = useFragment(
+export function NotificationsModal({ fullscreen }: NotificationsModalProps) {
+  const query = useLazyLoadQuery<NotificationsModalQuery>(
     graphql`
-      fragment NotificationsModalFragment on Query {
-        viewer {
-          ... on Viewer {
-            notifications(last: 10) {
-              edges {
-                node {
-                  id
-                  ...NotificationFragment
-                }
-              }
-            }
-          }
-        }
+      query NotificationsModalQuery($notificationsLast: Int!, $notificationsBefore: String) {
+        ...NotificationListFragment
       }
     `,
-    queryRef
+    { notificationsLast: NOTIFICATIONS_PER_PAGE }
   );
-
-  const nonNullNotifications = useMemo(() => {
-    const notifications = [];
-
-    for (const edge of query.viewer?.notifications?.edges ?? []) {
-      if (edge?.node) {
-        notifications.push(edge.node);
-      }
-    }
-
-    return notifications;
-  }, [query.viewer?.notifications?.edges]);
-
-  const hasNotifications = nonNullNotifications.length > 0;
 
   return (
     <ModalContent fullscreen={fullscreen}>
-      <VStack>
-        <StyledHeader>
-          <TitleDiatypeM>Notifications</TitleDiatypeM>
-        </StyledHeader>
-        <NotificationsContent grow>
-          {hasNotifications ? (
-            <>
-              {nonNullNotifications.map((notification) => {
-                return <Notification key={notification.id} notificationRef={notification} />;
-              })}
-              <div>
-                <TitleXS>See more</TitleXS>
-              </div>
-            </>
-          ) : (
-            <>
-              <EmptyNotificationsText>Nothing to see here yet.</EmptyNotificationsText>
-            </>
-          )}
-        </NotificationsContent>
-      </VStack>
+      <StyledHeader>
+        <TitleDiatypeM>Notifications</TitleDiatypeM>
+      </StyledHeader>
+
+      <Suspense fallback={null}>
+        <NotificationList queryRef={query} />
+      </Suspense>
     </ModalContent>
   );
 }
-const EmptyNotificationsText = styled(TitleDiatypeL)`
-  text-align: center;
-`;
-
-const NotificationsContent = styled(VStack)`
-  margin-top: 24px;
-  width: 100%;
-`;
 
 const StyledHeader = styled.div`
   padding-bottom: ${MODAL_PADDING_THICC_PX}px;
