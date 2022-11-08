dc351f85af72b3f86af00cf6c29e017ce0ad4053
Terence Bezman <terencebezman@gmail.com>
2022-11-08 15:12:14 -0500

    fix width of feed events for flash animation

--- src/components/Feed/dimensions.ts
+++ src/components/Feed/dimensions.ts
@@ -3,7 +3,7 @@ import { size } from '~/components/core/breakpoints';
 import { FEED_EVENT_TOKEN_MARGIN, SMALL_DIMENSIONS_DESKTOP } from './FeedEventTokenPreviews';
 
 // 105px per column * 10 columns
-export const FEED_MAX_WIDTH = 950;
+export const FEED_MAX_WIDTH = 810 + FEED_EVENT_TOKEN_MARGIN * 2;
 
 export const FEED_EVENT_ROW_WIDTH_DESKTOP =
   SMALL_DIMENSIONS_DESKTOP * 4 + FEED_EVENT_TOKEN_MARGIN * 3;
