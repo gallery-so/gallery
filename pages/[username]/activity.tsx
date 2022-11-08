186408d3694328d915ad3db795b02477c45c1331
Terence Bezman <terencebezman@gmail.com>
2022-11-08 10:44:58 -0500

    preload activity page

--- pages/[username]/activity.tsx
+++ pages/[username]/activity.tsx
@@ -1,6 +1,6 @@
 import { GetServerSideProps } from 'next';
 import { useLazyLoadQuery } from 'react-relay';
-import { graphql } from 'relay-runtime';
+import { fetchQuery, graphql } from 'relay-runtime';
 
 import { ITEMS_PER_PAGE, MAX_PIECES_DISPLAYED_PER_FEED_EVENT } from '~/components/Feed/constants';
 import { NOTES_PER_PAGE } from '~/components/Feed/Socialize/NotesModal/NotesModal';
@@ -9,34 +9,34 @@ import { activityQuery } from '~/generated/activityQuery.graphql';
 import { MetaTagProps } from '~/pages/_app';
 import GalleryRoute from '~/scenes/_Router/GalleryRoute';
 import UserActivityPage from '~/scenes/UserActivityPage/UserActivityPage';
+import { PreloadQueryArgs } from '~/types/PageComponentPreloadQuery';
 import { openGraphMetaTags } from '~/utils/openGraphMetaTags';
 
 type UserActivityProps = MetaTagProps & {
   username: string;
 };
 
+const activityQueryNode = graphql`
+  query activityQuery(
+    $username: String!
+    $interactionsFirst: Int!
+    $interactionsAfter: String
+    $viewerLast: Int!
+    $viewerBefore: String
+    $visibleTokensPerFeedEvent: Int!
+  ) {
+    ...UserActivityPageFragment
+    ...GalleryNavbarFragment
+  }
+`;
+
 export default function UserFeed({ username }: UserActivityProps) {
-  const query = useLazyLoadQuery<activityQuery>(
-    graphql`
-      query activityQuery(
-        $username: String!
-        $interactionsFirst: Int!
-        $interactionsAfter: String
-        $viewerLast: Int!
-        $viewerBefore: String
-        $visibleTokensPerFeedEvent: Int!
-      ) {
-        ...UserActivityPageFragment
-        ...GalleryNavbarFragment
-      }
-    `,
-    {
-      username: username,
-      interactionsFirst: NOTES_PER_PAGE,
-      viewerLast: ITEMS_PER_PAGE,
-      visibleTokensPerFeedEvent: MAX_PIECES_DISPLAYED_PER_FEED_EVENT,
-    }
-  );
+  const query = useLazyLoadQuery<activityQuery>(activityQueryNode, {
+    username: username,
+    interactionsFirst: NOTES_PER_PAGE,
+    viewerLast: ITEMS_PER_PAGE,
+    visibleTokensPerFeedEvent: MAX_PIECES_DISPLAYED_PER_FEED_EVENT,
+  });
 
   return (
     <GalleryRoute
@@ -45,6 +45,17 @@ export default function UserFeed({ username }: UserActivityProps) {
     />
   );
 }
+
+UserFeed.preloadQuery = ({ relayEnvironment, query }: PreloadQueryArgs) => {
+  if (query.username && typeof query.username === 'string') {
+    fetchQuery<activityQuery>(relayEnvironment, activityQueryNode, {
+      username: query.username,
+      interactionsFirst: NOTES_PER_PAGE,
+      viewerLast: ITEMS_PER_PAGE,
+      visibleTokensPerFeedEvent: MAX_PIECES_DISPLAYED_PER_FEED_EVENT,
+    }).toPromise();
+  }
+};
 
 export const getServerSideProps: GetServerSideProps<UserActivityProps> = async ({ params }) => {
   const username = params?.username ? (params.username as string) : undefined;
