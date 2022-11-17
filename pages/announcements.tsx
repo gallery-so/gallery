ec4ebe2f50b9a2df02b2ee4b6c8060dfbc79fd53
Robinnnnn <rokim8@gmail.com>
2022-11-17 14:44:51 -0500

    popover

--- pages/announcements.tsx
+++ pages/announcements.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { graphql, useLazyLoadQuery } from 'react-relay';
 
-import { TEZOS_ANNOUNCEMENT_STORAGE_KEY } from '~/constants/storageKeys';
+import { WHITE_RHINO_STORAGE_KEY } from '~/constants/storageKeys';
 import GlobalAnnouncementPopover, {
   FEATURED_COLLECTION_IDS,
 } from '~/contexts/globalLayout/GlobalAnnouncementPopover/GlobalAnnouncementPopover';
@@ -10,7 +10,7 @@ import usePersistedState from '~/hooks/usePersistedState';
 import GalleryRoute from '~/scenes/_Router/GalleryRoute';
 
 export default function Announcements() {
-  const [, setDismissed] = usePersistedState(TEZOS_ANNOUNCEMENT_STORAGE_KEY, false);
+  const [, setDismissed] = usePersistedState(WHITE_RHINO_STORAGE_KEY, false);
 
   const query = useLazyLoadQuery<announcementsQuery>(
     graphql`
