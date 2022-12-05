e024e1b2dfc66991aeafa0c5034451c933e66340
jakzaizzat <jakzaizzat@gmail.com>
2022-12-05 11:42:40 +0800

    back to chromium

--- cypress/integration/pages/page.ts
+++ cypress/integration/pages/page.ts
@@ -1,9 +1,12 @@
 export default class Page {
   acceptMetamaskAccessRequest() {
     // cy.wait(1000);
-    cy.acceptMetamaskAccess(false).should('be.true');
-    cy.confirmMetamaskSignatureRequest();
+    // cy.acceptMetamaskAccess(false).should('be.true');
     // cy.switchToCypressWindow();
     // cy.wait(1000);
+    cy.acceptMetamaskAccess().then((connected) => {
+      expect(connected).to.be.true;
+      cy.confirmMetamaskSignatureRequest();
+    });
   }
 }
