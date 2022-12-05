ba19613ece446889622cc431538f7c43994a94f3
Evgeny Boxer <evgeny.boxer@gmail.com>
2022-12-05 17:33:48 +1100

    chore: remove unused function

--- src/utils/browserHistory.ts
+++ src/utils/browserHistory.ts
@@ -1,9 +1,3 @@
-export function historyPushState(url: string | URL) {
-  if (typeof window !== 'undefined') {
-    window.history.pushState({}, '', url);
-  }
-}
-
 export function historyReplaceState(url: string | URL) {
   if (typeof window !== 'undefined') {
     window.history.replaceState({}, '', url);
