c35821da4cc8f0004a7ada2f995e0922f569b4aa
Terence Bezman <terencebezman@gmail.com>
2022-12-05 11:37:26 -0500

    add ethereum providers to shop

--- pages/shop.tsx
+++ pages/shop.tsx
@@ -1,6 +1,16 @@
+import EthereumProviders from '~/contexts/auth/EthereumProviders';
 import GalleryRoute from '~/scenes/_Router/GalleryRoute';
 import MerchStorePage from '~/scenes/MerchStorePage/MerchStorePage';
 
 export default function Privacy() {
-  return <GalleryRoute element={<MerchStorePage />} navbar={false} />;
+  return (
+    <GalleryRoute
+      element={
+        <EthereumProviders>
+          <MerchStorePage />
+        </EthereumProviders>
+      }
+      navbar={false}
+    />
+  );
 }
