2e9a23b33190bbb6c610c351e4e12594215ea4e6
Terence Bezman <terencebezman@gmail.com>
2022-12-06 15:59:24 -0500

    cleanup a few spots

--- src/components/core/Markdown/IconContainer.tsx
+++ src/components/core/Markdown/IconContainer.tsx
@@ -5,22 +5,32 @@ import colors from '../colors';
 
 type Size = 'sm' | 'md' | 'lg';
 
+type Props = {
+  onClick?: () => void;
+  icon: React.ReactElement;
+  className?: string;
+  size?: Size;
+
+  stacked?: boolean;
+
+  disabled?: boolean;
+};
+
 export default function IconContainer({
   icon,
   className,
   onClick,
   size = 'md',
-}: {
-  onClick?: () => void;
-  icon: React.ReactElement;
-  className?: string;
-  size?: Size;
-}) {
+  disabled,
+  stacked,
+}: Props) {
   return (
     <StyledIcon
       onClick={onClick}
       size={size}
+      disabled={disabled}
       className={className}
+      stacked={stacked}
       onMouseDown={(e) => {
         // This will prevent the textarea from losing focus when user clicks a markdown icon
         e.preventDefault();
@@ -31,35 +41,58 @@ export default function IconContainer({
   );
 }
 
-const StyledIcon = styled.div<{ size: Size }>`
+const StyledIcon = styled.div<{
+  size: Size;
+  disabled?: boolean;
+  stacked?: boolean;
+}>`
   display: flex;
   justify-content: center;
   align-items: center;
   cursor: pointer;
+
+  ${({ disabled }) =>
+    disabled
+      ? css`
+          color: ${colors.porcelain};
+        `
+      : css`
+          color: ${colors.offBlack};
+        `}
 
   ${({ size }) => {
     if (size === 'sm') {
       return css`
         height: 24px;
         width: 24px;
+
+        border-radius: 99999999px;
       `;
     } else if (size === 'md') {
       return css`
         height: 32px;
         width: 32px;
+
+        border-radius: 99999999px;
       `;
     } else if (size === 'lg') {
       return css`
         height: 40px;
         width: 40px;
+
+        border-radius: 99999999px;
       `;
     }
   }}
 
-  color: ${colors.offBlack};
 
   &:hover {
-    background: ${colors.faint};
+    ${({ disabled, stacked }) =>
+      disabled
+        ? null
+        : css`
+            background: ${stacked ? colors.porcelain : colors.faint};
+          `}
   }
 
   &:active {
