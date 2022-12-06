73fd2ca89173787857daab85cfb5b6d22df17284
robin <rokim8@gmail.com>
2022-12-06 18:45:18 -0500

    chore: sentry improvements (#1190)
    * disable sentry reporting in local environment
    
    * reduce sample rate to 5%

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -6,7 +6,9 @@ import * as Sentry from '@sentry/nextjs';
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
-const SENTRY_ENV = process.env.NEXT_PUBLIC_VERCEL_ENV;
+const ENV = process.env.NEXT_PUBLIC_VERCEL_ENV;
+
+const SENTRY_TRACING_SAMPLE_RATE = ENV === 'production' ? 0.05 : 1.0;
 
 Sentry.init({
   // DSNs are safe to keep public because they only allow submission of
@@ -16,10 +18,13 @@ Sentry.init({
   // want, this is a rare occurrence.
   dsn: SENTRY_DSN || 'https://bd40a4affc1740e8b7516502389262fe@o1135798.ingest.sentry.io/6187637',
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1.0,
+  tracesSampleRate: SENTRY_TRACING_SAMPLE_RATE,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
-  environment: SENTRY_ENV || 'local',
+  environment: ENV || 'local',
+  // disable sentry reporting by default if in local development.
+  // NEXT_PUBLIC_VERCEL_ENV is only set in a deployed environment.
+  enabled: ENV !== undefined,
 });
