c3e14a5a2dd6921a3ad7458a6b6d0d8180f00510
Terence Bezman <terencebezman@gmail.com>
2022-12-08 11:45:58 -0500

    add authorization to apq upload script

--- scripts/upload-persisted-queries.mjs
+++ scripts/upload-persisted-queries.mjs
@@ -7,17 +7,24 @@ const query = `
 mutation UploadPersistedQueriesMutation($persistedQueries: String!) {
   uploadPersistedQueries(input: {persistedQueries: $persistedQueries}) {
     ... on UploadPersistedQueriesPayload {
-      message
+      successMessage: message
+    }
+    ... on ErrNotAuthorized {
+      errorMessage: message
     }
   }
 }
 `
 
 
+const base64Encoded =  Buffer.from(`FrontendBuildAuth:${process.env.FRONTEND_APQ_UPLOAD_AUTH_TOKEN}`).toString('base64');
+
+let start = Date.now();
 fetch('http://localhost:4000/glry/graphql/query', {
   method: 'POST',
   headers: {
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+    'Authorization': `Basic ${base64Encoded}`
   },
   body: JSON.stringify({
     variables: {
@@ -28,5 +35,15 @@ fetch('http://localhost:4000/glry/graphql/query', {
 })
   .then((response) => response.json())
   .then((response) => {
-    console.log(JSON.stringify(response, null, 2));
-  });
+    if(response.data.uploadPersistedQueries.successMessage) {
+      console.log(`Successfully uploaded persisted queries in ${Date.now() - start}ms`);
+    } else {
+      console.log("Something went wrong while uploading the persisted queries. Here's the full resposne");
+      console.log(JSON.stringify(response, null, 2));
+    }
+
+  }).catch((error) => {
+    console.error("There was an error while trying to upload the persisted queries.", error);
+
+    process.exit(1);
+});
