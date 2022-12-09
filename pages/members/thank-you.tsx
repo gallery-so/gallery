af6534254c78f893619faf16595feb9bdffe3b96
Robinnnnn <rokim8@gmail.com>
2022-12-09 18:51:16 -0500

    /thank-you-token => /thank-you

--- pages/members/thank-you.tsx
+++ pages/members/thank-you.tsx
@@ -1,7 +1,7 @@
+import EthereumProviders from '~/contexts/auth/EthereumProviders';
 import { useIsMobileWindowWidth } from '~/hooks/useWindowSize';
 import GalleryRoute from '~/scenes/_Router/GalleryRoute';
 import ThankYouTokenPage from '~/scenes/MintPages/ThankYouTokenPage';
-import EthereumProviders from '~/contexts/auth/EthereumProviders';
 
 export default function ThankYouToken() {
   const isMobile = useIsMobileWindowWidth();
