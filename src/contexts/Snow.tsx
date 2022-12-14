ce8f05b7caeff922cd0af0feb246368656c3e417
Robinnnnn <rokim8@gmail.com>
2022-12-13 21:17:46 -0500

    improved viz

--- src/contexts/Snow.tsx
+++ src/contexts/Snow.tsx
@@ -1,74 +1,77 @@
-import { useEffect } from 'react';
+import Script from 'next/script';
 import styled from 'styled-components';
 
 export default function Snow() {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js';
-    script.onload = function () {
-      console.log('logging');
-      particlesJS('snow', {
-        particles: {
-          number: {
-            value: 50,
-            density: {
-              enable: true,
-              value_area: 800,
-            },
-          },
-          color: {
-            value: '#ffffff',
-          },
-          opacity: {
-            value: 0.7,
-            random: false,
-            anim: {
-              enable: false,
-            },
-          },
-          size: {
-            value: 5,
-            random: true,
-            anim: {
-              enable: false,
-            },
-          },
-          line_linked: {
-            enable: false,
-          },
-          move: {
-            enable: true,
-            speed: 2,
-            direction: 'bottom',
-            random: true,
-            straight: false,
-            out_mode: 'out',
-            bounce: false,
-            attract: {
-              enable: true,
-              rotateX: 300,
-              rotateY: 1200,
-            },
-          },
-        },
-        interactivity: {
-          events: {
-            onhover: {
-              enable: false,
-            },
-            onclick: {
-              enable: false,
-            },
-            resize: false,
-          },
-        },
-        retina_detect: true,
-      });
-    };
-    document.head.append(script);
-  }, []);
+  return (
+    <>
+      <StyledDiv id="snow" />
+      <Script
+        src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"
+        onLoad={handleLoad}
+      />
+    </>
+  );
+}
 
-  return <StyledDiv id="snow" />;
+function handleLoad() {
+  // @ts-expect-error: temp importing above library
+  particlesJS('snow', {
+    particles: {
+      number: {
+        value: 50,
+        density: {
+          enable: true,
+          value_area: 800,
+        },
+      },
+      color: {
+        value: '#E9E9E9',
+      },
+      opacity: {
+        value: 0.7,
+        random: false,
+        anim: {
+          enable: false,
+        },
+      },
+      size: {
+        value: 5,
+        random: true,
+        anim: {
+          enable: false,
+        },
+      },
+      line_linked: {
+        enable: false,
+      },
+      move: {
+        enable: true,
+        speed: 2,
+        direction: 'bottom',
+        random: true,
+        straight: false,
+        out_mode: 'out',
+        bounce: false,
+        attract: {
+          enable: true,
+          rotateX: 300,
+          rotateY: 1200,
+        },
+      },
+    },
+    interactivity: {
+      events: {
+        onhover: {
+          enable: false,
+        },
+        onclick: {
+          enable: false,
+        },
+        resize: false,
+      },
+    },
+    retina_detect: true,
+  });
 }
 
 const StyledDiv = styled.div`
