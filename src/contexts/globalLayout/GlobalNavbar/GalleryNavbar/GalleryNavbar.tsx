c9742243f9948af1120570dcde5850fda8f7b386
robin <rokim8@gmail.com>
2022-12-14 17:08:09 -0500

    Small changes (#1217)
    * snowflake inactive color to metal
    
    * fix spacing between title and desc
    
    * remove snowflake toggle for logged in mobile
    
    * comment

--- src/contexts/globalLayout/GlobalNavbar/GalleryNavbar/GalleryNavbar.tsx
+++ src/contexts/globalLayout/GlobalNavbar/GalleryNavbar/GalleryNavbar.tsx
@@ -38,6 +38,12 @@ export function GalleryNavbar({ queryRef, username }: Props) {
         ...GalleryNavLinksFragment
         ...FollowButtonQueryFragment
 
+        viewer {
+          ... on Viewer {
+            __typename
+          }
+        }
+
         userByUsername(username: $username) {
           ...FollowButtonUserFragment
         }
@@ -51,6 +57,8 @@ export function GalleryNavbar({ queryRef, username }: Props) {
   const { pathname } = useRouter();
 
   const userGalleryRoute: Route = { pathname: '/[username]', query: { username } };
+
+  const isLoggedInOnMobile = isMobile && query.viewer?.__typename === 'Viewer';
 
   return (
     <VStack>
@@ -80,7 +88,11 @@ export function GalleryNavbar({ queryRef, username }: Props) {
           )}
         </NavbarCenterContent>
         <NavbarRightContent>
-          <SnowToggleIcon />
+          {
+            // hide if user is logged in on mobile. this is a very rare state
+            // but there are just too many icons in this scenario
+            isLoggedInOnMobile ? null : <SnowToggleIcon />
+          }
           <GalleryRightContent username={username} queryRef={query} />
         </NavbarRightContent>
       </StandardNavbarContainer>
