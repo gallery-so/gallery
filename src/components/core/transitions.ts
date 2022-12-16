4566e4d96de0a49f085e869898af5e54f84895ad
Terence Bezman <terencebezman@gmail.com>
2022-12-16 11:31:38 -0500

    export raw animation values for bezier transition

--- src/components/core/transitions.ts
+++ src/components/core/transitions.ts
@@ -5,7 +5,12 @@ export const ANIMATED_COMPONENT_TRANSLATION_PIXELS_LARGE = 16;
 // translation for smaller items like tooltips
 export const ANIMATED_COMPONENT_TRANSLATION_PIXELS_SMALL = 8;
 
-const cubic = `${ANIMATED_COMPONENT_TRANSITION_MS}ms cubic-bezier(0.4, 0, 0.6, 1)`;
+const cubicValues = [0.4, 0, 0.6, 1];
+const cubic = `${ANIMATED_COMPONENT_TRANSITION_MS}ms cubic-bezier(${cubicValues.join(', ')})`;
+
+export const rawTransitions = {
+  cubicValues,
+};
 
 const transitions = {
   cubic,
