51cf52edd3019a0b97d37c2bdf367df92198672d
robin <rokim8@gmail.com>
2022-12-19 18:48:18 -0500

    google analytics (#1220)

---
+++ src/components/GoogleAnalytics.tsx
@@ -0,0 +1,21 @@
+import Script from 'next/script';
+
+export default function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-0WWCB2PQB5"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-0WWCB2PQB5');
+        `}
+      </Script>
+    </>
+  );
+}
