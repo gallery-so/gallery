ecdb36f230b2c995821c1ddeb5e25bf51041a49c
Terence Bezman <terencebezman@gmail.com>
2022-12-23 14:43:29 -0500

    disable probelmatic integrations

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -2,6 +2,8 @@
 // The config you add here will be used whenever a page is visited.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
+import { Breadcrumbs, Dedupe, HttpContext, LinkedErrors } from '@sentry/browser';
+import { Integrations as CoreIntegrations } from '@sentry/core';
 import * as Sentry from '@sentry/nextjs';
 
 import { ErrorWithSentryMetadata } from './src/errors/ErrorWithSentryMetadata';
@@ -17,17 +19,35 @@ const SENTRY_TRACING_ORIGIN = IS_PROD ? 'api.gallery.so' : 'api.dev.gallery.so';
 const SENTRY_TRACING_SAMPLE_RATE = IS_PROD ? 0.05 : 1.0;
 
 Sentry.init({
+  defaultIntegrations: false,
+  integrations: [
+    // Defaults from Sentry
+    // https://sourcegraph.com/github.com/getsentry/sentry-javascript@36488ec3ef5394c97dcf3c3ef25c09bab266e5ea/-/blob/packages/browser/src/sdk.ts?L23
+    new CoreIntegrations.InboundFilters(),
+    new CoreIntegrations.FunctionToString(),
+    new Breadcrumbs(),
+    new LinkedErrors(),
+    new Dedupe(),
+    new HttpContext(),
+
+    // The defaults include these two but we want to disable them to ensure
+    // that errors thrown during a render don't get double reported
+    // new TryCatch(),
+    // new GlobalHandlers(),
+
+    // This gives us extra metadata about HTTP requests, page navigation, etc.
+    new Sentry.BrowserTracing({
+      tracingOrigins: [SENTRY_TRACING_ORIGIN, 'localhost'],
+    }),
+  ],
+
   // DSNs are safe to keep public because they only allow submission of
   // new events and related event data; they do not allow read access to
   // any information. While there is a risk of abusing a DSN, where any
   // user can send events to your organization with any information they
   // want, this is a rare occurrence.
   dsn: SENTRY_DSN || 'https://bd40a4affc1740e8b7516502389262fe@o1135798.ingest.sentry.io/6187637',
-  integrations: [
-    new Sentry.BrowserTracing({
-      tracingOrigins: [SENTRY_TRACING_ORIGIN, 'localhost'],
-    }),
-  ],
+
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: SENTRY_TRACING_SAMPLE_RATE,
   // ...
