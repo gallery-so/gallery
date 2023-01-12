8151b6a9920af082ee2f604cb5c544ec90f76c49
robin <rokim8@gmail.com>
2023-01-11 23:52:14 -0500

    Hyperlink discord channel (#1254)

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -3,6 +3,6 @@ export const GALLERY_FAQ =
 export const GALLERY_JOBS =
   'https://gallery-so.notion.site/Careers-e8d78dea54834630928f075f4a4ccdba';
 export const GALLERY_TWITTER = 'https://twitter.com/gallery';
-export const GALLERY_DISCORD = 'https://discord.gg/BM4qhEy3Qj';
+export const GALLERY_DISCORD = 'https://discord.gg/MREFJFvBTe';
 export const GALLERY_MEMBERSHIP_OPENSEA = 'https://opensea.io/collection/gallery-membership-cards';
 export const GALLERY_BLOG = 'https://gallery.mirror.xyz';
