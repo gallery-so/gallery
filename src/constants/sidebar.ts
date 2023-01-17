eda19042da7a4857f9ffdf8fe952dc3ae24759db
Robinnnnn <rokim8@gmail.com>
2023-01-17 17:24:19 -0500

    reduce gap between sidebar pieces

--- src/constants/sidebar.ts
+++ src/constants/sidebar.ts
@@ -1,5 +1,5 @@
 export const COLUMN_COUNT = 3;
-export const SIDEBAR_ICON_GAP = 16;
-export const SIDEBAR_ICON_DIMENSIONS = 60;
+export const SIDEBAR_ICON_GAP = 2;
+export const SIDEBAR_ICON_DIMENSIONS = 69;
 export const SIDEBAR_COLLECTION_TITLE_HEIGHT = 32;
 export const SIDEBAR_COLLECTION_TITLE_BOTTOM_SPACE = 12;
