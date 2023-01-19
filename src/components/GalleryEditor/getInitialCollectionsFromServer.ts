719ad595bc47e995204a176613a9f43ba382e7c5
Terence Bezman <terencebezman@gmail.com>
2023-01-19 12:39:36 -0500

    initialize editor with default state when creating a collection

--- src/components/GalleryEditor/getInitialCollectionsFromServer.ts
+++ src/components/GalleryEditor/getInitialCollectionsFromServer.ts
@@ -1,9 +1,15 @@
 import { graphql } from 'react-relay';
 import { readInlineData } from 'relay-runtime';
 
-import { CollectionMap, StagedSectionMap } from '~/components/GalleryEditor/GalleryEditorContext';
+import {
+  CollectionMap,
+  CollectionState,
+  StagedSection,
+  StagedSectionMap,
+} from '~/components/GalleryEditor/GalleryEditorContext';
 import { getInitialCollectionsFromServerFragment$key } from '~/generated/getInitialCollectionsFromServerFragment.graphql';
 import { parseCollectionLayoutGraphql } from '~/utils/collectionLayout';
+import { generate12DigitId } from '~/utils/generate12DigitId';
 import { removeNullValues } from '~/utils/removeNullValues';
 
 export function getInitialCollectionsFromServer(
@@ -51,6 +57,34 @@ export function getInitialCollectionsFromServer(
   const collections: CollectionMap = {};
 
   const queryCollections = removeNullValues(gallery?.collections);
+
+  if (queryCollections.length === 0) {
+    const generatedCollectionId = generate12DigitId();
+    const generatedSectionId = generate12DigitId();
+
+    const initialSection: StagedSection = {
+      columns: 3,
+      items: [],
+    };
+
+    const initialCollection: CollectionState = {
+      dbid: generatedCollectionId,
+      localOnly: true,
+
+      liveDisplayTokenIds: new Set(),
+
+      name: '',
+      collectorsNote: '',
+      hidden: false,
+
+      sections: {
+        [generatedSectionId]: initialSection,
+      },
+      activeSectionId: generatedSectionId,
+    };
+
+    collections[generatedCollectionId] = initialCollection;
+  }
 
   for (const collection of queryCollections) {
     const sections: StagedSectionMap = {};
