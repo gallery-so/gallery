d9750d1967d09850a3fb38a0b90f77112fe21fc5
Terence Bezman <terencebezman@gmail.com>
2023-01-24 16:47:10 -0500

    use nextjs link instead of a tag

--- src/contexts/globalLayout/GlobalNavbar/HomeNavbar/HomeNavbar.tsx
+++ src/contexts/globalLayout/GlobalNavbar/HomeNavbar/HomeNavbar.tsx
@@ -59,7 +59,9 @@ export function HomeNavbar({ queryRef }: Props) {
       <NavbarCenterContent>
         <HStack gap={8}>
           <NavbarLink
+            legacyBehavior={false}
             active={pathname === activityRoute.pathname}
+            // @ts-expect-error We're not using the legacy Link
             href={route(activityRoute)}
             onClick={handleActivityModeClick}
           >
@@ -67,7 +69,9 @@ export function HomeNavbar({ queryRef }: Props) {
           </NavbarLink>
 
           <NavbarLink
+            legacyBehavior={false}
             active={pathname === featuredRoute.pathname}
+            // @ts-expect-error We're not using the legacy Link
             href={route(featuredRoute)}
             onClick={handleFeaturedModeClick}
           >
