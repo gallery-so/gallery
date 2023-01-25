c8773c252daef3156a34d19aeb08287754d23ac1
Terence Bezman <terencebezman@gmail.com>
2023-01-25 12:55:08 -0500

    flash "Saved" state if the user has already saved

--- src/contexts/globalLayout/EditGalleryNavbar/EditGalleryNavbar.tsx
+++ src/contexts/globalLayout/EditGalleryNavbar/EditGalleryNavbar.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import { useCallback, useMemo } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import styled from 'styled-components';
 
 import breakpoints from '~/components/core/breakpoints';
@@ -15,6 +15,7 @@ import {
   NavbarRightContent,
   StandardNavbarContainer,
 } from '~/contexts/globalLayout/GlobalNavbar/StandardNavbarContainer';
+import { useSaveHotkey } from '~/hooks/useSaveHotkey';
 import { useIsMobileOrMobileLargeWindowWidth } from '~/hooks/useWindowSize';
 import { AllGalleriesIcon } from '~/icons/AllGalleriesIcon';
 
@@ -71,13 +72,29 @@ export function EditGalleryNavbar({
     return 'no-changes';
   }, [canSave, hasSaved, hasUnsavedChanges]);
 
+  // Flash the "Saved" state if the user has nothing to save
+  // and the hit "Cmd+S" anyway
+  const [showSaved, setShowSaved] = useState(true);
+  useSaveHotkey(() => {
+    if (doneAction === 'saved') {
+      setShowSaved(false);
+
+      setTimeout(() => {
+        setShowSaved(true);
+      }, 500);
+    }
+  });
+
   const doneButton = useMemo(() => {
     if (doneAction === 'no-changes') {
       return <DoneButton onClick={onBack}>Done</DoneButton>;
     } else if (doneAction === 'saved') {
       return (
         <>
-          <BaseM color={colors.metal}>Saved</BaseM>
+          <SavedText show={showSaved} color={colors.metal}>
+            Saved
+          </SavedText>
+
           <DoneButton onClick={onBack}>Done</DoneButton>
         </>
       );
@@ -97,7 +114,7 @@ export function EditGalleryNavbar({
         </>
       );
     }
-  }, [doneAction, hasUnsavedChanges, onBack, onDone]);
+  }, [doneAction, hasUnsavedChanges, onBack, onDone, showSaved]);
 
   return (
     <Wrapper>
@@ -134,6 +151,11 @@ const AllGalleriesWrapper = styled(HStack)`
   :hover {
     background-color: ${colors.faint};
   }
+`;
+
+const SavedText = styled(BaseM)<{ show: boolean }>`
+  transition: opacity 300ms ease-in-out;
+  opacity: ${({ show }) => (show ? '1' : '0')};
 `;
 
 const DoneButton = styled(Button)`
