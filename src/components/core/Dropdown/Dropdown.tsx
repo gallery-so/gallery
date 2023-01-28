02555d6813ba451751804db7f47ae65aed8dfc77
Robinnnnn <rokim8@gmail.com>
2023-01-27 21:55:01 -0500

    prevent redirect when clicking outside modal

--- src/components/core/Dropdown/Dropdown.tsx
+++ src/components/core/Dropdown/Dropdown.tsx
@@ -29,6 +29,7 @@ export function Dropdown({
 }: Props) {
   const handleClose = useCallback<MouseEventHandler>(
     (event) => {
+      event.preventDefault();
       event.stopPropagation();
 
       onClose();
