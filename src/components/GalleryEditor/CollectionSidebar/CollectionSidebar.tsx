be8f770b28f2fece2f5904865cca2107cc2dd3f5
Jakz <jakzaizzat@gmail.com>
2023-02-02 02:27:04 +0800

    [GAL-2036] add link to support article in gallery editor (#1304)
    * Added placement support to tooltip
    
    * Add support link to editor
    
    * Update svg icon
    
    * new link
    
    ---------
    
    Co-authored-by: Robinnnnn <rokim8@gmail.com>

--- src/components/GalleryEditor/CollectionSidebar/CollectionSidebar.tsx
+++ src/components/GalleryEditor/CollectionSidebar/CollectionSidebar.tsx
@@ -3,12 +3,20 @@ import styled from 'styled-components';
 
 import colors from '~/components/core/colors';
 import IconContainer from '~/components/core/IconContainer';
+import InteractiveLink from '~/components/core/InteractiveLink/InteractiveLink';
 import { HStack, VStack } from '~/components/core/Spacer/Stack';
 import { TitleS } from '~/components/core/Text/Text';
 import { CollectionSearch } from '~/components/GalleryEditor/CollectionSidebar/CollectionSearch';
 import { CreateCollectionIcon } from '~/components/GalleryEditor/CollectionSidebar/CreateCollectionIcon';
 import { useGalleryEditorContext } from '~/components/GalleryEditor/GalleryEditorContext';
+import { NewTooltip } from '~/components/Tooltip/NewTooltip';
+import { useTooltipHover } from '~/components/Tooltip/useTooltipHover';
 import { CollectionSidebarQueryFragment$key } from '~/generated/CollectionSidebarQueryFragment.graphql';
+import { QuestionMarkIcon } from '~/icons/QuestionMarkIcon';
+
+// TODO: Replace with notion docs link
+const NOTION_DOCS_URL =
+  'ghttps://www.notion.so/gallery-so/Creating-a-new-gallery-and-organizing-your-NFTs-b407a174a2ee44748bb9952abd803290';
 
 function TitleSection() {
   const { createCollection } = useGalleryEditorContext();
@@ -16,7 +24,6 @@ function TitleSection() {
   return (
     <TitleSectionWrapper align="center" justify="space-between">
       <TitleS>Collections</TitleS>
-
       <IconContainer
         variant="default"
         size="sm"
@@ -30,6 +37,7 @@ function TitleSection() {
 type Props = {
   queryRef: CollectionSidebarQueryFragment$key;
 };
+
 export function CollectionSidebar({ queryRef }: Props) {
   const query = useFragment(
     graphql`
@@ -40,16 +48,39 @@ export function CollectionSidebar({ queryRef }: Props) {
     queryRef
   );
 
+  const { floating, reference, getFloatingProps, getReferenceProps, floatingStyle } =
+    useTooltipHover({
+      placement: 'top-start',
+    });
+
   return (
     <CollectionSidebarWrapper gap={8}>
       <TitleSection />
-
       <CollectionSearch queryRef={query} />
+
+      <NewTooltip
+        {...getFloatingProps()}
+        style={floatingStyle}
+        ref={floating}
+        text="How to set up your gallery"
+      />
+      <InteractiveLink href={NOTION_DOCS_URL}>
+        <a>
+          <StyledIconContainer
+            variant="blue"
+            size="md"
+            icon={<QuestionMarkIcon />}
+            ref={reference}
+            {...getReferenceProps()}
+          />
+        </a>
+      </InteractiveLink>
     </CollectionSidebarWrapper>
   );
 }
 
 const CollectionSidebarWrapper = styled(VStack)`
+  position: relative;
   min-width: 250px;
   max-width: 250px;
 
@@ -64,4 +95,16 @@ const CollectionSidebarWrapper = styled(VStack)`
 
 const TitleSectionWrapper = styled(HStack)`
   padding: 0 16px;
+`;
+
+const StyledIconContainer = styled(IconContainer)`
+  background-color: ${colors.offBlack};
+  position: absolute;
+  bottom: 16px;
+  left: 16px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${colors.metal};
+  }
 `;
