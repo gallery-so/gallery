b2619a81e96cac97313ef01aeae8fe4131f6d948
kaito <80802871+kaitoo1@users.noreply.github.com>
2023-02-03 18:58:25 +0900

    update env var name

--- apps/web/src/hooks/useMagicLink.tsx
+++ apps/web/src/hooks/useMagicLink.tsx
@@ -2,7 +2,7 @@ import { Magic } from 'magic-sdk';
 import { useCallback } from 'react';
 
 export default function useMagicLogin() {
-  const m = new Magic(process.env.NEXT_MAGIC_LINK_PUBLISHABLE_KEY ?? '');
+  const m = new Magic(process.env.NEXT_PUBLIC_MAGIC_LINK_PUBLISHABLE_KEY ?? '');
 
   return useCallback(
     async (email: string) => await m.auth.loginWithMagicLink({ email, showUI: false }),
