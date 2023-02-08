5e684ed4034c04f13bbdca2523802db2f5a96c49
Jakz <jakzaizzat@gmail.com>
2023-02-08 21:19:48 +0800

    [GAL-2325] fix help button position (#1328)
    * help button fixed position
    
    * Add bottom spacing to the sidebar

--- apps/web/src/components/GalleryEditor/CollectionSidebar/CollectionSidebar.tsx
+++ apps/web/src/components/GalleryEditor/CollectionSidebar/CollectionSidebar.tsx
@@ -87,7 +87,7 @@ const CollectionSidebarWrapper = styled(VStack)`
   max-height: 100%;
   height: 100%;
 
-  padding-top: 16px;
+  padding: 16px 0 48px;
 
   border-right: 1px solid ${colors.porcelain};
 `;
@@ -98,7 +98,7 @@ const TitleSectionWrapper = styled(HStack)`
 
 const StyledIconContainer = styled(IconContainer)`
   background-color: ${colors.offBlack};
-  position: absolute;
+  position: fixed;
   bottom: 16px;
   left: 16px;
   cursor: pointer;
