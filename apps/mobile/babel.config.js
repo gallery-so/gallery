410064d2fd3b55bc4fc7ca3d4856c7a9d35ef63c
Terence Bezman <terencebezman@gmail.com>
2023-02-08 18:04:16 -0500

    setup babel plugin relay

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -2,6 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["tsconfig-paths-module-resolver"],
+    plugins: ["babel-plugin-relay", "tsconfig-paths-module-resolver"],
   };
 };
