0f719d436b4a7cde6363755c442136a06c313fc7
jakzaizzat <jakzaizzat@gmail.com>
2023-02-09 16:44:49 +0800

    Added moweb reorder

---
+++ apps/web/src/icons/ArrowDownIcon.tsx
@@ -0,0 +1,7 @@
+export default function ArrowDownIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="20" fill="none">
+      <path stroke="#000" strokeMiterlimit="10" d="m1 12 7 7 7-7M8 19V0" />
+    </svg>
+  );
+}
---
+++ apps/web/src/icons/ArrowUpIcon.tsx
@@ -0,0 +1,7 @@
+export default function ArrowUpIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="20" fill="none">
+      <path stroke="#000" strokeMiterlimit="10" d="M15 8 8 1 1 8M8 1v19" />
+    </svg>
+  );
+}
