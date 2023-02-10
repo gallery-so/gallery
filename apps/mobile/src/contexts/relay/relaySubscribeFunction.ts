050e096fe5ec1ad002df8e976d5ab81aa2771298
Terence Bezman <terencebezman@gmail.com>
2023-02-10 14:29:11 -0500

    fix ws implementation on mobile

--- apps/mobile/src/contexts/relay/relaySubscribeFunction.ts
+++ apps/mobile/src/contexts/relay/relaySubscribeFunction.ts
@@ -1,19 +1,23 @@
-import { Client, createClient } from 'graphql-ws';
-import { Observable, SubscribeFunction } from 'relay-runtime';
+import { Client, createClient } from "graphql-ws";
+import { Observable, SubscribeFunction } from "relay-runtime";
 
 let websocketClient: Client;
 
-if (typeof window !== 'undefined') {
-  websocketClient = createClient({
-    url:
-      process.env.NEXT_PUBLIC_GRAPHQL_SUBSCRIPTION_URL ?? 'ws://localhost:4000/glry/graphql/query',
-  });
-}
+websocketClient = createClient({
+  url:
+    process.env.NEXT_PUBLIC_GRAPHQL_SUBSCRIPTION_URL ??
+    "ws://localhost:4000/glry/graphql/query",
+});
 
-export const relaySubscribeFunction: SubscribeFunction = (operation, variables) => {
+export const relaySubscribeFunction: SubscribeFunction = (
+  operation,
+  variables
+) => {
   return Observable.create((sink) => {
     if (!operation.text) {
-      throw new Error('Relay subscribe function called without any operation text');
+      throw new Error(
+        "Relay subscribe function called without any operation text"
+      );
     }
 
     return websocketClient.subscribe(
