f797e5b93eb6d3b8fd866f58672a782816001ae9
jakzaizzat <jakzaizzat@gmail.com>
2023-02-15 13:51:39 +0800

    Added connected pill

---
+++ apps/web/src/icons/LockIcon.tsx
@@ -0,0 +1,12 @@
+export default function LockIcon() {
+  return (
+    <svg width="14" height="15" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M12.6666 7H1.33325V14.3333H12.6666V7Z" stroke="black" />
+      <path d="M7 9.66667V11.3333" stroke="black" />
+      <path
+        d="M11 7V5.33333C11 4.27247 10.5786 3.25505 9.82843 2.50491C9.07828 1.75476 8.06087 1.33333 7 1.33333C5.93913 1.33333 4.92172 1.75476 4.17157 2.50491C3.42143 3.25505 3 4.27247 3 5.33333V7"
+        stroke="black"
+      />
+    </svg>
+  );
+}
