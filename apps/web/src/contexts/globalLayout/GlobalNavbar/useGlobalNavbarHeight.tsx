22e60713bd031f3644ba8bf156154759d26609a2
Robinnnnn <rokim8@gmail.com>
2023-02-16 01:06:40 -0500

    potentially kill useGlobalNavbarHeight

--- apps/web/src/contexts/globalLayout/GlobalNavbar/useGlobalNavbarHeight.tsx
+++ apps/web/src/contexts/globalLayout/GlobalNavbar/useGlobalNavbarHeight.tsx
@@ -1,31 +1,36 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 
-const INITIAL_HEIGHT = 64;
+// const INITIAL_HEIGHT = 64;
 
 export function useGlobalNavbarHeight() {
-  const [height, setHeight] = useState(INITIAL_HEIGHT);
+  return 64;
+  // TODO: unclear what this logic was for; toss if no longer necessary
+  //
+  // const [height, setHeight] = useState(INITIAL_HEIGHT);
 
-  useEffect(() => {
-    if (!global.ResizeObserver) {
-      return;
-    }
+  // useEffect(() => {
+  //   if (!global.ResizeObserver) {
+  //     return;
+  //   }
 
-    const observer = new ResizeObserver((entries) => {
-      const [entry] = entries;
+  //   const observer = new ResizeObserver((entries) => {
+  //     const [entry] = entries;
 
-      if (!entry) {
-        return;
-      }
+  //     if (!entry) {
+  //       return;
+  //     }
 
-      setHeight(entry.borderBoxSize?.[0]?.blockSize ?? INITIAL_HEIGHT);
-    });
+  //     console.log('setting', entry.borderBoxSize?.[0]?.blockSize);
 
-    const element = document.querySelector('.GlobalNavbar');
+  //     setHeight(entry.borderBoxSize?.[0]?.blockSize ?? INITIAL_HEIGHT);
+  //   });
 
-    if (element) {
-      observer.observe(element);
-    }
-  }, []);
+  //   const element = document.querySelector('.GlobalNavbar');
 
-  return height;
+  //   if (element) {
+  //     observer.observe(element);
+  //   }
+  // }, []);
+
+  // return height;
 }
