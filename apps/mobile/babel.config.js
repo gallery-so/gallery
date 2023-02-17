a9469cca8647cf0278a189c600d6a314c09de6cd
Terence Bezman <terencebezman@gmail.com>
2023-02-16 21:57:54 -0500

    install nativewind babel plugin

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -2,6 +2,10 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["babel-plugin-relay", "tsconfig-paths-module-resolver"],
+    plugins: [
+      "nativewind/babel",
+      "babel-plugin-relay",
+      "tsconfig-paths-module-resolver",
+    ],
   };
 };
