b12f48cc7155f338c44eb99ced54e5ef8f9f3a38
Terence Bezman <terencebezman@gmail.com>
2023-02-16 22:03:32 -0500

    add colors to tailwind config

--- apps/mobile/tailwind.config.js
+++ apps/mobile/tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ["App.tsx", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        offWhite: "#F9F9F9",
+        white: "#FEFEFE",
+        offBlack: "#141414",
+        shadow: "#707070",
+        metal: "#9e9e9e",
+        faint: "#f2f2f2",
+        porcelain: "#e2e2e2",
+        hyperBlue: "#001CC1",
+        activeBlue: "#0022F0",
+        error: "#FF6666",
+        red: "#F00000",
+      },
+    },
   },
   plugins: [],
 };
