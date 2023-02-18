77085a4fc3f0cdf5b872e2db57a77d7bb939457b
Terence Bezman <terencebezman@gmail.com>
2023-02-17 19:21:15 -0500

    use multi project config format

--- relay.config.js
+++ relay.config.js
@@ -1,18 +1,36 @@
 const path = require("path");
 
+function generateConfig(projectDir) {
+  return {
+    schema: "schema.graphql",
+    language: "typescript",
+    output: path.join(projectDir, "__generated__"),
+
+    customScalarTypes: {
+      Email: "string",
+      Address: "string",
+      DBID: "string",
+    },
+
+    persist: {
+      file: "persisted_queries.json",
+      algorithm: "SHA256", // this can be one of MD5, SHA256, SHA1
+    },
+  };
+}
+
 module.exports = {
-  src: "./",
-  exclude: [".next/**/*", "node_modules/**/*"],
-  schema: path.resolve(__dirname, "schema.graphql"),
-  language: "typescript",
-  artifactDirectory: path.join(process.cwd(), "__generated__"),
-  customScalars: {
-    Email: "string",
-    Address: "string",
-    DBID: "string",
+  root: __dirname,
+  sources: {
+    "apps/web": "web",
+    "apps/mobile": "mobile",
+
+    // This means web, mobile, and the shared package can use fragments from the shared package.
+    "packages/shared": ["web", "mobile", "shared"],
   },
-  persistConfig: {
-    file: path.join(process.cwd(), "persisted_queries.json"),
-    algorithm: "SHA256", // this can be one of MD5, SHA256, SHA1
+  projects: {
+    web: generateConfig("apps/web"),
+    mobile: generateConfig("apps/mobile"),
+    shared: generateConfig("packages/shared"),
   },
 };
