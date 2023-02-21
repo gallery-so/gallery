a9cb35dec181780a3cc322b61c48ca66e4e3dac9
Terence Bezman <terencebezman@gmail.com>
2023-02-21 11:58:11 -0500

    add ts expect errors and logbox ignores

--- apps/mobile/index.ts
+++ apps/mobile/index.ts
@@ -1,6 +1,12 @@
+// @ts-expect-error No type declarations
 import { polyfill as polyfillEncoding } from "react-native-polyfill-globals/src/encoding";
+// @ts-expect-error No type declarations
 import { polyfill as polyfillFetch } from "react-native-polyfill-globals/src/fetch";
+// @ts-expect-error No type declarations
 import { polyfill as polyfillReadableStream } from "react-native-polyfill-globals/src/readable-stream";
+
+// Unimportant warnings from the fetch polyfill
+LogBox.ignoreLogs(["The provided value 'moz", "The provided value 'ms-stream"]);
 
 polyfillEncoding();
 polyfillFetch();
@@ -9,5 +15,6 @@ polyfillReadableStream();
 import registerRootComponent from "expo/build/launch/registerRootComponent";
 
 import App from "./App";
+import { LogBox } from "react-native";
 
 registerRootComponent(App);
