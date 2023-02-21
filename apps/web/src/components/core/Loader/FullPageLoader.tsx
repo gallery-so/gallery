4ee5c01b72e1f68eb22e66df524b39bd2665c8eb
Robinnnnn <rokim8@gmail.com>
2023-02-21 00:32:13 -0500

    deprecate FullPageLoaderWithLayoutTransitionSupport

--- apps/web/src/components/core/Loader/FullPageLoader.tsx
+++ apps/web/src/components/core/Loader/FullPageLoader.tsx
@@ -1,10 +1,4 @@
-import { useEffect } from 'react';
 import styled from 'styled-components';
-
-import {
-  useGlobalLayoutActions,
-  useGlobalLayoutState,
-} from '~/contexts/globalLayout/GlobalLayoutContext';
 
 import CapitalGLoader from './CapitalGLoader';
 
@@ -22,59 +16,4 @@ const StyledFullPageLoader = styled.div`
   align-items: center;
   height: 100vh;
   width: 100vw;
-`;
-
-/**
- * Same component as above, but with transition support. It handles:
- * 1) communicating when it's mounted vs. not (e.g. when we're hitting Suspense during a transition)
- * 2) appearing *over* or *under* the navbar depending on context
- *
- * This can only be used on components mounted beneath the GlobalLayoutContext.
- */
-export function FullPageLoaderWithLayoutTransitionSupport() {
-  const { isPageInSuspenseRef, isNavbarVisible, wasNavbarVisible } = useGlobalLayoutState();
-  const { setIsPageInSuspenseState } = useGlobalLayoutActions();
-
-  useEffect(() => {
-    // why we use both React State and Ref is explained in `GlobalLayoutContext.tsx`
-    isPageInSuspenseRef.current = true;
-    setIsPageInSuspenseState(true);
-    return () => {
-      isPageInSuspenseRef.current = false;
-      setIsPageInSuspenseState(false);
-    };
-  }, [isPageInSuspenseRef, setIsPageInSuspenseState]);
-
-  return (
-    <StyledFullPageLoaderWithLayoutTransitionSupport
-      /**
-       * The navbar should only stay within view if it was visible on the previous page, and is
-       * also visible on the next page.
-       *
-       * Otherwise, we force the FullPageLoader to appear *over* the navbar to avoid seeing it
-       * flash in and out.
-       */
-      keepNavbarInView={isNavbarVisible && wasNavbarVisible}
-    >
-      <CapitalGLoader />
-    </StyledFullPageLoaderWithLayoutTransitionSupport>
-  );
-}
-
-const StyledFullPageLoaderWithLayoutTransitionSupport = styled.div<{ keepNavbarInView?: boolean }>`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  height: 100vh;
-
-  ${({ keepNavbarInView }) => {
-    return keepNavbarInView
-      ? ''
-      : `
-      position: relative;
-      z-index: 4;
-      background: white;
-    `;
-  }}
 `;
