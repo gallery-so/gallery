9c9ab4fd0aa71ea9d25accd2c0476c2eaad1af11
Terence Bezman <terencebezman@gmail.com>
2023-02-21 13:43:35 -0500

    Setup GraphQL Defer support on mobile (#1360)
    * update all relay tasks to run their respective project
    
    * add multipart fetch lib
    
    * add defer directive to schema
    
    * add all the polyfill libraries
    
    * use graphql gateway
    
    * fix artifact directory in babel plugin
    
    * use polyfill libs
    
    * shitty implementation
    
    * split out relay network into defer / no defer w/ shared utils
    
    * add ts expect errors and logbox ignores
    
    * wire up defer support on mobile
    
    * try it out
    
    * make sure relay knows which part is the final response

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,9 +1,54 @@
 import "expo-dev-client";
 
-import { RelayEnvironmentProvider } from "react-relay";
+import {
+  graphql,
+  RelayEnvironmentProvider,
+  useFragment,
+  useLazyLoadQuery,
+} from "react-relay";
 import { Suspense, useState } from "react";
 import { createRelayEnvironment } from "./src/contexts/relay/RelayProvider";
-import { Text } from "react-native";
+import { AppDeferredDataQuery } from "~/generated/relay/AppDeferredDataQuery.graphql";
+import { SafeAreaView, Text } from "react-native";
+import { AppRobinFragment$key } from "~/generated/relay/AppRobinFragment.graphql";
+
+function RobinUser({ queryRef }: { queryRef: AppRobinFragment$key }) {
+  const query = useFragment(
+    graphql`
+      fragment AppRobinFragment on Query {
+        userByUsername(username: "robin") {
+          ... on GalleryUser {
+            username
+          }
+        }
+      }
+    `,
+    queryRef
+  );
+
+  return <Text>{query.userByUsername?.username}</Text>;
+}
+
+const QueryNode = graphql`
+  query AppDeferredDataQuery {
+    test: userByUsername(username: "xd") {
+      __typename
+    }
+    ...AppRobinFragment @defer(label: "RobinFragment")
+  }
+`;
+
+function DeferredData() {
+  const query = useLazyLoadQuery<AppDeferredDataQuery>(QueryNode, {});
+
+  return (
+    <SafeAreaView>
+      <Suspense fallback={<Text>Loading Robin...</Text>}>
+        <RobinUser queryRef={query} />
+      </Suspense>
+    </SafeAreaView>
+  );
+}
 
 export default function App() {
   const [relayEnvironment] = useState(() => createRelayEnvironment());
@@ -11,7 +56,7 @@ export default function App() {
   return (
     <RelayEnvironmentProvider environment={relayEnvironment}>
       <Suspense fallback={null}>
-        <Text>Hello, World!</Text>
+        <DeferredData />
       </Suspense>
     </RelayEnvironmentProvider>
   );
---
+++ packages/shared/src/relay/deferNetwork.ts
@@ -0,0 +1,197 @@
+import fetchMultipart from "fetch-multipart-graphql";
+import {
+  FetchFunction,
+  GraphQLSingularResponse,
+  Observable,
+} from "relay-runtime";
+import { isResponsePersistedQueryNotFound } from "./utils";
+
+export type PersistedQueriesMap = Record<string, string>;
+
+export type PersistedQueriesFetcher = () => Promise<PersistedQueriesMap>;
+
+type DeferResponse =
+  | {
+      hasNext?: boolean;
+      incremental: GraphQLSingularResponse[];
+    }
+  | { data: GraphQLSingularResponse; hasNext?: boolean }
+  | GraphQLSingularResponse;
+
+type InternalFetchFunction = (
+  createRelayFetchFunctionArgs: CreateRelayFetchFunctionArgs,
+  sinkHandlers: SinkHandlers,
+  ...args: Parameters<FetchFunction>
+) => ReturnType<typeof fetchMultipart<DeferResponse>>;
+
+type SinkHandlers = {
+  onError: (error: unknown) => void;
+  onNext: (data: DeferResponse[]) => void;
+  onComplete: () => void;
+};
+
+type FlattenedDeferResponse = {
+  data: ReadonlyArray<GraphQLSingularResponse>;
+  isLast: boolean;
+};
+function flattenDeferResponse(
+  responses: DeferResponse[]
+): FlattenedDeferResponse {
+  const formatted: ReadonlyArray<GraphQLSingularResponse> = responses.flatMap(
+    (part) => {
+      if ("incremental" in part) {
+        return part.incremental;
+      }
+
+      return [part];
+    }
+  );
+
+  const anyOfTheResponsesAreTheLast = responses.some((response) => {
+    if ("hasNext" in response) {
+      return !response.hasNext;
+    }
+
+    return false;
+  });
+
+  if (anyOfTheResponsesAreTheLast) {
+    const lastPart = formatted[formatted.length - 1];
+
+    if (lastPart) {
+      // Shoutout the Coinbase folks https://github.com/facebook/relay/issues/3904
+      lastPart.extensions = { is_final: true };
+    }
+  }
+
+  return { data: formatted, isLast: anyOfTheResponsesAreTheLast };
+}
+
+const fetchWithJustHash: InternalFetchFunction = async (
+  { url },
+  sinkHandlers,
+  request,
+  variables,
+  cacheConfig,
+  uploadables
+) => {
+  return fetchMultipart<DeferResponse>(
+    url(request, variables, cacheConfig, uploadables),
+    {
+      method: "POST",
+      credentials: "include",
+      body: JSON.stringify({
+        operationName: request.name,
+        extensions: {
+          persistedQuery: {
+            version: 1,
+            sha256Hash: request.id,
+          },
+        },
+        variables,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "multipart/mixed; deferSpec=20220824",
+      },
+      ...sinkHandlers,
+    }
+  );
+};
+
+const fetchWithHashAndQueryText: InternalFetchFunction = async (
+  { url, persistedQueriesFetcher },
+  sinkHandlers,
+  request,
+  variables,
+  cacheConfig,
+  uploadables
+) => {
+  const persisted_queries = await persistedQueriesFetcher();
+
+  // @ts-expect-error Types aren't lining up here
+  const queryText = persisted_queries[request.id] as string;
+
+  return fetchMultipart(url(request, variables, cacheConfig, uploadables), {
+    method: "POST",
+    credentials: "include",
+    body: JSON.stringify({
+      operationName: request.name,
+      query: queryText,
+      extensions: {
+        persistedQuery: {
+          version: 1,
+          sha256Hash: request.id,
+        },
+      },
+      variables,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "multipart/mixed; deferSpec=20220824",
+    },
+    ...sinkHandlers,
+  });
+};
+
+type CreateRelayFetchFunctionArgs = {
+  url: (...args: Parameters<FetchFunction>) => string;
+  persistedQueriesFetcher: PersistedQueriesFetcher;
+};
+
+export function createRelayFetchFunctionWithDefer(
+  args: CreateRelayFetchFunctionArgs
+): FetchFunction {
+  const relayFetchFunction: FetchFunction = (
+    request,
+    variables,
+    cacheConfig,
+    uploadables
+  ) => {
+    return Observable.create((sink) => {
+      const sinkHandlers: SinkHandlers = {
+        onComplete: () => sink.complete(),
+        onError: (error) => sink.error(error as Error),
+        onNext: (responses) => {
+          const wasPersistedQueryNotFound = responses.some((response) => {
+            if ("errors" in response) {
+              return isResponsePersistedQueryNotFound(response);
+            }
+
+            return false;
+          });
+
+          if (wasPersistedQueryNotFound) {
+            fetchWithHashAndQueryText(
+              args,
+              sinkHandlers,
+              request,
+              variables,
+              cacheConfig,
+              uploadables
+            );
+          } else {
+            const { data, isLast } = flattenDeferResponse(responses);
+
+            sink.next(data);
+
+            if (isLast) {
+              sink.complete();
+            }
+          }
+        },
+      };
+
+      fetchWithJustHash(
+        args,
+        sinkHandlers,
+        request,
+        variables,
+        cacheConfig,
+        uploadables
+      );
+    });
+  };
+
+  return relayFetchFunction;
+}
