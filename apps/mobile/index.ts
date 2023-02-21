a1bf119da0027683a642633bcae0b9accd952e2b
Terence Bezman <terencebezman@gmail.com>
2023-02-20 19:41:32 -0500

    use polyfill libs

--- apps/mobile/index.ts
+++ apps/mobile/index.ts
@@ -1,3 +1,11 @@
+import { polyfill as polyfillEncoding } from "react-native-polyfill-globals/src/encoding";
+import { polyfill as polyfillFetch } from "react-native-polyfill-globals/src/fetch";
+import { polyfill as polyfillReadableStream } from "react-native-polyfill-globals/src/readable-stream";
+
+polyfillEncoding();
+polyfillFetch();
+polyfillReadableStream();
+
 import registerRootComponent from "expo/build/launch/registerRootComponent";
 
 import App from "./App";
