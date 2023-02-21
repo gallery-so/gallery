9f574922679965b6a16ea3e2299e8c3974b1cefc
kaito <80802871+kaitoo1@users.noreply.github.com>
2023-02-21 18:07:49 +0900

    Add see all button and popover

--- apps/web/src/components/Featured/SuggestedSection.tsx
+++ apps/web/src/components/Featured/SuggestedSection.tsx
@@ -1,13 +1,16 @@
-import { useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 import { graphql, useFragment } from 'react-relay';
 import styled from 'styled-components';
 
+import { useModalActions } from '~/contexts/modal/ModalContext';
 import { SuggestedSectionQueryFragment$key } from '~/generated/SuggestedSectionQueryFragment.graphql';
 
 import colors from '../core/colors';
-import { VStack } from '../core/Spacer/Stack';
+import InteractiveLink from '../core/InteractiveLink/InteractiveLink';
+import { HStack, VStack } from '../core/Spacer/Stack';
 import { TitleDiatypeL } from '../core/Text/Text';
 import FeaturedList from './FeaturedList';
+import FeaturedPopoverList from './FeaturedPopoverList';
 
 type Props = {
   title: string;
@@ -31,6 +34,7 @@ export default function SuggestedSection({ queryRef, title, subTitle }: Props) {
                     __typename
 
                     ...FeaturedListFragment
+                    ...FeaturedPopoverListFragment
                   }
                 }
               }
@@ -38,6 +42,7 @@ export default function SuggestedSection({ queryRef, title, subTitle }: Props) {
           }
         }
         ...FeaturedListQueryFragment
+        ...FeaturedPopoverListQueryFragment
       }
     `,
     queryRef
@@ -56,12 +61,26 @@ export default function SuggestedSection({ queryRef, title, subTitle }: Props) {
     return users;
   }, [query.viewer.suggestedUsers?.edges]);
 
+  const { showModal } = useModalActions();
+
+  const handleSeeAllClick = useCallback(() => {
+    showModal({
+      content: <FeaturedPopoverList featuredUsersRef={nonNullUsers} queryRef={query} />,
+      isFullPage: false,
+      isPaddingDisabled: true,
+    });
+  }, [nonNullUsers, query, showModal]);
+
   return (
     <StyledSuggestedSection gap={32}>
-      <VStack gap={4}>
-        <Title>{title}</Title>
-        <TitleDiatypeL color={colors.metal}>{subTitle}</TitleDiatypeL>
-      </VStack>
+      <HStack justify="space-between" align="center">
+        <VStack gap={4}>
+          <Title>{title}</Title>
+          <TitleDiatypeL color={colors.metal}>{subTitle}</TitleDiatypeL>
+        </VStack>
+
+        <InteractiveLink onClick={handleSeeAllClick}>See all</InteractiveLink>
+      </HStack>
       <FeaturedList featuredUsersRef={nonNullUsers} queryRef={query} />
     </StyledSuggestedSection>
   );
