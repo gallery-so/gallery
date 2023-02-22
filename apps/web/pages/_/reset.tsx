7a361af8a7b8adcb318071e91e1a683a2e161a0e
Robinnnnn <rokim8@gmail.com>
2023-02-22 16:17:41 -0500

    refactor /reset

--- apps/web/pages/_/reset.tsx
+++ apps/web/pages/_/reset.tsx
@@ -2,21 +2,29 @@ import { useEffect } from 'react';
 import styled from 'styled-components';
 
 import { VStack } from '~/components/core/Spacer/Stack';
-import { TitleL, TitleS } from '~/components/core/Text/Text';
-import { WHITE_RHINO_STORAGE_KEY } from '~/constants/storageKeys';
-import usePersistedState from '~/hooks/usePersistedState';
+import { TitleL } from '~/components/core/Text/Text';
+import useUpdateUserExperience from '~/components/GalleryEditor/GalleryOnboardingGuide/useUpdateUserExperience';
+import { UserExperienceType } from '~/generated/enums';
 
+// This page allows users to reset their dismissed experience flags
 export default function Secret() {
-  const [, setDismissed] = usePersistedState(WHITE_RHINO_STORAGE_KEY, false);
-
+  const updateUserExperience = useUpdateUserExperience();
+  
+  const experiences: UserExperienceType[] = [
+    'MultiGalleryAnnouncement',  'EmailUpsell',  'MerchStoreUpsell',  'MaintenanceFeb2023'
+  ]
   useEffect(() => {
-    setDismissed(false);
-  }, [setDismissed]);
+    experiences.forEach(experience => {
+      updateUserExperience({
+        type: experience,
+        experienced: false,
+      })
+    })
+  }, [updateUserExperience]);
 
   return (
     <StyledSecret gap={8}>
       <TitleL>You've found the secret page</TitleL>
-      <TitleS>The banner has been restored</TitleS>
     </StyledSecret>
   );
 }
