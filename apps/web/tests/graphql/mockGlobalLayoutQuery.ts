64e4a6f8df37b9887fb1a6c6220b9cdad86cc2b7
robin <rokim8@gmail.com>
2023-02-22 01:12:33 -0500

    Replace FadeTransitioner with framer motion, simplify global layout context (#1344)
    * deprecate FadeTransitioner
    
    * export useStabilizedRouteTransitionKey
    
    * deprecate GlobalNavbar
    
    * export transitionTiming
    
    * potentially kill useGlobalNavbarHeight
    
    * massively simplify GalleryRoute
    
    * refactor GalleryLayoutContext
    
    * fix crash
    
    * sidebar
    
    * fix flickering nav on route transition
    
    * simplify
    
    * more polish
    
    * toss log
    
    * add back GlobalNavbar classname
    
    * fix ridiculous bug
    
    * wrap FullPageLoader
    
    * deprecate FullPageLoaderWithLayoutTransitionSupport
    
    * toss suspense tracking

--- apps/web/tests/graphql/mockGlobalLayoutQuery.ts
+++ apps/web/tests/graphql/mockGlobalLayoutQuery.ts
@@ -15,6 +15,8 @@ export function mockGlobalLayoutQuery() {
         id: GALLERY_USER_ID,
         username: 'Test Gallery User',
         wallets: [],
+        // @ts-expect-error not sure what the issue is
+        roles: ['ADMIN'],
         primaryWallet: {
           __typename: 'Wallet',
           id: '123',
