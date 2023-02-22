28f3f36d778a50f658ece7c3e50f97514f1182db
Terence Bezman <terencebezman@gmail.com>
2023-02-22 13:48:12 -0500

    Monorepo ESLint / Prettier Setup (#1365)
    * hoist eslint config to root with sub lint config
    
    * hoist moon task to root
    
    * run lint fix
    
    * hosit prettier config to root
    
    * hoist up prettier dependencies
    
    * hoist up prettier config
    
    * add global task for prettier
    
    * add prettier fix task
    
    * prettier format
    
    * update github actions
    
    * ignore tsconfig since moon touches those
    
    * update animation type => ease
    
    * fix visibility test

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,15 +1,15 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['babel-preset-expo'],
     plugins: [
       [
-        "babel-plugin-relay",
+        'babel-plugin-relay',
         {
-          artifactDirectory: "./__generated__/relay",
+          artifactDirectory: './__generated__/relay',
         },
       ],
-      "tsconfig-paths-module-resolver",
+      'tsconfig-paths-module-resolver',
     ],
   };
 };
