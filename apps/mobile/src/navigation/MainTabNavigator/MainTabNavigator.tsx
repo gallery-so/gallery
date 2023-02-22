d9e44b2ec3a0e686119231fa6f5b380639a11c97
Terence Bezman <terencebezman@gmail.com>
2023-02-22 14:03:08 -0500

    remove stack navigator

--- apps/mobile/src/navigation/MainTabNavigator/MainTabNavigator.tsx
+++ apps/mobile/src/navigation/MainTabNavigator/MainTabNavigator.tsx
@@ -1,16 +1,15 @@
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import { AccountScreen } from "src/screens/AccountScreen";
-import { HomeScreen } from "src/screens/HomeScreen";
-import { NotificationsScreen } from "src/screens/NotificationsScreen";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { TabBar } from "~/navigation/MainTabNavigator/TabBar";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { MainStackNavigatorParamList } from "~/navigation/types";
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { AccountScreen } from 'src/screens/AccountScreen';
+import { HomeScreen } from 'src/screens/HomeScreen';
+import { NotificationsScreen } from 'src/screens/NotificationsScreen';
 
-const Tab = createMaterialTopTabNavigator();
-const Stack = createNativeStackNavigator<MainStackNavigatorParamList>();
+import { TabBar } from '~/navigation/MainTabNavigator/TabBar';
+import { MainTabNavigatorParamList } from '~/navigation/types';
 
-function TabNavigator() {
+const Tab = createMaterialTopTabNavigator<MainTabNavigatorParamList>();
+
+export function MainTabNavigator() {
   const { top } = useSafeAreaInsets();
 
   return (
@@ -18,26 +17,11 @@ function TabNavigator() {
       tabBarPosition="bottom"
       initialRouteName="Home"
       tabBar={TabBar}
-      sceneContainerStyle={{ paddingTop: top, backgroundColor: "white" }}
+      sceneContainerStyle={{ paddingTop: top, backgroundColor: 'white' }}
     >
       <Tab.Screen name="Account" component={AccountScreen} />
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Notifications" component={NotificationsScreen} />
     </Tab.Navigator>
-  );
-}
-
-function EmptyHeader() {
-  return null;
-}
-
-export function MainTabNavigator() {
-  return (
-    <Stack.Navigator
-      initialRouteName="Tabs"
-      screenOptions={{ header: EmptyHeader }}
-    >
-      <Stack.Screen name="Tabs" component={TabNavigator} />
-    </Stack.Navigator>
   );
 }
--- apps/mobile/src/navigation/types.ts
+++ apps/mobile/src/navigation/types.ts
@@ -1,5 +1,8 @@
-export type MainStackNavigatorParamList = {
-  Tabs: {};
+export type MainTabNavigatorParamList = {
+  // eslint-disable-next-line @typescript-eslint/ban-types
   Account: {};
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  Home: {};
+  // eslint-disable-next-line @typescript-eslint/ban-types
   Notifications: {};
 };
