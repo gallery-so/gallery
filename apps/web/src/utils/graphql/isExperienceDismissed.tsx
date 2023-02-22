6cdb19f39cd8e5b6b5ffa4249c372749e65ae696
Robinnnnn <rokim8@gmail.com>
2023-02-22 16:19:10 -0500

    isExperienceDismissed util

---
+++ apps/web/src/utils/graphql/isExperienceDismissed.tsx
@@ -0,0 +1,32 @@
+import { graphql, readInlineData } from "relay-runtime";
+import { UserExperienceType } from "__generated__/enums";
+import { isExperienceDismissedFragment$key } from "~/generated/isExperienceDismissedFragment.graphql";
+
+export default function isExperienceDismissed(experience: UserExperienceType, queryRef: isExperienceDismissedFragment$key) {
+    const result = readInlineData(
+        graphql`
+            fragment isExperienceDismissedFragment on Query @inline {
+                viewer {
+                    ... on Viewer {
+                        __typename
+                        userExperiences {
+                            type
+                            experienced
+                        }
+                    }
+                }
+            }
+        `,
+        queryRef
+    )
+
+    if (result.viewer?.__typename !== 'Viewer') {
+        return true;
+    }
+
+    return Boolean(
+        result.viewer?.userExperiences?.find(
+          (userExperience) => userExperience.type === experience
+        )?.experienced
+      );
+}
