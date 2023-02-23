dbe4c4a0615bb54db1177267fe373e071a6297e4
Terence Bezman <terencebezman@gmail.com>
2023-02-23 16:59:02 -0500

    fix types

--- packages/shared/src/relay/getVideoOrImageUrlForNftPreview.ts
+++ packages/shared/src/relay/getVideoOrImageUrlForNftPreview.ts
@@ -1,6 +1,6 @@
-import { getVideoOrImageUrlForNftPreviewFragment$key } from 'apps/web/__generated__/relay/getVideoOrImageUrlForNftPreviewFragment.graphql';
-import { ReportFn } from 'apps/web/src/contexts/errorReporting/ErrorReportingContext';
 import { graphql, readInlineData } from 'relay-runtime';
+
+import { getVideoOrImageUrlForNftPreviewFragment$key } from '~/generated/getVideoOrImageUrlForNftPreviewFragment.graphql';
 
 type UrlSet = { small: string | null; medium: string | null; large: string | null };
 export type getVideoOrImageUrlForNftPreviewResult =
@@ -10,7 +10,7 @@ export type getVideoOrImageUrlForNftPreviewResult =
 
 export default function getVideoOrImageUrlForNftPreview(
   tokenRef: getVideoOrImageUrlForNftPreviewFragment$key,
-  handleReportError?: ReportFn
+  handleReportError?: (error: Error, additionalContext: { tags: Record<string, string> }) => void
 ): getVideoOrImageUrlForNftPreviewResult {
   const result = readInlineData(
     graphql`
