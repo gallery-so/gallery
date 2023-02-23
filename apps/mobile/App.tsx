c350c3fc7c22f59e27ff48ca4185f2b7f436c1af
Terence Bezman <terencebezman@gmail.com>
2023-02-22 20:07:07 -0500

    setup font loading

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,7 +1,10 @@
 import 'expo-dev-client';
 
 import { NavigationContainer } from '@react-navigation/native';
-import { useState } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback, useEffect, useState } from 'react';
+import { View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { RelayEnvironmentProvider } from 'react-relay';
 
@@ -9,8 +12,39 @@ import { MainTabNavigator } from '~/navigation/MainTabNavigator/MainTabNavigator
 
 import { createRelayEnvironment } from './src/contexts/relay/RelayProvider';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
   const [relayEnvironment] = useState(() => createRelayEnvironment());
+
+  const [fontsLoaded] = useFonts({
+    GTAlpinaStandardLight: require('~/shared/fonts/GT-Alpina-Standard-Light.ttf'),
+    GTAlpinaStandardLightItalic: require('~/shared/fonts/GT-Alpina-Standard-Light-Italic.ttf'),
+
+    GTAlpinaMedium: require('~/shared/fonts/GT-Alpina-Condensed-Medium.ttf'),
+    GTAlpinaMediumItalic: require('~/shared/fonts/GT-Alpina-Condensed-Medium-Italic.ttf'),
+
+    GTAlpinaLight: require('~/shared/fonts/GT-Alpina-Condensed-Light.ttf'),
+    GTAlpinaLightItalic: require('~/shared/fonts/GT-Alpina-Condensed-Light-Italic.ttf'),
+
+    GTAlpinaBold: require('~/shared/fonts/GT-Alpina-Condensed-Bold-Italic.ttf'),
+    GTAlpinaBoldItalic: require('~/shared/fonts/GT-Alpina-Condensed-Bold-Italic.ttf'),
+
+    ABCDiatypeMono: require('~/shared/fonts/ABCDiatypeMono-Medium.ttf'),
+    ABCDiatypeRegular: require('~/shared/fonts/ABCDiatype-Regular.ttf'),
+    ABCDiatypeMedium: require('~/shared/fonts/ABCDiatype-Medium.ttf'),
+    ABCDiatypeBold: require('~/shared/fonts/ABCDiatype-Bold.ttf'),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
     <RelayEnvironmentProvider environment={relayEnvironment}>
