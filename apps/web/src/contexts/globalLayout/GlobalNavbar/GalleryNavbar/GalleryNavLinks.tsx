748a10b8e5dc018333cbfc43320f742029e9b374
jakzaizzat <jakzaizzat@gmail.com>
2023-02-23 14:59:24 +0800

    add navbar count

--- apps/web/src/contexts/globalLayout/GlobalNavbar/GalleryNavbar/GalleryNavLinks.tsx
+++ apps/web/src/contexts/globalLayout/GlobalNavbar/GalleryNavbar/GalleryNavLinks.tsx
@@ -1,20 +1,52 @@
 import { useRouter } from 'next/router';
 import { Route, route } from 'nextjs-routes';
+import { useMemo } from 'react';
+import { graphql, useFragment } from 'react-relay';
 
 import { HStack } from '~/components/core/Spacer/Stack';
+import { BaseS } from '~/components/core/Text/Text';
+import { GalleryNavLinksFragment$key } from '~/generated/GalleryNavLinksFragment.graphql';
+import { useIsMobileWindowWidth } from '~/hooks/useWindowSize';
+import { removeNullValues } from '~/utils/removeNullValues';
 
 import { NavbarLink } from '../NavbarLink';
 
 type Props = {
   username: string;
+  queryRef: GalleryNavLinksFragment$key;
 };
 
-export function GalleryNavLinks({ username }: Props) {
+export function GalleryNavLinks({ username, queryRef }: Props) {
+  const query = useFragment(
+    graphql`
+      fragment GalleryNavLinksFragment on GalleryUser {
+        galleries {
+          __typename
+          hidden
+        }
+        followers {
+          __typename
+        }
+      }
+    `,
+    queryRef
+  );
+
+  const totalFollowers = query.followers?.length ?? 0;
+  const totalGalleries = useMemo(() => {
+    return (
+      removeNullValues(query.galleries?.map((gallery) => (gallery?.hidden ? null : gallery)))
+        .length ?? 0
+    );
+  }, [query.galleries]);
+
   const { pathname } = useRouter();
 
   const galleriesRoute: Route = { pathname: '/[username]/galleries', query: { username } };
   const followersRoute: Route = { pathname: '/[username]/followers', query: { username } };
   const activityRoute: Route = { pathname: '/[username]/activity', query: { username } };
+
+  const isMobile = useIsMobileWindowWidth();
 
   return (
     <HStack gap={8}>
@@ -23,7 +55,10 @@ export function GalleryNavLinks({ username }: Props) {
         href={route(galleriesRoute)}
         active={pathname === galleriesRoute.pathname}
       >
-        Galleries
+        <HStack gap={4} align={isMobile ? 'center' : 'flex-end'}>
+          <span>Galleries</span>
+          {totalGalleries > 0 && <BaseS>{totalGalleries}</BaseS>}
+        </HStack>
       </NavbarLink>
 
       <NavbarLink
@@ -31,7 +66,10 @@ export function GalleryNavLinks({ username }: Props) {
         href={route(followersRoute)}
         active={pathname === followersRoute.pathname}
       >
-        Followers
+        <HStack gap={4} align={isMobile ? 'center' : 'flex-end'}>
+          Followers
+          {totalFollowers > 0 && <BaseS>{totalFollowers}</BaseS>}
+        </HStack>
       </NavbarLink>
 
       <NavbarLink
