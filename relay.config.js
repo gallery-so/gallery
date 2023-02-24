16872bbb5c2f8814f944a56ca67ac0035e41a0d3
Terence Bezman <terencebezman@gmail.com>
2023-02-24 12:14:42 -0500

    format relay config

--- relay.config.js
+++ relay.config.js
@@ -1,35 +1,31 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 function generateConfig(projectDir) {
-  const generatedDirectory = path.join(projectDir, "__generated__", "relay");
-  const persistedQueriesFile = path.join(projectDir, "persisted_queries.json");
+  const generatedDirectory = path.join(projectDir, '__generated__', 'relay');
+  const persistedQueriesFile = path.join(projectDir, 'persisted_queries.json');
 
   // Automatically touch the generated directory
   fs.mkdirSync(path.resolve(__dirname, generatedDirectory), {
     recursive: true,
   });
   // Automatically touch the persisted_queries file
-  fs.writeFileSync(
-    path.resolve(__dirname, persistedQueriesFile),
-    "{}",
-    "utf-8"
-  );
+  fs.writeFileSync(path.resolve(__dirname, persistedQueriesFile), '{}', 'utf-8');
 
   return {
-    schema: "schema.graphql",
-    language: "typescript",
+    schema: 'schema.graphql',
+    language: 'typescript',
     output: generatedDirectory,
 
     customScalarTypes: {
-      Email: "string",
-      Address: "string",
-      DBID: "string",
+      Email: 'string',
+      Address: 'string',
+      DBID: 'string',
     },
 
     persist: {
-      file: "persisted_queries.json",
-      algorithm: "SHA256", // this can be one of MD5, SHA256, SHA1
+      file: 'persisted_queries.json',
+      algorithm: 'SHA256', // this can be one of MD5, SHA256, SHA1
     },
   };
 }
@@ -37,15 +33,15 @@ function generateConfig(projectDir) {
 module.exports = {
   root: __dirname,
   sources: {
-    "apps/web": "web",
-    "apps/mobile": "mobile",
+    'apps/web': 'web',
+    'apps/mobile': 'mobile',
 
     // This means web, mobile, and the shared package can use fragments from the shared package.
-    "packages/shared": ["web", "mobile", "shared"],
+    'packages/shared': ['web', 'mobile', 'shared'],
   },
   projects: {
-    web: generateConfig("apps/web"),
-    mobile: generateConfig("apps/mobile"),
-    shared: generateConfig("packages/shared"),
+    web: generateConfig('apps/web'),
+    mobile: generateConfig('apps/mobile'),
+    shared: generateConfig('packages/shared'),
   },
 };
