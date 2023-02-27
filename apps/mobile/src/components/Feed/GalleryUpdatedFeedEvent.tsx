8a07bfbf3510dd3ba90799021c489ce1a0557a70
Terence Bezman <terencebezman@gmail.com>
2023-02-27 16:23:17 -0500

    fix persisted queries output path

--- apps/mobile/src/components/Feed/GalleryUpdatedFeedEvent.tsx
+++ apps/mobile/src/components/Feed/GalleryUpdatedFeedEvent.tsx
@@ -1,44 +1,38 @@
-import { ResizeMode } from 'expo-av';
 import { ScrollView, StyleProp, Text, useWindowDimensions, View, ViewStyle } from 'react-native';
-import FastImage from 'react-native-fast-image';
 import { useFragment } from 'react-relay';
 import { graphql } from 'relay-runtime';
 
-import { GalleryUpdatedFeedEventFragment$key } from '~/generated/GalleryUpdatedFeedEventFragment.graphql';
-import { GalleryUpdatedFeedEventTokenUrlsFragment$key } from '~/generated/GalleryUpdatedFeedEventTokenUrlsFragment.graphql';
-import getVideoOrImageUrlForNftPreview from '~/shared/relay/getVideoOrImageUrlForNftPreview';
+import { RecusriveFeedEventFragment$key } from '~/generated/RecusriveFeedEventFragment.graphql';
+import { RecusriveFeedEventTokenUrlsFragment$key } from '~/generated/RecusriveFeedEventTokenUrlsFragment.graphql';
 import { removeNullValues } from '~/shared/relay/removeNullValues';
 
-import { SvgWebView } from '../SvgWebView';
 import { EventTokenGrid } from './EventTokenGrid';
 
-type GalleryUpdatedFeedEventProps = {
-  galleryUpdatedFeedEventDataRef: GalleryUpdatedFeedEventFragment$key;
+type RecusriveFeedEventProps = {
+  RecusriveFeedEventDataRef: RecusriveFeedEventFragment$key;
 };
 
-export function GalleryUpdatedFeedEvent({
-  galleryUpdatedFeedEventDataRef,
-}: GalleryUpdatedFeedEventProps) {
+export function RecursiveFeedEvent({ RecusriveFeedEventDataRef }: RecusriveFeedEventProps) {
   const eventData = useFragment(
     graphql`
-      fragment GalleryUpdatedFeedEventFragment on GalleryUpdatedFeedEventData {
+      fragment RecusriveFeedEventFragment on RecusriveFeedEventData {
         owner {
           username
         }
 
         subEventDatas {
-          ...GalleryUpdatedFeedEventTokenUrlsFragment
+          ...RecusriveFeedEventTokenUrlsFragment
         }
       }
     `,
-    galleryUpdatedFeedEventDataRef
+    RecusriveFeedEventDataRef
   );
 
   const { width } = useWindowDimensions();
 
-  const subEventDatas = useFragment<GalleryUpdatedFeedEventTokenUrlsFragment$key>(
+  const subEventDatas = useFragment<RecusriveFeedEventTokenUrlsFragment$key>(
     graphql`
-      fragment GalleryUpdatedFeedEventTokenUrlsFragment on FeedEventData @relay(plural: true) {
+      fragment RecusriveFeedEventTokenUrlsFragment on FeedEventData @relay(plural: true) {
         ... on CollectionCreatedFeedEventData {
           newTokens {
             token {
@@ -77,32 +71,36 @@ export function GalleryUpdatedFeedEvent({
 
   return (
     <View className="flex flex-col space-y-3">
-      <View className="flex flex-row space-x-1 px-3 py-2">
-        <Text style={{ fontFamily: 'ABCDiatypeBold', fontSize: 12 }}>
-          {eventData.owner?.username}
-        </Text>
+      <View className="flex flex-col">
+        <View className="flex flex-row space-x-1 px-3 py-2">
+          <Text style={{ fontFamily: 'ABCDiatypeBold', fontSize: 12 }}>
+            {eventData.owner?.username}
+          </Text>
 
-        <Text style={{ fontFamily: 'ABCDiatypeRegular', fontSize: 12 }}>/</Text>
+          <Text style={{ fontFamily: 'ABCDiatypeRegular', fontSize: 12 }}>/</Text>
 
-        <Text style={{ fontFamily: 'ABCDiatypeBold', fontSize: 12 }}>Photography</Text>
+          <Text style={{ fontFamily: 'ABCDiatypeBold', fontSize: 12 }}>Photography</Text>
+        </View>
+
+        <ScrollView
+          horizontal
+          pagingEnabled
+          decelerationRate={0}
+          snapToInterval={width}
+          snapToAlignment={'center'}
+          showsHorizontalScrollIndicator={false}
+        >
+          <View className="flex flex-col">
+            <View className="flex px-3 py-2">
+              <Text style={{ fontFamily: 'ABCDiatypeBold', fontSize: 12 }}>Merik Aktar</Text>
+
+              <Text style={{ fontFamily: 'ABCDiatypeRegular', fontSize: 12 }}>62 additions</Text>
+            </View>
+
+            <EventTokenGrid tokenRefs={tokens} />
+          </View>
+        </ScrollView>
       </View>
-
-      <View className="flex px-3 py-[2]">
-        <Text style={{ fontFamily: 'ABCDiatypeBold', fontSize: 12 }}>Merik Aktar</Text>
-
-        <Text style={{ fontFamily: 'ABCDiatypeRegular', fontSize: 12 }}>62 additions</Text>
-      </View>
-
-      <ScrollView
-        horizontal
-        pagingEnabled
-        decelerationRate={0}
-        snapToInterval={width}
-        snapToAlignment={'center'}
-        showsHorizontalScrollIndicator={false}
-      >
-        <EventTokenGrid tokenRefs={tokens} />
-      </ScrollView>
 
       <View className="flex w-full flex-row justify-center">
         <View className="flex flex-row space-x-1">
