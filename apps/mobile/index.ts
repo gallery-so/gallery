57d833f82ae83accf9224b0301267365aa77cbea
Terence Bezman <terencebezman@gmail.com>
2023-03-01 15:20:00 -0500

    configure DSN

--- apps/mobile/index.ts
+++ apps/mobile/index.ts
@@ -7,6 +7,7 @@ import { polyfill as polyfillFetch } from 'react-native-polyfill-globals/src/fet
 // @ts-expect-error No type declarations
 import { polyfill as polyfillReadableStream } from 'react-native-polyfill-globals/src/readable-stream';
 import { enableFreeze } from 'react-native-screens';
+import * as Sentry from 'sentry-expo';
 
 // Unimportant warnings from the fetch polyfill
 LogBox.ignoreLogs(["The provided value 'moz", "The provided value 'ms-stream"]);
@@ -16,6 +17,14 @@ polyfillFetch();
 polyfillReadableStream();
 
 enableFreeze(true);
+
+Sentry.init({
+  dsn: 'https://bd40a4affc1740e8b7516502389262fe@o1135798.ingest.sentry.io/6187637',
+  enableInExpoDevelopment: true,
+
+  // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+  debug: true,
+});
 
 import App from './App';
 
