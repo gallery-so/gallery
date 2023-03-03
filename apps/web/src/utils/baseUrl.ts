1eb868c16603bfef83f13bfacb86f9318f2c2cd5
kaito <80802871+kaitoo1@users.noreply.github.com>
2023-03-03 11:41:01 +0900

    weirdness

--- apps/web/src/utils/baseUrl.ts
+++ apps/web/src/utils/baseUrl.ts
@@ -1,15 +1,20 @@
-export default function getBaseUrl() {
+export const getBaseUrl = () => {
+  // const env = 'preview';
   const env = process.env.NEXT_PUBLIC_VERCEL_ENV;
+  console.log('getBaseUrl env', env);
+  console.log('getBaseUrl', process.env.NEXT_PUBLIC_VERCEL_ENV);
   // If we're inside the Vercel environment
   switch (env) {
     case 'production':
       return 'https://gallery.so';
     case 'development':
+      console.log('case dev');
       return `https://gallery-dev.vercel.app`;
     case 'preview':
+      console.log('case preview');
       return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
   }
 
   // Otherwise, we're probably running locally?
   return 'http://localhost:3000';
-}
+};
