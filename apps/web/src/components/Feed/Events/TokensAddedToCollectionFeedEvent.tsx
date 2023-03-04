a0bcc4ca17ea10f906a2d55e3e9474c96ba2d529
Robinnnnn <rokim8@gmail.com>
2023-03-04 17:11:34 -0500

    parse markdown in captions

--- apps/web/src/components/Feed/Events/TokensAddedToCollectionFeedEvent.tsx
+++ apps/web/src/components/Feed/Events/TokensAddedToCollectionFeedEvent.tsx
@@ -7,6 +7,7 @@ import styled from 'styled-components';
 
 import colors from '~/components/core/colors';
 import { UnstyledLink } from '~/components/core/Link/UnstyledLink';
+import Markdown from '~/components/core/Markdown/Markdown';
 import { HStack, VStack } from '~/components/core/Spacer/Stack';
 import { BaseM, BaseS } from '~/components/core/Text/Text';
 import HoverCardOnUsername from '~/components/HoverCard/HoverCardOnUsername';
@@ -125,7 +126,9 @@ export default function TokensAddedToCollectionFeedEvent({
         <StyledEventContent gap={8} hasCaption={Boolean(caption)}>
           {caption && (
             <StyledCaptionContainer gap={8} align="center">
-              <BaseM>{caption}</BaseM>
+              <BaseM>
+                <Markdown text={caption} />
+              </BaseM>
             </StyledCaptionContainer>
           )}
           <FeedEventTokenPreviews
