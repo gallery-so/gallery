0af907cc05ea1ea87daca1b6aaad3d082c406e1c
Terence Bezman <terencebezman@gmail.com>
2023-03-06 15:00:40 -0500

    Setup Sentry on the mobile app (#1399)
    * install sentry deps
    
    * setup plugin in app json
    
    * configure expo to upload source maps to sentry as a post publish step
    
    * configure DSN
    
    * update DSN
    
    * Update to correct expo owner
    
    * make appropriate updates to app json

--- apps/mobile/index.ts
+++ apps/mobile/index.ts
@@ -7,6 +7,7 @@ import { polyfill as polyfillFetch } from 'react-native-polyfill-globals/src/fet
 // @ts-expect-error No type declarations
 import { polyfill as polyfillReadableStream } from 'react-native-polyfill-globals/src/readable-stream';
 import { enableFreeze } from 'react-native-screens';
+import * as Sentry from 'sentry-expo';
 
 // Unimportant warnings from the fetch polyfill
 LogBox.ignoreLogs(["The provided value 'moz", "The provided value 'ms-stream"]);
@@ -16,6 +17,14 @@ polyfillFetch();
 polyfillReadableStream();
 
 enableFreeze(true);
+
+Sentry.init({
+  dsn: 'https://c26c57d7db254a8f9805fa316c6d2055@o1135798.ingest.sentry.io/4504770503114752',
+  enableInExpoDevelopment: true,
+
+  // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+  debug: true,
+});
 
 import App from './App';
 
