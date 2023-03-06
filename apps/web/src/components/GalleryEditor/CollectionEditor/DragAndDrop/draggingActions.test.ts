ad5c19e1fc9dd78862e5de6179b669118a6a0dd4
Terence Bezman <terencebezman@gmail.com>
2023-03-06 10:01:55 -0500

    fix tests properly

--- apps/web/src/components/GalleryEditor/CollectionEditor/DragAndDrop/draggingActions.test.ts
+++ apps/web/src/components/GalleryEditor/CollectionEditor/DragAndDrop/draggingActions.test.ts
@@ -2,25 +2,29 @@ import {
   dragEnd,
   dragOver,
 } from '~/components/GalleryEditor/CollectionEditor/DragAndDrop/draggingActions';
-import { StagedSection } from '~/components/GalleryEditor/GalleryEditorContext';
-import { generate12DigitId } from '~/utils/generate12DigitId';
+import { StagedSectionMap } from '~/components/GalleryEditor/GalleryEditorContext';
 
 type TestCaseArgs = {
-  initial: Record<string, StagedSection>;
+  initial: StagedSectionMap;
   action: { from: string; to: string };
-  result: Record<string, StagedSection>;
+  result: StagedSectionMap;
 };
 
 type TestCaseNoChangeArgs = {
-  initial: Record<string, StagedSection>;
+  initial: StagedSectionMap;
   action: { from: string; to: string };
 };
 
 describe('draggingActions', () => {
   describe('dragOver', () => {
     function testCase({ initial, action: { from, to }, result }: TestCaseArgs) {
-      // @ts-expect-error we're just mocking whatever details the event actually needs
-      expect(dragOver(initial, { over: { id: to }, active: { id: from } })).toMatchObject(result);
+      expect(
+        dragOver(
+          initial,
+          // @ts-expect-error we're just mocking whatever details the event actually needs
+          { over: { id: to }, active: { id: from } }
+        )
+      ).toMatchObject(result);
     }
 
     function testCaseNoChange({ initial, action: { from, to } }: TestCaseNoChangeArgs) {
@@ -30,108 +34,108 @@ describe('draggingActions', () => {
 
     it('can drag a token between sections', () => {
       testCase({
-        initial: {
-          A: {
-            id: generate12DigitId(),
+        initial: [
+          {
+            id: 'A',
             columns: 1,
             items: [{ kind: 'token', id: 'token-1' }],
           },
-          B: {
-            id: generate12DigitId(),
+          {
+            id: 'B',
             columns: 1,
             items: [{ kind: 'token', id: 'token-2' }],
           },
-        },
+        ],
         action: {
           from: 'token-1',
           to: 'B',
         },
-        result: {
-          A: {
-            id: generate12DigitId(),
+        result: [
+          {
+            id: 'A',
             columns: 1,
             items: [],
           },
-          B: {
-            id: generate12DigitId(),
+          {
+            id: 'B',
             columns: 1,
             items: [
               { kind: 'token', id: 'token-2' },
               { kind: 'token', id: 'token-1' },
             ],
           },
-        },
+        ],
       });
     });
 
     it('can drag a token to a token in another section', () => {
       testCase({
-        initial: {
-          A: {
-            id: generate12DigitId(),
+        initial: [
+          {
+            id: 'A',
             columns: 1,
             items: [{ kind: 'token', id: 'token-1' }],
           },
-          B: {
-            id: generate12DigitId(),
+          {
+            id: 'B',
             columns: 1,
             items: [{ kind: 'token', id: 'token-2' }],
           },
-        },
+        ],
         action: {
           from: 'token-1',
           to: 'token-2',
         },
-        result: {
-          A: {
-            id: generate12DigitId(),
+        result: [
+          {
+            id: 'A',
             columns: 1,
             items: [],
           },
-          B: {
-            id: generate12DigitId(),
+          {
+            id: 'B',
             columns: 1,
             items: [
               { kind: 'token', id: 'token-2' },
               { kind: 'token', id: 'token-1' },
             ],
           },
-        },
+        ],
       });
     });
 
     it('does not do anything when dragging section to section', () => {
       testCaseNoChange({
-        initial: {
-          A: {
-            id: generate12DigitId(),
+        initial: [
+          {
+            id: 'A',
             columns: 1,
             items: [],
           },
-          B: {
-            id: generate12DigitId(),
+          {
+            id: 'B',
             columns: 1,
             items: [],
           },
-        },
+        ],
         action: { from: 'A', to: 'B' },
       });
     });
 
     it('does not do anything when dragging section to token', () => {
       testCaseNoChange({
-        initial: {
-          A: {
-            id: generate12DigitId(),
+        initial: [
+          {
+            id: 'A',
             columns: 1,
             items: [],
           },
-          B: {
-            id: generate12DigitId(),
+          {
+            id: 'B',
             columns: 1,
             items: [{ kind: 'token', id: 'token-1' }],
           },
-        },
+        ],
         action: { from: 'A', to: 'token-1' },
       });
     });
@@ -156,188 +160,188 @@ describe('draggingActions', () => {
 
     it('swaps tokens when dragging token over token', () => {
       testCase({
-        initial: {
-          A: {
-            id: generate12DigitId(),
+        initial: [
+          {
+            id: 'A',
             columns: 1,
             items: [
               { kind: 'token', id: 'token-1' },
               { kind: 'token', id: 'token-2' },
             ],
           },
-        },
+        ],
         action: { from: 'token-1', to: 'token-2' },
-        result: {
-          A: {
-            id: generate12DigitId(),
+        result: [
+          {
+            id: 'A',
             columns: 1,
             items: [
               { kind: 'token', id: 'token-2' },
               { kind: 'token', id: 'token-1' },
             ],
           },
-        },
+        ],
       });
     });
 
     it('moves token to other section when dragging token over token in other section', () => {
       testCase({
-        initial: {
-          A: {
-            id: generate12DigitId(),
+        initial: [
+          {
+            id: 'A',
             columns: 1,
             items: [{ kind: 'token', id: 'token-1' }],
           },
-          B: {
-            id: generate12DigitId(),
+          {
+            id: 'B',
             columns: 1,
             items: [{ kind: 'token', id: 'token-2' }],
           },
-        },
+        ],
         action: { from: 'token-1', to: 'token-2' },
-        result: {
-          A: {
-            id: generate12DigitId(),
+        result: [
+          {
+            id: 'A',
             columns: 1,
             items: [],
           },
-          B: {
-            id: generate12DigitId(),
+          {
+            id: 'B',
             columns: 1,
             items: [
               { kind: 'token', id: 'token-1' },
               { kind: 'token', id: 'token-2' },
             ],
           },
-        },
+        ],
       });
     });
 
     it('moves token to other section when dragging token over empty section', () => {
       testCase({
-        initial: {
-          A: {
-            id: generate12DigitId(),
+        initial: [
+          {
+            id: 'A',
             columns: 1,
             items: [{ kind: 'token', id: 'token-1' }],
           },
-          B: {
-            id: generate12DigitId(),
+          {
+            id: 'B',
             columns: 1,
             items: [],
           },
-        },
+        ],
         action: { from: 'token-1', to: 'B' },
-        result: {
-          A: {
-            id: generate12DigitId(),
+        result: [
+          {
+            id: 'A',
             columns: 1,
             items: [],
           },
-          B: {
-            id: generate12DigitId(),
+          {
+            id: 'B',
             columns: 1,
             items: [{ kind: 'token', id: 'token-1' }],
           },
-        },
+        ],
       });
     });
 
     it('does nothing when moving token over itself', () => {
       testCaseNoChange({
-        initial: {
-          A: {
-            id: generate12DigitId(),
+        initial: [
+          {
+            id: 'A',
             columns: 1,
             items: [{ kind: 'token', id: 'token-1' }],
           },
-          B: {
-            id: generate12DigitId(),
+          {
+            id: 'B',
             columns: 1,
             items: [],
           },
-        },
+        ],
         action: { from: 'token-1', to: 'token-1' },
       });
     });
 
     it('swaps sections when moving section over another section', () => {
       testCase({
-        initial: {
-          A: {
-            id: generate12DigitId(),
+        initial: [
+          {
+            id: 'A',
             columns: 1,
             items: [],
           },
-          B: {
-            id: generate12DigitId(),
+          {
+            id: 'B',
             columns: 1,
             items: [],
           },
-        },
+        ],
         action: {
           from: 'A',
           to: 'B',
         },
-        result: {
-          B: {
-            id: generate12DigitId(),
+        result: [
+          {
+            id: 'B',
             columns: 1,
             items: [],
           },
-          A: {
-            id: generate12DigitId(),
+          {
+            id: 'A',
             columns: 1,
             items: [],
           },
-        },
+        ],
       });
     });
 
     it('can move token over itself', () => {
       testCase({
-        initial: {
-          A: {
-            id: generate12DigitId(),
+        initial: [
+          {
+            id: 'A',
             columns: 1,
             items: [{ kind: 'token', id: 'token-1' }],
           },
-          B: {
-            id: generate12DigitId(),
+          {
+            id: 'B',
             columns: 1,
             items: [],
           },
-        },
+        ],
         action: { from: 'token-1', to: 'token-1' },
-        result: {
-          A: {
-            id: generate12DigitId(),
+        result: [
+          {
+            id: 'A',
             columns: 1,
             items: [{ kind: 'token', id: 'token-1' }],
           },
-          B: {
-            id: generate12DigitId(),
+          {
+            id: 'B',
             columns: 1,
             items: [],
           },
-        },
+        ],
       });
     });
 
     it('does nothing when moving section over itself', () => {
       testCaseNoChange({
-        initial: {
-          A: {
-            id: generate12DigitId(),
+        initial: [
+          {
+            id: 'A',
             columns: 1,
             items: [{ kind: 'token', id: 'token-1' }],
           },
-          B: {
-            id: generate12DigitId(),
+          {
+            id: 'B',
             columns: 1,
             items: [],
           },
-        },
+        ],
         action: { from: 'A', to: 'A' },
       });
     });
