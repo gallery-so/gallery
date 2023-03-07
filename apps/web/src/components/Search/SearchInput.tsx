9f4871504800ddad13c61606ad966d66b1b6d84d
jakzaizzat <jakzaizzat@gmail.com>
2023-03-07 15:52:18 +0800

    auto focus input

--- apps/web/src/components/Search/SearchInput.tsx
+++ apps/web/src/components/Search/SearchInput.tsx
@@ -1,9 +1,24 @@
+import { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 
 import colors from '../core/colors';
 
-export default function SearchInput() {
-  return <StyledInput type="text" placeholder="Search for anything..." />;
+type Props = {
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export default function SearchInput({ onChange }: Props) {
+  const ref = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.focus();
+    }
+  }, []);
+
+  return (
+    <StyledInput ref={ref} type="text" placeholder="Search for anything..." onChange={onChange} />
+  );
 }
 
 const StyledInput = styled.input`
