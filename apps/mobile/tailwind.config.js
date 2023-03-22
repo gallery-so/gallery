1b035235b161f7f397c8c5b822757ff4f4b3ccf4
Terence Bezman <terencebezman@gmail.com>
2023-03-22 12:55:33 -0400

    add xxs font size

--- apps/mobile/tailwind.config.js
+++ apps/mobile/tailwind.config.js
@@ -1,7 +1,12 @@
+const { fontScale } = require('nativewind/dist/theme-functions');
+
 module.exports = {
   content: ['App.tsx', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      fontSize: {
+        xxs: fontScale(10),
+      },
       colors: {
         offWhite: '#F9F9F9',
         white: '#FEFEFE',
