6bf65e671868e9164e5cc000a84189251af7c3c7
Terence Bezman <terencebezman@gmail.com>
2023-03-23 22:00:19 -0400

    use built in w-0.5 instead of w-[2]

--- apps/mobile/src/components/Feed/FeedListCollectorsNote.tsx
+++ apps/mobile/src/components/Feed/FeedListCollectorsNote.tsx
@@ -9,7 +9,7 @@ type Props = {
 export function FeedListCollectorsNote({ collectorsNote }: Props) {
   return (
     <View className="flex flex-row space-x-2 px-3 py-2">
-      <View className="bg-porcelain h-full w-[2]" />
+      <View className="bg-porcelain h-full w-0.5" />
       <View>
         <Markdown>{collectorsNote}</Markdown>
       </View>
