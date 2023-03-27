fd6402ec3ad9e4e96d3c0387da608186e417afc9
robin <rokim8@gmail.com>
2023-03-27 13:45:58 -0400

    new allowlist, mint price (#1457)

--- apps/web/src/scenes/MintPages/config.ts
+++ apps/web/src/scenes/MintPages/config.ts
@@ -1,5 +1,5 @@
 // time is in EST (GMT-05:00)
-export const MINT_START = '2023-03-27T14:00:00-05:00';
+export const MINT_START = '2023-03-27T13:00:00-05:00';
 export const MINT_END = '2023-04-03T23:59:00-05:00';
 
 // increment this each time we introduce a new token
@@ -13,4 +13,4 @@ export const pathToImage = featuredImage;
 
 // this probably won't change unless the cache is broken
 export const ALLOWLIST_URL =
-  'https://storage.googleapis.com/gallery-prod-325303.appspot.com/allowlists/mementos-allowlist-v2.json';
+  'https://storage.googleapis.com/gallery-prod-325303.appspot.com/allowlists/mementos-allowlist-v3.json';
