9bfd1c2d9e39747709f6e383c89ff11aab41885a
Terence Bezman <terencebezman@gmail.com>
2023-03-27 12:14:50 -0400

    add some variation to the shimmer speed

--- apps/mobile/src/components/GallerySkeleton.tsx
+++ apps/mobile/src/components/GallerySkeleton.tsx
@@ -1,11 +1,21 @@
+import { useMemo } from 'react';
 import SkeletonPlaceholder from 'react-native-skeleton-placeholder';
 
 // The types here are not great from the Skeleton library
 // Idiomatically, the type of children would ReactNode so
 // we could use PropsWithChildren, but this is fine.
 export function GallerySkeleton({ children }: { children: JSX.Element }) {
+  const speed = useMemo(() => {
+    return 800 + Math.random() * 800;
+  }, []);
+
   return (
-    <SkeletonPlaceholder borderRadius={4} highlightColor="#f2f2f2" backgroundColor="#e2e2e2">
+    <SkeletonPlaceholder
+      speed={speed}
+      borderRadius={4}
+      highlightColor="#f2f2f2"
+      backgroundColor="#e2e2e2"
+    >
       {children}
     </SkeletonPlaceholder>
   );
