ce5c56f6d5956ac28183db6320c25c77c79ea718
Terence Bezman <terencebezman@gmail.com>
2023-03-28 11:00:29 -0400

    make sure the rootstacknavigator chooses the right initial route based on whether you're logged in

--- apps/mobile/src/navigation/RootStackNavigator.tsx
+++ apps/mobile/src/navigation/RootStackNavigator.tsx
@@ -1,20 +1,36 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { graphql, useLazyLoadQuery } from 'react-relay';
 
+import { RootStackNavigatorQuery } from '~/generated/RootStackNavigatorQuery.graphql';
+import { LoginStackNavigator } from '~/navigation/LoginStackNavigator';
 import { MainTabNavigator } from '~/navigation/MainTabNavigator/MainTabNavigator';
+import { RootStackNavigatorParamList } from '~/navigation/types';
 
-import { NftDetailScreen } from '../screens/NftDetailScreen/NftDetailScreen';
-
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<RootStackNavigatorParamList>();
 
 export function RootStackNavigator() {
+  const query = useLazyLoadQuery<RootStackNavigatorQuery>(
+    graphql`
+      query RootStackNavigatorQuery {
+        viewer {
+          ... on Viewer {
+            __typename
+          }
+        }
+      }
+    `,
+    {}
+  );
+
+  const isLoggedIn = query.viewer?.__typename === 'Viewer';
+
   return (
-    <Stack.Navigator screenOptions={{ header: Empty }} initialRouteName="MainTabs">
+    <Stack.Navigator
+      screenOptions={{ header: Empty }}
+      initialRouteName={isLoggedIn ? 'MainTabs' : 'Login'}
+    >
+      <Stack.Screen name="Login" component={LoginStackNavigator} />
       <Stack.Screen name="MainTabs" component={MainTabNavigator} />
-      <Stack.Screen
-        name="NftDetail"
-        options={{ presentation: 'modal' }}
-        component={NftDetailScreen}
-      />
     </Stack.Navigator>
   );
 }
