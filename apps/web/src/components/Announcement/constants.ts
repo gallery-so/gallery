3e2590bcd887d748ca75debdc8847d27fdc2e99c
jakzaizzat <jakzaizzat@gmail.com>
2023-03-28 11:21:59 +0800

    add mint content

--- apps/web/src/components/Announcement/constants.ts
+++ apps/web/src/components/Announcement/constants.ts
@@ -2,28 +2,11 @@
 
 export const ANNOUNCEMENT_CONTENT = [
   {
-    key: 'MultiGalleryAnnouncement',
-    title: 'Multi Gallery',
-    description: 'You can now create multiple galleries and add them to your profile.',
-    date: '2023-03-14T04:56:26.154845Z',
+    key: 'UpsellMintMemento4',
+    title: 'Now Minting: Blooming Connections',
+    description:
+      'Gallery Memento #4 is now available for minting, a beautiful and symbolic representation of the growing network within the Gallery community.',
+    date: '2023-03-27T13:00:00.154845Z',
     link: '/mint/mementos',
-  },
-  {
-    key: 'EmailUpsell',
-    title: 'Email Upsell',
-    description: 'You can now add your email address to your profile. ',
-    date: '2023-03-26T04:56:26.154845Z',
-  },
-  {
-    key: 'MaintenanceFeb2023',
-    title: 'Maintenance Feb 2023',
-    description: 'We will be performing maintenance on the site on Feb 2023.',
-    date: '2023-03-14T04:56:26.154845Z',
-  },
-  {
-    key: 'TwitterConnectionOnboardingUpsell',
-    title: 'Connect Twitter',
-    description: 'Connect your Twitter account to your profile.',
-    date: '2023-03-14T04:56:26.154845Z',
   },
 ];
