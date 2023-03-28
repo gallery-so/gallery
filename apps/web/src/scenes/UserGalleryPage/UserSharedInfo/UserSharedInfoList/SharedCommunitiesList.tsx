ef6e985bfcb4436fa91b068f8aae8b7f1f0e5b17
jakzaizzat <jakzaizzat@gmail.com>
2023-03-28 22:20:33 +0800

    migrate shared community

--- apps/web/src/scenes/UserGalleryPage/UserSharedInfo/UserSharedInfoList/SharedCommunitiesList.tsx
+++ apps/web/src/scenes/UserGalleryPage/UserSharedInfo/UserSharedInfoList/SharedCommunitiesList.tsx
@@ -1,9 +1,10 @@
-import { useCallback, useMemo } from 'react';
+import { useVirtualizer } from '@tanstack/react-virtual';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { graphql, usePaginationFragment } from 'react-relay';
-import { AutoSizer, Index, InfiniteLoader, List, ListRowRenderer } from 'react-virtualized';
 import styled from 'styled-components';
 
 import { VStack } from '~/components/core/Spacer/Stack';
+import VirtualizeContainer from '~/components/Virtualize/VirtualizeContainer';
 import { SharedCommunitiesListFragment$key } from '~/generated/SharedCommunitiesListFragment.graphql';
 import { useIsMobileWindowWidth } from '~/hooks/useWindowSize';
 import { getUrlForCommunity } from '~/utils/getCommunityUrlForToken';
@@ -44,96 +45,72 @@ export default function SharedCommunitiesList({ queryRef }: Props) {
     [data.sharedCommunities?.edges]
   );
 
-  const rowCount = hasNext ? sharedCommunities.length + 1 : sharedCommunities.length;
+  const parentRef = useRef<HTMLDivElement | null>(null);
+
+  const virtualizer = useVirtualizer({
+    count: sharedCommunities.length,
+    getScrollElement: () => parentRef.current,
+    estimateSize: () => 40,
+    overscan: 5,
+  });
+
+  const virtualItems = virtualizer.getVirtualItems();
+
+  const [isFetchingNextPage, setIsFetchingNextPage] = useState(false);
 
   const handleLoadMore = useCallback(async () => {
+    setIsFetchingNextPage(true);
     loadNext(COMMUNITIES_PER_PAGE);
+    setIsFetchingNextPage(false);
   }, [loadNext]);
 
-  const isRowLoaded = ({ index }: { index: number }) =>
-    !hasNext || index < sharedCommunities.length;
+  useEffect(() => {
+    const [lastItem] = [...virtualizer.getVirtualItems()].reverse();
 
-  const getRowHeight = useCallback(
-    ({ index }: Index) => {
-      const community = sharedCommunities[index]?.node;
-      if (!community) {
-        return 0;
-      }
+    if (!lastItem) {
+      return;
+    }
 
-      const unescapedDescription = community.description ? unescape(community.description) : '';
-      const descriptionFirstLine = unescapedDescription.split('\n')[0] ?? '';
-
-      if (!community.name && !descriptionFirstLine.length) {
-        return 0;
-      }
-
-      if (descriptionFirstLine.length === 0) {
-        return 40;
-      }
-
-      return 56;
-    },
-    [sharedCommunities]
-  );
-
-  const rowRenderer = useCallback<ListRowRenderer>(
-    ({ index, key, style }: { index: number; key: string; style: React.CSSProperties }) => {
-      const community = sharedCommunities[index]?.node;
-      if (!community) {
-        return null;
-      }
-
-      const unescapedDescription = community.description ? unescape(community.description) : '';
-      const descriptionFirstLine = unescapedDescription.split('\n')[0] ?? '';
-
-      const communityUrlPath =
-        community.contractAddress?.address && community.chain
-          ? getUrlForCommunity(community.contractAddress?.address, community.chain)
-          : null;
-
-      if (!community.name && !descriptionFirstLine) {
-        return null;
-      }
-
-      return (
-        <div style={style} key={key}>
-          <PaginatedListRow
-            title={community.name ?? ''}
-            subTitle={descriptionFirstLine}
-            href={communityUrlPath}
-          />
-        </div>
-      );
-    },
-    [sharedCommunities]
-  );
+    if (lastItem.index >= sharedCommunities.length - 1 && hasNext && !isFetchingNextPage) {
+      handleLoadMore();
+    }
+  }, [handleLoadMore, hasNext, isFetchingNextPage, sharedCommunities.length, virtualizer]);
 
   const isMobile = useIsMobileWindowWidth();
 
   return (
     <StyledList fullscreen={isMobile} gap={24}>
-      <AutoSizer>
-        {({ width, height }) => (
-          <InfiniteLoader
-            isRowLoaded={isRowLoaded}
-            loadMoreRows={handleLoadMore}
-            rowCount={rowCount}
-          >
-            {({ onRowsRendered, registerChild }) => (
-              <List
-                ref={registerChild}
-                onRowsRendered={onRowsRendered}
-                rowRenderer={rowRenderer}
-                width={width}
-                height={height}
-                rowHeight={getRowHeight}
-                rowCount={sharedCommunities.length}
-              />
-            )}
-          </InfiniteLoader>
-        )}
-      </AutoSizer>
+      <VirtualizeContainer virtualizer={virtualizer} ref={parentRef}>
+        {virtualItems.map((item) => {
+          const community = sharedCommunities[item.index]?.node;
 
+          if (!community) {
+            return null;
+          }
+
+          const unescapedDescription = community.description ? unescape(community.description) : '';
+          const descriptionFirstLine = unescapedDescription.split('\n')[0] ?? '';
+
+          const communityUrlPath =
+            community.contractAddress?.address && community.chain
+              ? getUrlForCommunity(community.contractAddress?.address, community.chain)
+              : null;
+
+          if (!community.name && !descriptionFirstLine) {
+            return null;
+          }
+
+          return (
+            <div data-index={item.index} ref={virtualizer.measureElement} key={item.key}>
+              <PaginatedListRow
+                title={community.name ?? ''}
+                subTitle={descriptionFirstLine}
+                href={communityUrlPath}
+              />
+            </div>
+          );
+        })}
+      </VirtualizeContainer>
       <VStack></VStack>
     </StyledList>
   );
