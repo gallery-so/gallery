dbdd5aec52a06a7ba5ebd8e16a46405fc873a441
Terence Bezman <terencebezman@gmail.com>
2023-03-28 10:36:50 -0400

    fix types for navigators and add new login stack navigator

--- apps/mobile/src/navigation/types.ts
+++ apps/mobile/src/navigation/types.ts
@@ -1,35 +1,37 @@
 import { MaterialTopTabNavigationProp } from '@react-navigation/material-top-tabs';
-import { CompositeNavigationProp, CompositeScreenProps } from '@react-navigation/native';
+import { CompositeNavigationProp, NavigatorScreenParams } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { NativeStackNavigatorProps } from 'react-native-screens/lib/typescript/native-stack/types';
 
 export type RootStackNavigatorParamList = {
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  MainTabs: {};
-  NftDetail: {
-    tokenId: string;
-  };
+  MainTabs: NavigatorScreenParams<MainTabNavigatorParamList>;
+  Login: NavigatorScreenParams<LoginStackNavigatorParamList>;
 };
 
 export type FeedTabNavigatorParamList = {
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  Trending: {};
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  Latest: {};
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  Featured: {};
+  Trending: undefined;
+  Latest: undefined;
+  Featured: undefined;
 };
 
 export type MainTabNavigatorParamList = {
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  Account: {};
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  Home: {};
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  Notifications: {};
+  Account: undefined;
+  Home: NavigatorScreenParams<FeedTabNavigatorParamList>;
+  Notifications: undefined;
+};
+
+export type LoginStackNavigatorParamList = {
+  Landing: undefined;
+  EnterEmail: undefined;
+  WaitingForEmailVerification: undefined;
+  QRCode: undefined;
 };
 
 export type RootStackNavigatorProp = NativeStackNavigationProp<RootStackNavigatorParamList>;
+
+export type LoginStackNavigatorProp = CompositeNavigationProp<
+  NativeStackNavigationProp<RootStackNavigatorParamList, 'Login'>,
+  NativeStackNavigationProp<LoginStackNavigatorParamList>
+>;
 
 export type MainTabNavigatorProp = CompositeNavigationProp<
   NativeStackNavigationProp<RootStackNavigatorParamList, 'MainTabs'>,
