e3bc7687e18ecac21e8b169516dde06667fd93d5
Robinnnnn <rokim8@gmail.com>
2023-04-04 15:24:08 -0400

    extend mint and add stragglers

--- apps/web/src/scenes/MintPages/config.ts
+++ apps/web/src/scenes/MintPages/config.ts
@@ -1,6 +1,6 @@
 // time is in EST (GMT-05:00)
 export const MINT_START = '2023-03-27T13:00:00-05:00';
-export const MINT_END = '2023-04-03T23:59:00-05:00';
+export const MINT_END = '2023-04-07T23:59:00-05:00';
 
 // increment this each time we introduce a new token
 export const MEMENTOS_NFT_TOKEN_ID = 3;
@@ -13,4 +13,4 @@ export const pathToImage = featuredImage;
 
 // this probably won't change unless the cache is broken
 export const ALLOWLIST_URL =
-  'https://storage.googleapis.com/gallery-prod-325303.appspot.com/allowlists/mementos-allowlist-v3.json';
+  'https://storage.googleapis.com/gallery-prod-325303.appspot.com/allowlists/mementos-allowlist-v4.json';
