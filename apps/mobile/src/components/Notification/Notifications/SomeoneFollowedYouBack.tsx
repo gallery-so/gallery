b5bc970ad26d6f00c62f16cef16cee2c453d10b2
jakzaizzat <jakzaizzat@gmail.com>
2023-04-08 01:46:35 +0800

    Add SomeoneFollowedYouBack

---
+++ apps/mobile/src/components/Notification/Notifications/SomeoneFollowedYouBack.tsx
@@ -0,0 +1,46 @@
+import { Text } from 'react-native';
+import { useFragment } from 'react-relay';
+import { graphql } from 'relay-runtime';
+import { Typography } from '~/components/Typography';
+
+import { SomeoneFollowedYouBackFragment$key } from '~/generated/SomeoneFollowedYouBackFragment.graphql';
+
+type SomeoneFollowedYouBackProps = {
+  notificationRef: SomeoneFollowedYouBackFragment$key;
+};
+
+export function SomeoneFollowedYouBack({ notificationRef }: SomeoneFollowedYouBackProps) {
+  const notification = useFragment(
+    graphql`
+      fragment SomeoneFollowedYouBackFragment on SomeoneFollowedYouBackNotification {
+        count
+
+        followers(last: 1) {
+          edges {
+            node {
+              username
+            }
+          }
+        }
+      }
+    `,
+    notificationRef
+  );
+
+  const count = notification.count ?? 1;
+  const lastFollower = notification.followers?.edges?.[0]?.node;
+
+  return (
+    <Text>
+      <Typography
+        font={{
+          family: 'ABCDiatype',
+          weight: 'Bold',
+        }}
+      >
+        {count > 1 ? `${count} collectors` : `${lastFollower?.username ?? 'Someone'}`}
+      </Typography>{' '}
+      followed you back
+    </Text>
+  );
+}
