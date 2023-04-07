787eb333d692ce9e632ebd5b28f7f7b8a111af4e
jakzaizzat <jakzaizzat@gmail.com>
2023-04-08 01:41:43 +0800

    Add SomeoneAdmiredYourFeedEvent

---
+++ apps/mobile/src/components/Notification/Notifications/SomeoneAdmiredYourFeedEvent.tsx
@@ -0,0 +1,113 @@
+import { useMemo } from 'react';
+import { Text } from 'react-native';
+import { useFragment } from 'react-relay';
+import { graphql } from 'relay-runtime';
+import { Typography } from '~/components/Typography';
+
+import { SomeoneAdmiredYourFeedEventFragment$key } from '~/generated/SomeoneAdmiredYourFeedEventFragment.graphql';
+
+type SomeoneAdmiredYourFeedEventProps = {
+  notificationRef: SomeoneAdmiredYourFeedEventFragment$key;
+};
+
+export function SomeoneAdmiredYourFeedEvent({ notificationRef }: SomeoneAdmiredYourFeedEventProps) {
+  const notification = useFragment(
+    graphql`
+      fragment SomeoneAdmiredYourFeedEventFragment on SomeoneAdmiredYourFeedEventNotification {
+        count
+
+        feedEvent {
+          eventData {
+            ... on CollectionCreatedFeedEventData {
+              __typename
+              collection {
+                __typename
+                name
+              }
+            }
+
+            ... on CollectorsNoteAddedToCollectionFeedEventData {
+              __typename
+              collection {
+                __typename
+                name
+              }
+            }
+
+            ... on TokensAddedToCollectionFeedEventData {
+              __typename
+              collection {
+                __typename
+                name
+              }
+            }
+
+            ... on CollectionUpdatedFeedEventData {
+              __typename
+              collection {
+                __typename
+                name
+              }
+            }
+          }
+        }
+
+        admirers(last: 1) {
+          edges {
+            node {
+              __typename
+              username
+            }
+          }
+        }
+      }
+    `,
+    notificationRef
+  );
+
+  const count = notification.count ?? 1;
+  const firstAdmirer = notification.admirers?.edges?.[0]?.node;
+  const eventType = notification.feedEvent?.eventData?.__typename;
+
+  const verb = useMemo(() => {
+    switch (eventType) {
+      case 'CollectionCreatedFeedEventData':
+      case 'TokensAddedToCollectionFeedEventData':
+        return 'admired your additions to';
+      case 'CollectorsNoteAddedToCollectionFeedEventData':
+        return 'admired your note on';
+      case 'CollectionUpdatedFeedEventData':
+        return 'admired your updates to';
+      default:
+        return 'admired your updates to';
+    }
+  }, [eventType]);
+
+  // @ts-expect-error: property `collection` does not exist on type { readonly __typename: "%other" };
+  const collection = notification.feedEvent?.eventData?.collection;
+
+  return (
+    <Text>
+      {count > 1 ? (
+        <strong>{notification.count} collectors</strong>
+      ) : (
+        <>
+          {firstAdmirer ? (
+            <Typography
+              font={{
+                family: 'ABCDiatype',
+                weight: 'Bold',
+              }}
+            >
+              {firstAdmirer.username}
+            </Typography>
+          ) : (
+            <strong>Someone</strong>
+          )}
+        </>
+      )}
+      {` ${verb} `}
+      <Text>{collection ? collection?.name : 'your collection'}</Text>
+    </Text>
+  );
+}
