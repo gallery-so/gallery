1c731cb1a829e77b2125693bab9601b3cbbb0a01
Robinnnnn <rokim8@gmail.com>
2023-04-21 14:45:06 -0400

    undo

---
+++ apps/web/src/icons/UndoIcon.tsx
@@ -0,0 +1,16 @@
+export function UndoIcon() {
+  return (
+    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M5.99935 13.3337L2.66602 10.0003L5.99935 6.66699"
+        stroke="#fefefe"
+        stroke-miterlimit="10"
+      />
+      <path
+        d="M2.66602 10.0003H11.3327L13.3327 8.00033V2.66699"
+        stroke="#fefefe"
+        stroke-miterlimit="10"
+      />
+    </svg>
+  );
+}
