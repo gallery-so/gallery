b329cc2a3070c5c5eaf7c6e6ea5b3c0498110d31
Terence Bezman <terencebezman@gmail.com>
2023-04-24 12:37:07 -0400

    go back to enum

--- packages/shared/src/theme/colors.js
+++ packages/shared/src/theme/colors.js
@@ -1,15 +1,15 @@
-const colors = {
-  offWhite: '#F9F9F9',
-  white: '#FEFEFE',
-  offBlack: '#141414',
-  shadow: '#707070',
-  metal: '#9e9e9e',
-  faint: '#f2f2f2',
-  porcelain: '#e2e2e2',
-  hyperBlue: '#001CC1',
-  activeBlue: '#0022F0',
-  error: '#FF6666',
-  red: '#F00000',
-};
+enum colors {
+  offWhite = '#F9F9F9',
+  white = '#FEFEFE',
+  offBlack = '#141414',
+  shadow = '#707070',
+  metal = '#9e9e9e',
+  faint = '#f2f2f2',
+  porcelain = '#e2e2e2',
+  hyperBlue = '#001CC1',
+  activeBlue = '#0022F0',
+  error = '#FF6666',
+  red = '#F00000',
+}
 
 export default colors;
