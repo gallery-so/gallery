f9c5c507ad61811910a1c2cf58309b8bf76d7ebc
Terence Bezman <terencebezman@gmail.com>
2023-04-25 13:52:11 -0400

    simplify dimension calcs on token preview

--- apps/mobile/src/components/Gallery/GalleryTokenPreview.tsx
+++ apps/mobile/src/components/Gallery/GalleryTokenPreview.tsx
@@ -1,10 +1,11 @@
 import { ResizeMode } from 'expo-av';
-import { useCallback, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useWindowDimensions, View } from 'react-native';
 import { useFragment } from 'react-relay';
 import { graphql } from 'relay-runtime';
 
 import { ImageState, NftPreview } from '~/components/NftPreview/NftPreview';
+import { useGalleryTokenDimensionCache } from '~/contexts/GalleryTokenDimensionCacheContext';
 import { GalleryTokenPreviewFragment$key } from '~/generated/GalleryTokenPreviewFragment.graphql';
 import { fitDimensionsToContainerContain } from '~/screens/NftDetailScreen/NftDetailAsset/fitDimensionToContainer';
 import { Dimensions } from '~/screens/NftDetailScreen/NftDetailAsset/types';
@@ -47,34 +48,42 @@ export function GalleryTokenPreview({ tokenRef, containerWidth }: GalleryTokenPr
     throw new CouldNotRenderNftError('GalleryTokenPreview', 'Missing token url');
   }
 
+  const { cache, addDimensionsToCache } = useGalleryTokenDimensionCache();
+
   const [imageDimensions, setImageDimensions] = useState<Dimensions | null>(null);
 
+  const screenDimensions = useWindowDimensions();
   const handleImageStateChange = useCallback((imageState: ImageState) => {
     if (imageState.kind === 'loaded') {
       setImageDimensions(imageState.dimensions);
     }
   }, []);
 
-  const screenDimensions = useWindowDimensions();
-  const resultingDimensions = useMemo((): Dimensions | null => {
-    if (imageDimensions) {
+  const resultDimensions = useMemo(() => {
+    const cachedDimensions = cache.get(tokenUrl) ?? null;
+
+    if (cachedDimensions) {
+      return cachedDimensions;
+    } else if (imageDimensions) {
       const result = fitDimensionsToContainerContain({
         container: { width: containerWidth, height: screenDimensions.height / 2 },
         source: imageDimensions,
       });
 
       return result;
-    } else if (containerWidth) {
-      return { width: containerWidth, height: containerWidth };
     }
+  }, [cache, containerWidth, imageDimensions, screenDimensions.height, tokenUrl]);
 
-    return null;
-  }, [containerWidth, imageDimensions, screenDimensions.height]);
+  useEffect(() => {
+    if (resultDimensions) {
+      addDimensionsToCache(tokenUrl, resultDimensions);
+    }
+  }, [addDimensionsToCache, resultDimensions, tokenUrl]);
 
   return (
     <View
-      className="flex-shrink-0 flex-grow"
-      style={resultingDimensions ? resultingDimensions : { height: 200, width: '100%' }}
+      className="flex-grow"
+      style={resultDimensions ? resultDimensions : { height: 200, width: '100%' }}
     >
       <NftPreview
         onImageStateChange={handleImageStateChange}
