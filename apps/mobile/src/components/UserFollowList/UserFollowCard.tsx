df892efc0e621744cf26f49d26f5e24695105fe3
Terence Bezman <terencebezman@gmail.com>
2023-04-27 16:32:40 -0400

    implement navigation for user follow list

--- apps/mobile/src/components/UserFollowList/UserFollowCard.tsx
+++ apps/mobile/src/components/UserFollowList/UserFollowCard.tsx
@@ -1,4 +1,6 @@
-import { View } from 'react-native';
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
+import { useCallback } from 'react';
+import { TouchableOpacity, View } from 'react-native';
 import { useFragment } from 'react-relay';
 import { graphql } from 'relay-runtime';
 
@@ -7,6 +9,7 @@ import { Markdown } from '~/components/Markdown';
 import { Typography } from '~/components/Typography';
 import { UserFollowCardFragment$key } from '~/generated/UserFollowCardFragment.graphql';
 import { UserFollowCardQueryFragment$key } from '~/generated/UserFollowCardQueryFragment.graphql';
+import { RootStackNavigatorProp } from '~/navigation/types';
 
 type UserFollowCardProps = {
   userRef: UserFollowCardFragment$key;
@@ -37,14 +40,34 @@ export function UserFollowCard({ userRef, queryRef }: UserFollowCardProps) {
 
   const bioFirstLine = user.bio?.split('\n')[0];
 
+  const navigation = useNavigation<RootStackNavigatorProp>();
+  const route = useRoute();
+  const handlePress = useCallback(() => {
+    let delay = 0;
+
+    // These routes need to close their modal first
+    if (route.name === 'UserSuggestionList' || route.name === 'TwitterSuggestionList') {
+      delay = 250;
+      navigation.goBack();
+    }
+
+    setTimeout(() => {
+      if (!user.username) {
+        return;
+      }
+
+      navigation.push('Profile', { username: user.username });
+    }, delay);
+  }, [navigation, route.name, user.username]);
+
   return (
-    <View className="flex w-full flex-row items-center justify-between space-x-8 overflow-hidden py-3 px-4">
-      <View className="flex flex-1 flex-grow flex-col">
+    <View className="flex w-full flex-row items-center justify-between space-x-8 overflow-hidden py-3 px-4 h-16">
+      <TouchableOpacity onPress={handlePress} className="flex flex-1 flex-grow flex-col h-full">
         <Typography className="text-sm" font={{ family: 'ABCDiatype', weight: 'Bold' }}>
           {user.username}
         </Typography>
         <Markdown numberOfLines={1}>{bioFirstLine}</Markdown>
-      </View>
+      </TouchableOpacity>
 
       <FollowButton queryRef={query} userRef={user} />
     </View>
