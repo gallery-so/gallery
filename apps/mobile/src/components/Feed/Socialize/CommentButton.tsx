67b8f519d0081d751428b659e03215418ecafdd3
jakzaizzat <jakzaizzat@gmail.com>
2023-05-01 14:24:08 +0800

    toggle comment box

--- apps/mobile/src/components/Feed/Socialize/CommentButton.tsx
+++ apps/mobile/src/components/Feed/Socialize/CommentButton.tsx
@@ -1,5 +1,5 @@
 import { BottomSheetModal } from '@gorhom/bottom-sheet';
-import { useCallback, useMemo, useRef } from 'react';
+import { useCallback, useMemo, useRef, useState } from 'react';
 import { TouchableOpacity, View, ViewProps } from 'react-native';
 import { graphql, useFragment } from 'react-relay';
 
@@ -34,16 +34,25 @@ export function CommentButton({ eventRef, queryRef, style }: Props) {
     queryRef
   );
 
+  const [isCommentBoxOpen, setIsCommentBoxOpen] = useState(false);
+
   const bottomSheetRef = useRef<BottomSheetModal>(null);
   const snapPoints = useMemo(() => ['15%'], []);
 
-  const handleOpenCommentBox = useCallback(() => {
+  const toggleCommentBox = useCallback(() => {
+    if (isCommentBoxOpen) {
+      bottomSheetRef.current?.dismiss();
+      setIsCommentBoxOpen(false);
+      return;
+    }
+
     bottomSheetRef.current?.present();
-  }, []);
+    setIsCommentBoxOpen(true);
+  }, [isCommentBoxOpen]);
 
   return (
     <View className="flex flex-row space-x-4" style={style}>
-      <TouchableOpacity onPress={handleOpenCommentBox}>
+      <TouchableOpacity onPress={toggleCommentBox}>
         <CommentIcon />
       </TouchableOpacity>
 
