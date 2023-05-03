94dd57b6bcd2463551d0d8a2e43a2bdbe7ada0ad
Terence Bezman <terencebezman@gmail.com>
2023-05-03 00:14:22 -0400

    setup jest in shared package

---
+++ packages/shared/jest.config.js
@@ -0,0 +1,19 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const {
+  compilerOptions: { paths },
+} = require('./tsconfig.json');
+
+module.exports = {
+  setupFiles: ['<rootDir>/setupFile.js'],
+
+  transform: {
+    '^.+\\.(ts|tsx)$': 'babel-jest',
+  },
+
+  modulePaths: ['<rootDir>'],
+
+  moduleNameMapper: {
+    // Ensure all ouf our aliases from tsconfig work in jext
+    ...pathsToModuleNameMapper(paths, { prefix: '<rootDir>/' }),
+  },
+};
