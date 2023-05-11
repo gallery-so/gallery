2746d9ce23903d9a39e6b8420363203275ce7be4
Jakz <jakzaizzat@gmail.com>
2023-05-11 12:49:46 +0800

    [GAL-3028] Show Galleries & Followers Counter (#1550)
    * enable check spelling on search
    
    * Fix share url on profile/gallery/collection screen
    
    * Add galleries & follower counter

--- apps/mobile/src/components/ProfileView/ProfileTabBar.tsx
+++ apps/mobile/src/components/ProfileView/ProfileTabBar.tsx
@@ -1,15 +1,16 @@
 import { useCallback } from 'react';
-import { TouchableOpacity, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 
 import { Typography } from '../../components/Typography';
 
 type TabItemProps = {
   activeRoute: string;
   route: string;
+  counter?: number;
   onRouteChange: (route: string) => void;
 };
 
-function TabItem({ activeRoute, route, onRouteChange }: TabItemProps) {
+function TabItem({ activeRoute, counter = 0, route, onRouteChange }: TabItemProps) {
   const isFocused = activeRoute === route;
 
   const onPress = useCallback(() => {
@@ -28,15 +29,21 @@ function TabItem({ activeRoute, route, onRouteChange }: TabItemProps) {
         font={{ family: 'ABCDiatype', weight: 'Medium' }}
       >
         {route}
+        {counter > 0 && <Text> {counter}</Text>}
       </Typography>
     </TouchableOpacity>
   );
 }
 
+type ProfileTabRoutes = {
+  name: string;
+  counter?: number;
+};
+
 type Props = {
   activeRoute: string;
   onRouteChange: (route: string) => void;
-  routes: string[];
+  routes: ProfileTabRoutes[];
 };
 
 export function ProfileTabBar({ routes, activeRoute, onRouteChange }: Props) {
@@ -45,10 +52,11 @@ export function ProfileTabBar({ routes, activeRoute, onRouteChange }: Props) {
       {routes.map((route) => {
         return (
           <TabItem
-            key={route}
-            route={route}
+            key={route.name}
+            route={route.name}
             onRouteChange={onRouteChange}
             activeRoute={activeRoute}
+            counter={route.counter}
           />
         );
       })}
