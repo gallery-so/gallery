ee8e1600be7d334535cd51253793f76325c6aab7
Terence Bezman <terencebezman@gmail.com>
2023-05-15 15:47:25 -0400

    update color scheme

--- apps/web/src/utils/isDarkMode.ts
+++
@@ -1,7 +0,0 @@
-export function isDarkMode() {
-  if (typeof window === 'undefined') {
-    return false;
-  }
-
-  return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-}
