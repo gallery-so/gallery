9a52b446f7502fb80e5b1c929faa70fa4ab827d7
Terence Bezman <terencebezman@gmail.com>
2023-05-15 15:15:00 -0400

    update some icons to use current color

--- apps/web/src/icons/UserIcon.tsx
+++ apps/web/src/icons/UserIcon.tsx
@@ -3,9 +3,9 @@ export default function UserIcon() {
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
         d="M12 11.5C14.2091 11.5 16 9.70914 16 7.5C16 5.29086 14.2091 3.5 12 3.5C9.79086 3.5 8 5.29086 8 7.5C8 9.70914 9.79086 11.5 12 11.5Z"
-        stroke="black"
+        stroke="currentColor"
       />
-      <path d="M4.5 21V17L7.5 14L12 15.5L16.5 14L19.5 17V21" stroke="black" />
+      <path d="M4.5 21V17L7.5 14L12 15.5L16.5 14L19.5 17V21" stroke="currentColor" />
     </svg>
   );
 }
