c9a17af6e70d7d3b2b25fc5b707685dc98eed110
Robinnnnn <rokim8@gmail.com>
2023-05-18 18:53:41 -0400

    deprecate `Navigate to` tracking

--- apps/mobile/src/navigation/MainTabNavigator/TabBar.tsx
+++ apps/mobile/src/navigation/MainTabNavigator/TabBar.tsx
@@ -13,7 +13,6 @@ import { GLogo } from '~/navigation/MainTabNavigator/GLogo';
 import { NotificationsIcon } from '~/navigation/MainTabNavigator/NotificationsIcon';
 import { SearchIcon } from '~/navigation/MainTabNavigator/SearchIcon';
 import { MainTabNavigatorParamList } from '~/navigation/types';
-import { useTrack } from '~/shared/contexts/AnalyticsContext';
 
 type TabItemProps = {
   icon: ReactNode;
@@ -22,17 +21,8 @@ type TabItemProps = {
   navigation: MaterialTopTabBarProps['navigation'];
 };
 
-const TAB_NAMES: { [key in keyof MainTabNavigatorParamList]: string } = {
-  AccountTab: 'Account',
-  HomeTab: 'Home',
-  SearchTab: 'Search',
-  NotificationsTab: 'Notifications',
-};
-
 function TabItem({ navigation, route, icon, activeRoute }: TabItemProps) {
   const isFocused = activeRoute === route.name;
-
-  const track = useTrack();
 
   const onPress = useCallback(() => {
     const event = navigation.emit({
@@ -41,13 +31,10 @@ function TabItem({ navigation, route, icon, activeRoute }: TabItemProps) {
       canPreventDefault: true,
     });
 
-    const routeName = TAB_NAMES[route.name as keyof typeof TAB_NAMES];
-    track(`Navigate to ${routeName}`, {});
-
     if (!isFocused && !event.defaultPrevented) {
       navigation.navigate(route.name);
     }
-  }, [isFocused, navigation, route, track]);
+  }, [isFocused, navigation, route]);
 
   const isHome = route.name === 'Home';
 
