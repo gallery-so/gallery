191e251d01a8e9e3d6997ac356a80b4ab692146c
Robinnnnn <rokim8@gmail.com>
2023-06-13 18:43:54 -0400

    wip dark banner component

--- apps/web/src/contexts/globalLayout/GlobalBanner/GlobalBanner.tsx
+++ apps/web/src/contexts/globalLayout/GlobalBanner/GlobalBanner.tsx
@@ -1,9 +1,11 @@
+import { useRouter } from 'next/router';
 import { useCallback } from 'react';
 import { graphql, useFragment } from 'react-relay';
 import styled from 'styled-components';
 
-import breakpoints, { contentSize } from '~/components/core/breakpoints';
+import { Chip } from '~/components/core/Chip/Chip';
 import Markdown from '~/components/core/Markdown/Markdown';
+import { HStack } from '~/components/core/Spacer/Stack';
 import { BaseM, TitleS } from '~/components/core/Text/Text';
 import { useGlobalNavbarHeight } from '~/contexts/globalLayout/GlobalNavbar/useGlobalNavbarHeight';
 import { UserExperienceType } from '~/generated/enums';
@@ -12,9 +14,12 @@ import { DecoratedCloseIcon } from '~/icons/CloseIcon';
 import colors from '~/shared/theme/colors';
 import useExperience from '~/utils/graphql/experiences/useExperience';
 
+type Variant = 'default' | 'lit';
+
 type Props = {
   queryRef: GlobalBannerFragment$key;
   experienceFlag: UserExperienceType;
+  variant: Variant;
   text: string;
   title?: React.ReactNode | string;
   requireAuth?: boolean;
@@ -25,6 +30,7 @@ type Props = {
 export default function GlobalBanner({
   queryRef,
   experienceFlag,
+  variant = 'default',
   text,
   title,
   requireAuth = false,
@@ -50,14 +56,14 @@ export default function GlobalBanner({
 
   const isAuthenticated = Boolean(query.viewer?.user?.id);
 
-  const [isMaintenanceBannerExperienced, setMaintenanceBannerExperienced] = useExperience({
+  const [isBannerExperienced, setBannerExperienced] = useExperience({
     type: experienceFlag,
     queryRef: query,
   });
 
   const hideBanner = useCallback(async () => {
-    await setMaintenanceBannerExperienced();
-  }, [setMaintenanceBannerExperienced]);
+    await setBannerExperienced();
+  }, [setBannerExperienced]);
 
   const handleActionClick = useCallback(() => {
     if (dismissOnActionComponentClick) {
@@ -67,20 +73,20 @@ export default function GlobalBanner({
 
   const navbarHeight = useGlobalNavbarHeight();
 
-  if (text.length === 0 || isMaintenanceBannerExperienced || (requireAuth && !isAuthenticated)) {
+  if (text.length === 0 || isBannerExperienced || (requireAuth && !isAuthenticated)) {
     return null;
   }
 
   return (
-    <StyledContainer navbarHeight={navbarHeight}>
-      <StyledBanner>
+    <StyledContainer navbarHeight={navbarHeight} variant={variant}>
+      <StyledBanner justify="space-between" align="center" gap={24}>
         <TextContainer>
           {title && <StyledTitle>{title}</StyledTitle>}
           <BaseM>
             <Markdown text={text} />
           </BaseM>
         </TextContainer>
-        <StyledAction>
+        <StyledAction align="center">
           <span onClick={handleActionClick}>{actionComponent}</span>
           <StyledClose onClick={hideBanner} />
         </StyledAction>
@@ -89,20 +95,38 @@ export default function GlobalBanner({
   );
 }
 
-const StyledContainer = styled.div<{ navbarHeight: number }>`
+const StyledContainer = styled.div<{ navbarHeight: number; variant: Variant }>`
+  // commenting this out for now, might be dangerous, but seems OK for now
+  // height: ${({ navbarHeight }) => navbarHeight}px;
+
   position: absolute;
   width: 100%;
-  height: ${({ navbarHeight }) => navbarHeight}px;
   z-index: 4;
 
-  // TODO: standardize these settings
-  background: rgba(254, 254, 254, 0.95);
-  backdrop-filter: blur(48px);
+  ${({ variant }) => {
+    if (variant === 'default') {
+      return `
+        border: 1px solid ${colors.shadow};
+        background: rgba(254, 254, 254, 0.95);
+        backdrop-filter: blur(48px);
+      `;
+    }
+    if (variant === 'lit') {
+      return `
+        border: 1px solid ${colors.black['800']};
+        background: ${colors.black['800']};
+        
+        ${BaseM} {
+          color: ${colors.white};
+        }
 
-  padding: 17px;
-  @media (max-width: ${contentSize.desktop}px) {
-    padding: 0px;
-  }
+        // TODO: this is a hack; we should probably just toggle dark mode for underlying icon container
+        svg {
+          stroke: ${colors.white};
+        }
+      `;
+    }
+  }}
 `;
 
 const TextContainer = styled.div`
@@ -111,56 +135,48 @@ const TextContainer = styled.div`
   max-width: calc(100% - 20px);
 `;
 
-const StyledBanner = styled.div`
+const StyledBanner = styled(HStack)`
   text-align: left;
   width: 100%;
-  padding: 16px 16px 0px 16px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  top: 0;
-  border: 1px solid ${colors.shadow};
   position: relative;
-  align-items: flex-start;
-  flex-direction: column;
-  background: white;
-
-  @media only screen and ${breakpoints.tablet} {
-    flex-direction: row;
-    align-items: center;
-    padding: 8px 16px;
-  }
+  padding: 8px 16px;
 `;
 
 const StyledClose = styled(DecoratedCloseIcon)`
-  padding: 10px;
+  padding: 8px;
   display: flex;
   justify-content: center;
   align-items: center;
 
-  position: absolute;
-  right: 0;
-  top: 0;
-
-  @media only screen and ${breakpoints.tablet} {
-    padding: 8px;
-    height: 100%;
-  }
+  // override default height induced by icon
+  height: 0px;
 `;
 
 const StyledTitle = styled(TitleS)`
   display: inline-block;
 `;
 
-const StyledAction = styled.div`
-  display: flex;
-  align-items: center;
-  margin-top: 16px;
-  line-height: 14px;
+const StyledAction = styled(HStack)`
+  height: 100%;
+  margin-right: -16px;
+`;
 
-  @media only screen and ${breakpoints.tablet} {
-    margin-top: 0px;
-    margin-left: 20px;
-    margin-right: 16px;
-  }
+export const CTAChip = function () {
+  const { push } = useRouter();
+
+  return (
+    <StyledChip
+      onClick={() => {
+        push('/mobile');
+      }}
+    >
+      Download
+    </StyledChip>
+  );
+};
+
+const StyledChip = styled(Chip)`
+  background-color: ${colors.white};
+  color: ${colors.black['800']};
+  width: 88px;
 `;
