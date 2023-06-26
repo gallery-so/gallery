d6a3163d52034b93a9cd90f697d525b50110513c
Terence Bezman <terencebezman@gmail.com>
2023-06-26 16:09:52 -0400

    create raw select component

--- apps/mobile/src/components/Select.tsx
+++ apps/mobile/src/components/Select.tsx
@@ -1,11 +1,38 @@
-type Option = {
-  id: string;
+import { View, ViewProps } from 'react-native';
+import { DropdownIcon } from 'src/icons/DropdownIcon';
+
+import { GalleryTouchableOpacity } from './GalleryTouchableOpacity';
+import { Typography } from './Typography';
+
+type Option<T> = {
+  id: T;
   label: string;
 };
 
-export type SelectProps = {
-  selected: Option;
-  options: Option[];
+export type SelectProps<T> = {
+  selectedId: T;
+  options: Option<T>[];
+  onChange: (id: T) => void;
+
+  className?: string;
+  style?: ViewProps['style'];
 };
 
-export function Select() {}
+export function Select<T extends string>({ selectedId, options, onChange, style }: SelectProps<T>) {
+  const selected = options.find((option) => option.id === selectedId);
+
+  return (
+    <GalleryTouchableOpacity
+      className="bg-faint h-7 py-1 px-2 flex flex-row items-center justify-between space-x-2"
+      style={style}
+    >
+      <Typography font={{ family: 'ABCDiatype', weight: 'Regular' }} className="text-sm">
+        {selected?.label}
+      </Typography>
+
+      <View className="flex flex-col">
+        <DropdownIcon />
+      </View>
+    </GalleryTouchableOpacity>
+  );
+}
