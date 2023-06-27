3b72d247192ffc0337cd4ab223f3fc71d37eaa31
jakzaizzat <jakzaizzat@gmail.com>
2023-06-27 12:01:30 +0800

    Add FF for admire

--- apps/web/src/components/Feed/Socialize/AdmireLine.tsx
+++ apps/web/src/components/Feed/Socialize/AdmireLine.tsx
@@ -1,12 +1,17 @@
 import { useMemo } from 'react';
 import { useFragment } from 'react-relay';
 import { graphql } from 'relay-runtime';
+import styled from 'styled-components';
 
 import { HStack } from '~/components/core/Spacer/Stack';
+import { BODY_FONT_FAMILY } from '~/components/core/Text/Text';
+import HoverCardOnUsername from '~/components/HoverCard/HoverCardOnUsername';
 import { ProfilePictureStack } from '~/components/ProfilePictureStack';
 import { AdmireLineEventFragment$key } from '~/generated/AdmireLineEventFragment.graphql';
 import { AdmireLineQueryFragment$key } from '~/generated/AdmireLineQueryFragment.graphql';
 import { removeNullValues } from '~/shared/relay/removeNullValues';
+import colors from '~/shared/theme/colors';
+import isFeatureEnabled, { FeatureFlag } from '~/utils/graphql/isFeatureEnabled';
 
 import { useAdmireModal } from './AdmireModal/useAdmireModal';
 
@@ -29,7 +34,10 @@ export function AdmireLine({ eventRef, queryRef }: CommentLineProps) {
             node {
               __typename
               admirer {
+                dbid
+                username
                 ...ProfilePictureStackFragment
+                ...HoverCardOnUsernameFragment
               }
             }
           }
@@ -43,11 +51,21 @@ export function AdmireLine({ eventRef, queryRef }: CommentLineProps) {
   const query = useFragment(
     graphql`
       fragment AdmireLineQueryFragment on Query {
+        viewer {
+          ... on Viewer {
+            user {
+              dbid
+            }
+          }
+        }
         ...useAdmireModalQueryFragment
+        ...isFeatureEnabledFragment
       }
     `,
     queryRef
   );
+
+  const isPfpEnabled = isFeatureEnabled(FeatureFlag.PFP, query);
 
   const openAdmireModal = useAdmireModal({ eventRef: event, queryRef: query });
 
@@ -65,11 +83,88 @@ export function AdmireLine({ eventRef, queryRef }: CommentLineProps) {
     return removeNullValues(admires);
   }, [event.admires?.edges]);
 
+  const [admire] = nonNullAdmires;
+
+  const admirerName = useMemo(() => {
+    if (!admire) return '<unknown>';
+
+    const isTheAdmirerTheLoggedInUser = query.viewer?.user?.dbid === admire.dbid;
+
+    if (isTheAdmirerTheLoggedInUser) {
+      return 'You';
+    } else if (admire.username) {
+      return admire.username;
+    } else {
+      return '<unknown>';
+    }
+  }, [admire, query.viewer?.user?.dbid]);
+
   const totalAdmires = event.admires?.pageInfo.total ?? 0;
 
+  if (isPfpEnabled) {
+    return (
+      <HStack gap={4} align="flex-end" onClick={openAdmireModal}>
+        <ProfilePictureStack usersRef={nonNullAdmires} total={totalAdmires} />
+      </HStack>
+    );
+  }
+
   return (
-    <HStack gap={4} align="flex-end" onClick={openAdmireModal}>
-      <ProfilePictureStack usersRef={nonNullAdmires} total={totalAdmires} />
+    <HStack gap={4} align="flex-end">
+      {admire && (
+        <HoverCardOnUsername userRef={admire}>
+          <AdmirerName>{admirerName}</AdmirerName>
+        </HoverCardOnUsername>
+      )}
+      {totalAdmires === 1 ? (
+        <AdmirerText>admired this</AdmirerText>
+      ) : (
+        <>
+          <AdmirerText>and</AdmirerText>
+          <AdmirerLink onClick={openAdmireModal}>
+            {/*                                  |-- Checking for two here since we have  */}
+            {/*                                  |   to subtract one to get the remaining count */}
+            {totalAdmires - 1} {totalAdmires === 2 ? 'other' : 'others'}
+          </AdmirerLink>
+          <AdmirerText>admired this</AdmirerText>
+        </>
+      )}
     </HStack>
   );
 }
+
+const AdmirerName = styled.a`
+  font-family: ${BODY_FONT_FAMILY};
+  vertical-align: bottom;
+  font-size: 12px;
+  line-height: 1;
+  font-weight: 700;
+  text-decoration: none;
+  color: ${colors.black['800']};
+`;
+
+const AdmirerText = styled.div`
+  font-family: ${BODY_FONT_FAMILY};
+  font-size: 12px;
+  line-height: 1;
+  font-weight: 400;
+  flex-shrink: 1;
+  min-width: 0;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+`;
+
+const AdmirerLink = styled.span`
+  cursor: pointer;
+  font-family: ${BODY_FONT_FAMILY};
+  font-size: 12px;
+  line-height: 1;
+  font-weight: 400;
+  text-decoration: underline;
+  color: ${colors.shadow};
+
+  &:hover {
+    text-decoration: none;
+  }
+`;
