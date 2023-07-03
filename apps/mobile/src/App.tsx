3a80885f3db0a50cb88f190886d66dc044ba9a64
Terence Bezman <terencebezman@gmail.com>
2023-07-02 20:42:33 -0400

    revert color scheme testing

--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -66,7 +66,7 @@ export default function App() {
             message: `Loaded initial color scheme: ${colorScheme}`,
           });
 
-          setColorScheme('dark' as 'light' | 'dark');
+          setColorScheme(colorScheme as 'light' | 'dark');
 
           setColorSchemeLoaded(true);
         })
