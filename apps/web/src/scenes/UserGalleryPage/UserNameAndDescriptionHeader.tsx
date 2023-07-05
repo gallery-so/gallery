dcf83a780d582f7e0a7c4f9265d6a46db8dc9c07
Robinnnnn <rokim8@gmail.com>
2023-07-05 15:13:10 -0400

    fix typo

--- apps/web/src/scenes/UserGalleryPage/UserNameAndDescriptionHeader.tsx
+++ apps/web/src/scenes/UserGalleryPage/UserNameAndDescriptionHeader.tsx
@@ -80,7 +80,7 @@ export function UserNameAndDescriptionHeader({ userRef, queryRef }: Props) {
 
     showModal({
       content: <EditUserInfoModal queryRef={query} />,
-      headerText: 'Edit user info clicked',
+      headerText: 'Edit profile',
     });
   }, [isAuthenticatedUser, query, showModal, track]);
 
