7fb4db53db8765b82384bf472415b63788733ba3
Robinnnnn <rokim8@gmail.com>
2023-07-12 14:02:44 -0400

    test WIP

--- packages/shared/src/utils/extractMirrorXyz.test.tsx
+++ packages/shared/src/utils/extractMirrorXyz.test.tsx
@@ -1,12 +1,18 @@
 import { extractMirrorXyzUrl } from './extractMirrorXyzUrl';
 
+function generateData(description: string) {
+  return `{"animation_url":"https://mirror.xyz/10/0x9fe9f4b985234ff185ecddb980e7567262b71b6f/render","description":"${description}","external_url":"","format":"png","image_url":"ipfs://QmPj5kFNhugP1V7s6yMRJtKSQUagMb2iDcNsKXc2miJpdy","media_type":"png","name":"When the sun hits 3"}`;
+}
+
 describe('extractMirrorXyzUrl', () => {
   test('extracts correctly', () => {
-    expect(extractMirrorXyzUrl('https://mirror.xyz/test/post')).toEqual('https://mirror.xyz');
-    expect(extractMirrorXyzUrl('https://mirror.xyz/')).toEqual('https://mirror.xyz');
-    expect(extractMirrorXyzUrl('https://mirror')).toEqual('');
-    expect(extractMirrorXyzUrl('https://mirror.xzy')).toEqual('');
-    expect(extractMirrorXyzUrl('https://mirror.xyz is the link')).toEqual('');
-    expect(extractMirrorXyzUrl('mirror.xyz')).toEqual('');
+    expect(extractMirrorXyzUrl(generateData('https://mirror.xyz/test/post'))).toEqual(
+      'https://mirror.xyz/test/post'
+    );
+    // expect(extractMirrorXyzUrl('https://mirror.xyz/')).toEqual('https://mirror.xyz');
+    // expect(extractMirrorXyzUrl('https://mirror')).toEqual('');
+    // expect(extractMirrorXyzUrl('https://mirror.xzy')).toEqual('');
+    // expect(extractMirrorXyzUrl('https://mirror.xyz is the link')).toEqual('');
+    // expect(extractMirrorXyzUrl('mirror.xyz')).toEqual('');
   });
 });
