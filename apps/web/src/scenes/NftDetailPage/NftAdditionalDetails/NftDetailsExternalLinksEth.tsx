2527f9bbf790c132f2c16364514677988fc4e6be
Sourav Kumar Nanda <souravkumarnanda4@gmail.com>
2023-07-29 01:04:35 +0530

    GAL-2990: last refreshed timestamp on NFT detail page (#1748)
    * add stylings for last refreshed metadata pill button
    
    * fetch the timestamp & toss it into the tooltip
    
    * add the refresh pill component for Tezos details page
    
    * fix a minor gap issue
    
    * fix a minor typecheck issue
    
    * add cursor hover on ButtonPill
    
    * place the tooltip above the button to accommodate screen sizes

--- apps/web/src/scenes/NftDetailPage/NftAdditionalDetails/NftDetailsExternalLinksEth.tsx
+++ apps/web/src/scenes/NftDetailPage/NftAdditionalDetails/NftDetailsExternalLinksEth.tsx
@@ -2,11 +2,18 @@ import { useMemo } from 'react';
 import { graphql, useFragment } from 'react-relay';
 import styled from 'styled-components';
 
+import IconContainer from '~/components/core/IconContainer';
 import InteractiveLink from '~/components/core/InteractiveLink/InteractiveLink';
-import { VStack } from '~/components/core/Spacer/Stack';
+import { HStack, VStack } from '~/components/core/Spacer/Stack';
+import { TitleDiatypeM } from '~/components/core/Text/Text';
+import { ButtonPill } from '~/components/Pill';
+import { NewTooltip } from '~/components/Tooltip/NewTooltip';
+import { useTooltipHover } from '~/components/Tooltip/useTooltipHover';
 import { NftDetailsExternalLinksEthFragment$key } from '~/generated/NftDetailsExternalLinksEthFragment.graphql';
+import { RefreshIcon } from '~/icons/RefreshIcon';
 import { extractMirrorXyzUrl } from '~/shared/utils/extractMirrorXyzUrl';
 import { getOpenseaExternalUrl, hexHandler } from '~/shared/utils/getOpenseaExternalUrl';
+import { DateFormatOption, formatDateString } from '~/utils/formatDateString';
 
 import { useRefreshMetadata } from './useRefreshMetadata';
 
@@ -24,6 +31,7 @@ export default function NftDetailsExternalLinksEth({ tokenRef }: Props) {
         tokenId
         tokenMetadata
         chain
+        lastUpdated
         contract {
           contractAddress {
             address
@@ -35,7 +43,14 @@ export default function NftDetailsExternalLinksEth({ tokenRef }: Props) {
     tokenRef
   );
 
-  const { tokenId, contract, externalUrl, tokenMetadata, chain } = token;
+  const {
+    tokenId,
+    contract,
+    externalUrl,
+    tokenMetadata,
+    chain,
+    lastUpdated: lastUpdatedRaw,
+  } = token;
 
   const [refresh, isRefreshing] = useRefreshMetadata(token);
 
@@ -66,20 +81,41 @@ export default function NftDetailsExternalLinksEth({ tokenRef }: Props) {
     }
     return null;
   }, [contract?.contractAddress?.address, tokenId]);
+  const { floating, reference, getFloatingProps, getReferenceProps, floatingStyle } =
+    useTooltipHover({
+      placement: 'top',
+    });
+
+  const lastUpdated = formatDateString(lastUpdatedRaw, DateFormatOption.ABBREVIATED);
 
   return (
-    <StyledExternalLinks gap={4}>
+    <StyledExternalLinks gap={14}>
       {mirrorXyzUrl && <InteractiveLink href={mirrorXyzUrl}>View on Mirror</InteractiveLink>}
       {prohibitionUrl && (
         <InteractiveLink href={prohibitionUrl}>View on Prohibition</InteractiveLink>
       )}
       {openSeaExternalUrl && (
-        <>
+        <VStack gap={14}>
           <InteractiveLink href={openSeaExternalUrl}>View on OpenSea</InteractiveLink>
-          <InteractiveLink onClick={refresh} disabled={isRefreshing}>
-            Refresh metadata
-          </InteractiveLink>
-        </>
+          <StartAlignedButtonPill
+            onClick={refresh}
+            disabled={isRefreshing}
+            ref={reference}
+            {...getReferenceProps()}
+          >
+            <HStack gap={6}>
+              <IconContainer size="xs" variant="default" icon={<RefreshIcon />} />
+              <TitleDiatypeM>Refresh metadata</TitleDiatypeM>
+            </HStack>
+            <NewTooltip
+              {...getFloatingProps()}
+              style={{ ...floatingStyle }}
+              ref={floating}
+              whiteSpace="pre-line"
+              text={`Last refreshed ${lastUpdated}`}
+            />
+          </StartAlignedButtonPill>
+        </VStack>
       )}
       {externalUrl && <InteractiveLink href={externalUrl}>More Info</InteractiveLink>}
     </StyledExternalLinks>
@@ -87,3 +123,7 @@ export default function NftDetailsExternalLinksEth({ tokenRef }: Props) {
 }
 
 const StyledExternalLinks = styled(VStack)``;
+
+const StartAlignedButtonPill = styled(ButtonPill)`
+  align-self: start;
+`;
---
+++ apps/web/src/utils/formatDateString.ts
@@ -0,0 +1,17 @@
+import { format } from 'date-fns';
+
+// Define an enum with user-friendly names for format options
+export enum DateFormatOption {
+  ABBREVIATED = 'MMM d h:mm a', // e.g., "Jul 28 4:47 PM"
+  FULL = 'MMMM d, yyyy h:mm a', // e.g., "July 28, 2023 4:47 PM"
+  ISO_8601 = 'yyyy-MM-dd HH:mm:ss', // e.g., "2023-07-28 15:47:51"
+}
+
+// Utility function to format a date string
+export const formatDateString = (
+  inputDateString: string,
+  formatOption: DateFormatOption
+): string => {
+  const date: Date = new Date(inputDateString);
+  return format(date, formatOption);
+};
