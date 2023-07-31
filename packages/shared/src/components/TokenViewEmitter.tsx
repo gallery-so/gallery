7619ce522a1d7e88db14f98e02df8b50ac030d90
Rohan-cp <rohancp9@icloud.com>
2023-07-31 19:05:38 -0400

    base

---
+++ packages/shared/src/components/TokenViewEmitter.tsx
@@ -0,0 +1,66 @@
+import { Suspense, useEffect } from 'react';
+import { graphql } from 'relay-runtime';
+
+import { TokenViewEmitterMutation } from '~/generated/TokenViewEmitterMutation.graphql';
+
+import { useReportError } from '../contexts/ErrorReportingContext';
+import { usePromisifiedMutation } from '../relay/usePromisifiedMutation';
+
+type TokenViewEmitterProps = {
+  collectionID: string;
+  tokenID: string;
+};
+
+function TokenViewEmitter({ collectionID, tokenID }: TokenViewEmitterProps) {
+  const [viewToken] = usePromisifiedMutation<TokenViewEmitterMutation>(graphql`
+    mutation TokenViewEmitterMutation($tokenID: DBID!, $collectionID: DBID!) {
+      viewToken(tokenID: $tokenID, collectionID: $collectionID) {
+        __typename
+      }
+    }
+  `);
+
+  const reportError = useReportError();
+
+  useEffect(() => {
+    // Uncomment to skip tracking of anonymous views
+    // const isLoggedIn = query.viewer?.user?.dbid;
+    // if (!isLoggedIn) {
+    //   return;
+    // }
+
+    if (!tokenID) {
+      return;
+    }
+
+    viewToken({ variables: { tokenID, collectionID } }).catch((error) => {
+      if (error instanceof Error) {
+        reportError(error);
+      } else {
+        reportError(
+          'Something unexpected went wrong while trying to submit the `viewToken` mutation'
+        );
+      }
+    });
+  }, [tokenID, collectionID, reportError, viewToken]);
+
+  return null;
+}
+
+type TokenViewEmitterWithSuspenseProps = {
+  collectionID: string;
+  tokenID: string;
+};
+
+function TokenViewEmitterWithSuspense({
+  tokenID,
+  collectionID,
+}: TokenViewEmitterWithSuspenseProps) {
+  return (
+    <Suspense fallback={null}>
+      <TokenViewEmitter tokenID={tokenID} collectionID={collectionID} />
+    </Suspense>
+  );
+}
+
+export default TokenViewEmitterWithSuspense;
