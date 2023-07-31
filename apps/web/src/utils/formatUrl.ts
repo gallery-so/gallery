357db1acd76ef05fc994e4ba79ba2c80fbe81743
Rohan-cp <rohancp9@icloud.com>
2023-07-31 17:25:32 -0400

    base

--- apps/web/src/utils/formatUrl.ts
+++ apps/web/src/utils/formatUrl.ts
@@ -1,7 +1,8 @@
 import { VALID_URL } from './regex';
 
 export default function formatUrl(str: string): string {
-  const matchesUrls = str.match(VALID_URL);
+  const unformattedStr = str.replace(/\[([^\]]+)\]\([^)]+\)/g, '$1');
+  const matchesUrls = unformattedStr.match(VALID_URL);
   let formattedText = str;
   matchesUrls?.map((url: string) => {
     formattedText = formattedText.replace(url, `[${url}](${url})`);
