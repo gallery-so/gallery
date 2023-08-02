dd27339552dc2b3b8b9daa3d46737328a533510c
robin <rokim8@gmail.com>
2023-08-02 18:59:16 -0400

    Node v16 -> v20 (#1769)
    * bump with mobile change
    
    * trigger mobile release

--- apps/mobile/src/icons/BackIcon.tsx
+++ apps/mobile/src/icons/BackIcon.tsx
@@ -7,7 +7,7 @@ import colors from '~/shared/theme/colors';
 export function BackIcon(props: SvgProps) {
   const { colorScheme } = useColorScheme();
   return (
-    <Svg width={16} height={16} fill="none" {...props}>
+    <Svg width={16} height={16} fill="none " {...props}>
       <Path
         d="M10 3.333 5.333 8 10 12.667"
         stroke={colorScheme === 'dark' ? colors.white : colors.black['800']}
