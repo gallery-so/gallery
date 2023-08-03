17b74d72635a784cc45f89ee11cbb6b30beb494b
jakzaizzat <jakzaizzat@gmail.com>
2023-08-03 16:47:24 +0800

    add scale support to post icon

--- apps/mobile/src/navigation/MainTabNavigator/PostIcon.tsx
+++ apps/mobile/src/navigation/MainTabNavigator/PostIcon.tsx
@@ -1,4 +1,4 @@
-import Svg, { Path, SvgProps } from 'react-native-svg';
+import Svg, { G, Path, SvgProps } from 'react-native-svg';
 
 type Props = {
   color: string;
@@ -6,13 +6,20 @@ type Props = {
 } & SvgProps;
 
 export const PostIcon = ({ color, size = 24, ...props }: Props) => {
+  const height = size;
+  const originalHeight = 24;
+  const scale = height / originalHeight;
+  const width = height;
+
   return (
-    <Svg width={size} height={size} fill="none" {...props}>
-      <Path
-        stroke={color}
-        strokeMiterlimit={10}
-        d="M.5 21.333V.5h23v23H.5v-2.167ZM12 6.666v10.667M17.333 12H6.667"
-      />
+    <Svg width={width} height={width} fill="none" viewBox={`0 0 ${width} ${width}`} {...props}>
+      <G transform={`scale(${scale})`}>
+        <Path
+          stroke={color}
+          strokeMiterlimit={10}
+          d="M.5 21.333V.5h23v23H.5v-2.167ZM12 6.666v10.667M17.333 12H6.667"
+        />
+      </G>
     </Svg>
   );
 };
