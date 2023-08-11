b6d9d2df6852062c40e33e49031d9c5334df3d06
jakzaizzat <jakzaizzat@gmail.com>
2023-08-11 10:24:13 +0800

    Add more chain options

---
+++ apps/mobile/src/utils/getChainIconComponent.tsx
@@ -0,0 +1,43 @@
+import { View } from 'react-native';
+import { ArbitrumIcon } from 'src/icons/ArbitrumIcon';
+import { BaseIcon } from 'src/icons/BaseIcon';
+import { EthIcon } from 'src/icons/EthIcon';
+import { OptimismIcon } from 'src/icons/OptimismIcon';
+import { PoapIcon } from 'src/icons/PoapIcon';
+import { PolygonIcon } from 'src/icons/PolygonIcon';
+import { TezosIcon } from 'src/icons/TezosIcon';
+import { ZoraIcon } from 'src/icons/ZoraIcon';
+
+import { ChainMetadata } from '~/shared/utils/chains';
+
+export function getChainIconComponent(chain: ChainMetadata) {
+  switch (chain.name) {
+    case 'Ethereum':
+      return <EthIcon />;
+
+    case 'Tezos':
+      return <TezosIcon />;
+
+    case 'POAP':
+      return <PoapIcon className="w-4 h-4" />;
+
+    case 'Zora':
+      return <ZoraIcon />;
+
+    case 'Arbitrum':
+      return <ArbitrumIcon />;
+
+    case 'Base':
+      return <BaseIcon />;
+
+    case 'Optimism':
+      return <OptimismIcon />;
+
+    case 'Polygon':
+      return <PolygonIcon />;
+
+    default:
+      return <View className="w-4 h-4 rounded-full bg-porcelain dark:bg-metal" />;
+      break;
+  }
+}
