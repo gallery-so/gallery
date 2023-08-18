af13dc096b300de492a4aa21f5f2d310630533cb
jakzaizzat <jakzaizzat@gmail.com>
2023-08-18 12:55:23 +0800

    Add feature flag

--- apps/mobile/src/screens/HomeScreen/CuratedScreen.tsx
+++ apps/mobile/src/screens/HomeScreen/CuratedScreen.tsx
@@ -1,6 +1,8 @@
 import { Suspense, useCallback, useMemo, useState } from 'react';
 import { graphql, useLazyLoadQuery, usePaginationFragment } from 'react-relay';
+import isFeatureEnabled, { FeatureFlag } from 'src/utils/isFeatureEnabled';
 
+import { CuratedScreenFeatureQuery } from '~/generated/CuratedScreenFeatureQuery.graphql';
 import { CuratedScreenFragment$key } from '~/generated/CuratedScreenFragment.graphql';
 import { CuratedScreenQuery } from '~/generated/CuratedScreenQuery.graphql';
 import { RefetchableCuratedScreenFragmentQuery } from '~/generated/RefetchableCuratedScreenFragmentQuery.graphql';
@@ -23,8 +25,11 @@ function CuratedScreenInner({ queryRef }: CuratedScreenInnerProps) {
     graphql`
       fragment CuratedScreenFragment on Query
       @refetchable(queryName: "RefetchableCuratedScreenFragmentQuery") {
-        curatedFeed(before: $curatedFeedBefore, last: $curatedFeedCount)
-          @connection(key: "CuratedScreenFragment_curatedFeed") {
+        curatedFeed(
+          before: $curatedFeedBefore
+          last: $curatedFeedCount
+          includePosts: $includePosts
+        ) @connection(key: "CuratedScreenFragment_curatedFeed") {
           edges {
             node {
               __typename
@@ -86,14 +91,30 @@ function CuratedScreenInner({ queryRef }: CuratedScreenInnerProps) {
 }
 
 export function CuratedScreen() {
+  const featureQuery = useLazyLoadQuery<CuratedScreenFeatureQuery>(
+    graphql`
+      query CuratedScreenFeatureQuery {
+        ...isFeatureEnabledFragment
+      }
+    `,
+    {}
+  );
+
+  const isPostEnabled = isFeatureEnabled(FeatureFlag.KOALA, featureQuery);
+
   const query = useLazyLoadQuery<CuratedScreenQuery>(
     graphql`
-      query CuratedScreenQuery($curatedFeedBefore: String, $curatedFeedCount: Int!) {
+      query CuratedScreenQuery(
+        $curatedFeedBefore: String
+        $curatedFeedCount: Int!
+        $includePosts: Boolean!
+      ) {
         ...CuratedScreenFragment
       }
     `,
     {
       curatedFeedCount: PER_PAGE,
+      includePosts: isPostEnabled,
     }
   );
 
