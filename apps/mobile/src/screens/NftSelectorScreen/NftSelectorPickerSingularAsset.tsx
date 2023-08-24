311780304a0b52bd4c62751c30b889c1af80be0c
kaito <80802871+kaitoo1@users.noreply.github.com>
2023-08-24 18:10:56 +0900

    add error boundary around nftselector nft to prevent whole page crash

--- apps/mobile/src/screens/NftSelectorScreen/NftSelectorPickerSingularAsset.tsx
+++ apps/mobile/src/screens/NftSelectorScreen/NftSelectorPickerSingularAsset.tsx
@@ -10,6 +10,7 @@ import { NftPreviewAsset } from '~/components/NftPreview/NftPreviewAsset';
 import { NftPreviewErrorFallback } from '~/components/NftPreview/NftPreviewErrorFallback';
 import { NftSelectorPickerSingularAssetFragment$key } from '~/generated/NftSelectorPickerSingularAssetFragment.graphql';
 import { MainTabStackNavigatorProp, RootStackNavigatorParamList } from '~/navigation/types';
+import { ReportingErrorBoundary } from '~/shared/errors/ReportingErrorBoundary';
 import getVideoOrImageUrlForNftPreview from '~/shared/relay/getVideoOrImageUrlForNftPreview';
 import colors from '~/shared/theme/colors';
 
@@ -68,26 +69,28 @@ export function NftSelectorPickerSingularAsset({
     }
   }, [currentScreen, navigation, onSelect, setProfileImage, token.dbid]);
   return (
-    <GalleryTouchableOpacity
-      style={style}
-      disabled={isSettingProfileImage}
-      onPress={handlePress}
-      className="flex-1 aspect-square relative"
-      eventElementId="NftSelectorPickerImage"
-      eventName="NftSelectorPickerImage pressed"
-      properties={{ tokenId: token.dbid }}
-    >
-      {tokenUrl ? (
-        <NftPreviewAsset tokenUrl={tokenUrl} resizeMode={ResizeMode.COVER} />
-      ) : (
-        <NftPreviewErrorFallback />
-      )}
+    <ReportingErrorBoundary fallback={<NftPreviewErrorFallback />}>
+      <GalleryTouchableOpacity
+        style={style}
+        disabled={isSettingProfileImage}
+        onPress={handlePress}
+        className="flex-1 aspect-square relative"
+        eventElementId="NftSelectorPickerImage"
+        eventName="NftSelectorPickerImage pressed"
+        properties={{ tokenId: token.dbid }}
+      >
+        {tokenUrl ? (
+          <NftPreviewAsset tokenUrl={tokenUrl} resizeMode={ResizeMode.COVER} />
+        ) : (
+          <NftPreviewErrorFallback />
+        )}
 
-      {isSettingProfileImage && (
-        <View className="absolute inset-0 bg-black opacity-50 flex items-center justify-center">
-          <ActivityIndicator color={colors.white} />
-        </View>
-      )}
-    </GalleryTouchableOpacity>
+        {isSettingProfileImage && (
+          <View className="absolute inset-0 bg-black opacity-50 flex items-center justify-center">
+            <ActivityIndicator color={colors.white} />
+          </View>
+        )}
+      </GalleryTouchableOpacity>
+    </ReportingErrorBoundary>
   );
 }
