187e4c9c9f62f6be9902712f1d807526751c1bd5
jakzaizzat <jakzaizzat@gmail.com>
2023-08-25 12:06:24 +0800

    Add sign in bottom sheet

---
+++ apps/mobile/src/components/Login/SignInBottomSheet.tsx
@@ -0,0 +1,83 @@
+import { useBottomSheetDynamicSnapPoints } from '@gorhom/bottom-sheet';
+import { useNavigation } from '@react-navigation/native';
+import { ForwardedRef, forwardRef, useCallback, useRef } from 'react';
+import { View } from 'react-native';
+import { QRCodeIcon } from 'src/icons/QRCodeIcon';
+
+import { LoginStackNavigatorProp } from '~/navigation/types';
+
+import { BottomSheetRow } from '../Feed/Posts/PostBottomSheet';
+import {
+  GalleryBottomSheetModal,
+  GalleryBottomSheetModalType,
+} from '../GalleryBottomSheet/GalleryBottomSheetModal';
+import { useSafeAreaPadding } from '../SafeAreaViewWithPadding';
+import { Typography } from '../Typography';
+
+const SNAP_POINTS = ['CONTENT_HEIGHT'];
+
+type Props = {
+  onQrCodePress: () => void;
+};
+
+function SignInBottomSheet(
+  { onQrCodePress }: Props,
+  ref: ForwardedRef<GalleryBottomSheetModalType>
+) {
+  const { bottom } = useSafeAreaPadding();
+
+  const bottomSheetRef = useRef<GalleryBottomSheetModalType | null>(null);
+
+  const navigation = useNavigation<LoginStackNavigatorProp>();
+  const { animatedHandleHeight, animatedSnapPoints, animatedContentHeight, handleContentLayout } =
+    useBottomSheetDynamicSnapPoints(SNAP_POINTS);
+
+  const handleEmailPress = useCallback(() => {
+    navigation.navigate('EnterEmail');
+  }, [navigation]);
+
+  return (
+    <GalleryBottomSheetModal
+      ref={(value) => {
+        bottomSheetRef.current = value;
+
+        if (typeof ref === 'function') {
+          ref(value);
+        } else if (ref) {
+          ref.current = value;
+        }
+      }}
+      snapPoints={animatedSnapPoints}
+      handleHeight={animatedHandleHeight}
+      contentHeight={animatedContentHeight}
+    >
+      <View
+        onLayout={handleContentLayout}
+        style={{ paddingBottom: bottom }}
+        className="p-4 flex flex-col space-y-6"
+      >
+        <View className="flex flex-col space-y-4">
+          <Typography
+            className="text-lg text-black-900 dark:text-offWhite"
+            font={{ family: 'ABCDiatype', weight: 'Bold' }}
+          >
+            Other sign in method
+          </Typography>
+        </View>
+
+        <View className="flex flex-col space-y-2">
+          <BottomSheetRow text="Email" onPress={handleEmailPress} />
+          <BottomSheetRow
+            icon={<QRCodeIcon />}
+            text="QR Code (Sync with desktop)"
+            onPress={onQrCodePress}
+          />
+        </View>
+      </View>
+    </GalleryBottomSheetModal>
+  );
+}
+
+const ForwardedSignInBottomSheet = forwardRef(SignInBottomSheet);
+
+export { ForwardedSignInBottomSheet as SignInBottomSheet };
