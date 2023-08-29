af68964555f2e21b4321bade6e8bd95e4f743827
Rohan-cp <rohancp9@icloud.com>
2023-08-29 17:24:28 -0400

    layout changes

--- apps/mobile/src/screens/PostScreen/PostComposerNftFallback.tsx
+++ apps/mobile/src/screens/PostScreen/PostComposerNftFallback.tsx
@@ -8,10 +8,10 @@ export function PostComposerNftFallback() {
     <View className="flex-1 bg-white dark:bg-black">
       <GallerySkeleton>
         <SkeletonPlaceholder.Item flexDirection="column" width="100%">
-          <SkeletonPlaceholder.Item marginBottom={16} width="100%" height={330} />
+          <SkeletonPlaceholder.Item marginBottom={16} width="100%" height={361} />
           <SkeletonPlaceholder.Item flexDirection="column" rowGap={16}>
-            <SkeletonPlaceholder.Item width={100} height={18} />
-            <SkeletonPlaceholder.Item width={100} height={18} />
+            <SkeletonPlaceholder.Item width={100} height={20} />
+            <SkeletonPlaceholder.Item width={100} height={20} />
           </SkeletonPlaceholder.Item>
         </SkeletonPlaceholder.Item>
       </GallerySkeleton>
