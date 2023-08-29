74fc731ea264d044dd0b84e0b4e4f0c213221000
kaito <80802871+kaitoo1@users.noreply.github.com>
2023-08-29 22:11:20 +0900

    remove congratulations step

--- apps/web/src/components/Onboarding/constants.ts
+++ apps/web/src/components/Onboarding/constants.ts
@@ -1,13 +1,12 @@
 export const FOOTER_HEIGHT = 56;
 
-export const STEPS = ['welcome', 'add-user-info', 'congratulations', 'add-email'] as const;
+export const STEPS = ['welcome', 'add-user-info', 'add-email'] as const;
 
 export type StepName = (typeof STEPS)[number];
 
 export const ONBOARDING_NEXT_BUTTON_TEXT_MAP: { [key in StepName]: string } = {
   welcome: 'Next',
   'add-user-info': 'Next',
-  congratulations: 'Done',
   'add-email': 'Done',
 };
 
