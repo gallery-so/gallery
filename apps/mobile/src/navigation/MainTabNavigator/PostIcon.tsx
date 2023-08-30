653c519ee8164795f67f83e20b085e4baec4ca22
Rohan-cp <rohancp9@icloud.com>
2023-08-30 14:13:10 -0400

    base

--- apps/mobile/src/navigation/MainTabNavigator/PostIcon.tsx
+++ apps/mobile/src/navigation/MainTabNavigator/PostIcon.tsx
@@ -4,6 +4,7 @@ type Props = {
   color: string;
   height?: number;
   width?: number;
+  strokeWidth?: number;
 } & SvgProps;
 
 export const PostIcon = ({ color, height = 24, width = 24, ...props }: Props) => {
@@ -12,6 +13,7 @@ export const PostIcon = ({ color, height = 24, width = 24, ...props }: Props) =>
       <Path
         stroke={color}
         strokeMiterlimit={10}
+        strokeWidth={2}
         d="M4.5 25.333V4.5h23v23h-23v-2.167ZM16 10.666v10.667M21.333 16H10.667"
       />
     </Svg>
