d1a77f5133f0232143d80eef540ca46ecad3d0c2
Robinnnnn <rokim8@gmail.com>
2023-08-30 23:48:58 -0400

    icons

--- apps/web/src/components/Posts/DiscardPostConfirmation.tsx
+++ apps/web/src/components/Posts/DiscardPostConfirmation.tsx
@@ -2,6 +2,9 @@ import { useCallback } from 'react';
 import styled from 'styled-components';
 
 import { useModalActions } from '~/contexts/modal/ModalContext';
+import { EditPencilIcon } from '~/icons/EditPencilIcon';
+import { TrashIconNew } from '~/icons/TrashIconNew';
+import colors from '~/shared/theme/colors';
 
 import { Button } from '../core/Button/Button';
 import { HStack } from '../core/Spacer/Stack';
@@ -30,9 +33,15 @@ export default function DiscardPostConfirmation({ onSaveDraft, onDiscard }: Prop
       <BaseM>If you go back now, this post will be discarded.</BaseM>
       <HStack justify="flex-end" gap={8}>
         <StyledButton onClick={handleSaveDraftClick} variant="secondary">
-          SAVE DRAFT
+          <HStack align="center" gap={6}>
+            <EditPencilIcon width={12} height={12} /> SAVE DRAFT
+          </HStack>
         </StyledButton>
-        <StyledButton onClick={handleDiscardConfirmClick}>DISCARD</StyledButton>
+        <StyledButton onClick={handleDiscardConfirmClick}>
+          <HStack align="center" gap={6}>
+            <TrashIconNew color={colors.white} /> DISCARD
+          </HStack>
+        </StyledButton>
       </HStack>
     </StyledConfirmation>
   );
