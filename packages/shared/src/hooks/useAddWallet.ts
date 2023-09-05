02861f34cecdd36fc31634177f51cb0263200b6a
jakzaizzat <jakzaizzat@gmail.com>
2023-09-05 14:52:14 +0800

    Move useAddWallet to shared

--- packages/shared/src/hooks/useAddWallet.ts
+++ packages/shared/src/hooks/useAddWallet.ts
@@ -5,7 +5,8 @@ import {
   useAddWalletMutation,
   useAddWalletMutation$variables,
 } from '~/generated/useAddWalletMutation.graphql';
-import { usePromisifiedMutation } from '~/shared/relay/usePromisifiedMutation';
+
+import { usePromisifiedMutation } from '../relay/usePromisifiedMutation';
 
 export default function useAddWallet() {
   const [addWallet] = usePromisifiedMutation<useAddWalletMutation>(
