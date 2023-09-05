3f928069f7561eedadc8697d4304a43c84197063
jakzaizzat <jakzaizzat@gmail.com>
2023-09-05 12:19:33 +0800

    Maintain old email screen design

--- apps/mobile/src/screens/Login/EnterEmailScreen.tsx
+++ apps/mobile/src/screens/Login/EnterEmailScreen.tsx
@@ -1,11 +1,8 @@
 import { useNavigation } from '@react-navigation/native';
-import clsx from 'clsx';
 import { useCallback, useState } from 'react';
-import { KeyboardAvoidingView, Platform, View } from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { KeyboardAvoidingView, View } from 'react-native';
 
-import { BackButton } from '~/components/BackButton';
-import { OnboardingTextInput } from '~/components/Onboarding/OnboardingTextInput';
+import { SafeAreaViewWithPadding } from '~/components/SafeAreaViewWithPadding';
 import { LoginStackNavigatorProp } from '~/navigation/types';
 import { navigateToNotificationUpsellOrHomeScreen } from '~/screens/Login/navigateToNotificationUpsellOrHomeScreen';
 import { useVerifyEmailMagicLink } from '~/screens/Login/useVerifyEmailMagicLink';
@@ -13,15 +10,17 @@ import { useTrack } from '~/shared/contexts/AnalyticsContext';
 import { useReportError } from '~/shared/contexts/ErrorReportingContext';
 
 import { Button } from '../../components/Button';
+import { FadedInput } from '../../components/FadedInput';
+import { IconContainer } from '../../components/IconContainer';
 import { Typography } from '../../components/Typography';
 import { useLogin } from '../../hooks/useLogin';
+import { BackIcon } from '../../icons/BackIcon';
 import { magic } from '../../magic';
 
 const FALLBACK_ERROR_MESSAGE = `Something unexpected went wrong while logging in. We've been notified and are looking into it`;
 
 export function EnterEmailScreen() {
   const navigation = useNavigation<LoginStackNavigatorProp>();
-  const { top, bottom } = useSafeAreaInsets();
 
   const [email, setEmail] = useState('');
   const [error, setError] = useState('');
@@ -104,54 +103,52 @@ export function EnterEmailScreen() {
       setIsLoggingIn(false);
     }
   }, [email, login, navigation, reportError, track, verifyEmail]);
-  const handleBack = useCallback(() => {
-    navigation.goBack();
-  }, [navigation]);
+
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      style={{ paddingTop: top }}
-      className="flex flex-1 flex-col bg-white dark:bg-black-900"
-    >
-      <View className="flex flex-col flex-grow space-y-8 px-4">
-        <View className="relative flex-row items-center justify-between ">
-          <BackButton onPress={handleBack} />
+    <SafeAreaViewWithPadding className="h-screen bg-white dark:bg-black-900">
+      <KeyboardAvoidingView behavior="padding" className="flex flex-1 flex-col">
+        <IconContainer
+          eventElementId={null}
+          eventName={null}
+          className="px-6 py-2"
+          icon={<BackIcon />}
+          onPress={navigation.goBack}
+        />
 
-          <View
-            className="absolute w-full flex flex-row justify-center items-center"
-            pointerEvents="none"
-          >
-            <Typography className="text-sm" font={{ family: 'ABCDiatype', weight: 'Bold' }}>
-              Enter email
+        <View className="flex flex-grow flex-col items-center justify-center">
+          <View className="flex max-w-xs flex-col space-y-4">
+            <Typography className="text-lg" font={{ family: 'ABCDiatype', weight: 'Bold' }}>
+              Enter your Verified email
             </Typography>
-          </View>
 
-          <View />
-        </View>
+            <Typography className="text-sm" font={{ family: 'ABCDiatype', weight: 'Regular' }}>
+              If you’re an existing Gallery user with a verified email address, we’ll deliver a
+              magic sign-in link to your inbox.
+            </Typography>
 
-        <View
-          className="flex-1  justify-center space-y-12 px-8"
-          style={{
-            marginBottom: bottom,
-          }}
-        >
-          <OnboardingTextInput
-            autoFocus
-            placeholder="name@email.com"
-            keyboardType="email-address"
-            autoComplete="email"
-            value={email}
-            onChangeText={setEmail}
-          />
-          <View className="space-y-4">
+            {error && (
+              <Typography
+                className="text-error text-sm"
+                font={{ family: 'ABCDiatype', weight: 'Regular' }}
+              >
+                {error}
+              </Typography>
+            )}
+
+            <FadedInput
+              autoFocus
+              placeholder="Email"
+              keyboardType="email-address"
+              autoCorrect={false}
+              autoCapitalize="none"
+              autoComplete="email"
+              value={email}
+              onChangeText={setEmail}
+            />
+
             <Button
               eventElementId="Submit Email Button"
               eventName="Sign In Attempt"
-              className={clsx(
-                'w-full',
-                email.length > 0 && 'opacity-100',
-                email.length === 0 && 'opacity-0'
-              )}
               loading={isLoggingIn}
               onPress={handleContinue}
               text="Continue"
@@ -160,20 +157,11 @@ export function EnterEmailScreen() {
               }}
             />
 
-            <Typography
-              className={clsx(
-                'text-sm text-red',
-                email.length > 0 && 'opacity-100',
-                email.length === 0 && 'opacity-0'
-              )}
-              font={{ family: 'ABCDiatype', weight: 'Regular' }}
-            >
-              {error}
-            </Typography>
+            {/* Add some extra space for the keyboard avoiding view */}
+            <View />
           </View>
-          <View />
         </View>
-      </View>
-    </KeyboardAvoidingView>
+      </KeyboardAvoidingView>
+    </SafeAreaViewWithPadding>
   );
 }
