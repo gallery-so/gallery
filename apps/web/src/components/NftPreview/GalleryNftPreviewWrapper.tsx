e9c4515ce744d786dee8dad21e5f7f0fda506a96
Robinnnnn <rokim8@gmail.com>
2023-09-08 18:58:20 -0400

    fix lint

--- apps/web/src/components/NftPreview/GalleryNftPreviewWrapper.tsx
+++ apps/web/src/components/NftPreview/GalleryNftPreviewWrapper.tsx
@@ -2,25 +2,12 @@ import { graphql, useFragment } from 'react-relay';
 
 import ShimmerProvider from '~/contexts/shimmer/ShimmerContext';
 import { GalleryNftPreviewWrapperFragment$key } from '~/generated/GalleryNftPreviewWrapperFragment.graphql';
-import { useIsMobileWindowWidth } from '~/hooks/useWindowSize';
 
 import CollectionTokenPreview from './CollectionTokenPreview';
 
 type Props = {
   tokenRef: GalleryNftPreviewWrapperFragment$key;
   columns: number;
-};
-
-const SINGLE_COLUMN_NFT_WIDTH = 600;
-const MOBILE_NFT_WIDTH = 288;
-
-const LAYOUT_DIMENSIONS: Record<number, number> = {
-  1: SINGLE_COLUMN_NFT_WIDTH,
-  2: 482,
-  3: 308,
-  4: 221,
-  5: 169,
-  6: 134,
 };
 
 // simple wrapper component so the child can pull state from ShimmerProvider
