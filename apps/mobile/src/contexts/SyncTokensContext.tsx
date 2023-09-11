e483bce960e9f92362ddece3a1a2b3aaaf135c2c
jakzaizzat <jakzaizzat@gmail.com>
2023-09-11 12:19:16 +0900

    Add sync tokens context

---
+++ apps/mobile/src/contexts/SyncTokensContext.tsx
@@ -0,0 +1,100 @@
+import { createContext, memo, ReactNode, useCallback, useContext, useMemo, useState } from 'react';
+import { graphql } from 'react-relay';
+
+import { Chain, SyncTokensContextMutation } from '~/generated/SyncTokensContextMutation.graphql';
+import { usePromisifiedMutation } from '~/shared/relay/usePromisifiedMutation';
+
+import { useToastActions } from './ToastContext';
+
+type SyncTokensActions = {
+  syncTokens: (chain: Chain) => void;
+  isSyncing: boolean;
+};
+
+const SyncTokensActionsContext = createContext<SyncTokensActions | undefined>(undefined);
+
+export const useSyncTokenstActions = (): SyncTokensActions => {
+  const context = useContext(SyncTokensActionsContext);
+  if (!context) {
+    throw new Error('Attempted to use SyncTokensActionsContext without a provider!');
+  }
+
+  return context;
+};
+
+type Props = { children: ReactNode };
+
+const SyncTokensProvider = memo(({ children }: Props) => {
+  const [syncTokens] = usePromisifiedMutation<SyncTokensContextMutation>(
+    graphql`
+      mutation SyncTokensContextMutation($chain: Chain!) {
+        syncTokens(chains: [$chain]) {
+          __typename
+          ... on SyncTokensPayload {
+            viewer {
+              ... on Viewer {
+                user {
+                  tokens(ownershipFilter: [Creator, Holder]) {
+                    dbid
+                  }
+                }
+              }
+            }
+          }
+          ... on ErrNotAuthorized {
+            message
+          }
+          ... on ErrSyncFailed {
+            message
+          }
+        }
+      }
+    `
+  );
+
+  const [isSyncing, setIsSyncing] = useState(false);
+
+  const { pushToast } = useToastActions();
+
+  const sync = useCallback(
+    async (chain: Chain) => {
+      function showFailure() {
+        pushToast({
+          autoClose: true,
+          message:
+            "Something went wrong while syncing your tokens. We're looking into it. Please try again in a few minutes.",
+        });
+      }
+
+      try {
+        setIsSyncing(true);
+        const response = await syncTokens({
+          variables: {
+            chain,
+          },
+        });
+
+        if (response.syncTokens?.__typename !== 'SyncTokensPayload') {
+          showFailure();
+        }
+      } catch (error) {
+        showFailure();
+      } finally {
+        setIsSyncing(false);
+      }
+    },
+    [pushToast, syncTokens]
+  );
+
+  const value = useMemo(() => {
+    return { syncTokens: sync, isSyncing };
+  }, [isSyncing, sync]);
+
+  return (
+    <SyncTokensActionsContext.Provider value={value}>{children}</SyncTokensActionsContext.Provider>
+  );
+});
+
+SyncTokensProvider.displayName = 'SyncTokensProvider';
+
+export default SyncTokensProvider;
