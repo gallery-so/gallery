807636a8725a73ef21f8382cc16690ee7b91a444
Robinnnnn <rokim8@gmail.com>
2023-09-12 20:07:08 -0400

    ensure proper fragments are refreshed

--- apps/web/src/components/NftFailureFallback/NftFailureBoundary.tsx
+++ apps/web/src/components/NftFailureFallback/NftFailureBoundary.tsx
@@ -70,6 +70,7 @@ export function NftFailureBoundary({
                       }
                     }
                   }
+                  ...getPreviewImageUrlsInlineDangerouslyFragment
                 }
               }
             }
