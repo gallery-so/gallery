337376e7e57c07dddd2bde1686ccd487865f8e8a
Rohan-cp <rohancp9@icloud.com>
2023-09-20 12:04:23 +0530

    Enable links in comments (#1931)

--- apps/mobile/src/components/Feed/CommentsBottomSheet/CommentsBottomSheetLine.tsx
+++ apps/mobile/src/components/Feed/CommentsBottomSheet/CommentsBottomSheetLine.tsx
@@ -1,14 +1,18 @@
 import { useNavigation } from '@react-navigation/native';
-import { useCallback } from 'react';
-import { View } from 'react-native';
+import { useCallback, useRef, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 import { useFragment } from 'react-relay';
 import { graphql } from 'relay-runtime';
 
+import { WarningLinkBottomSheet } from '~/components/Feed/Posts/WarningLinkBottomSheet';
+import { GalleryBottomSheetModalType } from '~/components/GalleryBottomSheet/GalleryBottomSheetModal';
 import { GalleryTouchableOpacity } from '~/components/GalleryTouchableOpacity';
+import { Markdown } from '~/components/Markdown';
 import { ProfilePicture } from '~/components/ProfilePicture/ProfilePicture';
 import { Typography } from '~/components/Typography';
 import { CommentsBottomSheetLineFragment$key } from '~/generated/CommentsBottomSheetLineFragment.graphql';
 import { MainTabStackNavigatorProp } from '~/navigation/types';
+import { replaceUrlsWithMarkdownFormat } from '~/shared/utils/replaceUrlsWithMarkdownFormat';
 import { getTimeSince } from '~/shared/utils/time';
 
 type CommentLineProps = {
@@ -34,6 +38,16 @@ export function CommentsBottomSheetLine({ commentRef }: CommentLineProps) {
 
   const timeAgo = getTimeSince(comment.creationTime);
   const navigation = useNavigation<MainTabStackNavigatorProp>();
+
+  const [redirectUrl, setRedirectUrl] = useState('');
+  const captionWithMarkdownLinks = replaceUrlsWithMarkdownFormat(comment.comment ?? '');
+
+  const bottomSheetRef = useRef<GalleryBottomSheetModalType | null>(null);
+
+  const handleLinkPress = useCallback((url: string) => {
+    bottomSheetRef.current?.present();
+    setRedirectUrl(url);
+  }, []);
 
   const handleUserPress = useCallback(() => {
     const username = comment?.commenter?.username;
@@ -67,11 +81,17 @@ export function CommentsBottomSheetLine({ commentRef }: CommentLineProps) {
           </Typography>
         </View>
         <View className="flex">
-          <Typography className="text-sm" font={{ family: 'ABCDiatype', weight: 'Regular' }}>
-            {comment.comment}
-          </Typography>
+          <Markdown onBypassLinkPress={handleLinkPress} style={markdownStyles}>
+            {captionWithMarkdownLinks}
+          </Markdown>
+          <WarningLinkBottomSheet redirectUrl={redirectUrl} ref={bottomSheetRef} />
         </View>
       </View>
     </GalleryTouchableOpacity>
   );
 }
+const markdownStyles = StyleSheet.create({
+  body: {
+    fontSize: 14,
+  },
+});
