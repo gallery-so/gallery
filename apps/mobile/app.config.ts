b8e233ec1d867e55821db5463ba6e645d3ad8ba3
Robinnnnn <rokim8@gmail.com>
2023-09-22 14:14:37 -0400

    1.0.22

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -33,7 +33,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   orientation: 'portrait',
   icon: './assets/icon.png',
   userInterfaceStyle: 'automatic',
-  version: '1.0.21',
+  version: '1.0.22',
   updates: {
     fallbackToCacheTimeout: 0,
   },
