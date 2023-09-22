a02ce413af8c287b388dcc893ae78844758f5a43
Jakz <jakzaizzat@gmail.com>
2023-09-22 16:05:20 +0800

    [GAL-4257] "Connect Wallet" Upsell banner for email-only users [web] (#1926)
    * Add upsell banner component & key
    
    * embed banner
    
    * Add copywriting to wallet modal
    
    * make email value optional
    
    * fix test
    
    * fix types
    
    * fix mobile view
    
    * clean up test
    
    * revert email graphql
    
    * Update mobile view
    
    * white space
    
    * Update mobile banner to match new design
    
    * change text

---
+++ apps/web/src/components/UpsellBanner/UpsellBanner.tsx
@@ -0,0 +1,57 @@
+import { useCallback } from 'react';
+import { graphql, useFragment } from 'react-relay';
+
+import { UpsellBannerQuery$key } from '~/generated/UpsellBannerQuery.graphql';
+import useAddWalletModal from '~/hooks/useAddWalletModal';
+import useExperience from '~/utils/graphql/experiences/useExperience';
+
+import { GlobalBanner } from '../core/GlobalBanner/GlobalBanner';
+
+type Props = {
+  queryRef: UpsellBannerQuery$key;
+};
+
+const USER_EXPERIENCE_KEY = 'UpsellBanner';
+
+export function UpsellBanner({ queryRef }: Props) {
+  const query = useFragment(
+    graphql`
+      fragment UpsellBannerQuery on Query {
+        ...useExperienceFragment
+      }
+    `,
+    queryRef
+  );
+
+  const showAddWalletModal = useAddWalletModal();
+  const [hasExperienced, updateUserExperience] = useExperience({
+    type: USER_EXPERIENCE_KEY,
+    queryRef: query,
+  });
+
+  const handleConnectWallet = useCallback(() => {
+    showAddWalletModal({
+      onConnectWalletSuccess: () => {
+        updateUserExperience();
+      },
+    });
+  }, [showAddWalletModal, updateUserExperience]);
+
+  const handleClose = useCallback(() => {
+    updateUserExperience();
+  }, [updateUserExperience]);
+
+  if (hasExperienced) {
+    return null;
+  }
+
+  return (
+    <GlobalBanner
+      title="Gallery is better with a wallet"
+      description="Add your wallet to start posting and curating"
+      onClose={handleClose}
+      onClick={handleConnectWallet}
+      ctaText="Connect"
+    />
+  );
+}
