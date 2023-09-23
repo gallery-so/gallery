6f6cb28f434e033fd9bc450299f05b3af479097c
kaito <80802871+kaitoo1@users.noreply.github.com>
2023-09-23 23:22:29 +0900

    bump app version

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -33,7 +33,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   orientation: 'portrait',
   icon: './assets/icon.png',
   userInterfaceStyle: 'automatic',
-  version: '1.0.22',
+  version: '1.0.23',
   updates: {
     fallbackToCacheTimeout: 0,
   },
