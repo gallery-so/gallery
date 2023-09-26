50f9f0d4b0dfabd53ebf5e50cf410687b453b7d2
jakzaizzat <jakzaizzat@gmail.com>
2023-09-26 15:08:24 +0800

    Fix can't post from community

--- apps/web/src/contexts/postComposer/PostComposerContext.tsx
+++ apps/web/src/contexts/postComposer/PostComposerContext.tsx
@@ -81,7 +81,9 @@ const PostComposerProvider = memo(({ children }: Props) => {
 
   useEffect(() => {
     if (chain && typeof chain === 'string' && isSupportedChain(chain)) {
-      setNetwork(chain);
+      // Capitalize the first letter of the chain name that we get from the router to match the Chain type
+      const capitalizedChainName = chain.charAt(0).toUpperCase() + chain.slice(1);
+      setNetwork(capitalizedChainName as Chain);
     }
   }, [chain, composer, isMobile]);
 
