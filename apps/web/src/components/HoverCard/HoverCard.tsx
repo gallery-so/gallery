be93052ce1d99eda7cb94955762884e492c52d00
Robinnnnn <rokim8@gmail.com>
2023-09-27 15:06:06 -0400

    generic HoverCard, UserHoverCard

---
+++ apps/web/src/components/HoverCard/HoverCard.tsx
@@ -0,0 +1,162 @@
+import {
+  autoUpdate,
+  flip,
+  FloatingFocusManager,
+  FloatingPortal,
+  inline,
+  shift,
+  useFloating,
+  useHover,
+  useInteractions,
+  useRole,
+} from '@floating-ui/react';
+import { AnimatePresence, motion } from 'framer-motion';
+import Link from 'next/link';
+import { Route } from 'nextjs-routes';
+import { MouseEventHandler, Suspense, useCallback, useEffect, useId, useState } from 'react';
+import { PreloadedQuery } from 'react-relay';
+import { OperationType } from 'relay-runtime';
+import styled from 'styled-components';
+
+import colors from '~/shared/theme/colors';
+import noop from '~/utils/noop';
+
+import breakpoints, { pageGutter } from '../core/breakpoints';
+import { SelfCenteredSpinner } from '../core/Spinner/Spinner';
+import {
+  ANIMATED_COMPONENT_TRANSITION_S,
+  ANIMATED_COMPONENT_TRANSLATION_PIXELS_SMALL,
+  rawTransitions,
+} from '../core/transitions';
+
+const HOVER_POPUP_DELAY_MS = 100;
+
+export type HoverCardProps<T extends OperationType> = {
+  HoverableElement?: React.ReactNode;
+  onHoverableElementClick?: () => void;
+  hoverableElementHref: Route;
+  preloadQuery: () => void;
+  preloadedQuery?: PreloadedQuery<T> | null;
+  HoveringContent: React.ReactNode;
+};
+
+export default function HoverCard<T extends OperationType>({
+  HoverableElement,
+  onHoverableElementClick = noop,
+  hoverableElementHref,
+  HoveringContent,
+  preloadedQuery,
+  preloadQuery,
+}: HoverCardProps<T>) {
+  const [isHovering, setIsHovering] = useState(false);
+
+  const { x, y, reference, floating, strategy, context } = useFloating({
+    placement: 'bottom-start',
+    open: isHovering,
+    onOpenChange: setIsHovering,
+    middleware: [flip(), shift(), inline()],
+    whileElementsMounted: autoUpdate,
+  });
+
+  const headingId = useId();
+  const role = useRole(context);
+  const hover = useHover(context, { delay: HOVER_POPUP_DELAY_MS });
+
+  const { getReferenceProps, getFloatingProps } = useInteractions([hover, role]);
+
+  useEffect(() => {
+    if (isHovering) {
+      preloadQuery();
+    }
+  }, [isHovering, preloadQuery]);
+
+  const handleClick = useCallback<MouseEventHandler>(
+    (event) => {
+      event.stopPropagation();
+      onHoverableElementClick();
+    },
+    [onHoverableElementClick]
+  );
+
+  console.log({ preloadedQuery });
+
+  return (
+    <StyledContainer>
+      <StyledLinkContainer ref={reference} {...getReferenceProps()}>
+        <StyledLink href={hoverableElementHref} onClick={handleClick}>
+          {HoverableElement}
+        </StyledLink>
+      </StyledLinkContainer>
+
+      <AnimatePresence>
+        {isHovering && preloadedQuery && (
+          <FloatingPortal preserveTabOrder={false}>
+            <FloatingFocusManager context={context} modal={false}>
+              <StyledCardWrapper
+                className="Popover"
+                aria-labelledby={headingId}
+                // Floating UI Props
+                ref={floating}
+                style={{
+                  position: strategy,
+                  top: y ?? 0,
+                  left: x ?? 0,
+                }}
+                {...getFloatingProps()}
+                // Framer Motion Props
+                transition={{
+                  duration: ANIMATED_COMPONENT_TRANSITION_S,
+                  ease: rawTransitions.cubicValues,
+                }}
+                initial={{ opacity: 0, y: 0 }}
+                animate={{ opacity: 1, y: ANIMATED_COMPONENT_TRANSLATION_PIXELS_SMALL }}
+                exit={{ opacity: 0, y: 0 }}
+              >
+                <StyledCardContainer>
+                  <Suspense fallback={<SelfCenteredSpinner />}>{HoveringContent}</Suspense>
+                </StyledCardContainer>
+              </StyledCardWrapper>
+            </FloatingFocusManager>
+          </FloatingPortal>
+        )}
+      </AnimatePresence>
+    </StyledContainer>
+  );
+}
+
+const StyledContainer = styled.span`
+  position: relative;
+  display: inline-grid;
+  cursor: initial;
+`;
+
+const StyledLinkContainer = styled.div`
+  display: inline-flex;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
+
+const StyledCardContainer = styled.div`
+  border: 1px solid ${colors.black['800']};
+  padding: 16px;
+  width: 375px;
+  min-height: 128px;
+  max-width: calc(100vw - ${pageGutter.mobile * 2}px);
+  display: flex;
+  gap: 8px;
+  background-color: ${colors.white};
+
+  @media only screen and ${breakpoints.desktop} {
+    max-width: initial;
+  }
+`;
+
+const StyledCardWrapper = styled(motion.div)`
+  z-index: 11;
+
+  :focus {
+    outline: none;
+  }
+`;
---
+++ apps/web/src/components/HoverCard/UserHoverCard.tsx
@@ -0,0 +1,250 @@
+import unescape from 'lodash/unescape';
+import Link from 'next/link';
+import { Route } from 'nextjs-routes';
+import { useCallback, useMemo } from 'react';
+import {
+  graphql,
+  PreloadedQuery,
+  useFragment,
+  usePreloadedQuery,
+  useQueryLoader,
+} from 'react-relay';
+import styled from 'styled-components';
+
+import { UserHoverCardContentQuery } from '~/generated/UserHoverCardContentQuery.graphql';
+import { UserHoverCardFragment$key } from '~/generated/UserHoverCardFragment.graphql';
+import { COMMUNITIES_PER_PAGE } from '~/scenes/UserGalleryPage/UserSharedInfo/UserSharedCommunities';
+import UserSharedInfo from '~/scenes/UserGalleryPage/UserSharedInfo/UserSharedInfo';
+import { FOLLOWERS_PER_PAGE } from '~/scenes/UserGalleryPage/UserSharedInfo/UserSharedInfoList/SharedFollowersList';
+import { ErrorWithSentryMetadata } from '~/shared/errors/ErrorWithSentryMetadata';
+import { useLoggedInUserId } from '~/shared/relay/useLoggedInUserId';
+import handleCustomDisplayName from '~/utils/handleCustomDisplayName';
+
+import Badge from '../Badge/Badge';
+import Markdown from '../core/Markdown/Markdown';
+import { HStack, VStack } from '../core/Spacer/Stack';
+import { BaseM, TitleDiatypeM, TitleM } from '../core/Text/Text';
+import FollowButton from '../Follow/FollowButton';
+import { ProfilePicture } from '../ProfilePicture/ProfilePicture';
+import HoverCard, { HoverCardProps } from './HoverCard';
+
+const UserHoverCardContentQueryNode = graphql`
+  query UserHoverCardContentQuery(
+    $userId: DBID!
+    $sharedCommunitiesFirst: Int
+    $sharedCommunitiesAfter: String
+    $sharedFollowersFirst: Int
+    $sharedFollowersAfter: String
+  ) {
+    userById(id: $userId) @required(action: THROW) {
+      ... on GalleryUser {
+        __typename
+        id
+        bio
+        username
+        badges {
+          name
+          imageURL
+          ...BadgeFragment
+        }
+        ...FollowButtonUserFragment
+        ...UserSharedInfoFragment
+        ...ProfilePictureFragment
+      }
+    }
+
+    ...FollowButtonQueryFragment
+    ...useLoggedInUserIdFragment
+  }
+`;
+
+type Props = {
+  userRef: UserHoverCardFragment$key;
+  HoverableElement?: HoverCardProps<UserHoverCardContentQuery>['HoverableElement'];
+  onClick?: HoverCardProps<UserHoverCardContentQuery>['onHoverableElementClick'];
+};
+
+export default function UserHoverCard({ userRef, HoverableElement, onClick }: Props) {
+  const user = useFragment(
+    graphql`
+      fragment UserHoverCardFragment on GalleryUser {
+        dbid
+        username
+      }
+    `,
+    userRef
+  );
+
+  const [preloadedHoverCardQuery, preloadHoverCardQuery] =
+    useQueryLoader<UserHoverCardContentQuery>(UserHoverCardContentQueryNode);
+
+  const userProfileLink = useMemo((): Route => {
+    return { pathname: '/[username]', query: { username: user.username as string } };
+  }, [user]);
+
+  const handlePreloadQuery = useCallback(() => {
+    preloadHoverCardQuery({
+      userId: user.dbid,
+      sharedCommunitiesFirst: COMMUNITIES_PER_PAGE,
+      sharedFollowersFirst: FOLLOWERS_PER_PAGE,
+    });
+  }, [preloadHoverCardQuery, user.dbid]);
+
+  if (!user.username) {
+    return null;
+  }
+
+  const displayName = handleCustomDisplayName(user.username);
+
+  return (
+    <HoverCard
+      HoverableElement={HoverableElement ?? <TitleDiatypeM>{displayName}</TitleDiatypeM>}
+      onHoverableElementClick={onClick}
+      hoverableElementHref={userProfileLink}
+      HoveringContent={
+        preloadedHoverCardQuery ? (
+          <UserHoverCardContent preloadedQuery={preloadedHoverCardQuery} />
+        ) : null
+      }
+      preloadQuery={handlePreloadQuery}
+      preloadedQuery={preloadedHoverCardQuery}
+    />
+  );
+}
+
+function UserHoverCardContent({
+  preloadedQuery,
+}: {
+  preloadedQuery: PreloadedQuery<UserHoverCardContentQuery>;
+}) {
+  console.log('user hoevr card content', { preloadedQuery });
+  const query = usePreloadedQuery(UserHoverCardContentQueryNode, preloadedQuery);
+
+  const user = query.userById;
+
+  if (user.__typename !== 'GalleryUser') {
+    throw new ErrorWithSentryMetadata('Expected userById to return w/ typename GalleryUser', {
+      typename: user.__typename,
+    });
+  }
+
+  const userBadges = useMemo(() => {
+    const badges = [];
+
+    for (const badge of user?.badges ?? []) {
+      if (badge?.imageURL) {
+        badges.push(badge);
+      }
+    }
+
+    return badges;
+  }, [user?.badges]);
+
+  const loggedInUserId = useLoggedInUserId(query);
+  const isOwnProfile = loggedInUserId === user?.id;
+  const isLoggedIn = Boolean(loggedInUserId);
+
+  const userProfileLink = useMemo((): Route => {
+    return { pathname: '/[username]', query: { username: user.username as string } };
+  }, [user]);
+
+  if (!user.username) {
+    return null;
+  }
+
+  const displayName = handleCustomDisplayName(user.username);
+
+  return (
+    <Section gap={4}>
+      <StyledCardHeaderContainer gap={8}>
+        <StyledCardHeader gap={2} align="center" justify="space-between">
+          <StyledUsernameAndBadge align="center" gap={4}>
+            <StyledLink href={userProfileLink}>
+              <HStack align="center" gap={4}>
+                <ProfilePicture userRef={user} size="md" />
+                <StyledCardTitle>{displayName}</StyledCardTitle>
+              </HStack>
+            </StyledLink>
+
+            <HStack align="center" gap={0}>
+              {userBadges.map((badge) => (
+                // Might need to rethink this layout when we have more badges
+                <Badge key={badge.name} badgeRef={badge} />
+              ))}
+            </HStack>
+          </StyledUsernameAndBadge>
+
+          {!isOwnProfile && (
+            <StyledFollowButtonWrapper>
+              <StyledFollowButton userRef={user} queryRef={query} source="user hover card" />
+            </StyledFollowButtonWrapper>
+          )}
+        </StyledCardHeader>
+
+        {user.bio && (
+          <StyledCardDescription>
+            <BaseM>
+              <Markdown text={unescape(user.bio)}></Markdown>
+            </BaseM>
+          </StyledCardDescription>
+        )}
+      </StyledCardHeaderContainer>
+      {isLoggedIn && !isOwnProfile && <UserSharedInfo userRef={user} />}
+    </Section>
+  );
+}
+
+const Section = styled(VStack)`
+  width: 100%;
+`;
+
+const StyledCardHeader = styled(HStack)`
+  display: flex;
+  min-width: 0;
+  // enforce height on container since the follow button causes additional height
+  height: 24px;
+`;
+
+const StyledCardHeaderContainer = styled(VStack)`
+  padding-top: 6px;
+  padding-bottom: 12px;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  outline: none;
+  min-width: 0;
+`;
+
+const StyledUsernameAndBadge = styled(HStack)`
+  min-width: 0;
+`;
+
+const StyledFollowButtonWrapper = styled.div`
+  margin-right: 8px;
+`;
+
+const StyledCardTitle = styled(TitleM)`
+  font-style: normal;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+`;
+
+const StyledCardDescription = styled.div`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  -webkit-box-pack: end;
+  p {
+    display: inline;
+  }
+`;
+
+const StyledFollowButton = styled(FollowButton)`
+  padding: 2px 8px;
+  width: 92px;
+  height: 24px;
+`;
