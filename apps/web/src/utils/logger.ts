abab0f957e0aa27deea4cf8de2185934bc7fb9bd
Robinnnnn <rokim8@gmail.com>
2023-10-18 02:12:27 -0400

    avoid logging in prod

--- apps/web/src/utils/logger.ts
+++ apps/web/src/utils/logger.ts
@@ -1,5 +1,10 @@
 import { getTimestamp } from '~/shared/utils/time';
 
+import isProduction from './isProduction';
+
 export const _log: Console['log'] = (...args) => {
+  if (isProduction()) {
+    return;
+  }
   return console.log(`%c[${getTimestamp()}]`, 'color:#4878fa', `${args[0]}`, ...args.slice(1));
 };
