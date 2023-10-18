7a065875140a49753b7a98d5e70bc95be5314427
kaito <80802871+kaitoo1@users.noreply.github.com>
2023-10-18 17:51:01 +0900

    conditionally show full page loader based on path

--- apps/web/src/contexts/boundary/Boundary.tsx
+++ apps/web/src/contexts/boundary/Boundary.tsx
@@ -1,12 +1,35 @@
-import { memo, PropsWithChildren, Suspense } from 'react';
+import { useRouter } from 'next/router';
+import { memo, PropsWithChildren, Suspense, useMemo } from 'react';
+
+import FullPageLoader from '~/components/core/Loader/FullPageLoader';
 
 import ErrorBoundary from './ErrorBoundary';
 
-const Boundary = memo(({ children }: PropsWithChildren) => (
-  <Suspense>
-    <ErrorBoundary>{children}</ErrorBoundary>
-  </Suspense>
-));
+const NO_FULL_PAGE_LOADER_PATHS = ['/features'];
+
+const isPathExcluded = (pathname: string) => {
+  return NO_FULL_PAGE_LOADER_PATHS.some((path) => pathname.startsWith(path));
+};
+
+const Boundary = memo(({ children }: PropsWithChildren) => {
+  const { pathname } = useRouter();
+
+  const isFullPageLoaderExcluded = useMemo(() => isPathExcluded(pathname), [pathname]);
+
+  if (isFullPageLoaderExcluded) {
+    return (
+      <Suspense>
+        <ErrorBoundary>{children}</ErrorBoundary>;
+      </Suspense>
+    );
+  }
+
+  return (
+    <Suspense fallback={<FullPageLoader />}>
+      <ErrorBoundary>{children}</ErrorBoundary>
+    </Suspense>
+  );
+});
 
 Boundary.displayName = 'Boundary';
 
