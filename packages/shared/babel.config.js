b4e13bbf157c48d70982cf301ad8afb12d293456
Jakz <jakzaizzat@gmail.com>
2023-10-20 11:08:05 +0800

    Add basic test

--- packages/shared/babel.config.js
+++ packages/shared/babel.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-typescript', '@babel/preset-react'],
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-typescript',
+    ['@babel/preset-react', { runtime: 'automatic' }],
+  ],
   plugins: [
     [
       'relay',
