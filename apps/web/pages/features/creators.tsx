7d0385d199f705e9e4a02ed8ddadb6447ea65cba
kaito <80802871+kaitoo1@users.noreply.github.com>
2023-11-06 15:00:40 +0900

    refactor reused query string

--- apps/web/pages/features/creators.tsx
+++ apps/web/pages/features/creators.tsx
@@ -1,7 +1,7 @@
 import GalleryRoute from '~/scenes/_Router/GalleryRoute';
 import { CmsTypes } from '~/scenes/ContentPages/cms_types';
 import PostsFeaturePage from '~/scenes/ContentPages/FeaturePage/PostsFeaturePage';
-import { fetchSanityContent } from '~/utils/sanity';
+import { fetchSanityContent, getFeaturePageQueryString } from '~/utils/sanity';
 
 type Props = {
   pageContent: CmsTypes.FeaturePage;
@@ -11,42 +11,7 @@ export default function CreatorSupportFeatureRoute({ pageContent }: Props) {
   return <GalleryRoute element={<PostsFeaturePage pageContent={pageContent} />} navbar={false} />;
 }
 
-// make most of query reusable/shared with Posts page
-export const featurePostsPageContentQuery = `
-*[ _type == "featurePage" && id == "creators" ]{
-  ...,
-  "featureHighlights": featureHighlights[]->{
-    heading,
-    orientation,
-    body,
-    externalLink,
-    media{
-      mediaType,
-      image{
-        asset->{
-          url
-        },
-        alt
-      },
-      video{
-        asset->{
-          url
-        }
-      }
-    }
-  },
-  "faqModule": faqModule->{
-    title,
-    faqs
-  },
-  "splashImage": {
-    "asset": splashImage.asset->{
-      url
-    },
-    alt
-  }
-} | order(date desc)
-`;
+export const featurePostsPageContentQuery = getFeaturePageQueryString('creators');
 
 export const getServerSideProps = async () => {
   const content = await fetchSanityContent(featurePostsPageContentQuery);
--- apps/web/src/utils/sanity.ts
+++ apps/web/src/utils/sanity.ts
@@ -2,3 +2,41 @@
 import { fetchSanityContent as _fetchSanityContent } from '~/shared/utils/sanity';
 
 export const fetchSanityContent = _fetchSanityContent(process.env.NEXT_PUBLIC_SANITY_PROJECT_ID);
+
+export function getFeaturePageQueryString(pageId: string) {
+  return `
+    *[ _type == "featurePage" && id == "${pageId}" ]{
+      ...,
+      "featureHighlights": featureHighlights[]->{
+        heading,
+        orientation,
+        body,
+        externalLink,
+        media{
+          mediaType,
+          image{
+            asset->{
+              url
+            },
+            alt
+          },
+          video{
+            asset->{
+              url
+            }
+          }
+        }
+      },
+      "faqModule": faqModule->{
+        title,
+        faqs
+      },
+      "splashImage": {
+        "asset": splashImage.asset->{
+          url
+        },
+        alt
+      }
+    } | order(date desc)
+    `;
+}
