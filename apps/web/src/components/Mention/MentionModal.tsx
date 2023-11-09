5886aadb7c881526226fb6a916a4d67ec037d988
Jakz <jakzaizzat@gmail.com>
2023-11-09 11:16:57 +0800

    add navigation to mention modal

--- apps/web/src/components/Mention/MentionModal.tsx
+++ apps/web/src/components/Mention/MentionModal.tsx
@@ -1,4 +1,5 @@
 import { Suspense, useCallback } from 'react';
+import { useHotkeys } from 'react-hotkeys-hook';
 import styled from 'styled-components';
 
 import { contexts } from '~/shared/analytics/constants';
@@ -20,6 +21,39 @@ type Props = {
 
 export function MentionModal({ keyword, onSelectMention, onEmptyResultsClose = noop }: Props) {
   const track = useTrack();
+
+  useHotkeys(
+    ['ArrowUp', 'ArrowDown', 'Enter'],
+    (event) => {
+      const allResults = Array.from(document.querySelectorAll('.SearchResult'));
+      const focused = document.querySelector('.SearchResult:focus');
+
+      let nextIndex = 0;
+      if (focused) {
+        const focusedIndex = allResults.indexOf(focused);
+        nextIndex = event.key === 'ArrowDown' ? focusedIndex + 1 : focusedIndex - 1;
+
+        // Loop around after reaching end / start
+        if (nextIndex >= allResults.length) {
+          nextIndex = 0;
+        } else if (nextIndex < 0) {
+          nextIndex = allResults.length - 1;
+        }
+      }
+
+      const nextResult = allResults[nextIndex];
+      if (nextResult instanceof HTMLElement) {
+        if (event.key === 'Enter') {
+          if (focused instanceof HTMLElement) {
+            focused.click();
+          }
+        } else {
+          nextResult.focus();
+        }
+      }
+    },
+    { enableOnFormTags: true, preventDefault: true }
+  );
 
   const handleSelectMention = useCallback(
     (item: SearchItemType) => {
