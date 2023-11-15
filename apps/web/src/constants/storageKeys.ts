1133fc1210fb3b777dda09bdf6bee9c8060ec534
kaito <80802871+kaitoo1@users.noreply.github.com>
2023-11-15 16:37:39 +0900

    Add creator announcement to composer on web (#2077)
    * add creator announcement to composer on web
    
    * address comments
    
    * use experience flag
    
    * adjust text
    
    * show announcement in Editor as well
    
    * adjust padding
    
    * update faq link

--- apps/web/src/constants/storageKeys.ts
+++ apps/web/src/constants/storageKeys.ts
@@ -2,3 +2,4 @@ export const GNOSIS_NONCE_STORAGE_KEY = 'gallery_gnosis_nonce';
 export const MOBILE_GALLERY_LAYOUT_STORAGE_KEY = 'gallery_mobile_gallery_layout';
 export const DEBUG_USERNAME_KEY = 'debug_username';
 export const DEBUG_PASSWORD_KEY = 'debug_password';
+export const CREATOR_BETA_ANNOUNCEMENT_SEEN = 'creator_beta_announcement_seen';
