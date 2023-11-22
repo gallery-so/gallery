0c6a2f3dbee7e34d54c297665cb9f6e27a2119b0
Robinnnnn <rokim8@gmail.com>
2023-11-22 12:49:22 -0500

    loading skeleton

--- apps/mobile/src/screens/NftSelectorScreen/NftSelectorLoadingSkeleton.tsx
+++ apps/mobile/src/screens/NftSelectorScreen/NftSelectorLoadingSkeleton.tsx
@@ -1,22 +1,41 @@
 import { View } from 'react-native';
+import { SpinnerIcon } from 'src/icons/SpinnerIcon';
 
-const ROWS = 6;
+import { Typography } from '~/components/Typography';
+
+const ROWS = 5;
 const COLUMNS = 3;
 
 export function NftSelectorLoadingSkeleton() {
   return (
-    <View className="flex flex-col space-y-2 p-2">
-      {Array.from({ length: ROWS }).map((_, i) => {
-        return (
-          <View key={i} className="flex flex-row space-x-2">
-            {Array.from({ length: COLUMNS }).map((_, j) => {
-              return (
-                <View key={j} className="flex-1 aspect-square bg-offWhite dark:bg-black-800" />
-              );
-            })}
-          </View>
-        );
-      })}
-    </View>
+    <>
+      <View className="flex flex-col space-y-2 p-2">
+        {Array.from({ length: ROWS }).map((_, i) => {
+          return (
+            <View key={i} className="flex flex-row space-x-2">
+              {Array.from({ length: COLUMNS }).map((_, j) => {
+                return (
+                  <View key={j} className="flex-1 aspect-square bg-offWhite dark:bg-black-800" />
+                );
+              })}
+            </View>
+          );
+        })}
+      </View>
+      <View className="absolute flex justify-center items-center w-full h-full">
+        <View className="space-y-2">
+          <Typography
+            className="text-3xl text-center"
+            font={{ family: 'GTAlpina', weight: 'StandardLight', italic: true }}
+          >
+            Fetching your collection
+          </Typography>
+          <Typography className="text-center" font={{ family: 'ABCDiatype', weight: 'Regular' }}>
+            This may take up to a minute
+          </Typography>
+        </View>
+        <SpinnerIcon spin size="s" />
+      </View>
+    </>
   );
 }
