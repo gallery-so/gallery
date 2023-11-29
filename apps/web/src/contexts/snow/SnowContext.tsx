935f173ff7e59248182f72337bd10cc8e5994df4
Robinnnnn <rokim8@gmail.com>
2023-11-29 18:40:48 -0500

    configure amount

--- apps/web/src/contexts/snow/SnowContext.tsx
+++ apps/web/src/contexts/snow/SnowContext.tsx
@@ -55,7 +55,7 @@ const SnowProvider = memo(({ children }: Props) => {
   return (
     <SnowContext.Provider value={value}>
       <VisibilityWrapper isVisible={isSnowEnabled}>
-        <AnimatedSnowfall />
+        <AnimatedSnowfall amount={50} />
       </VisibilityWrapper>
       {children}
     </SnowContext.Provider>
