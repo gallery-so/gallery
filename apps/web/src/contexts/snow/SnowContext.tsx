b4b3a09e4ecf9e1e73ed5c4caddf22ff5f5ed995
Robinnnnn <rokim8@gmail.com>
2023-11-30 01:29:55 -0500

    hide on editor route

--- apps/web/src/contexts/snow/SnowContext.tsx
+++ apps/web/src/contexts/snow/SnowContext.tsx
@@ -32,7 +32,7 @@ const SnowProvider = memo(({ children }: Props) => {
   const { asPath } = useRouter();
 
   const isEnabledBasedOnRoute = useMemo(() => {
-    if (asPath.includes('/gallery')) {
+    if (asPath.includes('/edit')) {
       return false;
     }
     return true;
