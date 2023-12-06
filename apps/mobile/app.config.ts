df246a5290158b94a3da83b7198fa8cb9072dab6
robin <rokim8@gmail.com>
2023-12-06 17:56:02 -0500

    [mobile] bump version 1.0.36 (#2164)

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -33,7 +33,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   orientation: 'portrait',
   icon: './assets/icon.png',
   userInterfaceStyle: 'automatic',
-  version: '1.0.35',
+  version: '1.0.36',
   updates: {
     fallbackToCacheTimeout: 0,
   },
