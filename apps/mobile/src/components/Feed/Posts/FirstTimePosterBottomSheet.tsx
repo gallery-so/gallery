ae5f449d30ef1d16c343ba5b266263fa5df55d45
Rohan-cp <rohancp9@icloud.com>
2023-12-08 05:59:45 +0530

    base

---
+++ apps/mobile/src/components/Feed/Posts/FirstTimePosterBottomSheet.tsx
@@ -0,0 +1,76 @@
+import { useBottomSheetDynamicSnapPoints } from '@gorhom/bottom-sheet';
+import { ForwardedRef, forwardRef, useRef } from 'react';
+import { View } from 'react-native';
+
+import { Button } from '~/components/Button';
+import {
+  GalleryBottomSheetModal,
+  GalleryBottomSheetModalType,
+} from '~/components/GalleryBottomSheet/GalleryBottomSheetModal';
+import { useSafeAreaPadding } from '~/components/SafeAreaViewWithPadding';
+import { Typography } from '~/components/Typography';
+import { contexts } from '~/shared/analytics/constants';
+
+const SNAP_POINTS = ['CONTENT_HEIGHT'];
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+type Props = {};
+
+function FirstTimePosterBottomSheet(props: Props, ref: ForwardedRef<GalleryBottomSheetModalType>) {
+  const { bottom } = useSafeAreaPadding();
+
+  const bottomSheetRef = useRef<GalleryBottomSheetModalType | null>(null);
+
+  const { animatedHandleHeight, animatedSnapPoints, animatedContentHeight, handleContentLayout } =
+    useBottomSheetDynamicSnapPoints(SNAP_POINTS);
+
+  return (
+    <GalleryBottomSheetModal
+      ref={(value) => {
+        bottomSheetRef.current = value;
+
+        if (typeof ref === 'function') {
+          ref(value);
+        } else if (ref) {
+          ref.current = value;
+        }
+      }}
+      snapPoints={animatedSnapPoints}
+      handleHeight={animatedHandleHeight}
+      contentHeight={animatedContentHeight}
+    >
+      <View
+        onLayout={handleContentLayout}
+        style={{ paddingBottom: bottom }}
+        className="p-4 flex flex-col space-y-4"
+      >
+        <View className="flex flex-col space-y-2">
+          <Typography
+            className="text-lg text-black-900 dark:text-offWhite"
+            font={{ family: 'ABCDiatype', weight: 'Bold' }}
+          >
+            First-time poster
+          </Typography>
+          <Typography
+            className="text-lg text-black-900 dark:text-offWhite"
+            font={{ family: 'ABCDiatype', weight: 'Regular' }}
+          >
+            This is my first post, say hi!
+          </Typography>
+        </View>
+
+        <Button
+          onPress={() => bottomSheetRef.current?.dismiss()}
+          text="CLOSE"
+          eventElementId={null}
+          eventName={null}
+          eventContext={contexts.Posts}
+        />
+      </View>
+    </GalleryBottomSheetModal>
+  );
+}
+
+const ForwardedFirstTimePosterBottomSheet = forwardRef(FirstTimePosterBottomSheet);
+
+export { ForwardedFirstTimePosterBottomSheet as FirstTimePosterBottomSheet };
