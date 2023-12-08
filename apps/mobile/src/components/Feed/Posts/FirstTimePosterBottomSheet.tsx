ac15767805920a11d2455b53b3347af917785cb5
Rohan-cp <rohancp9@icloud.com>
2023-12-08 06:19:38 +0530

    layout spacing fixed

--- apps/mobile/src/components/Feed/Posts/FirstTimePosterBottomSheet.tsx
+++ apps/mobile/src/components/Feed/Posts/FirstTimePosterBottomSheet.tsx
@@ -42,9 +42,9 @@ function FirstTimePosterBottomSheet(props: Props, ref: ForwardedRef<GalleryBotto
       <View
         onLayout={handleContentLayout}
         style={{ paddingBottom: bottom }}
-        className="p-4 flex flex-col space-y-4"
+        className="p-4 flex flex-col space-y-6"
       >
-        <View className="flex flex-col space-y-2">
+        <View className="flex flex-col space-y-4">
           <Typography
             className="text-lg text-black-900 dark:text-offWhite"
             font={{ family: 'ABCDiatype', weight: 'Bold' }}
