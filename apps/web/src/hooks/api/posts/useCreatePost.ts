a24352c1c765d1c402c25b26c73aa5167865af0e
Jakz <jakzaizzat@gmail.com>
2023-12-13 15:30:01 +0800

    optimistic response

--- apps/web/src/hooks/api/posts/useCreatePost.ts
+++ apps/web/src/hooks/api/posts/useCreatePost.ts
@@ -36,6 +36,7 @@ export default function useCreatePost() {
                   }
                 }
               }
+              userAddedMintURL
               ...PostHeaderFragment
               ...PostNftsFragment
             }
