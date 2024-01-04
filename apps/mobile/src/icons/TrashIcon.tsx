97bb6235ed2caa83cbce86903122406dec52aaf8
Jakz <jakzaizzat@gmail.com>
2024-01-04 11:48:44 +0800

    Add swipe delete feature (#2220)
    * Add swipe delete feature
    
    * wrap with runonjs
    
    * rename
    
    * remove gradient
    
    * Remove bottom sheet confirmation
    
    * Fix unable to scroll view
    
    * Add fade in

--- apps/mobile/src/icons/TrashIcon.tsx
+++ apps/mobile/src/icons/TrashIcon.tsx
@@ -8,7 +8,7 @@ type Props = {
 export function TrashIcon({ color = '#F00000', ...props }: Props) {
   return (
     <Svg width={24} height={24} viewBox="0 0 24 24" fill="none" {...props}>
-      <Path d="M3 7h18M18.5 7l-1 14h-11l-1-14M8 7l.5-3.5h7L16 7" stroke={color} />
+      <Path d="M3 7h18M18.5 7l-1 14h-11l-1-14M8 7l.5-3.5h7L16 7" stroke={color} strokeWidth={2} />
     </Svg>
   );
 }
