ae80b0ee57ecf99a3afac88243339baeac44143b
Jakz <jakzaizzat@gmail.com>
2024-01-08 14:47:15 +0800

    Polish fallback mobile (#2239)

--- apps/mobile/src/components/Feed/CommentsBottomSheet/CommentListFallback.tsx
+++ apps/mobile/src/components/Feed/CommentsBottomSheet/CommentListFallback.tsx
@@ -7,12 +7,21 @@ export function CommentCardFallback() {
   return (
     <View className="px-4 py-3">
       <GallerySkeleton>
-        <SkeletonPlaceholder.Item alignItems="center" flexDirection="row" gap={4}>
-          <SkeletonPlaceholder.Item width={24} height={24} borderRadius={80} />
-          <SkeletonPlaceholder.Item flexDirection="column" rowGap={4}>
-            <SkeletonPlaceholder.Item width={50} height={12} />
-            <SkeletonPlaceholder.Item width={100} height={10} />
+        <SkeletonPlaceholder.Item
+          alignItems="center"
+          justifyContent="space-between"
+          flexDirection="row"
+          gap={4}
+        >
+          <SkeletonPlaceholder.Item alignItems="center" flexDirection="row" gap={4}>
+            <SkeletonPlaceholder.Item width={24} height={24} borderRadius={80} />
+            <SkeletonPlaceholder.Item flexDirection="column" rowGap={4}>
+              <SkeletonPlaceholder.Item width={50} height={12} />
+              <SkeletonPlaceholder.Item width={140} height={10} />
+              <SkeletonPlaceholder.Item width={20} height={6} />
+            </SkeletonPlaceholder.Item>
           </SkeletonPlaceholder.Item>
+          <SkeletonPlaceholder.Item width={16} height={16} />
         </SkeletonPlaceholder.Item>
       </GallerySkeleton>
     </View>
@@ -22,6 +31,8 @@ export function CommentCardFallback() {
 export function CommentListFallback() {
   return (
     <View className="flex flex-col space-y-1">
+      <CommentCardFallback />
+      <CommentCardFallback />
       <CommentCardFallback />
       <CommentCardFallback />
       <CommentCardFallback />
