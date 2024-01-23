511d4627c868e55076386fece1c859ace710c7f0
Jakz <jakzaizzat@gmail.com>
2024-01-23 18:26:00 +0800

    Add community grid/list header

--- apps/mobile/src/icons/GridIcon.tsx
+++ apps/mobile/src/icons/GridIcon.tsx
@@ -1,11 +1,15 @@
-import Svg, { Path,SvgProps } from 'react-native-svg';
+import Svg, { Path, SvgProps } from 'react-native-svg';
 
-export function GridIcon(props: SvgProps) {
+type Props = {
+  color?: string;
+} & SvgProps;
+
+export function GridIcon({ color = '#141414', ...props }: Props) {
   return (
     <Svg width={16} height={16} fill="none" {...props}>
-      <Path stroke="#141414" strokeMiterlimit={10} d="M10 2v12M6 2v12" />
-      <Path stroke="#141414" d="M14 2H2v12h12V2Z" />
-      <Path stroke="#141414" strokeMiterlimit={10} d="M14 10H2M14 6H2" />
+      <Path stroke={color} strokeMiterlimit={10} d="M10 2v12M6 2v12" />
+      <Path stroke={color} d="M14 2H2v12h12V2Z" />
+      <Path stroke={color} strokeMiterlimit={10} d="M14 10H2M14 6H2" />
     </Svg>
   );
 }
