a552d6d5d73770d66db85606aab16ba6ba126d4e
Jakz <jakzaizzat@gmail.com>
2024-01-23 12:43:00 +0800

    Add admire icon to web notification

--- apps/web/src/components/Notifications/notifications/SomeoneAdmiredYourComment.tsx
+++ apps/web/src/components/Notifications/notifications/SomeoneAdmiredYourComment.tsx
@@ -1,12 +1,17 @@
 import { graphql, useFragment } from 'react-relay';
+import colors from 'shared/theme/colors';
 import styled from 'styled-components';
 
-import { HStack } from '~/components/core/Spacer/Stack';
+import { HStack, VStack } from '~/components/core/Spacer/Stack';
 import { BaseM } from '~/components/core/Text/Text';
 import UserHoverCard from '~/components/HoverCard/UserHoverCard';
 import { ProfilePicture } from '~/components/ProfilePicture/ProfilePicture';
 import { SomeoneAdmiredYourCommentFragment$key } from '~/generated/SomeoneAdmiredYourCommentFragment.graphql';
+import { AdmireIcon } from '~/icons/SocializeIcons';
 import { ErrorWithSentryMetadata } from '~/shared/errors/ErrorWithSentryMetadata';
+import unescape from '~/shared/utils/unescape';
+
+import { NotificationPostPreviewWithBoundary } from './NotificationPostPreview';
 
 type Props = {
   notificationRef: SomeoneAdmiredYourCommentFragment$key;
@@ -20,6 +25,18 @@ export default function SomeoneAdmiredYourComment({ notificationRef, onClose }: 
         __typename
         dbid
         count
+        comment {
+          comment
+          source {
+            __typename
+            ... on Post {
+              dbid
+              tokens {
+                ...NotificationPostPreviewWithBoundaryFragment
+              }
+            }
+          }
+        }
 
         admirers(last: 1) {
           edges {
@@ -33,10 +50,16 @@ export default function SomeoneAdmiredYourComment({ notificationRef, onClose }: 
     `,
     notificationRef
   );
+  const { comment } = notification;
 
   const notificationCount = notification.count ?? 1;
 
   const firstAdmirer = notification.admirers?.edges?.[0]?.node;
+
+  let token;
+  if (comment?.source?.__typename === 'Post') {
+    token = comment.source.tokens?.[0];
+  }
 
   if (!firstAdmirer) {
     throw new ErrorWithSentryMetadata('SomeoneAdmiredYourCommentNotification was missing admirer', {
@@ -47,24 +70,35 @@ export default function SomeoneAdmiredYourComment({ notificationRef, onClose }: 
   return (
     <StyledNotificationContent align="center" justify="space-between" gap={8}>
       <HStack align="center" gap={8}>
-        <ProfilePicture size="md" userRef={firstAdmirer} />
-        <StyledTextWrapper align="center" as="span" wrap="wrap">
-          <UserHoverCard userRef={firstAdmirer} onClick={onClose} />
-          &nbsp;
-          <BaseM as="span">
-            {notificationCount > 1 && (
-              <>
-                {'and '}
-                <strong>
-                  {notificationCount - 1} other{notificationCount - 1 > 1 && 's'}&nbsp;
-                </strong>
-              </>
-            )}
-            admired your {''}
-            <strong>comment</strong>
-          </BaseM>
-        </StyledTextWrapper>
+        <StyledProfilePictureWrapper>
+          <ProfilePicture size="md" userRef={firstAdmirer} />
+          <StyledAdmireIconWrapper align="center" justify="center">
+            <AdmireIcon active height={12} />
+          </StyledAdmireIconWrapper>
+        </StyledProfilePictureWrapper>
+        <VStack>
+          <VStack gap={4}>
+            <StyledTextWrapper align="center" as="span" wrap="wrap">
+              <UserHoverCard userRef={firstAdmirer} onClick={onClose} />
+              &nbsp;
+              <BaseM as="span">
+                {notificationCount > 1 && (
+                  <>
+                    {'and '}
+                    <strong>
+                      {notificationCount - 1} other{notificationCount - 1 > 1 && 's'}&nbsp;
+                    </strong>
+                  </>
+                )}
+                admired your {''}
+                <strong>comment</strong>
+              </BaseM>
+            </StyledTextWrapper>
+            <StyledCaption>{unescape(comment?.comment ?? '')}</StyledCaption>
+          </VStack>
+        </VStack>
       </HStack>
+      {token && <NotificationPostPreviewWithBoundary tokenRef={token} />}
     </StyledNotificationContent>
   );
 }
@@ -75,4 +109,31 @@ const StyledNotificationContent = styled(HStack)`
 
 const StyledTextWrapper = styled(HStack)`
   display: inline;
+`;
+
+const StyledCaption = styled(BaseM)`
+  font-size: 12px;
+  border-left: 2px solid ${colors.porcelain};
+  padding-left: 8px;
+  word-break: break-word;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  line-clamp: 1;
+  -webkit-line-clamp: 1;
+`;
+
+const StyledProfilePictureWrapper = styled.div`
+  position: relative;
+`;
+
+const StyledAdmireIconWrapper = styled(VStack)`
+  position: absolute;
+  top: -4px;
+  right: -4px;
+  background-color: #e5e8fd;
+  border-radius: 50%;
+  height: 16px;
+  width: 16px;
+  border: 1px solid ${colors.offWhite};
 `;
--- apps/web/src/components/Notifications/notifications/SomeoneAdmiredYourPost.tsx
+++ apps/web/src/components/Notifications/notifications/SomeoneAdmiredYourPost.tsx
@@ -1,11 +1,13 @@
 import { graphql, useFragment } from 'react-relay';
+import colors from 'shared/theme/colors';
 import styled from 'styled-components';
 
-import { HStack } from '~/components/core/Spacer/Stack';
+import { HStack, VStack } from '~/components/core/Spacer/Stack';
 import { BaseM } from '~/components/core/Text/Text';
 import UserHoverCard from '~/components/HoverCard/UserHoverCard';
 import { ProfilePicture } from '~/components/ProfilePicture/ProfilePicture';
 import { SomeoneAdmiredYourPostFragment$key } from '~/generated/SomeoneAdmiredYourPostFragment.graphql';
+import { AdmireIcon } from '~/icons/SocializeIcons';
 import { ErrorWithSentryMetadata } from '~/shared/errors/ErrorWithSentryMetadata';
 
 import { NotificationPostPreviewWithBoundary } from './NotificationPostPreview';
@@ -60,7 +62,12 @@ export default function SomeoneAdmiredYourPost({ notificationRef, onClose }: Pro
   return (
     <StyledNotificationContent align="center" justify="space-between" gap={8}>
       <HStack align="center" gap={8}>
-        <ProfilePicture size="md" userRef={firstAdmirer} />
+        <StyledProfilePictureWrapper>
+          <ProfilePicture size="md" userRef={firstAdmirer} />
+          <StyledAdmireIconWrapper align="center" justify="center">
+            <AdmireIcon active height={12} />
+          </StyledAdmireIconWrapper>
+        </StyledProfilePictureWrapper>
         <StyledTextWrapper align="center" as="span" wrap="wrap">
           <UserHoverCard userRef={firstAdmirer} onClick={onClose} />
           &nbsp;
@@ -88,4 +95,19 @@ const StyledNotificationContent = styled(HStack)`
 
 const StyledTextWrapper = styled(HStack)`
   display: inline;
+`;
+
+const StyledProfilePictureWrapper = styled.div`
+  position: relative;
+`;
+
+const StyledAdmireIconWrapper = styled(VStack)`
+  position: absolute;
+  top: -4px;
+  right: -4px;
+  background-color: #e5e8fd;
+  border-radius: 50%;
+  height: 16px;
+  width: 16px;
+  border: 1px solid ${colors.offWhite};
 `;
