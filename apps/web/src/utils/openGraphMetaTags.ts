6a4cb4bd6c5357a5a4e06534f3d142dad0ac8033
robin <rokim8@gmail.com>
2024-01-26 06:00:18 -0500

    Farcaster frames support (#2274)
    * first commit
    
    * add explicit post_url
    
    * support user path

--- apps/web/src/utils/openGraphMetaTags.ts
+++ apps/web/src/utils/openGraphMetaTags.ts
@@ -1,3 +1,5 @@
+import { removeNullValues } from 'shared/relay/removeNullValues';
+
 import { HEIGHT_OPENGRAPH_IMAGE, WIDTH_OPENGRAPH_IMAGE } from '~/constants/opengraph';
 import { MetaTagProps } from '~/pages/_app';
 
@@ -15,26 +17,56 @@ const baseurl = `https://gallery-opengraph${
 export const openGraphMetaTags = ({
   title,
   previewPath,
-}: Params): Required<MetaTagProps['metaTags']> => [
-  { property: 'og:title', content: title },
-  // TODO: add description
-  {
-    property: 'og:image',
-    content: `${baseurl}?${new URLSearchParams({
-      path: previewPath,
-      fallback:
-        'https://storage.googleapis.com/gallery-prod-325303.appspot.com/gallery_full_logo_v2.1.png',
-    }).toString()}`,
-  },
-  { property: 'twitter:card', content: 'summary_large_image' },
-  {
-    property: 'twitter:image',
-    content: `${baseurl}?${new URLSearchParams({
-      path: previewPath,
-      width: WIDTH_OPENGRAPH_IMAGE.toString(),
-      height: HEIGHT_OPENGRAPH_IMAGE.toString(),
-      fallback:
-        'https://storage.googleapis.com/gallery-prod-325303.appspot.com/gallery_full_logo_v2.1.png',
-    }).toString()}`,
-  },
-];
+}: Params): Required<MetaTagProps['metaTags']> => {
+  const tags = [
+    { property: 'og:title', content: title },
+    // TODO: add description
+    {
+      property: 'og:image',
+      content: `${baseurl}?${new URLSearchParams({
+        path: previewPath,
+        fallback:
+          'https://storage.googleapis.com/gallery-prod-325303.appspot.com/gallery_full_logo_v2.1.png',
+      }).toString()}`,
+    },
+    { property: 'twitter:card', content: 'summary_large_image' },
+    {
+      property: 'twitter:image',
+      content: `${baseurl}?${new URLSearchParams({
+        path: previewPath,
+        width: WIDTH_OPENGRAPH_IMAGE.toString(),
+        height: HEIGHT_OPENGRAPH_IMAGE.toString(),
+        fallback:
+          'https://storage.googleapis.com/gallery-prod-325303.appspot.com/gallery_full_logo_v2.1.png',
+      }).toString()}`,
+    },
+    // farcaster frame embed
+    // https://warpcast.notion.site/Farcaster-Frames-4bd47fe97dc74a42a48d3a234636d8c5
+    {
+      property: 'fc:frame',
+      content: 'vNext',
+    },
+    {
+      property: `fc:frame:button:1`,
+      content: '→',
+    },
+    {
+      property: 'fc:frame:image',
+      content: `${baseurl}?${new URLSearchParams({
+        path: `${previewPath}/fcframe`,
+        fallback:
+          'https://storage.googleapis.com/gallery-prod-325303.appspot.com/gallery_full_logo_v2.1.png',
+      }).toString()}`,
+    },
+    {
+      property: 'fc:frame:post_url',
+      content: `${baseurl}?${new URLSearchParams({
+        path: `${previewPath}/fcframe`,
+        fallback:
+          'https://storage.googleapis.com/gallery-prod-325303.appspot.com/gallery_full_logo_v2.1.png',
+      }).toString()}`,
+    },
+  ];
+
+  return removeNullValues(tags);
+};
