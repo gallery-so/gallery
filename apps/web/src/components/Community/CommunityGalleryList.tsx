58d71d58542f18ffa781582c86083b5c7bd462aa
Jakz <jakzaizzat@gmail.com>
2024-01-29 13:56:58 +0800

    Add infinite scroll

--- apps/web/src/components/Community/CommunityGalleryList.tsx
+++ apps/web/src/components/Community/CommunityGalleryList.tsx
@@ -1,11 +1,15 @@
-import { useMemo } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { graphql, useFragment, usePaginationFragment } from 'react-relay';
 import { removeNullValues } from 'shared/relay/removeNullValues';
 import styled from 'styled-components';
 
+import { LIST_ITEM_PER_PAGE } from '~/constants/community';
+import { GLOBAL_FOOTER_HEIGHT } from '~/contexts/globalLayout/GlobalFooter/GlobalFooter';
 import { CommunityGalleryListFragment$key } from '~/generated/CommunityGalleryListFragment.graphql';
 import { CommunityGalleryListQueryFragment$key } from '~/generated/CommunityGalleryListQueryFragment.graphql';
 
+import Loader from '../core/Loader/Loader';
+import { VStack } from '../core/Spacer/Stack';
 import { CommunityGalleryListItem } from './CommunityGalleryListItem';
 
 type Props = {
@@ -14,7 +18,11 @@ type Props = {
 };
 
 export function CommunityGalleryList({ communityRef, queryRef }: Props) {
-  const { data: community } = usePaginationFragment(
+  const {
+    data: community,
+    loadNext,
+    hasNext,
+  } = usePaginationFragment(
     graphql`
       fragment CommunityGalleryListFragment on Community
       @refetchable(queryName: "CommunityGalleriesListRefetchableFragment") {
@@ -44,20 +52,53 @@ export function CommunityGalleryList({ communityRef, queryRef }: Props) {
     queryRef
   );
 
+  const [isFetching, setIsFetching] = useState(false);
+
   const nonNullGalleries = useMemo(() => {
     return removeNullValues(community?.galleries?.edges?.map((edge) => edge?.node));
   }, [community?.galleries?.edges]);
 
+  const handleSeeMore = useCallback(() => {
+    setIsFetching(true);
+    loadNext(LIST_ITEM_PER_PAGE, {
+      onComplete: () => {
+        setIsFetching(false);
+      },
+    });
+  }, [loadNext]);
+
+  useEffect(() => {
+    function handleScrollPosition() {
+      const pixelsFromBottomOfPage =
+        document.body.offsetHeight - window.pageYOffset - window.innerHeight;
+      if (pixelsFromBottomOfPage < GLOBAL_FOOTER_HEIGHT && hasNext && !isFetching) {
+        handleSeeMore();
+      }
+    }
+
+    window.addEventListener('scroll', handleScrollPosition);
+
+    return () => window.removeEventListener('scroll', handleScrollPosition);
+  }, [handleSeeMore, hasNext, isFetching]);
+
   return (
-    <StyledCommunityGalleryListItemWrapper>
-      {nonNullGalleries.map((gallery) => (
-        <CommunityGalleryListItem
-          key={gallery.gallery?.dbid}
-          communityGalleryRef={gallery}
-          queryRef={query}
-        />
-      ))}
-    </StyledCommunityGalleryListItemWrapper>
+    <VStack gap={56}>
+      <StyledCommunityGalleryListItemWrapper>
+        {nonNullGalleries.map((gallery) => (
+          <CommunityGalleryListItem
+            key={gallery.gallery?.dbid}
+            communityGalleryRef={gallery}
+            queryRef={query}
+          />
+        ))}
+      </StyledCommunityGalleryListItemWrapper>
+
+      {isFetching && (
+        <VStack align="center" justify="center">
+          <Loader inverted size="small" />
+        </VStack>
+      )}
+    </VStack>
   );
 }
 
