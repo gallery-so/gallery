773407cd2924856e848ccc8a4f3182555737e07b
kaito <80802871+kaitoo1@users.noreply.github.com>
2024-01-29 19:02:07 +0900

    reorder sidebar (#2277)

---
+++ apps/web/src/icons/HomeIcon.tsx
@@ -0,0 +1,9 @@
+export default function HomeIcon() {
+  return (
+    <svg width="22" height="23" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M1 11.6667L12 2.66675L23 11.6667" stroke="black" />
+      <path d="M20 9.16675V22.6667H4V9.16675" stroke="black" />
+      <path d="M9.5 22.6667V14.6667H14.5V22.6667" stroke="black" />
+    </svg>
+  );
+}
