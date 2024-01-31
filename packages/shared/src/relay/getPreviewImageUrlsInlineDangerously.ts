ff52f1e5b23e708d2f523f3932187b3a50e2d1c2
Rohan-cp <rohancp9@icloud.com>
2024-01-30 20:20:34 -0500

    base

--- packages/shared/src/relay/getPreviewImageUrlsInlineDangerously.ts
+++ packages/shared/src/relay/getPreviewImageUrlsInlineDangerously.ts
@@ -23,7 +23,11 @@ export type GetPreviewImageUrlsResult =
   | {
       type: 'error';
       error: CouldNotRenderNftError;
-    };
+    }
+  | {
+    // TODO: rohan - remove later and throw error
+    type: 'temp';
+  };
 
 type Props = {
   tokenRef: getPreviewImageUrlsInlineDangerouslyFragment$key;
@@ -236,17 +240,10 @@ export function getPreviewImageUrlsInlineDangerously({
     }
   }
 
+
   if (!previewUrls) {
     return {
-      type: 'error',
-      error: new CouldNotRenderNftError(
-        'getPreviewImageUrlsInlineDangerously',
-        'No preview URLs returned for token!',
-        {
-          id: token?.dbid,
-          assetType: media?.__typename,
-        }
-      ),
+      type: 'temp',
     };
   }
 
