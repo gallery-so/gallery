91713bd80fb375a0e0b8e64c39293f0be953f049
estiventhneira <estiventhneira@gmail.com>
2024-02-12 17:46:17 -0500

    add tab to expand feature mobile

--- apps/mobile/src/components/Feed/Posts/PostListCaption.tsx
+++ apps/mobile/src/components/Feed/Posts/PostListCaption.tsx
@@ -1,17 +1,29 @@
-import { useMemo } from 'react';
+import { useMemo, useRef, useState } from 'react';
 import { View } from 'react-native';
 import { graphql, useFragment } from 'react-relay';
 
+import { GalleryTouchableOpacity } from '~/components/GalleryTouchableOpacity';
 import ProcessedText from '~/components/ProcessedText/ProcessedText';
 import { PostListCaptionFragment$key } from '~/generated/PostListCaptionFragment.graphql';
+import { GalleryElementTrackingProps } from '~/shared/contexts/AnalyticsContext';
 import { removeNullValues } from '~/shared/relay/removeNullValues';
 import { replaceUrlsWithMarkdownFormat } from '~/shared/utils/replaceUrlsWithMarkdownFormat';
 
 type Props = {
   feedPostRef: PostListCaptionFragment$key;
+  eventContext: GalleryElementTrackingProps['eventContext'];
 };
 
-export function PostListCaption({ feedPostRef }: Props) {
+export function PostListCaption({ feedPostRef, eventContext }: Props) {
+  const [showAll, setShowAll] = useState(false);
+  const ref = useRef(null);
+  const toggleText = () => {
+    if (!showAll) {
+      setShowAll(!showAll);
+    }
+  };
+
+
   const feedPost = useFragment(
     graphql`
       fragment PostListCaptionFragment on Post {
@@ -32,7 +44,19 @@ export function PostListCaption({ feedPostRef }: Props) {
 
   return (
     <View className="px-4 pb-4">
-      <ProcessedText text={captionWithMarkdownLinks} mentionsRef={nonNullMentions} />
+      <View>
+        <GalleryTouchableOpacity eventElementId="Show more lines"
+          eventName="Show more lines"
+          eventContext={eventContext}
+          onPress={toggleText}
+          withoutFeedback={true}
+          activeOpacity={0}
+          ref={ref}
+        >
+          <ProcessedText suppressHighlighting={true} numberOfLines={showAll ? undefined : 4} text={captionWithMarkdownLinks} mentionsRef={nonNullMentions} />
+
+        </GalleryTouchableOpacity>
+      </View>
     </View>
   );
 }
