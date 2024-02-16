854e7bcfdefb071ead7276b29e315b7b56de36ce
robin <rokim8@gmail.com>
2024-02-16 01:07:00 -0500

    Bump max post char count 600 -> 1200 (#2262)

--- packages/shared/src/utils/getRemainingCharacterCount.ts
+++ packages/shared/src/utils/getRemainingCharacterCount.ts
@@ -1,5 +1,5 @@
 export const MAX_COMMENT_LENGTH = 300;
-export const MAX_POST_LENGTH = 600;
+export const MAX_POST_LENGTH = 1200;
 export const MAX_COLLECTION_DESCRIPTION_LENGTH = 2400;
 
 export function getRemainingCharacterCount(message: string, MAX_TEXT_LENGTH: number) {
