55a1ec78dd3137a166fff39ed5cd4bdf6bd4e087
Jakz <jakzaizzat@gmail.com>
2024-02-26 11:06:25 +0800

    Add onboarding progress bar

--- apps/web/src/components/Onboarding/FullPageCenteredStep.tsx
+++ apps/web/src/components/Onboarding/FullPageCenteredStep.tsx
@@ -1,22 +1,31 @@
+import { motion } from 'framer-motion';
 import { ReactNode } from 'react';
+import colors from 'shared/theme/colors';
 import styled from 'styled-components';
 
 import { FOOTER_HEIGHT } from '~/components/Onboarding/constants';
 import GalleryRoute from '~/scenes/_Router/GalleryRoute';
 
+import { ANIMATED_COMPONENT_TRANSITION_S, rawTransitions } from '../core/transitions';
+
 type Props = {
   children: ReactNode | ReactNode[];
   // If this is true, the page height will be reduced to account for the footer height
   withFooter?: boolean;
-};
+} & OnboardingProgressBarProps;
 
 /**
  * A helper component to easily generate full-page steps where the content is centered
  */
-export default function FullPageCenteredStep({ children, withFooter }: Props) {
+export default function FullPageCenteredStep({ children, withFooter, ...props }: Props) {
   return (
     <GalleryRoute
-      element={<StyledPage withFooter={withFooter}>{children}</StyledPage>}
+      element={
+        <StyledPage withFooter={withFooter}>
+          <OnboardingProgressBar {...props} />
+          {children}
+        </StyledPage>
+      }
       navbar={false}
       footer={false}
     />
@@ -29,4 +38,32 @@ const StyledPage = styled.div<{ withFooter?: boolean }>`
   align-items: center;
   justify-content: center;
   height: calc(100vh - ${({ withFooter }) => (withFooter ? FOOTER_HEIGHT : 0)}px);
+  position: relative;
+`;
+
+type OnboardingProgressBarProps = {
+  from: number;
+  to: number;
+};
+
+function OnboardingProgressBar({ from, to }: OnboardingProgressBarProps) {
+  return (
+    <StyledBar
+      initial={{ width: from }}
+      animate={{ width: `${to}%` }}
+      transition={{
+        duration: ANIMATED_COMPONENT_TRANSITION_S,
+        ease: rawTransitions.cubicValues,
+      }}
+    />
+  );
+}
+
+const StyledBar = styled(motion.div)`
+  height: 12px;
+  background-color: ${colors.activeBlue};
+  position: absolute;
+  top: 0;
+  left: 0;
+  border-bottom-right-radius: 8px;
 `;
