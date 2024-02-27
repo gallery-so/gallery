9455122fd2df901f9b7fc89cbe566c6bd57c9e9c
Jakz <jakzaizzat@gmail.com>
2024-02-27 16:46:18 +0800

    Add onboarding persona

---
+++ apps/web/src/scenes/Onboarding/OnboardingAddPersonaPage.tsx
@@ -0,0 +1,77 @@
+import { useCallback } from 'react';
+import { graphql } from 'react-relay';
+import { usePromisifiedMutation } from 'shared/relay/usePromisifiedMutation';
+import colors from 'shared/theme/colors';
+import styled from 'styled-components';
+
+import { VStack } from '~/components/core/Spacer/Stack';
+import { BaseM, TitleDiatypeM } from '~/components/core/Text/Text';
+import FullPageCenteredStep from '~/components/Onboarding/FullPageCenteredStep';
+import { OnboardingFooter } from '~/components/Onboarding/OnboardingFooter';
+import {
+  OnboardingAddPersonaPageMutation,
+  Persona,
+} from '~/generated/OnboardingAddPersonaPageMutation.graphql';
+
+const PERSONAS = ['Collector', 'Creator', 'Both'] as Persona[];
+
+export function OnboardingAddPersonaPage() {
+  const [setPersona] = usePromisifiedMutation<OnboardingAddPersonaPageMutation>(graphql`
+    mutation OnboardingAddPersonaPageMutation($input: Persona!) @raw_response_type {
+      setPersona(persona: $input) {
+        ... on SetPersonaPayload {
+          __typename
+        }
+      }
+    }
+  `);
+
+  const handlePersonaClick = useCallback(
+    (persona: Persona) => {
+      setPersona({
+        variables: {
+          input: persona,
+        },
+      });
+    },
+    [setPersona]
+  );
+
+  const handleSkip = useCallback(() => {
+    handlePersonaClick('None');
+  }, [handlePersonaClick]);
+
+  return (
+    <VStack>
+      <FullPageCenteredStep from={90} to={100}>
+        <Container gap={16}>
+          <TitleDiatypeM>What best describes you?</TitleDiatypeM>
+          {PERSONAS.map((persona) => (
+            <Row onClick={() => handlePersonaClick(persona)} key={persona}>
+              <BaseM>{persona}</BaseM>
+            </Row>
+          ))}
+        </Container>
+      </FullPageCenteredStep>
+      <OnboardingFooter
+        step="add-persona"
+        onNext={handleSkip}
+        isNextEnabled
+        nextButtonVariant="secondary"
+        previousTextOverride="Back"
+      />
+    </VStack>
+  );
+}
+
+const Container = styled(VStack)`
+  width: 480px;
+`;
+
+const Row = styled.button`
+  padding: 16px;
+  border: 1px solid ${colors.black[800]};
+  text-align: left;
+  background-color: ${colors.white};
+  cursor: pointer;
+`;
