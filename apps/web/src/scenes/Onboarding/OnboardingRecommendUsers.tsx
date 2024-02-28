d5e6cbb1774532afce800b0a488ec7daf40fbe41
Jakz <jakzaizzat@gmail.com>
2024-02-28 15:12:25 +0800

    Add recommend component

---
+++ apps/web/src/scenes/Onboarding/OnboardingRecommendUsers.tsx
@@ -0,0 +1,188 @@
+import { useRouter } from 'next/router';
+import { Route } from 'nextjs-routes';
+import { useCallback, useMemo } from 'react';
+import { graphql, useLazyLoadQuery } from 'react-relay';
+import { contexts } from 'shared/analytics/constants';
+import colors from 'shared/theme/colors';
+import styled from 'styled-components';
+
+import { Button } from '~/components/core/Button/Button';
+import { HStack, VStack } from '~/components/core/Spacer/Stack';
+import { BaseM } from '~/components/core/Text/Text';
+import FollowButton from '~/components/Follow/FollowButton';
+import FullPageCenteredStep from '~/components/Onboarding/FullPageCenteredStep';
+import { OnboardingFooter } from '~/components/Onboarding/OnboardingFooter';
+import ProcessedText from '~/components/ProcessedText/ProcessedText';
+import { ProfilePicture } from '~/components/ProfilePicture/ProfilePicture';
+import { OnboardingRecommendUsersQuery } from '~/generated/OnboardingRecommendUsersQuery.graphql';
+import useFollowAllRecommendedUsers from '~/shared/relay/useFollowAllRecommendedUsers';
+export function OnboardingRecommendUsers() {
+  const query = useLazyLoadQuery<OnboardingRecommendUsersQuery>(
+    graphql`
+      query OnboardingRecommendUsersQuery {
+        viewer @required(action: THROW) {
+          ... on Viewer {
+            id
+            user {
+              username
+            }
+            socialAccounts {
+              farcaster {
+                __typename
+              }
+            }
+            suggestedUsers(last: 5) {
+              edges {
+                node {
+                  __typename
+                  dbid
+                  id
+                  username
+                  bio
+                  ...ProfilePictureFragment
+                  ...FollowButtonUserFragment
+                }
+              }
+            }
+          }
+        }
+        ...FollowButtonQueryFragment
+        ...useFollowAllRecommendedUsersQueryFragment
+      }
+    `,
+    {}
+  );
+
+  const { push } = useRouter();
+  const username = query.viewer?.user?.username;
+  const farcaster = query.viewer?.socialAccounts?.farcaster;
+
+  const subheadingText = useMemo(() => {
+    if (farcaster) {
+      return 'Based on your onchain contacts from places like Farcaster and Lens';
+    }
+
+    return 'Based on your collection';
+  }, [farcaster]);
+
+  const nonNullUsers = useMemo(() => {
+    const users = [];
+
+    for (const edge of query.viewer.suggestedUsers?.edges ?? []) {
+      if (edge?.node) {
+        users.push(edge.node);
+      }
+    }
+
+    return users;
+  }, [query.viewer.suggestedUsers?.edges]);
+
+  const suggestedFollowingIds = useMemo(() => {
+    const userIds = [];
+
+    for (const edge of query.viewer?.suggestedUsers?.edges ?? []) {
+      if (edge?.node?.__typename === 'GalleryUser') {
+        userIds.push(edge?.node.id);
+      }
+    }
+
+    return userIds;
+  }, [query]);
+
+  const userProfileRoute: Route = useMemo(
+    () => ({
+      pathname: '/[username]',
+      query: {
+        username: `${username as string}`,
+        onboarding: 'true',
+      },
+    }),
+    [username]
+  );
+
+  const redirectToProfile = useCallback(() => {
+    push(userProfileRoute);
+  }, [push, userProfileRoute]);
+
+  const followAllRecommendedUsers = useFollowAllRecommendedUsers({
+    suggestedFollowingIds: suggestedFollowingIds,
+    queryRef: query,
+  });
+  const handleFollowAll = useCallback(() => {
+    followAllRecommendedUsers();
+    redirectToProfile();
+  }, [followAllRecommendedUsers, redirectToProfile]);
+
+  return (
+    <VStack>
+      <FullPageCenteredStep from={90} to={100}>
+        <Container gap={16}>
+          <VStack gap={4}>
+            <StyledHeaderText>Recommended collectors</StyledHeaderText>
+            <BaseM color={colors.shadow}>{subheadingText}</BaseM>
+          </VStack>
+
+          <StyledListUsersContainer gap={16}>
+            {nonNullUsers.map((user) => {
+              return (
+                <HStack key={user.dbid} gap={8} justify="space-between" align="center">
+                  <ProfilePicture userRef={user} size="md" />
+                  <VStack grow>
+                    <BaseM>{user.username}</BaseM>
+                    <StyledSubHeaderText>
+                      <ProcessedText text={user.bio ?? ''} eventContext={null}></ProcessedText>
+                    </StyledSubHeaderText>
+                  </VStack>
+                  <FollowButton userRef={user} queryRef={query} variant="secondary" />
+                </HStack>
+              );
+            })}
+          </StyledListUsersContainer>
+
+          <StyledButtonContainer>
+            <Button
+              eventElementId="Follow All button on onboarding recommended users screen"
+              eventName="Click Follow All button on onboarding recommended users screen"
+              eventContext={contexts.Onboarding}
+              onClick={handleFollowAll}
+            >
+              Follow all
+            </Button>
+          </StyledButtonContainer>
+        </Container>
+      </FullPageCenteredStep>
+      <OnboardingFooter
+        step="recommend-users"
+        onNext={redirectToProfile}
+        isNextEnabled
+        nextButtonVariant="secondary"
+        previousTextOverride="Back"
+      />
+    </VStack>
+  );
+}
+
+const Container = styled(VStack)`
+  width: 480px;
+`;
+
+const StyledHeaderText = styled(BaseM)`
+  font-weight: 700;
+`;
+
+const StyledSubHeaderText = styled(BaseM)`
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  min-height: 24px;
+  max-height: 24px;
+`;
+
+const StyledListUsersContainer = styled(VStack)`
+  padding: 0 16px;
+`;
+const StyledButtonContainer = styled(VStack)`
+  padding: 24px 16px;
+`;
