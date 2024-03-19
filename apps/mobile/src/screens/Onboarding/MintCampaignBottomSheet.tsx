cc3a00c507811f99a6f4175c68e70be6f8f783d7
kaito <80802871+kaitoo1@users.noreply.github.com>
2024-03-19 17:32:59 +0900

    add placeholder ui to represent different tx states

--- apps/mobile/src/screens/Onboarding/MintCampaignBottomSheet.tsx
+++ apps/mobile/src/screens/Onboarding/MintCampaignBottomSheet.tsx
@@ -1,16 +1,31 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { Image, View } from 'react-native';
 
 import { BaseM, BaseS, TitleLItalic, TitleS } from '~/components/Text';
 import { Button } from '~/components/Button';
 import { Typography } from '~/components/Typography';
+import { contexts } from 'shared/analytics/constants';
+import { SpinnerIcon } from 'src/icons/SpinnerIcon';
+import { GalleryTouchableOpacity } from '~/components/GalleryTouchableOpacity';
+
+type MintState = 'PRE_TXN' | 'TXN_PENDING' | 'TXN_COMPLETE' | 'TOKEN_SYNCED';
 export default function MintCampaignBottomSheet() {
-  return <IntroScreen />;
+  const [state, setState] = useState<MintState>('PRE_TXN');
+
+  const handleTxnComplete = useCallback(() => {
+    setState('TXN_COMPLETE');
+  }, []);
+
+  if (state === 'PRE_TXN' || state === 'TXN_PENDING') {
+    return <PreTransactionScreen onTransactionSuccess={handleTxnComplete} />;
+  } else if (state === 'TXN_COMPLETE' || state === 'TOKEN_SYNCED') {
+    return <PostTransactionScreen />;
+  }
 }
 
 export const MCHX_MINT_CAMPAIGN_END_DATE = '2024-03-30T00:00:00-05:00';
 
-function IntroScreen() {
+function PreTransactionScreen({ onTransactionSuccess }: { onTransactionSuccess: () => void }) {
   const calculateTimeLeftText = useCallback(() => {
     const endDate = new Date(MCHX_MINT_CAMPAIGN_END_DATE).getTime();
     const now = new Date().getTime();
@@ -21,11 +36,13 @@ function IntroScreen() {
       const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
       const minutes = Math.floor((difference / 1000 / 60) % 60);
 
-      return `Mint closes in ${days}d ${hours}h ${minutes}m`;
+      return `Ending ${days}d ${hours}h ${minutes}m`;
     }
 
     return 'Campaign ended';
   }, []);
+
+  const [mintPending, setMintPending] = useState(false);
 
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeftText());
 
@@ -36,17 +53,33 @@ function IntroScreen() {
 
     return () => clearTimeout(timer);
   });
+
+  //   TXN_PENDING
+  //   TXN_COMPLETE
+  //   TOKEN_SYNCED
+
+  const handlePress = useCallback(() => {
+    console.log('mint');
+    setMintPending(true);
+
+    setTimeout(() => {
+      onTransactionSuccess();
+    }, 2000);
+
+    // trigger mint mutation
+    //
+  }, [onTransactionSuccess]);
   return (
     <View>
       <TitleS>Exclusive free mint</TitleS>
-      <BaseM>
+      <BaseM classNameOverride="mt-1">
         Thank you for supporting Gallery. To show our appreciation, enjoy a a free generative
         artwork by MCHX, on us
       </BaseM>
       <Image
         className="w-full aspect-square my-4"
         source={{
-          uri: 'https://highlight-creator-assets.highlight.xyz/tmp/gen-previews/26666dba-e5ab-4a24-a6ee-af9e358ed115/1710381305822.png',
+          uri: 'https://highlight-creator-assets.highlight.xyz/main/base-dir/0e3a9abb-5b8a-4ecb-92c2-881ef173f299/previews/1.png',
         }}
       />
       <View className="flex flex-row space-apart justify-between">
@@ -60,15 +93,80 @@ function IntroScreen() {
         </View>
       </View>
       <Button
-        text="mint"
+        text={mintPending ? 'Minting...' : 'Mint'}
         eventElementId="Mint Campaign Mint Button"
         eventName="Pressed Mint Campaign Mint Button"
-        eventContext={null}
+        eventContext={contexts.MintCampaign}
+        className="my-3"
+        onPress={handlePress}
+        disabled={mintPending}
       />
       <BaseS>
         Note: Image above is an indicative preview only, final artwork will be uniquely generated.
         Powered by highlight.xyz
       </BaseS>
+    </View>
+  );
+}
+
+function PostTransactionScreen() {
+  const [state, setState] = useState<MintState>('TXN_COMPLETE');
+
+  const handlePostPress = useCallback(() => {
+    // handlePress
+  }, []);
+
+  const handleSeeCollectionPress = useCallback(() => {
+    // handlePress
+  }, []);
+
+  const testHandler = useCallback(() => {
+    console.log('test');
+    if (state === 'TXN_COMPLETE') {
+      setState('TOKEN_SYNCED');
+    } else {
+      setState('TXN_COMPLETE');
+    }
+  }, [state]);
+
+  return (
+    <View>
+      <TitleS>{state === 'TOKEN_SYNCED' ? 'Mint revealed' : 'Mint confirmed'}</TitleS>
+      <BaseM>{state === 'TOKEN_SYNCED' ? 'Token ID here' : 'Revealing your artwork'}</BaseM>
+      {state === 'TOKEN_SYNCED' ? (
+        <Image
+          className="w-full aspect-square my-4"
+          source={{
+            uri: 'https://highlight-creator-assets.highlight.xyz/tmp/gen-previews/cb47e6ac-93f0-41e9-b8a7-ae85f5bff594/1710746816957.png',
+          }}
+        />
+      ) : (
+        <View className="my-4 flex justify-center items-center bg-faint dark:bg-black-700 w-full aspect-square ">
+          <GalleryTouchableOpacity onPress={testHandler}>
+            <SpinnerIcon spin size="s" />
+          </GalleryTouchableOpacity>
+        </View>
+      )}
+      {state === 'TOKEN_SYNCED' && (
+        <>
+          <Button
+            text="Post"
+            eventElementId="Mint Campaign Post Button"
+            eventName="Pressed Mint Campaign Post Button"
+            eventContext={contexts.MintCampaign}
+            className="mb-2"
+            onPress={handlePostPress}
+          />
+          <Button
+            text="See collection"
+            variant="secondary"
+            eventElementId="Mint Campaign See Collection Button"
+            eventName="Pressed Mint Campaign See Collection Button"
+            eventContext={contexts.MintCampaign}
+            onPress={handleSeeCollectionPress}
+          />
+        </>
+      )}
     </View>
   );
 }
