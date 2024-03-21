c0194013494f701a047b792b418d23494b939c9c
Jakz <jakzaizzat@gmail.com>
2024-03-21 12:19:50 +0800

    Simplify the component usage

--- apps/mobile/src/screens/CommunityScreen/CommunityNftSelectorScreen.tsx
+++ apps/mobile/src/screens/CommunityScreen/CommunityNftSelectorScreen.tsx
@@ -1,10 +1,8 @@
 import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
-import { useCallback, useMemo } from 'react';
+import { useCallback } from 'react';
 import { graphql, useLazyLoadQuery } from 'react-relay';
 
-import { NftSelectorContractHeader } from '~/components/NftSelector/NftSelectorContract/NftSelectorContractHeader';
-import { NftSelectorContractPickerGrid } from '~/components/NftSelector/NftSelectorContract/NftSelectorContractPickerGrid';
-import { NftSelectorContractWrapper } from '~/components/NftSelector/NftSelectorContract/NftSelectorContractWrapper';
+import { NftSelectorContract } from '~/components/NftSelector/NftSelectorContract/NftSelectorContract';
 import { CommunityNftSelectorScreenQuery } from '~/generated/CommunityNftSelectorScreenQuery.graphql';
 import { MainTabStackNavigatorParamList, MainTabStackNavigatorProp } from '~/navigation/types';
 
@@ -12,24 +10,7 @@ export function CommunityNftSelectorScreen() {
   const query = useLazyLoadQuery<CommunityNftSelectorScreenQuery>(
     graphql`
       query CommunityNftSelectorScreenQuery {
-        viewer {
-          ... on Viewer {
-            user {
-              tokens {
-                __typename
-                definition {
-                  contract {
-                    name
-                    contractAddress {
-                      address
-                    }
-                  }
-                }
-                ...NftSelectorContractPickerGridFragment
-              }
-            }
-          }
-        }
+        ...NftSelectorContractFragment
       }
     `,
     {}
@@ -37,8 +18,6 @@ export function CommunityNftSelectorScreen() {
 
   const navigation = useNavigation<MainTabStackNavigatorProp>();
   const route = useRoute<RouteProp<MainTabStackNavigatorParamList, 'NftSelectorContractScreen'>>();
-  const contractAddress = route.params.contractAddress;
-  const isCreator = route.params.ownerFilter === 'Created';
 
   const handleSelectNft = useCallback(
     (tokenId: string) => {
@@ -50,31 +29,12 @@ export function CommunityNftSelectorScreen() {
     [navigation]
   );
 
-  const nonNullableTokens = useMemo(() => {
-    const tokens = [];
-
-    for (const token of query.viewer?.user?.tokens ?? []) {
-      if (token?.definition?.contract?.contractAddress?.address === contractAddress) {
-        tokens.push(token);
-      }
-    }
-
-    return tokens;
-  }, [query.viewer?.user?.tokens, contractAddress]);
-
-  const contractName = useMemo(
-    () => nonNullableTokens[0]?.definition?.contract?.name ?? '',
-    [nonNullableTokens]
-  );
-
   return (
-    <NftSelectorContractWrapper>
-      <NftSelectorContractHeader title={contractName} />
-      <NftSelectorContractPickerGrid
-        isCreator={isCreator}
-        tokenRefs={nonNullableTokens}
-        onSelect={handleSelectNft}
-      />
-    </NftSelectorContractWrapper>
+    <NftSelectorContract
+      contractAddress={route.params.contractAddress}
+      onSelectNft={handleSelectNft}
+      queryRef={query}
+      isCreator={route.params.ownerFilter === 'Created'}
+    />
   );
 }
--- apps/mobile/src/screens/PostScreen/PostNftSelectorScreen.tsx
+++ apps/mobile/src/screens/PostScreen/PostNftSelectorScreen.tsx
@@ -1,29 +1,12 @@
 import { useNavigation } from '@react-navigation/native';
-import { Suspense, useCallback } from 'react';
-import { View } from 'react-native';
+import { useCallback } from 'react';
 
-import { NftSelectorHeader } from '~/components/NftSelector/NftSelectorHeader';
-import { NftSelectorToolbar } from '~/components/NftSelector/NftSelectorToolbar';
-import { NftSelectorWrapper } from '~/components/NftSelector/NftSelectorWrapper';
+import { NftSelector } from '~/components/NftSelector/NftSelector';
 import { useNftSelector } from '~/components/NftSelector/useNftSelector';
 import { MainTabStackNavigatorProp } from '~/navigation/types';
-import { NftSelectorLoadingSkeleton } from '~/screens/NftSelectorScreen/NftSelectorLoadingSkeleton';
-import { NftSelectorPickerGrid } from '~/screens/NftSelectorScreen/NftSelectorPickerGrid';
 
 export function PostNftSelectorScreen() {
-  const {
-    searchQuery,
-    setSearchQuery,
-    ownershipTypeFilter,
-    setFilter,
-    networkFilter,
-    setNetworkFilter,
-    sortView,
-    setSortView,
-    sync,
-    isSyncing,
-    isSyncingCreatedTokens,
-  } = useNftSelector();
+  const { ownershipTypeFilter } = useNftSelector();
 
   const navigation = useNavigation<MainTabStackNavigatorProp>();
 
@@ -41,45 +24,19 @@ export function PostNftSelectorScreen() {
       navigation.navigate('NftSelectorContractScreen', {
         contractAddress: contractAddress,
         page: 'Post',
-        ownerFilter: 'Collected',
+        ownerFilter: ownershipTypeFilter,
         fullScreen: true,
       });
     },
-    [navigation]
+    [navigation, ownershipTypeFilter]
   );
+
   return (
-    <NftSelectorWrapper ownershipTypeFilter={ownershipTypeFilter} isFullscreen>
-      <View className="gap-8">
-        <NftSelectorHeader title="Select item to post" />
-        <NftSelectorToolbar
-          searchQuery={searchQuery}
-          setSearchQuery={setSearchQuery}
-          ownershipTypeFilter={ownershipTypeFilter}
-          setFilter={setFilter}
-          networkFilter={networkFilter}
-          setNetworkFilter={setNetworkFilter}
-          sortView={sortView}
-          setSortView={setSortView}
-          isSyncing={isSyncing}
-          isSyncingCreatedTokens={isSyncingCreatedTokens}
-          handleSync={sync}
-        />
-      </View>
-      <View className="flex-grow flex-1 w-full">
-        <Suspense fallback={<NftSelectorLoadingSkeleton />}>
-          <NftSelectorPickerGrid
-            searchCriteria={{
-              searchQuery,
-              ownerFilter: ownershipTypeFilter,
-              networkFilter: networkFilter,
-              sortView,
-            }}
-            onRefresh={sync}
-            onSelect={handleSelectNft}
-            onSelectNftGroup={handleSelectNftGroup}
-          />
-        </Suspense>
-      </View>
-    </NftSelectorWrapper>
+    <NftSelector
+      onSelectNft={handleSelectNft}
+      onSelectNftGroup={handleSelectNftGroup}
+      title="Select item to post"
+      isFullscreen
+    />
   );
 }
