b9205617dbd2477caae00e3250014858f7a0b0ec
Rohan-cp <rohancp9@icloud.com>
2024-03-25 00:43:10 -0400

    better

--- apps/web/src/components/Feed/FeedSuggestedProfileSection.tsx
+++ apps/web/src/components/Feed/FeedSuggestedProfileSection.tsx
@@ -4,15 +4,13 @@ import styled from 'styled-components';
 
 import breakpoints from '~/components/core/breakpoints';
 import { Carousel } from '~/components/core/Carousel/Carousel';
-import GalleryLink from '~/components/core/GalleryLink/GalleryLink';
 import { HStack, VStack } from '~/components/core/Spacer/Stack';
 import { BaseM } from '~/components/core/Text/Text';
 import transitions from '~/components/core/transitions';
 import { FEED_EVENT_ROW_WIDTH_DESKTOP } from '~/components/Feed/dimensions';
 import { FeedSuggestedProfileSectionQueryFragment$key } from '~/generated/FeedSuggestedProfileSectionQueryFragment.graphql';
-import { CmsTypes } from '~/scenes/ContentPages/cms_types';
 import { ReportingErrorBoundary } from '~/shared/errors/ReportingErrorBoundary';
-import colors from '~/shared/theme/colors';
+import SuggestedProfileCard from './SuggestedProfileCard';
 
 type FeedSuggestedProfileSectionProps = {
   queryRef: FeedSuggestedProfileSectionQueryFragment$key;
@@ -32,6 +30,7 @@ function FeedSuggestedProfileSection({ queryRef }: FeedSuggestedProfileSectionPr
                   __typename
                   ... on GalleryUser {
                     __typename
+                    ...SuggestedProfileCardFragment
 
                     ...ExploreListFragment
                     ...ExplorePopoverListFragment
@@ -41,6 +40,7 @@ function FeedSuggestedProfileSection({ queryRef }: FeedSuggestedProfileSectionPr
             }
           }
         }
+        ...SuggestedProfileCardFollowFragment
       }
     `,
     queryRef
@@ -48,6 +48,10 @@ function FeedSuggestedProfileSection({ queryRef }: FeedSuggestedProfileSectionPr
 
   const itemsPerSlide = 4;
   const [profiles, setProfiles] = useState([]);
+
+  if (!query.viewer?.suggestedUsers) {
+    return null;
+  }
 
   const slideContent = useMemo(() => {
     const rows = [];
@@ -57,7 +61,7 @@ function FeedSuggestedProfileSection({ queryRef }: FeedSuggestedProfileSectionPr
       const rowElements = (
         <StyledSuggestedProfileSet gap={16}>
           {row.map((profile) => (
-            <SuggestedProfile key={profile.id} profile={profile} />
+            <SuggestedProfileCard queryRef={query} userRef={profile} />
           ))}
           {row.length < itemsPerSlide && <StyledPlaceholder />}
         </StyledSuggestedProfileSet>
@@ -101,97 +105,6 @@ const StyledSuggestedProfileSet = styled(HStack)`
 const StyledPlaceholder = styled.div`
   width: 100%;
 `;
-
-type FeaturedProfileProps = {
-  profile: CmsTypes.FeaturedProfile;
-};
-
-function SuggestedProfile({ profile }: FeaturedProfileProps) {
-  // add a follow button
-
-  return (
-    <StyledLinkWrapper
-      href={`/${profile.username}`}
-      target="_blank"
-      eventElementId="Feed Suggested Profile"
-      eventName="Clicked on Feed Suggested Profile"
-      properties={{ username: profile.username }}
-    >
-      <StyledProfile gap={16}>
-        <HStack gap={4}>
-          {profile.coverImages.map((image) => (
-            <StyledImageContainer key={image.asset.url}>
-              <StyledImage key={image.asset.url} src={image.asset.url} />
-            </StyledImageContainer>
-          ))}
-        </HStack>
-        <VStack gap={4}>
-          <HStack gap={4} align="center" justify="space-between">
-            <HStack gap={6} align="center">
-              <StyledPfp src={profile.pfp?.asset.url} />
-              <StyledUsername>{profile.username}</StyledUsername>
-            </HStack>
-          </HStack>
-          <StyledBio>{profile.bio}</StyledBio>
-        </VStack>
-      </StyledProfile>
-    </StyledLinkWrapper>
-  );
-}
-
-const StyledLinkWrapper = styled(GalleryLink)`
-  width: 100%;
-`;
-
-const StyledProfile = styled(VStack)`
-  width: 100%;
-  background-color: ${colors.faint};
-  padding: 16px;
-  border-radius: 16px;
-  transition: background-color ${transitions.cubic};
-  @media only screen and ${breakpoints.desktop} {
-    min-width: 436px;
-    max-width: 436px;
-  }
-
-  &:hover {
-    background-color: ${colors.porcelain};
-  }
-`;
-
-const StyledPfp = styled.img`
-  height: 34px;
-  width: 34px;
-  border-radius: 50%;
-`;
-
-const StyledImageContainer = styled.div`
-  display: flex;
-  width: 100%;
-`;
-
-const StyledUsername = styled(BaseM)`
-  font-weight: 700;
-  font-size: 18px;
-`;
-
-const StyledBio = styled(BaseM)`
-  height: 20px; // fix height to get consistent spacing across profiles even if bio is not present
-  overflow: hidden;
-  text-overflow: ellipsis;
-  display: -webkit-box;
-  -webkit-line-clamp: 1;
-  -webkit-box-orient: vertical;
-  -webkit-box-pack: end;
-`;
-
-const StyledImage = styled.img`
-  width: 100%;
-  height: 100%;
-  aspect-ratio: 1;
-  object-fit: cover;
-`;
-
 export default function FeedSuggestedProfileSectionWithBoundary({ queryRef }) {
   const query = useFragment(
     graphql`
---
+++ apps/web/src/components/Feed/SuggestedProfileCard.tsx
@@ -0,0 +1,229 @@
+import { useMemo } from 'react';
+import { graphql, useFragment } from 'react-relay';
+import styled from 'styled-components';
+
+import { SuggestedProfileCardFollowFragment$key } from '~/generated/SuggestedProfileCardFollowFragment.graphql';
+import { SuggestedProfileCardFragment$key } from '~/generated/SuggestedProfileCardFragment.graphql';
+import { useIsMobileWindowWidth } from '~/hooks/useWindowSize';
+import { contexts } from '~/shared/analytics/constants';
+import { removeNullValues } from '~/shared/relay/removeNullValues';
+import { useLoggedInUserId } from '~/shared/relay/useLoggedInUserId';
+import colors from '~/shared/theme/colors';
+import unescape from '~/shared/utils/unescape';
+
+import Badge from '../Badge/Badge';
+import breakpoints from '../core/breakpoints';
+import GalleryLink from '../core/GalleryLink/GalleryLink';
+import Markdown from '../core/Markdown/Markdown';
+import { HStack, VStack } from '../core/Spacer/Stack';
+import { BaseM, TitleM } from '../core/Text/Text';
+import FollowButton from '../Follow/FollowButton';
+
+type Props = {
+  userRef: SuggestedProfileCardFragment$key;
+  queryRef: SuggestedProfileCardFollowFragment$key;
+};
+
+export default function SuggestedProfileCard({ userRef, queryRef }: Props) {
+  const user = useFragment(
+    graphql`
+      fragment SuggestedProfileCardFragment on GalleryUser {
+        id
+        username
+        badges {
+          name
+          imageURL
+          ...BadgeFragment
+        }
+        bio
+        galleries {
+          tokenPreviews {
+            large
+          }
+          hidden
+        }
+        ...FollowButtonUserFragment
+      }
+    `,
+    userRef
+  );
+
+  const query = useFragment(
+    graphql`
+      fragment SuggestedProfileCardFollowFragment on Query {
+        ...FollowButtonQueryFragment
+        ...useLoggedInUserIdFragment
+      }
+    `,
+    queryRef
+  );
+
+  const loggedInUserId = useLoggedInUserId(query);
+  const isOwnProfile = loggedInUserId && loggedInUserId === user?.id;
+
+  if (!user) {
+    throw new Error('No user available to showcase ExploreUserCard');
+  }
+
+  const { bio } = user;
+
+  const unescapedBio = useMemo(() => (bio ? unescape(bio) : ''), [bio]);
+
+  const userGalleries = useMemo(() => {
+    return user.galleries ?? [];
+  }, [user.galleries]);
+
+  const tokenPreviews = useMemo(() => {
+    const gallery = userGalleries.find(
+      (gallery) => !gallery?.hidden && removeNullValues(gallery?.tokenPreviews).length > 0
+    );
+
+    return gallery?.tokenPreviews?.slice(0, 2) ?? [];
+  }, [userGalleries]);
+
+  const userBadges = useMemo(() => {
+    const badges = [];
+
+    for (const badge of user.badges ?? []) {
+      if (badge?.imageURL) {
+        badges.push(badge);
+      }
+    }
+
+    return badges;
+  }, [user.badges]);
+
+  const isMobile = useIsMobileWindowWidth();
+
+  const bioFirstLine = useMemo(() => {
+    if (!unescapedBio) {
+      return '';
+    }
+    return unescapedBio.split('\n')[0] ?? '';
+  }, [unescapedBio]);
+
+  return (
+    <StyledSuggestedProfileCard
+      to={{
+        pathname: '/[username]',
+        query: { username: user?.username ?? '' },
+      }}
+      eventElementId="Explore User Card"
+      eventName="Explore User Card Click"
+      eventContext={contexts.Explore}
+    >
+      <StyledContent gap={12} justify="space-between">
+        <TokenPreviewContainer>
+          {tokenPreviews.map(
+            (url) => url?.large && <TokenPreview src={url.large} key={url.large} />
+          )}
+        </TokenPreviewContainer>
+        <ProfileDetailsContainer>
+          <ProfileDetailsText>
+            <HStack gap={4} align="center" justify="space-between">
+              <HStack>
+                <Username>
+                  <strong>{user.username}</strong>
+                </Username>
+                <HStack align="center" gap={0}>
+                  {userBadges.map((badge) => (
+                    <Badge key={badge.name} badgeRef={badge} eventContext={contexts['Explore']} />
+                  ))}
+                </HStack>
+              </HStack>
+              {!isMobile && !isOwnProfile && (
+                <StyledFollowButton
+                  userRef={user}
+                  queryRef={query}
+                  source="Explore Page user card"
+                />
+              )}
+            </HStack>
+            <StyledUserBio>
+              <Markdown text={bioFirstLine} eventContext={contexts.Explore} />
+            </StyledUserBio>
+            {isMobile && !isOwnProfile && (
+              <StyledFollowButton userRef={user} queryRef={query} source="Explore Page user card" />
+            )}
+          </ProfileDetailsText>
+        </ProfileDetailsContainer>
+      </StyledContent>
+    </StyledSuggestedProfileCard>
+  );
+}
+
+const StyledSuggestedProfileCard = styled(GalleryLink)`
+  border-radius: 12px;
+  background-color: ${colors.offWhite};
+  padding: 12px;
+  cursor: pointer;
+  text-decoration: none;
+  overflow: hidden;
+
+  &:hover {
+    background-color: ${colors.faint};
+  }
+
+  @media only screen and ${breakpoints.desktop} {
+    height: 100%;
+  }
+`;
+
+const StyledContent = styled(VStack)`
+  height: 100%;
+  width: 100%;
+`;
+
+const ProfileDetailsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  width: 100%;
+
+  @media only screen and ${breakpoints.desktop} {
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+
+const ProfileDetailsText = styled(VStack)`
+  overflow: hidden;
+  width: 100%;
+`;
+
+const StyledUserBio = styled(BaseM)`
+  height: 20px; // ensure consistent height even if bio is not present
+
+  line-clamp: 1;
+  -webkit-line-clamp: 1;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+`;
+
+export const TokenPreviewContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 2px;
+`;
+
+export const TokenPreview = styled.img`
+  height: auto;
+  width: 100%;
+  aspect-ratio: 1 / 1;
+  object-fit: cover;
+`;
+
+const Username = styled(TitleM)`
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+const StyledFollowButton = styled(FollowButton)`
+  padding: 2px 8px;
+  width: 100%;
+
+  @media only screen and ${breakpoints.desktop} {
+    width: initial;
+  }
+`;
