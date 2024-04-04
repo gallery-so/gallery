3f621bd3dbd2e4dc035e3aeb7ab397fdaa61d426
kaito <80802871+kaitoo1@users.noreply.github.com>
2024-04-04 18:54:35 +0900

    include community in query

--- apps/mobile/src/components/Mint/MintCampaign/MintCampaignPostTransaction.tsx
+++ apps/mobile/src/components/Mint/MintCampaign/MintCampaignPostTransaction.tsx
@@ -60,6 +60,9 @@ export default function MintCampaignPostTransaction({
                       dbid
                       definition {
                         name
+                        community {
+                          ...useNavigateToCommunityScreenFragment
+                        }
                       }
                       ...NftDetailAssetFragment
                     }
