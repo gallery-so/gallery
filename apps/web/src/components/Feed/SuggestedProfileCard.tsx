117f5243d03d022b784d9610a663e4feba057d8c
Rohan-cp <rohancp9@icloud.com>
2024-04-08 18:31:49 -0400

    feeedback addressed

--- apps/web/src/components/Feed/SuggestedProfileCard.tsx
+++ apps/web/src/components/Feed/SuggestedProfileCard.tsx
@@ -8,7 +8,7 @@ import { contexts } from '~/shared/analytics/constants';
 import { removeNullValues } from '~/shared/relay/removeNullValues';
 import { useLoggedInUserId } from '~/shared/relay/useLoggedInUserId';
 import colors from '~/shared/theme/colors';
-import unescape from '~/shared/utils/unescape';
+import { getUnescapedBioFirstLine } from '~/utils/sanity';
 
 import Badge from '../Badge/Badge';
 import breakpoints from '../core/breakpoints';
@@ -72,15 +72,7 @@ export default function SuggestedProfileCard({
     throw new Error('No user available to showcase SuggestedProfileCard');
   }
 
-  const { bio } = user;
-  const unescapedBio = useMemo(() => (bio ? unescape(bio) : ''), [bio]);
-
-  const bioFirstLine = useMemo(() => {
-    if (!unescapedBio) {
-      return '';
-    }
-    return unescapedBio.split('\n')[0] ?? '';
-  }, [unescapedBio]);
+  const bioFirstLine = useMemo(() => getUnescapedBioFirstLine(user?.bio), [user?.bio]);
 
   const userGalleries = useMemo(() => {
     return user.galleries ?? [];
